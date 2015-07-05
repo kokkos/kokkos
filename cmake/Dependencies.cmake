49b78cd9307a3d412b895534ce5445e34ce8dce7
crtrott <crtrott@sandia.gov>
2015-07-05 15:55:28 -0600

    Remove Profiler subpackage from CMake list again.
    This has been moved out of Kokkos for the moment and lives
    in its own private github repo.

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -2,7 +2,6 @@ SET(SUBPACKAGES_DIRS_CLASSIFICATIONS_OPTREQS
   #SubPackageName       Directory         Class    Req/Opt
   #
   # New Kokkos subpackages:
-  Profiler              profiler          EX       OPTIONAL
   Core                  core              PS       REQUIRED
   Containers            containers        PS       OPTIONAL
   Algorithms            algorithms        PS       OPTIONAL
