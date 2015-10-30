62d12dc8b11afb4d8dd48d2193a35a2d22f24cbc
Roscoe A. Bartlett <bartlettra@ornl.gov>
2015-10-29 22:51:43 -0400

    Kokkos: Use TRIBITS_PACKAGE_DEFINE_DEPENDENCIES() and remove bad TPL
    Someone added the TPL HWLOC to teh outer Kokkos package.  That does absolutely
    nothing since there is no library or executables in the outer Kokkos package.

--- example/cmake/Dependencies.cmake
+++ example/cmake/Dependencies.cmake
@@ -1,15 +1,4 @@
-#
-# These variables must be set for every package or subpackage.
-#
-# Including Kokkos subpackage library interdependencies for this subpackage.
-# Subpackage names specified as {Package}{Subpackage}
-# where {Subpackage} name is defined in kokkos/cmake/Dependencies.cmake
-#
-SET(LIB_REQUIRED_DEP_PACKAGES KokkosCore KokkosContainers KokkosAlgorithms)
-SET(LIB_OPTIONAL_DEP_PACKAGES)
-SET(TEST_REQUIRED_DEP_PACKAGES)
-SET(TEST_OPTIONAL_DEP_PACKAGES)
-SET(LIB_REQUIRED_DEP_TPLS)
-SET(LIB_OPTIONAL_DEP_TPLS)
-SET(TEST_REQUIRED_DEP_TPLS )
-SET(TEST_OPTIONAL_DEP_TPLS CUSPARSE MKL )
+TRIBITS_PACKAGE_DEFINE_DEPENDENCIES(
+  LIB_REQUIRED_DEP_PACKAGES KokkosCore KokkosContainers KokkosAlgorithms
+  TEST_OPTIONAL_DEP_TPLS CUSPARSE MKL
+  )
