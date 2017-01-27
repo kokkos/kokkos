19528d95115f019fda13f884fe48b4ae1156cb33
crtrott <crtrott@sandia.gov>
2017-01-26 18:01:43 -0700

    Update promotion script

--- config/test_kokkos_master_develop_promotion.sh
+++ config/test_kokkos_master_develop_promotion.sh
@@ -7,8 +7,11 @@ echo "backend $2"
 echo "module $3"
 echo "compiler $4"
 echo "cxxflags $5"
-echo "kokkosflags $6"
-echo "hwloc $7"
+echo "architecrure $6"
+echo "debug $7"
+echo "kokkos-options $8"
+echo "kokkos-cuda-options $9"
+echo "hwloc $9"
 
 NOW=`date "+%Y%m%d%H%M%S"`
 BASEDIR="$1-$NOW"
@@ -18,10 +21,46 @@ cd $BASEDIR
 
 module load $2
 
-../generate_makefile.sh --with-devices=$2 \
-	--compiler=$4 \
+if [ $9 == "yes" ]; then
+if [ $7 == "debug" ]; then
+  ../generate_makefile.sh --with-devices=$2 \
+  	--compiler=$4 \
 	--cxxflags=$5 \
-	--with-options=$6
+        --arch=$6 \
+        --debug \
+	--with-options=$8 \
+        --with-cuda-options=$9
+        --with-hwloc=${HWLOC_ROOT}
+else
+    ../generate_makefile.sh --with-devices=$2 \
+        --compiler=$4 \
+        --cxxflags=$5 \
+        --arch=$6 \
+        --debug \
+        --with-options=$8 \
+        --with-cuda-options=$9 
+        --with-hwloc=${HWLOC_ROOT}
+fi
+else
+if [ $7 == "debug" ]; then
+  ../generate_makefile.sh --with-devices=$2 \
+        --compiler=$4 \
+        --cxxflags=$5 \
+        --arch=$6 \
+        --debug \
+        --with-options=$8 \
+        --with-cuda-options=$9
+else
+    ../generate_makefile.sh --with-devices=$2 \
+        --compiler=$4 \
+        --cxxflags=$5 \
+        --arch=$6 \
+        --debug \
+        --with-options=$8 \
+        --with-cuda-options=$9 
+fi
+fi
+
 
 make test
 return $?
