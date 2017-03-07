f0ba2317e106c6beac2ade54aae46c3214444d42
crtrott <crtrott@sandia.gov>
2017-03-07 14:03:02 -0700

    Enable some performance tests again

--- core/perf_test/PerfTestHost.cpp
+++ core/perf_test/PerfTestHost.cpp
@@ -107,10 +107,10 @@ protected:
 //TEST_F( host, mdrange_lr ) {
 //  EXPECT_NO_THROW( (run_test_mdrange<TestHostDevice , Kokkos::LayoutRight> (5, 11, TestHostDeviceName) ) );
 //}
-/*
-TEST_F( host, mdrange_ll ) {
-  EXPECT_NO_THROW( (run_test_mdrange<TestHostDevice , Kokkos::LayoutLeft> (5, 8, TestHostDeviceName) ) ); //uncommenting and running both leads to segfault during run of the test above?????
-}
+
+//TEST_F( host, mdrange_ll ) {
+//  EXPECT_NO_THROW( (run_test_mdrange<TestHostDevice , Kokkos::LayoutLeft> (5, 8, TestHostDeviceName) ) ); //uncommenting and running both leads to segfault during run of the test above?????
+//}
 
 TEST_F( host, hexgrad ) {
   EXPECT_NO_THROW(run_test_hexgrad< TestHostDevice>( 10, 20, TestHostDeviceName ));
@@ -119,7 +119,7 @@ TEST_F( host, hexgrad ) {
 TEST_F( host, gramschmidt ) {
   EXPECT_NO_THROW(run_test_gramschmidt< TestHostDevice>( 10, 20, TestHostDeviceName ));
 }
-*/
+
 } // namespace Test
 
 
