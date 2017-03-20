c917ec9037a7dcdfe483b667091187a242f3b4af
Dan Sunderland <dsunder@sandia.gov>
2017-03-20 14:54:03 -0600

    Consolidate spinwait code into a single implementation and add memory barriers.
    Spinwait refactored into two different functions with simple
    exponential backoff behavior.
      spinwait_while_equal( volatile int & flag, const int value );
      spinwait_until_equal( volatile int & flag, const int value );
    
    This should fix the bug were a triple_nested_parallelism got the wrong
    answer.  Address issue #685

--- core/src/Threads/Kokkos_ThreadsTeam.hpp
+++ core/src/Threads/Kokkos_ThreadsTeam.hpp
@@ -104,13 +104,13 @@ public:
 
       // Wait for fan-in threads
       for ( n = 1 ; ( ! ( m_team_rank_rev & n ) ) && ( ( j = m_team_rank_rev + n ) < m_team_size ) ; n <<= 1 ) {
-        Impl::spinwait( m_team_base[j]->state() , ThreadsExec::Active );
+        Impl::spinwait_while_equal( m_team_base[j]->state() , ThreadsExec::Active );
       }
 
       // If not root then wait for release
       if ( m_team_rank_rev ) {
         m_exec->state() = ThreadsExec::Rendezvous ;
-        Impl::spinwait( m_exec->state() , ThreadsExec::Rendezvous );
+        Impl::spinwait_while_equal( m_exec->state() , ThreadsExec::Rendezvous );
       }
 
       return ! m_team_rank_rev ;
