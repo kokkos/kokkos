06fe1061a70b099a874ff3faf6a9d0b3b874f250
crtrott <crtrott@sandia.gov>
2017-05-12 12:11:52 -0600

    UnitTests: Move over Subviews

--- core/unit_test/threads/TestThreads_ViewOfClass.cpp
+++ core/unit_test/threads/TestThreads_ViewOfClass.cpp
@@ -42,13 +42,5 @@
 */
 
 #include <threads/TestThreads_Category.hpp>
-#include <TestViewSubview.hpp>
+#include <TestViewOfClass.hpp>
 
-namespace Test {
-
-TEST_F( TEST_CATEGORY, view_subview_3d_from_5d_right_randomaccess )
-{
-  TestViewSubview::test_3d_subview_5d_right< TEST_EXECSPACE, Kokkos::MemoryTraits<Kokkos::RandomAccess> >();
-}
-
-} // namespace Test
