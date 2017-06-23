dd5b8f11589e892b42e040a1ce1f11f559dde88a
LeviBarnes <levi.barnes@gmail.com>
2017-06-23 16:16:03 -0700

    More illustrative test case for launch_bounds

--- example/tutorial/launch_bounds/launch_bounds_reduce.cpp
+++ example/tutorial/launch_bounds/launch_bounds_reduce.cpp
@@ -51,52 +51,123 @@
 //      using a functor to define the loop body
 //   3. Shut down Kokkos
 //
-// Compare this example to 02_simple_reduce_lambda, which uses a C++11
-// lambda to define the loop body of the parallel_reduce.
-//
-
-// Reduction functor for computing the sum of squares.
-//
-// More advanced reduction examples will show how to control the
-// reduction's "join" operator.  If the join operator is not provided,
-// it defaults to binary operator+ (adding numbers together).
-struct squaresum {
-  // Specify the type of the reduction value with a "value_type"
-  // typedef.  In this case, the reduction value has type int.
+struct collision {
+// Reduction functor
+// For each i, we generate 10 hashes, look for and count collisions
+// We use parallel_reduce to count the total collisions
+// Note that we're just counting collisions within the 10 generated
+// one i.
+// This function was chosen as one that very simply can increase the
+// register count.
   typedef int value_type;
 
-  // The reduction functor's operator() looks a little different than
-  // the parallel_for functor's operator().  For the reduction, we
-  // pass in both the loop index i, and the intermediate reduction
-  // value lsum.  The latter MUST be passed in by nonconst reference.
-  // (If the reduction type is an array like int[], indicating an
-  // array reduction result, then the second argument is just int[].)
+  KOKKOS_INLINE_FUNCTION
+  int hash(int q) const {
+	  // A simple hash by Justin Sobel
+	  // Thanks to Arash Partow (partow.net)
+	  char* fourchars = (char*)&q;
+	  int hash = 1315423911;
+	  for (int i=0; i<4; fourchars++, i++) {
+		  hash ^= ((hash<<5) + *fourchars + (hash >> 2));
+	  }
+	  return hash;
+  }
+
   KOKKOS_INLINE_FUNCTION
   void operator () (const int i, int& lsum) const {
-    lsum += i*i; // compute the sum of squares
+	  //This is a silly function which generates 10 hashes
+	  // then checks for collisions
+	  int a = hash(i)%64;
+	  int b = hash(i*3)%64;
+	  int c = hash(i*5)%64;
+	  int d = hash(i*7)%64;
+	  int e = hash(i*11)%64;
+	  int f = hash(i*17)%64;
+	  int g = hash(i*23)%64;
+	  int h = hash(i*29)%64;
+	  int j = hash(i*31)%64;
+	  int k = hash(i*37)%64;
+
+
+	  if (a==b) lsum++;
+	  if (a==c) lsum++;
+	  if (a==d) lsum++;
+	  if (a==e) lsum++;
+	  if (a==f) lsum++;
+	  if (a==g) lsum++;
+	  if (a==h) lsum++;
+	  if (a==j) lsum++;
+	  if (a==k) lsum++;
+	  if (b==c) lsum++;
+	  if (b==d) lsum++;
+	  if (b==e) lsum++;
+	  if (b==f) lsum++;
+	  if (b==g) lsum++;
+	  if (b==h) lsum++;
+	  if (b==j) lsum++;
+	  if (b==k) lsum++;
+	  if (c==d) lsum++;
+	  if (c==e) lsum++;
+	  if (c==f) lsum++;
+	  if (c==g) lsum++;
+	  if (c==h) lsum++;
+	  if (c==j) lsum++;
+	  if (c==k) lsum++;
+	  if (d==e) lsum++;
+	  if (d==f) lsum++;
+	  if (d==g) lsum++;
+	  if (d==h) lsum++;
+	  if (d==j) lsum++;
+	  if (d==k) lsum++;
+	  if (e==f) lsum++;
+	  if (e==g) lsum++;
+	  if (e==h) lsum++;
+	  if (e==j) lsum++;
+	  if (e==k) lsum++;
+	  if (f==g) lsum++;
+	  if (f==h) lsum++;
+	  if (f==j) lsum++;
+	  if (f==k) lsum++;
+	  if (g==h) lsum++;
+	  if (g==j) lsum++;
+	  if (g==k) lsum++;
+	  if (h==j) lsum++;
+	  if (h==k) lsum++;
+	  if (j==k) lsum++;
   }
+
+
+
 };
 
 int main (int argc, char* argv[]) {
   Kokkos::initialize (argc, argv);
-  const int n = 10;
+  const int n = 10000;
 
-  // Compute the sum of squares of integers from 0 to n-1, in
+  // Compute and count hash collisions in
   // parallel, using Kokkos.
-  int sum = 0;
-  //Kokkos::parallel_reduce (n, squaresum (), sum);
-  Kokkos::parallel_reduce (Kokkos::RangePolicy<Kokkos::LaunchBounds<512,2>>(0,n), squaresum (), sum);
-  printf ("Sum of squares of integers from 0 to %i, "
-          "computed in parallel, is %i\n", n - 1, sum);
+  // This is not really a useful algorithm, but it demonstrates the
+  // LaunchBounds functionality
+  int sum1 = 0;
+  int sum2 = 0;
+  
+  //Without LaunchBounds, the kernel uses 56 registers
+  Kokkos::parallel_reduce (n, collision (), sum1);
 
-  // Compare to a sequential loop.
-  int seqSum = 0;
-  for (int i = 0; i < n; ++i) {
-    seqSum += i*i;
+  //With LaunchBounds, we can reduce the register usage to 32
+  Kokkos::parallel_reduce (Kokkos::RangePolicy<Kokkos::LaunchBounds<512,4>>(0,n), collision (), sum2);
+
+  printf ("Number of collisions, "
+          "computed in parallel, is %i\n", sum1);
+
+  if (sum1 != sum2) {
+	  printf( "Uh-oh! Results do not match\n");
+	  return -1;
   }
-  printf ("Sum of squares of integers from 0 to %i, "
-          "computed sequentially, is %i\n", n - 1, seqSum);
-  Kokkos::finalize ();
-  return (sum == seqSum) ? 0 : -1;
+
+  Kokkos::finalize();
+  
+
+  return 0;
 }
 
