1f42e2442b4a093bc77bf0fc08053629cdb19661
chipfreitag <chip.freitag@amd.com>
2017-07-14 12:46:14 -0500

    ROCm-Merge:  Triage unit tests.

--- core/unit_test/rocm/TestROCm_Other.cpp
+++ core/unit_test/rocm/TestROCm_Other.cpp
@@ -45,7 +45,7 @@
 #include<rocm/TestROCm_Category.hpp>
 #include<TestTemplateMetaFunctions.hpp>
 #include<TestAggregate.hpp>
-#include<TestMemoryPool.hpp>
+//include<TestMemoryPool.hpp>
 #include<TestCXX11.hpp>
 #include<TestTile.hpp>
 
