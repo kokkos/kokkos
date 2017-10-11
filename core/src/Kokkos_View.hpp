ba89acc72a35b958d683aa01a0409b62eaa6e40b
Dan Ibanez <daibane@sandia.gov>
2017-10-11 18:32:17 -0400

    First draft of #1161

--- core/src/Kokkos_View.hpp
+++ core/src/Kokkos_View.hpp
@@ -2242,6 +2242,32 @@ create_mirror_view(const Space& , const Kokkos::View<T,P...> & src
   return typename Impl::MirrorViewType<Space,T,P ...>::view_type(src.label(),src.layout());
 }
 
+// Create a mirror view and deep_copy in a new space (specialization for same space)
+template<class Space, class T, class ... P>
+typename Impl::MirrorViewType<Space,T,P ...>::view_type
+create_mirror_copy(const Space& , const Kokkos::View<T,P...> & src
+  , typename std::enable_if<Impl::MirrorViewType<Space,T,P ...>::is_same_memspace>::type* = 0 ) {
+  return src;
+}
+
+// Create a mirror view and deep_copy in a new space (specialization for different space)
+template<class Space, class T, class ... P>
+typename Impl::MirrorViewType<Space,T,P ...>::view_type
+create_mirror_copy(const Space& , const Kokkos::View<T,P...> & src
+  , typename std::enable_if<!Impl::MirrorViewType<Space,T,P ...>::is_same_memspace>::type* = 0 ) {
+  using Mirror = typename Impl::MirrorViewType<Space,T,P ...>::view_type;
+  auto mirror = Mirror(ViewAllocateWithoutInitializing(src.label(), src.layout());
+  deep_copy(mirror, src);
+  return mirror;
+}
+
+// Create a mirror view and deep_copy in default execution space
+template<class T, class ... P>
+typename Impl::MirrorViewType<DefaultExecutionSpace,T,P ...>::view_type
+create_mirror_copy(const Kokkos::View<T,P...> & src) {
+  return create_mirror_copy(DefaultExecutionSpace(), src);
+}
+
 } /* namespace Kokkos */
 
 //----------------------------------------------------------------------------
