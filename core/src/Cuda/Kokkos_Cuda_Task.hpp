4aa511a73283280a4e6b12df2dba840ea62d970c
Carter Edwards <hcedwar@sandia.gov>
2018-02-23 03:34:54 -0700

    Prevent calls to warp synchronization for task team size == 1

--- core/src/Cuda/Kokkos_Cuda_Task.hpp
+++ core/src/Cuda/Kokkos_Cuda_Task.hpp
@@ -124,14 +124,24 @@ namespace Impl {
  *  where
  *    blockDim.x * blockDim.y == WarpSize
  *
+ *  Current implementation requires blockDim.x == 1.
+ *  Vector level parallelism with blockDim.y > 1 on Volta will
+ *  require a vector-level synchronization mask for vector-level
+ *  collective operaitons.
+ *
  *  Both single thread and thread team tasks are run by a full Cuda warp.
  *  A single thread task is called by warp lane #0 and the remaining
  *  lanes of the warp are idle.
+ *
+ *  When executing a single thread task the syncwarp or other
+ *  warp synchronizing functions must not be called.
  */
 template<>
 class TaskExec< Kokkos::Cuda >
 {
 private:
+
+  enum : int { WarpSize = Kokkos::Impl::CudaTraits::WarpSize };
 
   TaskExec( TaskExec && ) = delete ;
   TaskExec( TaskExec const & ) = delete ;
@@ -144,6 +154,8 @@ private:
   int32_t * m_team_shmem ;
   const int m_team_size ;
 
+  // If constructed with arg_team_size == 1 the object
+  // can only be used by 0 == threadIdx.y.
   __device__
   TaskExec( int32_t * arg_team_shmem , int arg_team_size = blockDim.y )
     : m_team_shmem( arg_team_shmem )
@@ -170,7 +182,7 @@ public:
         // thread_id < blockDim.y
         KOKKOS_IMPL_CUDA_SYNCWARP ;
         ValueType tmp( val ); // input might not be register variable
-        cuda_shfl( val, tmp, blockDim.x * thread_id, blockDim.X * blockDim.y );
+        cuda_shfl( val, tmp, blockDim.x * thread_id, WarpSize );
       }
     }
 
@@ -443,12 +455,18 @@ void parallel_reduce
   }
   initialized_result = result;
 
-  strided_shfl_warp_reduction(
-                          [&] (ValueType& val1, const ValueType& val2) { val1 += val2; },
-                          initialized_result,
-                          loop_boundaries.thread.team_size(),
-                          blockDim.x);
-  initialized_result = shfl_warp_broadcast<ValueType>( initialized_result, threadIdx.x, Impl::CudaTraits::WarpSize );
+  if ( 1 < loop_boundaries.thread.team_size() ) {
+
+    strided_shfl_warp_reduction(
+      [&] (ValueType& val1, const ValueType& val2) { val1 += val2; },
+      initialized_result,
+      loop_boundaries.thread.team_size(),
+      blockDim.x);
+
+    initialized_result =
+      shfl_warp_broadcast<ValueType>(
+        initialized_result, threadIdx.x, Impl::CudaTraits::WarpSize );
+  }
 }
 
 template< typename iType, class Lambda, typename ReducerType >
@@ -467,12 +485,20 @@ void parallel_reduce
     lambda(i,result);
   }
 
-  strided_shfl_warp_reduction(
-                          [&] (ValueType& val1, const ValueType& val2) { reducer.join(val1,val2); },
-                          result,
-                          loop_boundaries.thread.team_size(),
-                          blockDim.x);
-  reducer.reference() = shfl_warp_broadcast<ValueType>( result, threadIdx.x, Impl::CudaTraits::WarpSize );
+  if ( 1 < loop_boundaries.thread.team_size() ) {
+    strided_shfl_warp_reduction(
+      [&] (ValueType& val1, const ValueType& val2) { reducer.join(val1,val2); },
+      result,
+      loop_boundaries.thread.team_size(),
+      blockDim.x);
+
+    reducer.reference() =
+      shfl_warp_broadcast<ValueType>(
+        result, threadIdx.x, Impl::CudaTraits::WarpSize );
+  }
+  else {
+    reducer.reference() = result ;
+  }
 }
 // all-reduce within team members within warp
 // assume vec_length*team_size == warp_size
@@ -520,12 +546,16 @@ void parallel_reduce
 
   initialized_result = result;
 
-  //initialized_result = multi_shfl_warp_reduction(
-  multi_shfl_warp_reduction(
-                          [&] (ValueType& val1, const ValueType& val2) { val1 += val2; },
-                          initialized_result,
-                          blockDim.x);
-  initialized_result = shfl_warp_broadcast<ValueType>( initialized_result, 0, blockDim.x );
+  if ( 1 < loop_boundaries.thread.team_size() ) {
+    //initialized_result = multi_shfl_warp_reduction(
+    multi_shfl_warp_reduction(
+      [&] (ValueType& val1, const ValueType& val2) { val1 += val2; },
+      initialized_result,
+      blockDim.x);
+
+    initialized_result =
+      shfl_warp_broadcast<ValueType>( initialized_result, 0, blockDim.x );
+  }
 }
 
 template< typename iType, class Lambda, typename ReducerType >
@@ -544,11 +574,18 @@ void parallel_reduce
     lambda(i,result);
   }
 
-  multi_shfl_warp_reduction(
-                          [&] (ValueType& val1, const ValueType& val2) { reducer.join(val1, val2); },
-                          result,
-                          blockDim.x);
-  reducer.reference() = shfl_warp_broadcast<ValueType>( result, 0, blockDim.x );
+  if ( 1 < loop_boundaries.thread.team_size() ) {
+    multi_shfl_warp_reduction(
+      [&] (ValueType& val1, const ValueType& val2) { reducer.join(val1,val2); },
+      result,
+      blockDim.x);
+
+    reducer.reference() =
+      shfl_warp_broadcast<ValueType>( result, 0, blockDim.x );
+  }
+  else {
+    reducer.reference() = result ;
+  }
 }
 // scan across corresponding vector lanes between team members within warp
 // assume vec_length*team_size == warp_size
@@ -570,35 +607,47 @@ void parallel_scan
       , void
       , Closure >::value_type ;
 
-  value_type accum = 0 ;
-  value_type val, y, local_total;
+  if ( 1 < loop_boundaries.thread.team_size() ) {
+    // TODO:  This is not a performant algorithm and needs to be replaced.
+    // TODO:  This algorithm is erroneous when all threads of the warp
+    //        do not have the exact same iteration.
 
-  for( iType i = loop_boundaries.start; i < loop_boundaries.end; i+=loop_boundaries.increment) {
-    val = 0;
-    closure(i,val,false);
+    value_type accum = 0 ;
+    value_type val, y, local_total;
 
-    // intra-blockDim.y exclusive scan on 'val'
-    // accum = accumulated, sum in total for this iteration
+    for( iType i = loop_boundaries.start; i < loop_boundaries.end; i+=loop_boundaries.increment) {
+      val = 0;
+      closure(i,val,false);
 
-    // INCLUSIVE scan
-    for( int offset = blockDim.x ; offset < Impl::CudaTraits::WarpSize ; offset <<= 1 ) {
-      y = Kokkos::shfl_up(val, offset, Impl::CudaTraits::WarpSize);
-      if(threadIdx.y*blockDim.x >= offset) { val += y; }
-    }
+      // intra-blockDim.y exclusive scan on 'val'
+      // accum = accumulated, sum in total for this iteration
 
-    // pass accum to all threads
-    local_total = shfl_warp_broadcast<value_type>(val,
+      // INCLUSIVE scan
+      for( int offset = blockDim.x ; offset < Impl::CudaTraits::WarpSize ; offset <<= 1 ) {
+        y = Kokkos::shfl_up(val, offset, Impl::CudaTraits::WarpSize);
+        if(threadIdx.y*blockDim.x >= offset) { val += y; }
+      }
+
+      // pass accum to all threads
+      local_total = shfl_warp_broadcast<value_type>(val,
                                             threadIdx.x+Impl::CudaTraits::WarpSize-blockDim.x,
                                             Impl::CudaTraits::WarpSize);
 
-    // make EXCLUSIVE scan by shifting values over one
-    KOKKOS_IMPL_CUDA_SYNCWARP ;
-    val = Kokkos::shfl_up(val, blockDim.x, Impl::CudaTraits::WarpSize);
-    if ( threadIdx.y == 0 ) { val = 0 ; }
+      // make EXCLUSIVE scan by shifting values over one
+      KOKKOS_IMPL_CUDA_SYNCWARP ;
+      val = Kokkos::shfl_up(val, blockDim.x, Impl::CudaTraits::WarpSize);
+      if ( threadIdx.y == 0 ) { val = 0 ; }
 
-    val += accum;
-    closure(i,val,true);
-    accum += local_total;
+      val += accum;
+      closure(i,val,true);
+      accum += local_total;
+    }
+  }
+  else {
+    value_type accum = 0 ;
+    for( iType i = loop_boundaries.start; i < loop_boundaries.end; i+=loop_boundaries.increment) {
+      closure(i,accum,true);
+    }
   }
 }
 
@@ -622,34 +671,46 @@ void parallel_scan
       , void
       , Closure >::value_type ;
 
-  value_type accum = 0 ;
-  value_type val, y, local_total;
+  if ( 1 < loop_boundaries.thread.team_size() ) {
+    // TODO:  This is not a performant algorithm and needs to be replaced.
+    // TODO:  This algorithm is erroneous when all threads of the warp
+    //        do not have the exact same iteration.
 
-  for( iType i = loop_boundaries.start; i < loop_boundaries.end; i+=loop_boundaries.increment) {
-    val = 0;
-    closure(i,val,false);
+    value_type accum = 0 ;
+    value_type val, y, local_total;
 
-    // intra-blockDim.x exclusive scan on 'val'
-    // accum = accumulated, sum in total for this iteration
+    for( iType i = loop_boundaries.start; i < loop_boundaries.end; i+=loop_boundaries.increment) {
+      val = 0;
+      closure(i,val,false);
 
-    // INCLUSIVE scan
-    for( int offset = 1 ; offset < blockDim.x ; offset <<= 1 ) {
+      // intra-blockDim.x exclusive scan on 'val'
+      // accum = accumulated, sum in total for this iteration
+
+      // INCLUSIVE scan
+      for( int offset = 1 ; offset < blockDim.x ; offset <<= 1 ) {
+        KOKKOS_IMPL_CUDA_SYNCWARP ;
+        y = Kokkos::shfl_up(val, offset, blockDim.x);
+        if(threadIdx.x >= offset) { val += y; }
+      }
+
+      // pass accum to all threads
+      local_total = shfl_warp_broadcast<value_type>(val, blockDim.x-1, blockDim.x);
+
+      // make EXCLUSIVE scan by shifting values over one
       KOKKOS_IMPL_CUDA_SYNCWARP ;
-      y = Kokkos::shfl_up(val, offset, blockDim.x);
-      if(threadIdx.x >= offset) { val += y; }
+      val = Kokkos::shfl_up(val, 1, blockDim.x);
+      if ( threadIdx.x == 0 ) { val = 0 ; }
+
+      val += accum;
+      closure(i,val,true);
+      accum += local_total;
     }
-
-    // pass accum to all threads
-    local_total = shfl_warp_broadcast<value_type>(val, blockDim.x-1, blockDim.x);
-
-    // make EXCLUSIVE scan by shifting values over one
-    KOKKOS_IMPL_CUDA_SYNCWARP ;
-    val = Kokkos::shfl_up(val, 1, blockDim.x);
-    if ( threadIdx.x == 0 ) { val = 0 ; }
-
-    val += accum;
-    closure(i,val,true);
-    accum += local_total;
+  }
+  else {
+    value_type accum = 0 ;
+    for( iType i = loop_boundaries.start; i < loop_boundaries.end; i+=loop_boundaries.increment) {
+      closure(i,accum,true);
+    }
   }
 }
 
@@ -675,11 +736,13 @@ namespace Kokkos {
   
   template<class FunctorType, class ValueType>
   KOKKOS_INLINE_FUNCTION
-  void single(const Impl::VectorSingleStruct<Impl::TaskExec< Kokkos::Cuda > >& , const FunctorType& lambda, ValueType& val) {
+  void single(const Impl::VectorSingleStruct<Impl::TaskExec< Kokkos::Cuda > >& s , const FunctorType& lambda, ValueType& val) {
 #ifdef __CUDA_ARCH__
     if(threadIdx.x == 0) lambda(val);
-    KOKKOS_IMPL_CUDA_SYNCWARP ;
-    val = shfl(val,0,blockDim.x);
+    if ( 1 < s.team_member.team_size() ) {
+      KOKKOS_IMPL_CUDA_SYNCWARP ;
+      val = shfl(val,0,blockDim.x);
+    }
 #endif
   }
   
