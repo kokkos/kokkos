e3e70a1bd6e1ae9ebbd8ec3750cbd3f1d14f8566
Daniel Holladay <dholladay00@.lanl.gov>
2018-08-25 21:59:26 -0600

    Passing in an alloc without initializing object into function per Marks suggestion.

--- core/src/Kokkos_CopyViews.hpp
+++ core/src/Kokkos_CopyViews.hpp
@@ -2121,7 +2121,8 @@ create_mirror_view_and_copy(const Space& , const Kokkos::View<T,P...> & src
 // Create a mirror view in a new space without initializing (specialization for same space)
 template<class Space, class T, class ... P>
 typename Impl::MirrorViewType<Space,T,P ...>::view_type
-create_mirror_view_without_initializing(const Space& , const Kokkos::View<T,P...> & src
+create_mirror_view(const Space& , const ViewAllocateWithoutInitializing & arg_prop
+  , const Kokkos::View<T,P...> & src
   , typename std::enable_if<Impl::MirrorViewType<Space,T,P ...>::is_same_memspace>::type* = 0 ) {
   return src;
 }
@@ -2129,10 +2130,12 @@ create_mirror_view_without_initializing(const Space& , const Kokkos::View<T,P...
 // Create a mirror view in a new space without initializing (specialization for different space)
 template<class Space, class T, class ... P>
 typename Impl::MirrorViewType<Space,T,P ...>::view_type
-create_mirror_view_without_initializing(const Space& , const Kokkos::View<T,P...> & src
+create_mirror_view(const Space& , const ViewAllocateWithoutInitializing & arg_prop
+  , const Kokkos::View<T,P...> & src
   , typename std::enable_if<!Impl::MirrorViewType<Space,T,P ...>::is_same_memspace>::type* = 0 ) {
   using Mirror = typename Impl::MirrorViewType<Space,T,P ...>::view_type;
-  return Mirror(ViewAllocateWithoutInitializing(src.label()),src.layout());
+  return Mirror(Impl::ViewCtorProp< std::string , Kokkos::Impl::WithoutInitializing_t >( arg_prop.label , Kokkos::WithoutInitializing )
+    , src.layout());
 }
 
 } /* namespace Kokkos */
