f3c4c1703c444a85ed7609a62fb2d06417149412
Christian Trott <crtrott@sandia.gov>
2018-10-16 18:04:54 -0600

    Fix test issue #1853

--- containers/unit_tests/TestScatterView.hpp
+++ containers/unit_tests/TestScatterView.hpp
@@ -80,7 +80,9 @@ void test_scatter_view_config(int n)
     Kokkos::Experimental::contribute(original_view, scatter_view);
   }
 #if defined( KOKKOS_ENABLE_CXX11_DISPATCH_LAMBDA )
+  Kokkos::fence();
   auto host_view = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), original_view);
+  Kokkos::fence();
   for (typename decltype(host_view)::size_type i = 0; i < host_view.extent(0); ++i) {
     auto val0 = host_view(i, 0);
     auto val1 = host_view(i, 1);
