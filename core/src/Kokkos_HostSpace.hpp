117df42aa552227a87aa36507b36c3079af83dec
James Elliott <jjellio@sandia.gov>
2019-01-30 17:15:33 -0700

    Draft of parallelized deep copy using memcpy+page aligned chunking

--- core/src/Kokkos_HostSpace.hpp
+++ core/src/Kokkos_HostSpace.hpp
@@ -57,6 +57,11 @@
 #include <impl/Kokkos_Error.hpp>
 #include <impl/Kokkos_SharedAlloc.hpp>
 
+#ifdef _OPENMP
+#include<cstdio>
+#include <omp.h>
+#endif
+
 /*--------------------------------------------------------------------------*/
 
 namespace Kokkos {
@@ -294,7 +299,95 @@ namespace Impl {
 template< class ExecutionSpace >
 struct DeepCopy< HostSpace, HostSpace, ExecutionSpace > {
   DeepCopy( void * dst, const void * src, size_t n ) {
-    memcpy( dst, src, n );
+    constexpr size_t page_sz_ = 4*1024;
+    constexpr size_t pages_per_chunk = 2*page_sz_;
+
+    // I can't use exec_space::concurrency here (or can I?)
+    // this may require specializing for each execSpace...
+    #ifdef _OPENMP
+      const int nthreads = omp_get_max_threads();
+    #else
+      const int nthreads = 1;
+    #endif
+
+    if( (n < 2*page_sz_) || (nthreads == 1) ) {
+      memcpy( dst, src, n );
+      return;
+    }
+
+    // otherwise, execute a parallel loop over memcpy,
+    // copying pages at a time
+
+    // This line tests if dst is page aligned. We don't 
+    // care about src, since it is readonly.
+    // Effectively, mask out the lower bits that account for page_sz-1.
+    // if the resulting address is the same as dst, then dst is page 
+    // aligned to begin with we also are promised that dst + page_sz 
+    // will always be valid, because we enforce that 2*page_sz the smallest
+    // work allowed in this region of code.
+    const unsigned char * pg_aligned_start = ( (((uintptr_t) dst) & ~(page_sz_-1)) == ((uintptr_t) dst) )
+                                              ? (unsigned char *) dst
+                                              : (unsigned char *)(  (((uintptr_t) dst) & ~(page_sz_-1)) + page_sz_);
+    // handle the remainder. How many bytes are there from dst to pg_aligned_start
+    const size_t remainder = (size_t ) (pg_aligned_start - ((unsigned char *) dst) );
+    const unsigned char * corrected_src_from_pg_aligned_dst = (unsigned char *) (((uintptr_t) src) + remainder);
+    const size_t new_n =  (n-remainder);
+    const size_t chunks = (new_n / pages_per_chunk ) + 1;
+
+    #ifdef JJE_DEBUG_PAR_DEEPCOPY
+    fprintf(stderr, "pg_aligned_start %p\n", pg_aligned_start);
+    fprintf(stderr, "dest             %p\n", dst);
+    fprintf(stderr, "src              %p\n", src);
+    fprintf(stderr, "remainder        %d\n", (int) remainder);
+    fprintf(stderr, "new_n            %d\n", (int) new_n);
+    fprintf(stderr, "chunks           %d\n", (int) chunks);
+    fprintf(stderr, "n                %d\n", (int) n);
+    #endif
+
+    #pragma omp parallel
+    {
+
+      // first thread out handles the remainder
+      #pragma omp single
+      {
+        // the initial dst and src are correct, only copy remainder bytes
+        memcpy(dst,src,remainder);
+        #ifdef JJE_DEBUG_PAR_DEEPCOPY
+        for(uint32_t i = 0; i < (remainder/8); ++i)
+          fprintf(stderr, "src[%d] = %d, dst[%d] =  %d\n", i, (int) (((int64_t*)src)[i]), i, (int) (((int64_t*)dst)[i]));
+        #endif
+      }
+
+      #pragma omp for nowait
+      for(size_t i=0; i < chunks; ++i)
+      {
+        const size_t my_offset = i * pages_per_chunk;
+        if (my_offset < new_n ) {
+          const unsigned char * my_dst = pg_aligned_start + ((uintptr_t) my_offset);
+          unsigned char * my_src = (unsigned char *) (((uintptr_t) corrected_src_from_pg_aligned_dst) + ((uintptr_t) my_offset));
+          const size_t my_n = (pages_per_chunk+my_offset) < new_n ? pages_per_chunk : new_n - my_offset;
+          #ifdef JJE_DEBUG_PAR_DEEPCOPY
+          #pragma omp critical
+          {
+            fprintf(stderr, "-------------------------------------------\n");
+            fprintf(stderr, "chunk                 %d\n", (int) i);
+            fprintf(stderr, "my_offset             %d\n", (int) my_offset);
+            fprintf(stderr, "my_dst                %p\n", my_dst);
+            fprintf(stderr, "my_src                %p\n", my_src);
+            fprintf(stderr, "my_n                  %d\n", (int) my_n);
+          
+
+            memcpy( (void*) my_dst, (void*) my_src, my_n );
+ 
+            for(uint32_t i = 0; i < (my_n/8); ++i)
+              fprintf(stderr, "src[%d] = %d, dst[%d] =  %d\n", i, (int) (((int64_t*)my_src)[i]), i, (int) (((int64_t*)my_dst)[i]));
+          }
+          #else
+          memcpy( (void*) my_dst, (void*) my_src, my_n );
+          #endif
+       }
+      }
+    }
   }
 
   DeepCopy( const ExecutionSpace& exec, void * dst, const void * src, size_t n ) {
