15dbb9d92a3e8a36502936b75a9d2a3181098b44
Daniel Arndt <daniel.arndt@iwr.uni-heidelberg.de>
2019-02-25 17:53:36 +0100

    Test AtomicOperations for Kokkos::complex

--- core/unit_test/TestAtomicOperations.hpp
+++ core/unit_test/TestAtomicOperations.hpp
@@ -536,7 +536,7 @@ bool DivAtomicTest( T i0, T i1 )
 
   bool passed = true;
 
-  if ( (resSerial-res)*(resSerial-res) > 1e-10 ) {
+  if ( abs( (resSerial-res) * 1.) > 1e-5 ) {
     passed = false;
 
     std::cout << "Loop<"
