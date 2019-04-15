2d7692b1db11160acc06df7a545aa6d2cd71f503
David Hollman <dshollm@sandia.gov>
2019-04-15 10:06:17 -0700

    all OpenMP parallel reduce implementations now flow through one parallel region

--- core/src/OpenMP/Kokkos_OpenMP_Parallel_Reduce.hpp
+++ core/src/OpenMP/Kokkos_OpenMP_Parallel_Reduce.hpp
@@ -75,9 +75,49 @@ private:
     return *static_cast<Derived const*>(this);
   }
 
+protected:
+
+  inline void
+  resize_thread_data() const
+  {
+    size_t pool_reduce_bytes =
+      Concepts::reducer_value_size(self().m_reducer_storage.get_reducer());
+
+    self().m_instance->resize_thread_data(
+      pool_reduce_bytes,
+      /* team_reduce_bytes = */ 0,
+      /* team_shared_bytes = */ 0,
+      /* thread_local_bytes = */ 0
+    );
+  }
+
+  inline constexpr bool
+  organize_team(HostThreadTeamData&) const
+  {
+    // in the absense of teams, all workers are active
+    return true;
+  }
+
+  inline void
+  set_work_partition(HostThreadTeamData& data) const
+  {
+    // TODO using begin and end customization points
+    // TODO use Kokkos::query to get chunk size property
+    data.set_work_partition(
+      self().m_policy.end() - self().m_policy.begin(),
+      self().m_policy.chunk_size()
+    );
+  }
+
+  inline void
+  disband_team(HostThreadTeamData&) const
+  {
+    // in the absense of teams, nothing to do here
+  }
+
 public:
 
-  inline void execute() const
+  inline void execute()
   {
     using Policy = typename Derived::Policy;
     using ReducerType = typename Derived::reducer;
@@ -88,15 +128,7 @@ public:
 
     OpenMPExec::verify_is_master("Kokkos::OpenMP parallel_reduce");
 
-    const size_t pool_reduce_bytes =
-      Concepts::reducer_value_size(self().m_reducer_storage.get_reducer());
-
-    self().m_instance->resize_thread_data(
-      pool_reduce_bytes,
-      /* team_reduce_bytes = */ 0,
-      /* team_shared_bytes = */ 0,
-      /* thread_local_bytes = */ 0
-    );
+    self().resize_thread_data();
 
 #ifdef KOKKOS_ENABLE_DEPRECATED_CODE
     const int pool_size = OpenMP::thread_pool_size();
@@ -108,12 +140,11 @@ public:
     {
       HostThreadTeamData& data = *(self().m_instance->get_thread_data());
 
-      // TODO using begin and end customization points
-      // TODO use Kokkos::query to get chunk size property
-      data.set_work_partition(
-        self().m_policy.end() - self().m_policy.begin(),
-        self().m_policy.chunk_size()
-      );
+      auto active = self().organize_team(data);
+
+      if(active) {
+        self().set_work_partition(data);
+      }
 
       if /* constexpr */ (is_dynamic) {
         // Make sure work partition is set before stealing
@@ -127,21 +158,22 @@ public:
         );
       Concepts::reducer_init(self().m_reducer_storage.get_reducer(), update);
 
-      // Why is this a pair and not an array??
-      std::pair<int64_t,int64_t> range(0,0);
+      if(active) {
+        // Why is this a pair and not an array??
+        std::pair<int64_t,int64_t> range(0,0);
 
-      do {
+        do {
 
-        range = is_dynamic ? data.get_work_stealing_chunk()
-          : data.get_work_partition();
+          range = is_dynamic ? data.get_work_stealing_chunk()
+            : data.get_work_partition();
 
-        self().exec_range(
-          range.first  + self().m_policy.begin(),
-          range.second + self().m_policy.begin(),
-          update
-        );
+          self().exec_range(data, range.first, range.second, update);
 
-      } while ( is_dynamic && 0 <= range.first );
+        } while ( is_dynamic && 0 <= range.first );
+      }
+
+      self().disband_team(data);
+
     } // end pragma omp parallel num_threads(pool_size)
 
     // Reduction:
@@ -206,11 +238,14 @@ private:
 
   inline
   void exec_range(
+    HostThreadTeamData&,
     Member ibeg,
     Member iend,
     Concepts::reducer_reference_type_t<ReducerType> update
   ) const
   {
+    auto begin = ibeg + m_policy.begin();
+    auto end = iend + m_policy.begin();
     for(Member iwork = ibeg; iwork < iend; ++iwork) {
       Concepts::functor_invoke_with_policy(
         m_policy, m_functor, iwork, update
@@ -295,12 +330,15 @@ private:
 
   inline void
   exec_range(
+    HostThreadTeamData&,
     Member ibeg,
     Member iend,
     Concepts::reducer_reference_type_t<ReducerType> update
   ) const
   {
-    for(Member iwork = ibeg; iwork < iend; ++iwork) {
+    auto begin = ibeg + m_policy.begin();
+    auto end = iend + m_policy.begin();
+    for(Member iwork = ibeg; iwork < end; ++iwork) {
       iterate_type(m_mdr_policy, m_functor, update)(iwork);
     }
   }
@@ -348,9 +386,19 @@ class ParallelReduce<
   Kokkos::TeamPolicy<Properties...>,
   ReducerType,
   Kokkos::OpenMP
->
+> : public OpenMPParallelReduceCommon<
+      ParallelReduce<
+        FunctorType,
+        Kokkos::TeamPolicy<Properties...>,
+        ReducerType,
+        Kokkos::OpenMP
+      >
+    >
 {
 private:
+
+  template <class Derived>
+  friend class OpenMPParallelReduceCommon;
 
   enum { TEAM_REDUCE_SIZE = 512 };
 
@@ -365,11 +413,11 @@ private:
   int m_shmem_size;
 
   inline void
-  exec_team(
+  exec_range(
     HostThreadTeamData& data,
-    Concepts::reducer_reference_type_t<ReducerType>& update,
     int league_rank_begin,
-    int league_rank_end
+    int league_rank_end,
+    Concepts::reducer_reference_type_t<ReducerType>& update
   ) const
   {
     const int league_size = m_policy.league_size();
@@ -387,20 +435,18 @@ private:
     }
   }
 
-public:
-
-  inline
-  void execute() const
+  inline void resize_thread_data() const
   {
-    enum { is_dynamic = std::is_same<typename Policy::schedule_type::type, Kokkos::Dynamic>::value };
-
-    OpenMPExec::verify_is_master("Kokkos::OpenMP parallel_reduce");
-
     const size_t pool_reduce_size =
       Concepts::reducer_value_size(m_reducer_storage.get_reducer());
 
+    auto shmem_size =
+      m_policy.scratch_size(0)
+        + m_policy.scratch_size(1)
+        + Concepts::functor_team_shmem_size(m_functor, m_policy.team_size());
+
     const size_t team_reduce_size = TEAM_REDUCE_SIZE * m_policy.team_size();
-    const size_t team_shared_size = m_shmem_size + m_policy.scratch_size(1);
+    const size_t team_shared_size = shmem_size + m_policy.scratch_size(1);
     const size_t thread_local_size = 0; // Never shrinks
 
     m_instance->resize_thread_data(
@@ -409,92 +455,47 @@ public:
       team_shared_size,
       thread_local_size
     );
-
-#ifdef KOKKOS_ENABLE_DEPRECATED_CODE
-    const int pool_size = OpenMP::thread_pool_size();
-#else
-    const int pool_size = OpenMP::impl_thread_pool_size();
-#endif
-#pragma omp parallel num_threads(pool_size)
-    {
-      HostThreadTeamData & data = *(m_instance->get_thread_data());
-
-      auto active = data.organize_team(m_policy.team_size());
-
-      if(active) {
-        data.set_work_partition(
-          m_policy.league_size(),
-          ( 0 < m_policy.chunk_size()
-            ? m_policy.chunk_size()
-            : m_policy.team_iter()
-          )
-        );
-      }
-
-      if(is_dynamic) {
-        // Must synchronize to make sure each team has set its
-        // partition before begining the work stealing loop.
-        if(data.pool_rendezvous()) data.pool_rendezvous_release();
-      }
-
-      Concepts::reducer_reference_type_t<ReducerType> update =
-        Concepts::reducer_bind_reference(
-          m_reducer_storage.get_reducer(),
-          data.pool_reduce_local()
-        );
-      Concepts::reducer_init(m_reducer_storage.get_reducer(), update);
-
-      if(active) {
-
-        std::pair<int64_t,int64_t> range(0,0);
-
-        do {
-
-          range = is_dynamic ? data.get_work_stealing_chunk()
-            : data.get_work_partition();
-
-          exec_team(data, update, range.first, range.second);
-
-        } while ( is_dynamic && 0 <= range.first );
-      }
-
-      data.disband_team();
-
-      //  This thread has updated 'pool_reduce_local()' with its
-      //  contributions to the reduction.  The parallel region is
-      //  about to terminate and the master thread will load and
-      //  reduce each 'pool_reduce_local()' contribution.
-      //  Must 'memory_fence()' to guarantee that storing the update to
-      //  'pool_reduce_local()' will complete before this thread
-      //  exits the parallel region.
-
-      memory_fence();
-
-    } // end omp parallel num_threads(pool_size
-
-    // Reduction:
-    Concepts::reducer_reference_type_t<ReducerType> dst =
-      Concepts::reducer_bind_reference(
-        m_reducer_storage.get_reducer(),
-        m_instance->get_thread_data(0)->pool_reduce_local()
-      );
-
-    for (int i = 1; i < pool_size; ++i) {
-
-      Concepts::reducer_reference_type_t<ReducerType> i_src =
-        Concepts::reducer_bind_reference(
-          m_reducer_storage.get_reducer(),
-          m_instance->get_thread_data(i)->pool_reduce_local()
-        );
-
-      Concepts::reducer_join(m_reducer_storage.get_reducer(), dst, i_src);
-
-    }
-
-    Concepts::reducer_final(m_reducer_storage.get_reducer(), dst);
-
-    Concepts::reducer_assign_result(m_reducer_storage.get_reducer(), dst);
   }
+
+  inline bool
+  organize_team(HostThreadTeamData& data) const
+  {
+    return static_cast<bool>(
+      data.organize_team(m_policy.team_size())
+    );
+  }
+
+  inline void
+  set_work_partition(HostThreadTeamData& data) const
+  {
+    data.set_work_partition(
+      m_policy.league_size(),
+      ( 0 < m_policy.chunk_size()
+        ? m_policy.chunk_size()
+        : m_policy.team_iter()
+      )
+    );
+  }
+
+  inline void
+  disband_team(HostThreadTeamData& data) const
+  {
+    data.disband_team();
+
+    // TODO move this to a separate function?
+
+    //  This thread has updated 'pool_reduce_local()' with its
+    //  contributions to the reduction.  The parallel region is
+    //  about to terminate and the master thread will load and
+    //  reduce each 'pool_reduce_local()' contribution.
+    //  Must 'memory_fence()' to guarantee that storing the update to
+    //  'pool_reduce_local()' will complete before this thread
+    //  exits the parallel region.
+
+    memory_fence();
+  }
+
+public:
 
   //----------------------------------------
 
@@ -512,12 +513,7 @@ public:
   ) : m_instance(t_openmp_instance),
       m_functor(std::move(arg_functor)),
       m_policy(std::move(arg_policy)),
-      m_reducer_storage(m_functor, m_policy, arg_result),
-      m_shmem_size(
-        arg_policy.scratch_size(0)
-        + arg_policy.scratch_size(1)
-        + Concepts::functor_team_shmem_size(arg_functor, arg_policy.team_size())
-      )
+      m_reducer_storage(m_functor, m_policy, arg_result)
   { }
 
   inline
@@ -528,12 +524,7 @@ public:
   ) : m_instance(t_openmp_instance),
       m_functor(std::move(arg_functor)),
       m_policy(std::move(arg_policy)),
-      m_reducer_storage(std::move(arg_reducer)),
-      m_shmem_size(
-        arg_policy.scratch_size(0)
-        + arg_policy.scratch_size(1)
-        + Concepts::functor_team_shmem_size(arg_functor, arg_policy.team_size())
-      )
+      m_reducer_storage(std::move(arg_reducer))
   { }
 
 };
