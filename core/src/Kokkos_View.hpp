b8982d54b48abdf518023c739b8a05cc029d37f9
crtrott <crtrott@sandia.gov>
2019-05-09 15:28:20 -0600

    Make View copy constructors/assignment use limited SFINAE
    This will fix issue #2127.

--- core/src/Kokkos_View.hpp
+++ core/src/Kokkos_View.hpp
@@ -74,7 +74,11 @@ template< class DataType , class ArrayLayout
 struct ViewDataAnalysis ;
 
 template< class , class ... >
-class ViewMapping { public: enum { is_assignable = false }; };
+class ViewMapping {
+  public:
+  enum { is_assignable_data_type = false };
+  enum { is_assignable = false };
+};
 
 
 
@@ -1990,7 +1994,10 @@ public:
 
   template< class RT , class ... RP >
   KOKKOS_INLINE_FUNCTION
-  View( const View<RT,RP...> & rhs )
+  View( const View<RT,RP...> & rhs,
+        typename std::enable_if<Kokkos::Impl::ViewMapping<
+        traits , typename View<RT,RP...>::traits , typename traits::specialize >::is_assignable_data_type>::type* = 0
+     )
     : m_track( rhs.m_track , traits::is_managed )
     , m_map()
     {
@@ -2002,7 +2009,9 @@ public:
 
   template< class RT , class ... RP >
   KOKKOS_INLINE_FUNCTION
-  View & operator = ( const View<RT,RP...> & rhs )
+  typename std::enable_if<Kokkos::Impl::ViewMapping<
+     traits , typename View<RT,RP...>::traits , typename traits::specialize >::is_assignable_data_type,
+     View>::type & operator = ( const View<RT,RP...> & rhs )
     {
       typedef typename View<RT,RP...>::traits  SrcTraits ;
       typedef Kokkos::Impl::ViewMapping< traits , SrcTraits , typename traits::specialize >  Mapping ;
--- core/unit_test/TestViewAPI_e.hpp
+++ core/unit_test/TestViewAPI_e.hpp
@@ -225,15 +225,15 @@ struct TestViewOverloadResolution {
   }
 
   static void test_function_overload() {
-    Kokkos::View<double**,Kokkos::LayoutRight,ExecSpace> a("A",10,3);
+    Kokkos::View<double**, typename ExecSpace::execution_space::array_layout, ExecSpace> a("A",10,3);
     int data_type_1 = foo(a);
-    int data_type_3 = foo(Kokkos::View<const double**,Kokkos::LayoutRight,ExecSpace>(a));
-    Kokkos::View<double***,Kokkos::LayoutRight,ExecSpace> b("B",10,3,4);
+    int data_type_3 = foo(Kokkos::View<const double**, typename ExecSpace::execution_space::array_layout, ExecSpace>(a));
+    Kokkos::View<double***, typename ExecSpace::execution_space::array_layout, ExecSpace> b("B",10,3,4);
     int data_type_2 = foo(b);
-    Kokkos::View<double*[3],Kokkos::LayoutRight,ExecSpace> c(a);
+    Kokkos::View<double*[3], typename ExecSpace::execution_space::array_layout, ExecSpace> c(a);
     int static_extent = bar(c);
     ASSERT_EQ(1,data_type_1);
-    ASSERT_EQ(1,data_type_2);
+    ASSERT_EQ(3,data_type_2);
     ASSERT_EQ(1,data_type_3);
     ASSERT_EQ(4,static_extent);
   }
