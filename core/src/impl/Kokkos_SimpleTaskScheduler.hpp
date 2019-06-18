7594d0945dc6ba90a1895087ac5fc03cc9f0dfdf
David Hollman <dshollm@sandia.gov>
2019-06-18 13:20:25 -0700

    fixed a bug from the rebase?

--- core/src/impl/Kokkos_SimpleTaskScheduler.hpp
+++ core/src/impl/Kokkos_SimpleTaskScheduler.hpp
@@ -100,12 +100,6 @@ protected:
 
   KOKKOS_INLINE_FUNCTION
   constexpr explicit
-  ExecutionSpaceInstanceStorage()
-    : base_t()
-  { }
-
-  KOKKOS_INLINE_FUNCTION
-  constexpr explicit
   ExecutionSpaceInstanceStorage(ExecutionSpace const& arg_execution_space)
     : base_t(arg_execution_space)
   { }
@@ -146,11 +140,6 @@ private:
 
 protected:
 
-  MemorySpaceInstanceStorage()
-    : base_t()
-  { }
-
-  KOKKOS_INLINE_FUNCTION
   MemorySpaceInstanceStorage()
     : base_t()
   { }
