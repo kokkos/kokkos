2da91ab871ed85b9f2e5d79430dd069824360d33
jjwilke <jjwilke@sandia.gov>
2019-09-12 13:11:07 -0600

    3.10 fixes: #2317

--- cmake/kokkos_preamble.cmake
+++ cmake/kokkos_preamble.cmake
@@ -3,5 +3,10 @@
 #this uses list dir for Trilinos 
 INCLUDE(${CMAKE_CURRENT_LIST_DIR}/kokkos_functions.cmake)
 INCLUDE(${CMAKE_CURRENT_LIST_DIR}/kokkos_pick_cxx_std.cmake)
+
+# NVCC does not accept all the -std flags
+# If NVCC receives a "bad flag", this variable tells nvcc_wrapper which 
+# -std flag to use. This is most often necessary during
+# feature tests in CMake
 SET(ENV{KOKKOS_CMAKE_CXX_STANDARD} ${KOKKOS_CXX_STANDARD})
 
