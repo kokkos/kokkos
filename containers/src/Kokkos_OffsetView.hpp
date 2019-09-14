29202c8986dacd5c979851ea724fcb2963d2c0b9
Nevin Liber <nevin@cplusplusguy.com>
2019-09-14 00:15:26 +0000

    Removed old implementation of OffsetView constructor for unmanaged data

--- containers/src/Kokkos_OffsetView.hpp
+++ containers/src/Kokkos_OffsetView.hpp
@@ -941,28 +941,6 @@ class OffsetView : public ViewTraits<DataType, Properties...> {
                                           Rank > 6 ? ends[6] - begins[6] : 0,
                                           Rank > 7 ? ends[7] - begins[7] : 0)),
         m_begins(begins) {}
-#if 0
-      : OffsetView(
-            Kokkos::Impl::ViewCtorProp<pointer_type>(p),
-            typename traits::array_layout(Rank > 0 ? ends[0] - begins[0] : 0,
-                                          Rank > 1 ? ends[1] - begins[1] : 0,
-                                          Rank > 2 ? ends[2] - begins[2] : 0,
-                                          Rank > 3 ? ends[3] - begins[3] : 0,
-                                          Rank > 4 ? ends[4] - begins[4] : 0,
-                                          Rank > 5 ? ends[5] - begins[5] : 0,
-                                          Rank > 6 ? ends[6] - begins[6] : 0,
-                                          Rank > 7 ? ends[7] - begins[7] : 0),
-            {
-                Rank > 0 ? begins[0] : KOKKOS_INVALID_OFFSET,
-                Rank > 1 ? begins[1] : KOKKOS_INVALID_OFFSET,
-                Rank > 2 ? begins[2] : KOKKOS_INVALID_OFFSET,
-                Rank > 3 ? begins[3] : KOKKOS_INVALID_OFFSET,
-                Rank > 4 ? begins[4] : KOKKOS_INVALID_OFFSET,
-                Rank > 5 ? begins[5] : KOKKOS_INVALID_OFFSET,
-                Rank > 6 ? begins[6] : KOKKOS_INVALID_OFFSET,
-                Rank > 7 ? begins[7] : KOKKOS_INVALID_OFFSET,
-            }) {}
-#endif
 
   //----------------------------------------
   // Allocation tracking properties
