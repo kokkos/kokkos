a69d4598f46b1f934191f70be0854ab5e18d3168
jjwilke <jjwilke@sandia.gov>
2019-09-19 10:40:35 -0700

    better handling of intermediate standards : Issue #2336

--- cmake/pgi.cmake
+++ cmake/pgi.cmake
@@ -1,6 +1,8 @@
 
-function(kokkos_set_pgi_flags standard)
-  STRING(TOLOWER ${standard} LC_STANDARD)
-  GLOBAL_SET(KOKKOS_CXX_STANDARD_FLAG "--c++${LC_STANDARD}")
+function(kokkos_set_pgi_flags full_standard int_standard)
+  STRING(TOLOWER ${full_standard} FULL_LC_STANDARD)
+  STRING(TOLOWER ${int_standard} INT_LC_STANDARD)
+  SET(KOKKOS_CXX_STANDARD_FLAG "--c++${FULL_LC_STANDARD}" PARENT_SCOPE)
+  SET(KOKKOS_CXX_INTERMDIATE_STANDARD_FLAG "--c++${INT_LC_STANDARD}" PARENT_SCOPE)
 endfunction()
 
