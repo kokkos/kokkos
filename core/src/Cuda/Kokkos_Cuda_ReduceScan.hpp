6d747178c42e4533569f2815516ff64a75ce3821
David Hollman <dshollm@sandia.gov>
2019-10-08 16:42:34 -0700

    more unused code removal

--- core/src/Cuda/Kokkos_Cuda_ReduceScan.hpp
+++ core/src/Cuda/Kokkos_Cuda_ReduceScan.hpp
@@ -228,30 +228,29 @@ __device__ inline
 
 template <class ValueType, class JoinOp>
 __device__ inline
-void
-    cuda_intra_block_reduction(ValueType& value, const JoinOp& join,
+void cuda_intra_block_reduction(ValueType&& value, JoinOp&& join,
                                const int max_active_thread = blockDim.y) {
   cuda_intra_warp_reduction(value, join, max_active_thread);
   cuda_inter_warp_reduction(value, join, max_active_thread);
 }
 
-template <class ReducerType>
-__device__ inline
-typename std::enable_if<Kokkos::is_reducer<ReducerType>::value>::type
-cuda_intra_block_reduction(const ReducerType& reducer,
-                           typename ReducerType::value_type value,
-                           const int max_active_thread = blockDim.y) {
-  cuda_intra_warp_reduction(reducer, value, max_active_thread);
-  cuda_inter_warp_reduction(reducer, value, max_active_thread);
-}
-
-template <class ReducerType>
-__device__ inline
-typename std::enable_if<Kokkos::is_reducer<ReducerType>::value>::type
-cuda_intra_block_reduction(const ReducerType& reducer,
-                           const int max_active_thread = blockDim.y) {
-  cuda_intra_block_reduction(reducer, reducer.reference(), max_active_thread);
-}
+//template <class ReducerType>
+//__device__ inline
+//typename std::enable_if<Kokkos::is_reducer<ReducerType>::value>::type
+//cuda_intra_block_reduction(const ReducerType& reducer,
+//                           typename ReducerType::value_type value,
+//                           const int max_active_thread = blockDim.y) {
+//  cuda_intra_warp_reduction(reducer, value, max_active_thread);
+//  cuda_inter_warp_reduction(reducer, value, max_active_thread);
+//}
+//
+//template <class ReducerType>
+//__device__ inline
+//typename std::enable_if<Kokkos::is_reducer<ReducerType>::value>::type
+//cuda_intra_block_reduction(const ReducerType& reducer,
+//                           const int max_active_thread = blockDim.y) {
+//  cuda_intra_block_reduction(reducer, reducer.reference(), max_active_thread);
+//}
 
 // </editor-fold> end cuda_intra_block_reduction }}}1
 //==============================================================================
