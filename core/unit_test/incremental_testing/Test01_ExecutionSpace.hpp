c72b10e36174e1dafea4108a62098883fde2e515
rgayatri <rgayatri@lbl.gov>
2019-11-04 11:18:57 -0800

    Adding the first incrememntal serial unit test for ExecSpace

---
+++ core/unit_test/incremental_testing/Test01_ExecutionSpace.hpp
@@ -0,0 +1,89 @@
+/*
+//@HEADER
+// ************************************************************************
+//
+//                        Kokkos v. 2.0
+//              Copyright (2014) Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact Christian R. Trott (crtrott@sandia.gov)
+//
+// ************************************************************************
+//@HEADER
+*/
+
+#include <Kokkos_Core.hpp>
+#include <cstdio>
+#include <sstream>
+
+namespace Test {
+
+// Test construction and assignment
+
+template <class ExecSpace>
+struct TestIncrExecSpaceTypedef {
+
+  void testit() {
+    bool passed = true;
+    passed &= !std::is_same<void,typename ExecSpace::memory_space>::value;
+    passed &=  std::is_same<ExecSpace,typename ExecSpace::execution_space>::value;
+    passed &= !std::is_same<void,typename ExecSpace::scratch_memory_space>::value;
+    passed &= !std::is_same<void,typename ExecSpace::array_layout>::value;
+    ASSERT_TRUE(passed);
+  }
+};
+
+template <class ExecSpace>
+struct TestIncrExecSpace {
+
+  void testit() {
+    bool passed = true;
+    typedef typename ExecSpace::device_type device_type;
+    typedef typename device_type::memory_space memory_space;
+    typedef typename device_type::execution_space execution_space;
+
+    passed &= std::is_same<device_type, Kokkos::Device<execution_space, memory_space>>::value;
+
+    ASSERT_TRUE(passed);
+  }
+};
+
+TEST_F(TEST_CATEGORY, incr_01_execspace_typedef) {
+  TestIncrExecSpaceTypedef<TEST_EXECSPACE> test;
+  test.testit();
+}
+
+TEST_F(TEST_CATEGORY, incr_01_execspace) {
+  ASSERT_TRUE(Kokkos::is_execution_space<TEST_EXECSPACE>::value);
+  ASSERT_FALSE(Kokkos::is_execution_space<TestIncrExecSpaceTypedef<TEST_EXECSPACE>>::value);
+}
+}  // namespace Test
