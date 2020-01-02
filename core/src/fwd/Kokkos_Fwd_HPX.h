50bb8872717150633d245f0916c1433d59ac9e9d
jeffmiles63 <jsmiles@apollo.sandia.gov>
2020-01-02 08:38:28 -0700

    Add forward header files for existing backends.

---
+++ core/src/fwd/Kokkos_Fwd_HPX.h
@@ -0,0 +1,6 @@
+
+#if defined(KOKKOS_ENABLE_HPX)
+namespace Experimental {
+class HPX;  ///< Execution space with HPX back-end.
+}
+#endif
---
+++ core/src/fwd/Kokkos_Fwd_Threads.h
@@ -0,0 +1,5 @@
+
+#if defined(KOKKOS_ENABLE_THREADS)
+class Threads;  ///< Execution space with pthreads back-end.
+#endif
+
