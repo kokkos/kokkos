0c2d8d39f57a5d5f292131e52c6f85684f693376
Dan Ibanez <daibane@sandia.gov>
2020-01-07 14:20:39 -0700

    improve CUDA defaulted handling for CUDA 10
    These changes do two things:
    1) Through the new macro KOKKOS_DEFAULTED_FUNCTION,
       we ensure that in CUDA 10 and above defaulted
       member methods have not __host__ __device__ on them,
       since they are truly not needed in CUDA 10
    2) Remove the old CUDA 9 workaround macro.
       CUDA 9 warned that it did not needed __host__ __device__
       even though it did.
       This macro was an early attempt at working around this
       by replacing every defaulted method with a hand-coded
       non-default implementation.
       However, that approach is very tedious, changes type
       properties, and is supeceded by the approach of adding
       a diagnostic suppression flag to the build.

--- core/src/impl/Kokkos_TaskBase.hpp
+++ core/src/impl/Kokkos_TaskBase.hpp
@@ -166,11 +166,7 @@ class TaskBase {
   TaskBase& operator=(TaskBase&&) = delete;
   TaskBase& operator=(const TaskBase&) = delete;
 
-#ifdef KOKKOS_CUDA_9_DEFAULTED_BUG_WORKAROUND
-  KOKKOS_INLINE_FUNCTION ~TaskBase(){};
-#else
-  KOKKOS_INLINE_FUNCTION ~TaskBase() = default;
-#endif
+  KOKKOS_DEFAULTED_FUNCTION ~TaskBase() = default;
 
   KOKKOS_INLINE_FUNCTION constexpr TaskBase()
       : m_apply(nullptr),
