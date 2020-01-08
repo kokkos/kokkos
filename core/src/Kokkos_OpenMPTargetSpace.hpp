a924e01c2ad189d42a41f7e52d30dead73105984
crtrott <crtrott@sandia.gov>
2020-01-08 14:32:34 -0700

    Add missing OpenMPTargetSpace::name() function

--- core/src/Kokkos_OpenMPTargetSpace.hpp
+++ core/src/Kokkos_OpenMPTargetSpace.hpp
@@ -127,7 +127,11 @@ class OpenMPTargetSpace {
   /**\brief  Deallocate untracked memory in the space */
   void deallocate(void* const arg_alloc_ptr, const size_t arg_alloc_size) const;
 
+  static constexpr const char* name() { return m_name; }
+
  private:
+  static constexpr const char* m_name = "OpenMPTargetSpace";
+
   friend class Kokkos::Impl::SharedAllocationRecord<
       Kokkos::Experimental::OpenMPTargetSpace, void>;
 };
