d4ab7449e55da5578687bab39ac7bbcac70a53ae
Damien Lebrun-Grandie <dalg24@gmail.com>
2020-01-10 10:31:23 -0600

    Add parallel_for for MDRange
    Co-Authored-By: Bruno Turcksin <bruno.turcksin@gmail.com>

--- core/src/HIP/Kokkos_HIP_Parallel_MDRange.hpp
+++ core/src/HIP/Kokkos_HIP_Parallel_MDRange.hpp
@@ -55,142 +55,124 @@ template <class FunctorType, class... Traits>
 class ParallelFor<FunctorType, Kokkos::MDRangePolicy<Traits...>,
                   Kokkos::Experimental::HIP> {
  public:
-  typedef Kokkos::MDRangePolicy<Traits...> Policy;
+  using Policy = Kokkos::MDRangePolicy<Traits...>;
 
  private:
-  using RP = Policy;
-  typedef typename Policy::array_index_type array_index_type;
-  typedef typename Policy::index_type index_type;
-  typedef typename Policy::launch_bounds LaunchBounds;
+  using array_index_type = typename Policy::array_index_type;
+  using index_type       = typename Policy::index_type;
+  using LaunchBounds     = typename Policy::launch_bounds;
 
   const FunctorType m_functor;
-  const Policy m_rp;
+  const Policy m_policy;
+
+  ParallelFor()        = delete;
+  ParallelFor& operator=(ParallelFor const&) = delete;
 
  public:
   inline __device__ void operator()(void) const {
-    // FIXME to implement
-    //  Kokkos::Impl::Refactor::DeviceIterateTile<Policy::rank, Policy,
-    //  FunctorType,
-    //                                            typename Policy::work_tag>(
-    //      m_rp, m_functor)
-    //      .exec_range();
+    Kokkos::Impl::DeviceIterateTile<Policy::rank, Policy, FunctorType,
+                                    typename Policy::work_tag>(m_policy,
+                                                               m_functor)
+        .exec_range();
   }
 
   inline void execute() const {
-    // FIXME to implement
-    //  if (m_rp.m_num_tiles == 0)
-    //    return;
-    //  const array_index_type maxblocks = static_cast<array_index_type>(
-    //      m_rp.space().impl_internal_space_instance()->m_maxBlock);
-    //  if (RP::rank == 2)
-    //  {
-    //    const dim3 block(m_rp.m_tile[0], m_rp.m_tile[1], 1);
-    //    const dim3 grid(
-    //        std::min((m_rp.m_upper[0] - m_rp.m_lower[0] + block.x - 1) /
-    //        block.x,
-    //                 maxblocks),
-    //        std::min((m_rp.m_upper[1] - m_rp.m_lower[1] + block.y - 1) /
-    //        block.y,
-    //                 maxblocks),
-    //        1);
-    //    Kokkos::Experimental::Impl::HIPParallelLaunch<ParallelFor,
-    //    LaunchBounds>(
-    //        *this, grid, block, 0,
-    //        m_rp.space().impl_internal_space_instance(), false);
-    //  }
-    //  else if (RP::rank == 3)
-    //  {
-    //    const dim3 block(m_rp.m_tile[0], m_rp.m_tile[1], m_rp.m_tile[2]);
-    //    const dim3 grid(
-    //        std::min((m_rp.m_upper[0] - m_rp.m_lower[0] + block.x - 1) /
-    //        block.x,
-    //                 maxblocks),
-    //        std::min((m_rp.m_upper[1] - m_rp.m_lower[1] + block.y - 1) /
-    //        block.y,
-    //                 maxblocks),
-    //        std::min((m_rp.m_upper[2] - m_rp.m_lower[2] + block.z - 1) /
-    //        block.z,
-    //                 maxblocks));
-    //    Kokkos::Experimental::Impl::HIPParallelLaunch<ParallelFor,
-    //    LaunchBounds>(
-    //        *this, grid, block, 0,
-    //        m_rp.space().impl_internal_space_instance(), false);
-    //  }
-    //  else if (RP::rank == 4)
-    //  {
-    //    // id0,id1 encoded within threadIdx.x; id2 to threadIdx.y; id3 to
-    //    // threadIdx.z
-    //    const dim3 block(m_rp.m_tile[0] * m_rp.m_tile[1], m_rp.m_tile[2],
-    //                     m_rp.m_tile[3]);
-    //    const dim3 grid(
-    //        std::min(
-    //            static_cast<index_type>(m_rp.m_tile_end[0] *
-    //            m_rp.m_tile_end[1]), static_cast<index_type>(maxblocks)),
-    //        std::min((m_rp.m_upper[2] - m_rp.m_lower[2] + block.y - 1) /
-    //        block.y,
-    //                 maxblocks),
-    //        std::min((m_rp.m_upper[3] - m_rp.m_lower[3] + block.z - 1) /
-    //        block.z,
-    //                 maxblocks));
-    //    Kokkos::Experimental::Impl::HIPParallelLaunch<ParallelFor,
-    //    LaunchBounds>(
-    //        *this, grid, block, 0,
-    //        m_rp.space().impl_internal_space_instance(), false);
-    //  }
-    //  else if (RP::rank == 5)
-    //  {
-    //    // id0,id1 encoded within threadIdx.x; id2,id3 to threadIdx.y; id4 to
-    //    // threadIdx.z
-    //    const dim3 block(m_rp.m_tile[0] * m_rp.m_tile[1],
-    //                     m_rp.m_tile[2] * m_rp.m_tile[3], m_rp.m_tile[4]);
-    //    const dim3 grid(
-    //        std::min(
-    //            static_cast<index_type>(m_rp.m_tile_end[0] *
-    //            m_rp.m_tile_end[1]), static_cast<index_type>(maxblocks)),
-    //        std::min(
-    //            static_cast<index_type>(m_rp.m_tile_end[2] *
-    //            m_rp.m_tile_end[3]), static_cast<index_type>(maxblocks)),
-    //        std::min((m_rp.m_upper[4] - m_rp.m_lower[4] + block.z - 1) /
-    //        block.z,
-    //                 maxblocks));
-    //    Kokkos::Experimental::Impl::HIPParallelLaunch<ParallelFor,
-    //    LaunchBounds>(
-    //        *this, grid, block, 0,
-    //        m_rp.space().impl_internal_space_instance(), false);
-    //  }
-    //  else if (RP::rank == 6)
-    //  {
-    //    // id0,id1 encoded within threadIdx.x; id2,id3 to threadIdx.y; id4,id5
-    //    to
-    //    // threadIdx.z
-    //    const dim3 block(m_rp.m_tile[0] * m_rp.m_tile[1],
-    //                     m_rp.m_tile[2] * m_rp.m_tile[3],
-    //                     m_rp.m_tile[4] * m_rp.m_tile[5]);
-    //    const dim3 grid(std::min(static_cast<index_type>(m_rp.m_tile_end[0] *
-    //                                                     m_rp.m_tile_end[1]),
-    //                             static_cast<index_type>(maxblocks)),
-    //                    std::min(static_cast<index_type>(m_rp.m_tile_end[2] *
-    //                                                     m_rp.m_tile_end[3]),
-    //                             static_cast<index_type>(maxblocks)),
-    //                    std::min(static_cast<index_type>(m_rp.m_tile_end[4] *
-    //                                                     m_rp.m_tile_end[5]),
-    //                             static_cast<index_type>(maxblocks)));
-    //    Kokkos::Experimental::Impl::HIPParallelLaunch<ParallelFor,
-    //    LaunchBounds>(
-    //        *this, grid, block, 0,
-    //        m_rp.space().impl_internal_space_instance(), false);
-    //  }
-    //  else
-    //  {
-    //    printf("Kokkos::MDRange Error: Exceeded rank bounds with HIP\n");
-    //    Kokkos::abort("Aborting");
-    //  }
+    if (m_policy.m_num_tiles == 0) return;
+    array_index_type const maxblocks = static_cast<array_index_type>(
+        m_policy.space().impl_internal_space_instance()->m_maxBlock);
+    if (Policy::rank == 2) {
+      dim3 const block(m_policy.m_tile[0], m_policy.m_tile[1], 1);
+      dim3 const grid(
+          std::min((m_policy.m_upper[0] - m_policy.m_lower[0] + block.x - 1) /
+                       block.x,
+                   maxblocks),
+          std::min((m_policy.m_upper[1] - m_policy.m_lower[1] + block.y - 1) /
+                       block.y,
+                   maxblocks),
+          1);
+      Kokkos::Experimental::Impl::HIPParallelLaunch<ParallelFor, LaunchBounds>(
+          *this, grid, block, 0,
+          m_policy.space().impl_internal_space_instance(), false);
+    } else if (Policy::rank == 3) {
+      dim3 const block(m_policy.m_tile[0], m_policy.m_tile[1],
+                       m_policy.m_tile[2]);
+      dim3 const grid(
+          std::min((m_policy.m_upper[0] - m_policy.m_lower[0] + block.x - 1) /
+                       block.x,
+                   maxblocks),
+          std::min((m_policy.m_upper[1] - m_policy.m_lower[1] + block.y - 1) /
+                       block.y,
+                   maxblocks),
+          std::min((m_policy.m_upper[2] - m_policy.m_lower[2] + block.z - 1) /
+                       block.z,
+                   maxblocks));
+      Kokkos::Experimental::Impl::HIPParallelLaunch<ParallelFor, LaunchBounds>(
+          *this, grid, block, 0,
+          m_policy.space().impl_internal_space_instance(), false);
+    } else if (Policy::rank == 4) {
+      // id0,id1 encoded within hipThreadIdx_x; id2 to hipThreadIdx_y; id3 to
+      // hipThreadIdx_z
+      dim3 const block(m_policy.m_tile[0] * m_policy.m_tile[1],
+                       m_policy.m_tile[2], m_policy.m_tile[3]);
+      dim3 const grid(
+          std::min(static_cast<index_type>(m_policy.m_tile_end[0] *
+                                           m_policy.m_tile_end[1]),
+                   static_cast<index_type>(maxblocks)),
+          std::min((m_policy.m_upper[2] - m_policy.m_lower[2] + block.y - 1) /
+                       block.y,
+                   maxblocks),
+          std::min((m_policy.m_upper[3] - m_policy.m_lower[3] + block.z - 1) /
+                       block.z,
+                   maxblocks));
+      Kokkos::Experimental::Impl::HIPParallelLaunch<ParallelFor, LaunchBounds>(
+          *this, grid, block, 0,
+          m_policy.space().impl_internal_space_instance(), false);
+    } else if (Policy::rank == 5) {
+      // id0,id1 encoded within hipThreadIdx_x; id2,id3 to hipThreadIdx_y; id4
+      // to hipThreadIdx_z
+      dim3 const block(m_policy.m_tile[0] * m_policy.m_tile[1],
+                       m_policy.m_tile[2] * m_policy.m_tile[3],
+                       m_policy.m_tile[4]);
+      dim3 const grid(
+          std::min(static_cast<index_type>(m_policy.m_tile_end[0] *
+                                           m_policy.m_tile_end[1]),
+                   static_cast<index_type>(maxblocks)),
+          std::min(static_cast<index_type>(m_policy.m_tile_end[2] *
+                                           m_policy.m_tile_end[3]),
+                   static_cast<index_type>(maxblocks)),
+          std::min((m_policy.m_upper[4] - m_policy.m_lower[4] + block.z - 1) /
+                       block.z,
+                   maxblocks));
+      Kokkos::Experimental::Impl::HIPParallelLaunch<ParallelFor, LaunchBounds>(
+          *this, grid, block, 0,
+          m_policy.space().impl_internal_space_instance(), false);
+    } else if (Policy::rank == 6) {
+      // id0,id1 encoded within hipThreadIdx_x; id2,id3 to hipThreadIdx_y;
+      // id4,id5 to hipThreadIdx_z
+      dim3 const block(m_policy.m_tile[0] * m_policy.m_tile[1],
+                       m_policy.m_tile[2] * m_policy.m_tile[3],
+                       m_policy.m_tile[4] * m_policy.m_tile[5]);
+      dim3 const grid(std::min(static_cast<index_type>(m_policy.m_tile_end[0] *
+                                                       m_policy.m_tile_end[1]),
+                               static_cast<index_type>(maxblocks)),
+                      std::min(static_cast<index_type>(m_policy.m_tile_end[2] *
+                                                       m_policy.m_tile_end[3]),
+                               static_cast<index_type>(maxblocks)),
+                      std::min(static_cast<index_type>(m_policy.m_tile_end[4] *
+                                                       m_policy.m_tile_end[5]),
+                               static_cast<index_type>(maxblocks)));
+      Kokkos::Experimental::Impl::HIPParallelLaunch<ParallelFor, LaunchBounds>(
+          *this, grid, block, 0,
+          m_policy.space().impl_internal_space_instance(), false);
+    } else {
+      printf("Kokkos::MDRange Error: Exceeded rank bounds with HIP\n");
+      Kokkos::abort("Aborting");
+    }
 
   }  // end execute
 
-  //  inline
-  ParallelFor(const FunctorType &arg_functor, Policy arg_policy)
-      : m_functor(arg_functor), m_rp(arg_policy) {}
+  ParallelFor(FunctorType const& arg_functor, Policy const& arg_policy)
+      : m_functor(arg_functor), m_policy(arg_policy) {}
 };
 }  // namespace Impl
 }  // namespace Kokkos
