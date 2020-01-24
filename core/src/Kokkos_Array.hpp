2800bfe5663efc9b13aefdbb3cbac903c278f78c
Damien Lebrun-Grandie <dalg24@gmail.com>
2020-01-24 16:12:22 -0500

    Fixup decoration of defaulted and deleted constructors/destructors in Kokkos_Array.hpp
    Co-Authored-By: Bruno Turcksin <bruno.turcksin@gmail.com>

--- core/src/Kokkos_Array.hpp
+++ core/src/Kokkos_Array.hpp
@@ -160,16 +160,15 @@ struct Array {
 
 #ifdef KOKKOS_IMPL_HIP_CLANG_WORKAROUND
   // Do not default unless move and move-assignment are also defined
-  KOKKOS_INLINE_FUNCTION
-  ~Array()            = default;
-  Array()             = default;
-  Array(const Array&) = default;
-  Array& operator=(const Array&) = default;
+  KOKKOS_DEFAULTED_FUNCTION ~Array()            = default;
+  KOKKOS_DEFAULTED_FUNCTION Array()             = default;
+  KOKKOS_DEFAULTED_FUNCTION Array(const Array&) = default;
+  KOKKOS_DEFAULTED_FUNCTION Array& operator=(const Array&) = default;
 
   // Some supported compilers are not sufficiently C++11 compliant
   // for default move constructor and move assignment operator.
-  Array(Array&&) = default;
-  Array& operator=(Array&&) = default;
+  KOKKOS_DEFAULTED_FUNCTION Array(Array&&) = default;
+  KOKKOS_DEFAULTED_FUNCTION Array& operator=(Array&&) = default;
 
   KOKKOS_INLINE_FUNCTION
   Array(const std::initializer_list<T>& vals) {
@@ -273,9 +272,9 @@ struct Array<T, KOKKOS_INVALID_INDEX, Array<>::contiguous> {
   KOKKOS_INLINE_FUNCTION pointer data() { return m_elem; }
   KOKKOS_INLINE_FUNCTION const_pointer data() const { return m_elem; }
 
-  KOKKOS_DEFAULTED_FUNCTION ~Array() = default;
-  Array()                            = delete;
-  Array(const Array& rhs)            = delete;
+  KOKKOS_DEFAULTED_FUNCTION ~Array()                     = default;
+  KOKKOS_INLINE_FUNCTION_DELETED Array()                 = delete;
+  KOKKOS_INLINE_FUNCTION_DELETED Array(const Array& rhs) = delete;
 
   // Some supported compilers are not sufficiently C++11 compliant
   // for default move constructor and move assignment operator.
@@ -342,9 +341,9 @@ struct Array<T, KOKKOS_INVALID_INDEX, Array<>::strided> {
   KOKKOS_INLINE_FUNCTION pointer data() { return m_elem; }
   KOKKOS_INLINE_FUNCTION const_pointer data() const { return m_elem; }
 
-  KOKKOS_DEFAULTED_FUNCTION ~Array() = default;
-  Array()                            = delete;
-  Array(const Array&)                = delete;
+  KOKKOS_DEFAULTED_FUNCTION ~Array()                 = default;
+  KOKKOS_INLINE_FUNCTION_DELETED Array()             = delete;
+  KOKKOS_INLINE_FUNCTION_DELETED Array(const Array&) = delete;
 
   // Some supported compilers are not sufficiently C++11 compliant
   // for default move constructor and move assignment operator.
