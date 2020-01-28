6a567d0f9fb5fe2bae1a902fe17bcddfac8c25dc
Jeff Miles <jsmiles@sandia.gov>
2020-01-28 16:25:56 -0700

    update kokkos_setup_cuda to match what was in kokkos_macros after rebasing

--- core/src/Kokkos_Setup_Cuda.hpp
+++ core/src/Kokkos_Setup_Cuda.hpp
@@ -1,4 +1,3 @@
-
 #if defined(KOKKOS_ENABLE_CUDA) && defined(__CUDACC__)
 // Compiling with a CUDA compiler.
 //
@@ -15,36 +14,16 @@
 #error "#include <cuda.h> did not define CUDA_VERSION."
 #endif
 
-#if (CUDA_VERSION < 7000)
-// CUDA supports C++11 in device code starting with version 7.0.
-// This includes auto type and device code internal lambdas.
-#error "Cuda version 7.0 or greater required."
-#endif
-
 #if defined(__CUDA_ARCH__) && (__CUDA_ARCH__ < 300)
 // Compiling with CUDA compiler for device code.
 #error "Cuda device capability >= 3.0 is required."
 #endif
 
 #ifdef KOKKOS_ENABLE_CUDA_LAMBDA
-#if (CUDA_VERSION < 7050)
-// CUDA supports C++11 lambdas generated in host code to be given
-// to the device starting with version 7.5. But the release candidate (7.5.6)
-// still identifies as 7.0.
-#error "Cuda version 7.5 or greater required for host-to-device Lambda support."
-#endif
-
-#if (CUDA_VERSION < 8000) && defined(__NVCC__)
-#define KOKKOS_LAMBDA [=] __device__
-#if defined(KOKKOS_INTERNAL_ENABLE_NON_CUDA_BACKEND)
-#undef KOKKOS_ENABLE_CXX11_DISPATCH_LAMBDA
-#endif
-#else
 #define KOKKOS_LAMBDA [=] __host__ __device__
 
 #if defined(KOKKOS_ENABLE_CXX17) || defined(KOKKOS_ENABLE_CXX20)
 #define KOKKOS_CLASS_LAMBDA [ =, *this ] __host__ __device__
-#endif
 #endif
 
 #if defined(__NVCC__)
@@ -53,12 +32,6 @@
 #else  // !defined(KOKKOS_ENABLE_CUDA_LAMBDA)
 #undef KOKKOS_ENABLE_CXX11_DISPATCH_LAMBDA
 #endif  // !defined(KOKKOS_ENABLE_CUDA_LAMBDA)
-
-#if (9000 <= CUDA_VERSION) && (CUDA_VERSION < 10000)
-// CUDA 9 introduced an incorrect warning,
-// see https://github.com/kokkos/kokkos/issues/1470
-#define KOKKOS_CUDA_9_DEFAULTED_BUG_WORKAROUND
-#endif
 
 #if (10000 > CUDA_VERSION)
 #define KOKKOS_ENABLE_PRE_CUDA_10_DEPRECATION_API
@@ -79,16 +52,20 @@
 #endif  // #if defined( KOKKOS_ENABLE_CUDA ) && defined( __CUDACC__ )
 
 #if defined(KOKKOS_ENABLE_CUDA)
+// Compiling Cuda code to 'ptx'
 
 #define KOKKOS_FORCEINLINE_FUNCTION __device__ __host__ __forceinline__
 #define KOKKOS_IMPL_FORCEINLINE __forceinline__
 #define KOKKOS_INLINE_FUNCTION __device__ __host__ inline
 #define KOKKOS_FUNCTION __device__ __host__
-
 #if defined(KOKKOS_COMPILER_NVCC)
 #define KOKKOS_INLINE_FUNCTION_DELETED inline
 #else
 #define KOKKOS_INLINE_FUNCTION_DELETED __device__ __host__ inline
 #endif
-
-#endif  // KOKKOS_ENABLE_CUDA
+#if (CUDA_VERSION < 10000)
+#define KOKKOS_DEFAULTED_FUNCTION __host__ __device__ inline
+#else
+#define KOKKOS_DEFAULTED_FUNCTION inline
+#endif
+#endif
