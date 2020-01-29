7a832aeb1c8833e220a2ebe94ff01fab6439bfdf
jeffmiles63 <jsmiles@apollo.sandia.gov>
2020-01-29 10:03:32 -0700

    add remaining HIP files

---
+++ core/src/fwd/Kokkos_Fwd_HIP.hpp
@@ -0,0 +1,7 @@
+
+#if defined(KOKKOS_ENABLE_HIP)
+namespace Experimental {
+class HIPSpace;  ///< Memory space on HIP GPU
+class HIP;       ///< Execution space for HIP GPU
+}  // namespace Experimental
+#endif
