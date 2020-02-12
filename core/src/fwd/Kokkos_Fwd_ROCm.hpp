f5b24f4d75ea15ce4537ab11597d04665b8ceea1
Jeffery Scott Miles <jsmiles@kokkos-dev-2.sandia.gov>
2020-02-12 09:13:16 -0800

    Temporarily add ROCm back into the mix...

---
+++ core/src/fwd/Kokkos_Fwd_ROCm.hpp
@@ -0,0 +1,7 @@
+
+#if defined(KOKKOS_ENABLE_HIP)
+namespace Experimental {
+class ROCmSpace;  ///< Memory space on HIP GPU
+class ROCm;       ///< Execution space for HIP GPU
+}  // namespace Experimental
+#endif
