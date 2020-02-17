8c080cc28fab14f4b1adaa76c5ada5047e5fbca4
Daniel Arndt <arndtd@ornl.gov>
2020-02-17 15:45:01 -0500

    Update copyrights for files deleted in develop

--- example/fenl/fenl.cpp
+++ example/fenl/fenl.cpp
@@ -4,7 +4,7 @@
 //   Kokkos: Manycore Performance-Portable Multidimensional Arrays
 //              Copyright (2012) Sandia Corporation
 //
-// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// Under the terms of Contract DE-NA0003525 with NTESS,
 // the U.S. Government retains certain rights in this software.
 //
 // Redistribution and use in source and binary forms, with or without
