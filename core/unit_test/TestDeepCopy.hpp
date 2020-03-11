18c18b14c735af86535096b16ec5c8ff0a073502
Christian Trott <crtrott@sandia.gov>
2020-03-11 08:22:55 -0700

    Disable Deep Copy Conversion Test for HIP

--- core/unit_test/TestDeepCopy.hpp
+++ core/unit_test/TestDeepCopy.hpp
@@ -213,6 +213,8 @@ TEST(TEST_CATEGORY, deep_copy_alignment) {
 }
 #endif
 
+// KOKKOS_IMPL_HIP_CLANG_WORKAROUND
+#ifndef KOKKOS_ENABLE_HIP
 namespace Impl {
 template <class Scalar1, class Scalar2, class Layout1, class Layout2>
 struct TestDeepCopyScalarConversion {
@@ -353,5 +355,5 @@ TEST(TEST_CATEGORY, deep_copy_conversion) {
   Impl::TestDeepCopyScalarConversion<double, float, right, stride>().run_tests(
       N0, N1);
 }
-
+#endif
 }  // namespace Test
