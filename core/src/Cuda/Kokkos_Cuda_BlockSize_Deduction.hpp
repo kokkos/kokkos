ebc9be3b50f4bb59489a6489df50f08713d107e9
Damien L-G <dalg24@gmail.com>
2020-06-22 14:32:42 -0400

    Remove macro and old blocksize deduction code

--- core/src/Cuda/Kokkos_Cuda_BlockSize_Deduction.hpp
+++ core/src/Cuda/Kokkos_Cuda_BlockSize_Deduction.hpp
@@ -151,15 +151,12 @@ inline int cuda_deduce_block_size(bool early_termination,
   return opt_block_size;
 }
 
-#define KOKKOS_IMPL_EXPERIMENTAL_BLOCK_DEDUCTION
-
 template <class FunctorType, class LaunchBounds>
 int cuda_get_max_block_size(const CudaInternal* cuda_instance,
                             const cudaFuncAttributes& attr,
                             const FunctorType& f, const size_t vector_length,
                             const size_t shmem_block,
                             const size_t shmem_thread) {
-#ifdef KOKKOS_IMPL_EXPERIMENTAL_BLOCK_DEDUCTION
   (void)cuda_instance;
 
   auto const& prop = Kokkos::Cuda().cuda_device_prop();
@@ -178,82 +175,6 @@ int cuda_get_max_block_size(const CudaInternal* cuda_instance,
 
   return cuda_deduce_block_size(true, prop, attr, block_size_to_dynamic_shmem,
                                 LaunchBounds{});
-#else
-  const int min_blocks_per_sm =
-      LaunchBounds::minBperSM == 0 ? 1 : LaunchBounds::minBperSM;
-  const int max_threads_per_block = LaunchBounds::maxTperB == 0
-                                        ? cuda_instance->m_maxThreadsPerBlock
-                                        : LaunchBounds::maxTperB;
-
-  const int regs_per_thread     = attr.numRegs;
-  const int regs_per_sm         = cuda_instance->m_regsPerSM;
-  const int shmem_per_sm        = cuda_instance->m_shmemPerSM;
-  const int max_shmem_per_block = cuda_instance->m_maxShmemPerBlock;
-  const int max_blocks_per_sm   = cuda_instance->m_maxBlocksPerSM;
-  const int max_threads_per_sm  = cuda_instance->m_maxThreadsPerSM;
-
-  int block_size = std::min(attr.maxThreadsPerBlock, max_threads_per_block);
-  KOKKOS_ASSERT(block_size > 0);
-
-  int functor_shmem =
-      FunctorTeamShmemSize<FunctorType>::value(f, block_size / vector_length);
-  int total_shmem = shmem_block + shmem_thread * (block_size / vector_length) +
-                    functor_shmem + attr.sharedSizeBytes;
-  int max_blocks_regs = regs_per_sm / (regs_per_thread * block_size);
-  int max_blocks_shmem =
-      (total_shmem < max_shmem_per_block)
-          ? (total_shmem > 0 ? shmem_per_sm / total_shmem : max_blocks_regs)
-          : 0;
-  int blocks_per_sm  = std::min(max_blocks_regs, max_blocks_shmem);
-  int threads_per_sm = blocks_per_sm * block_size;
-  if (threads_per_sm > max_threads_per_sm) {
-    blocks_per_sm  = max_threads_per_sm / block_size;
-    threads_per_sm = blocks_per_sm * block_size;
-  }
-  int opt_block_size = (blocks_per_sm >= min_blocks_per_sm) ? block_size : 0;
-  int opt_threads_per_sm = threads_per_sm;
-#if 0
-  // clang-format off
-  printf("BlockSizeMax: %i Shmem: %i %i %i %i Regs: %i %i Blocks: %i %i Achieved: %i %i Opt: %i %i\n",block_size,
-    shmem_per_sm,max_shmem_per_block,functor_shmem,total_shmem,
-    regs_per_sm,regs_per_thread,max_blocks_shmem,max_blocks_regs,blocks_per_sm,threads_per_sm,opt_block_size,opt_threads_per_sm);
-  // clang-format on
-#endif
-  block_size -= 32;
-  while ((blocks_per_sm == 0) && (block_size >= 32)) {
-    functor_shmem =
-        FunctorTeamShmemSize<FunctorType>::value(f, block_size / vector_length);
-    total_shmem = shmem_block + shmem_thread * (block_size / vector_length) +
-                  functor_shmem + attr.sharedSizeBytes;
-    max_blocks_regs = regs_per_sm / (regs_per_thread * block_size);
-    max_blocks_shmem =
-        (total_shmem < max_shmem_per_block)
-            ? (total_shmem > 0 ? shmem_per_sm / total_shmem : max_blocks_regs)
-            : 0;
-    blocks_per_sm  = std::min(max_blocks_regs, max_blocks_shmem);
-    threads_per_sm = blocks_per_sm * block_size;
-    if (threads_per_sm > max_threads_per_sm) {
-      blocks_per_sm  = max_threads_per_sm / block_size;
-      threads_per_sm = blocks_per_sm * block_size;
-    }
-    if ((blocks_per_sm >= min_blocks_per_sm) &&
-        (blocks_per_sm <= max_blocks_per_sm)) {
-      if (threads_per_sm >= opt_threads_per_sm) {
-        opt_block_size     = block_size;
-        opt_threads_per_sm = threads_per_sm;
-      }
-    }
-#if 0
-    // clang-format off
-    printf("BlockSizeMax: %i Shmem: %i %i %i %i Regs: %i %i Blocks: %i %i Achieved: %i %i Opt: %i %i\n",block_size,
-      shmem_per_sm,max_shmem_per_block,functor_shmem,total_shmem,
-      regs_per_sm,regs_per_thread,max_blocks_shmem,max_blocks_regs,blocks_per_sm,threads_per_sm,opt_block_size,opt_threads_per_sm);
-    // clang-format on
-#endif
-    block_size -= 32;
-  }
-  return opt_block_size;
-#endif
 }
 
 template <class FunctorType, class LaunchBounds>
@@ -262,7 +183,6 @@ int cuda_get_opt_block_size(const CudaInternal* cuda_instance,
                             const FunctorType& f, const size_t vector_length,
                             const size_t shmem_block,
                             const size_t shmem_thread) {
-#ifdef KOKKOS_IMPL_EXPERIMENTAL_BLOCK_DEDUCTION
   (void)cuda_instance;
 
   auto const& prop = Kokkos::Cuda().cuda_device_prop();
@@ -281,86 +201,7 @@ int cuda_get_opt_block_size(const CudaInternal* cuda_instance,
 
   return cuda_deduce_block_size(false, prop, attr, block_size_to_dynamic_shmem,
                                 LaunchBounds{});
-#else
-  const int min_blocks_per_sm =
-      LaunchBounds::minBperSM == 0 ? 1 : LaunchBounds::minBperSM;
-  const int max_threads_per_block = LaunchBounds::maxTperB == 0
-                                        ? cuda_instance->m_maxThreadsPerBlock
-                                        : LaunchBounds::maxTperB;
-
-  const int regs_per_thread     = attr.numRegs;
-  const int regs_per_sm         = cuda_instance->m_regsPerSM;
-  const int shmem_per_sm        = cuda_instance->m_shmemPerSM;
-  const int max_shmem_per_block = cuda_instance->m_maxShmemPerBlock;
-  const int max_blocks_per_sm   = cuda_instance->m_maxBlocksPerSM;
-  const int max_threads_per_sm  = cuda_instance->m_maxThreadsPerSM;
-
-  int block_size = std::min(attr.maxThreadsPerBlock, max_threads_per_block);
-  KOKKOS_ASSERT(block_size > 0);
-
-  int functor_shmem =
-      FunctorTeamShmemSize<FunctorType>::value(f, block_size / vector_length);
-  int total_shmem = shmem_block + shmem_thread * (block_size / vector_length) +
-                    functor_shmem + attr.sharedSizeBytes;
-  int max_blocks_regs = regs_per_sm / (regs_per_thread * block_size);
-  int max_blocks_shmem =
-      (total_shmem < max_shmem_per_block)
-          ? (total_shmem > 0 ? shmem_per_sm / total_shmem : max_blocks_regs)
-          : 0;
-  int blocks_per_sm  = std::min(max_blocks_regs, max_blocks_shmem);
-  int threads_per_sm = blocks_per_sm * block_size;
-  if (threads_per_sm > max_threads_per_sm) {
-    blocks_per_sm  = max_threads_per_sm / block_size;
-    threads_per_sm = blocks_per_sm * block_size;
-  }
-  int opt_block_size = (blocks_per_sm >= min_blocks_per_sm) ? block_size : 0;
-  int opt_threads_per_sm = threads_per_sm;
-
-#if 0
-  // clang-format off
-    printf("BlockSizeMax: %i Shmem: %i %i %i %i Regs: %i %i Blocks: %i %i Achieved: %i %i Opt: %i %i\n",block_size,
-      shmem_per_sm,max_shmem_per_block,functor_shmem,total_shmem,
-      regs_per_sm,regs_per_thread,max_blocks_shmem,max_blocks_regs,blocks_per_sm,threads_per_sm,opt_block_size,opt_threads_per_sm);
-  // clang-format on
-#endif
-  block_size -= 32;
-  while ((block_size >= 32)) {
-    functor_shmem =
-        FunctorTeamShmemSize<FunctorType>::value(f, block_size / vector_length);
-    total_shmem = shmem_block + shmem_thread * (block_size / vector_length) +
-                  functor_shmem + attr.sharedSizeBytes;
-    max_blocks_regs = regs_per_sm / (regs_per_thread * block_size);
-    max_blocks_shmem =
-        (total_shmem < max_shmem_per_block)
-            ? (total_shmem > 0 ? shmem_per_sm / total_shmem : max_blocks_regs)
-            : 0;
-    blocks_per_sm  = std::min(max_blocks_regs, max_blocks_shmem);
-    threads_per_sm = blocks_per_sm * block_size;
-    if (threads_per_sm > max_threads_per_sm) {
-      blocks_per_sm  = max_threads_per_sm / block_size;
-      threads_per_sm = blocks_per_sm * block_size;
-    }
-    if ((blocks_per_sm >= min_blocks_per_sm) &&
-        (blocks_per_sm <= max_blocks_per_sm)) {
-      if (threads_per_sm >= opt_threads_per_sm) {
-        opt_block_size     = block_size;
-        opt_threads_per_sm = threads_per_sm;
-      }
-    }
-#if 0
-    // clang-format off
-    printf("BlockSizeMax: %i Shmem: %i %i %i %i Regs: %i %i Blocks: %i %i Achieved: %i %i Opt: %i %i\n",block_size,
-      shmem_per_sm,max_shmem_per_block,functor_shmem,total_shmem,
-      regs_per_sm,regs_per_thread,max_blocks_shmem,max_blocks_regs,blocks_per_sm,threads_per_sm,opt_block_size,opt_threads_per_sm);
-    // clang-format on
-#endif
-    block_size -= 32;
-  }
-  return opt_block_size;
-#endif
 }
-
-#undef KOKKOS_IMPL_EXPERIMENTAL_BLOCK_DEDUCTION
 
 }  // namespace Impl
 }  // namespace Kokkos
