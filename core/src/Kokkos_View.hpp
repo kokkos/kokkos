3d4acfcdaad5a2dad1ccd7f0b81958cb8c94285d
Daniel Arndt <arndtd@ornl.gov>
2020-07-29 11:00:02 -0400

    Use defaulted special member functions for View and ViewMapping

--- core/src/Kokkos_View.hpp
+++ core/src/Kokkos_View.hpp
@@ -1423,16 +1423,16 @@ class View : public ViewTraits<DataType, Properties...> {
   View() = default;
 
   KOKKOS_DEFAULTED_FUNCTION
-  View(const View& rhs) = default;
+  View(const View& /*rhs*/) = default;
 
   KOKKOS_DEFAULTED_FUNCTION
-  View(View&& rhs) = default;
+  View(View&& /*rhs*/) = default;
 
   KOKKOS_DEFAULTED_FUNCTION
-  View& operator=(const View& rhs) = default;
+  View& operator=(const View& /*rhs*/) = default;
 
   KOKKOS_DEFAULTED_FUNCTION
-  View& operator=(View&& rhs) = default;
+  View& operator=(View&& /*rhs*/) = default;
 
   //----------------------------------------
   // Compatible view copy constructor and assignment
