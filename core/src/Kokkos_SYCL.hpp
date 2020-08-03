274afa97d54810207fb1bf772e73b41a67d6b0ec
Nevin Liber <nliber@anl.gov>
2020-08-03 13:23:29 -0500

    Expanded on the isTriviallyCopyable checks

--- core/src/Kokkos_SYCL.hpp
+++ core/src/Kokkos_SYCL.hpp
@@ -64,10 +64,35 @@
 #include <CL/sycl.hpp>
 
 /*--------------------------------------------------------------------------*/
-// Temp place for Trivially copyable check - velesko 
-template<class Obj>
-void isTriviallyCopyable() {
+// Temp place for Trivially copyable check - velesko
+template <class Obj>
+constexpr void isTriviallyCopyable() {
   static_assert(std::is_trivially_copyable<Obj>::value);
+
+  static_assert(std::is_copy_constructible<Obj>::value ||
+                    std::is_move_constructible<Obj>::value ||
+                    std::is_copy_assignable<Obj>::value ||
+                    std::is_move_assignable<Obj>::value,
+                "Obj copy/move constructors/assignments deleted");
+
+  static_assert(std::is_trivially_copy_constructible<Obj>::value ||
+                    !std::is_copy_constructible<Obj>::value,
+                "Obj not trivially copy constructible");
+
+  static_assert(std::is_trivially_move_constructible<Obj>::value ||
+                    !std::is_move_constructible<Obj>::value,
+                "Obj not trivially move constructible");
+
+  static_assert(std::is_trivially_copy_assignable<Obj>::value ||
+                    !std::is_copy_assignable<Obj>::value,
+                "Obj not trivially copy assignable");
+
+  static_assert(std::is_trivially_move_assignable<Obj>::value ||
+                    !std::is_move_assignable<Obj>::value,
+                "Obj not trivially move assignable");
+
+  static_assert(std::is_trivially_destructible<Obj>::value,
+                "Obj not trivially destructible");
 }
 
 /*--------------------------------------------------------------------------*/
