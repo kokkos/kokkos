d29eacf6ac85331f7c5fbe8e440d7cb94e31f723
CA Lewis <canlewi@sandia.gov>
2020-08-06 15:57:14 -0700

    Added work around for clang-cuda issue.

--- core/src/impl/Kokkos_Atomic_Fetch_And.hpp
+++ core/src/impl/Kokkos_Atomic_Fetch_And.hpp
@@ -69,6 +69,18 @@ __inline__ __device__ unsigned int atomic_fetch_and(
   return atomicAnd((unsigned int*)dest, val);
 }
 
+// 08/05/20 Overload to work around https://bugs.llvm.org/show_bug.cgi?id=46922
+__inline__ __device__ unsigned long atomic_fetch_and(
+    volatile unsigned long* const dest, const unsigned long val) {
+  return atomic_fetch_and<unsigned long>(dest, val);
+}
+
+// 08/05/20 Overload to work around https://bugs.llvm.org/show_bug.cgi?id=46922
+__inline__ __device__ long atomic_fetch_and(volatile long* const dest,
+                                            long val) {
+  return atomic_fetch_and<long>(dest, val);
+}
+
 #if defined(__CUDA_ARCH__) && (350 <= __CUDA_ARCH__)
 __inline__ __device__ unsigned long long int atomic_fetch_and(
     volatile unsigned long long int* const dest,
