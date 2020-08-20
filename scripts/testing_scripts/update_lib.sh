6ba3a5098c1befc69965e3f54f81a4ee10984ccf
Nathan Ellingwood <ndellin@sandia.gov>
2020-08-20 10:54:51 -0600

    update_lib.sh: check bowman for intel/17 and intel/18

--- scripts/testing_scripts/update_lib.sh
+++ scripts/testing_scripts/update_lib.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ "$1" = bowman ]; then
-  ICPCVER="$(icpc --version | grep 17 | cut -d ' ' -f 3)"
-  if [[ "${ICPCVER}" = 17.* ]]; then
+  ICPCVER="$(icpc --version | grep icpc | cut -d ' ' -f 3)"
+  if [[ "${ICPCVER}" = 17.* || "${ICPCVER}" = 18.* ]]; then
     module swap gcc/4.9.3 gcc/6.2.0
     module list
   fi
@@ -10,21 +10,21 @@ if [ "$1" = bowman ]; then
    export LD_LIBRARY_PATH=/home/projects/x86-64-knl/gcc/6.2.0/lib64:/home/projects/x86-64-knl/gcc/6.2.0/lib:/home/projects/x86-64-knl/cloog/0.18.4/lib:/home/projects/x86-64-knl/isl/0.16.1/lib:/home/projects/x86-64-knl/gmp/6.1.0/lib:/home/projects/x86-64-knl/mpfr/3.1.3/lib:/home/projects/x86-64-knl/mpc/1.0.3/lib:/home/projects/x86-64-knl/binutils/2.26.0/lib:/usr/lib/gcc/x86_64-redhat-linux/4.8.3:$LD_LIBRARY_PATH
 fi
 if [ "$1" = blake ]; then
-  ICPCVER="$(icpc --version | grep 17 | cut -d ' ' -f 3)"
+  ICPCVER="$(icpc --version | grep icpc | cut -d ' ' -f 3)"
   if [[ "${ICPCVER}" = 17.* ]]; then
     module swap gcc/4.9.3 gcc/6.4.0
     module list
   fi
 fi
 if [ "$1" = kokkos-dev ]; then
-  ICPCVER="$(icpc --version | grep 17 | cut -d ' ' -f 3)"
+  ICPCVER="$(icpc --version | grep icpc | cut -d ' ' -f 3)"
   if [[ "${ICPCVER}" = 17.* ]]; then
     module swap sems-gcc/4.8.4 sems-gcc/6.4.0
     module list
   fi
 fi
 if [ "$1" = kokkos-dev-2 ]; then
-  ICPCVER="$(icpc --version | grep 17 | cut -d ' ' -f 3)"
+  ICPCVER="$(icpc --version | grep icpc | cut -d ' ' -f 3)"
   if [[ "${ICPCVER}" = 17.* ]]; then
     module swap sems-gcc/4.8.4 sems-gcc/6.4.0
     module list
