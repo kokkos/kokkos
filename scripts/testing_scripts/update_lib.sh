33c90a96aa1565080d9b4c1331ef25d36f068555
Nathan Ellingwood <ndellin@sandia.gov>
2020-08-27 09:53:34 -0600

    update_libs: more module updates

--- scripts/testing_scripts/update_lib.sh
+++ scripts/testing_scripts/update_lib.sh
@@ -26,8 +26,15 @@ fi
 if [ "$1" = kokkos-dev-2 ]; then
   ICPCVER="$(icpc --version | grep icpc | cut -d ' ' -f 3)"
   if [[ "${ICPCVER}" = 17.* ]]; then
-    module swap sems-gcc/4.8.4 sems-gcc/6.4.0
+    module swap sems-gcc/4.9.3 sems-gcc/6.4.0
     module list
   fi
 fi
 
+if [ "$1" = sems ]; then
+  ICPCVER="$(icpc --version | grep icpc | cut -d ' ' -f 3)"
+  if [[ "${ICPCVER}" = 17.* ]]; then
+    module swap sems-gcc/4.8.4 sems-gcc/6.4.0
+    module list
+  fi
+fi
