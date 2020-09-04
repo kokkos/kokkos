24caeda2eafe041e8e9e2fca31ba490227cbe0aa
David Poliakoff <david.poliakoff@gmail.com>
2020-09-04 16:32:36 -0600

    First MDTuner implementation

--- core/src/Kokkos_Tuners.hpp
+++ core/src/Kokkos_Tuners.hpp
@@ -51,10 +51,11 @@
 #include <impl/Kokkos_Profiling_Interface.hpp>
 
 #include <array>
+#include <utility>
 #include <tuple>
 #include <string>
 #include <vector>
-
+#include <map>
 namespace Kokkos {
 namespace Tools {
 
@@ -67,24 +68,263 @@ size_t declare_output_type(const std::string&,
 void request_output_values(size_t, size_t,
                            Kokkos::Tools::Experimental::VariableValue*);
 VariableValue make_variable_value(size_t, int64_t);
+VariableValue make_variable_value(size_t, double);
+SetOrRange make_candidate_range(double lower, double upper, double step, bool openLower, bool openUpper);
+size_t get_new_context_id();
+void begin_context(size_t context_id);
 void end_context(size_t context_id);
-class TeamSizeTuner {
- public:
-  inline void declare_output_type(const std::string& name,
-                                  std::vector<int64_t> candidates) {
-    Kokkos::Tools::Experimental::VariableInfo variable_info;
+namespace Impl {
 
-    variable_info.type =
-        Kokkos::Tools::Experimental::ValueType::kokkos_value_int64;
-    variable_info.category = Kokkos::Tools::Experimental::StatisticalCategory::
-        kokkos_value_categorical;
-    variable_info.valueQuantity =
-        Kokkos::Tools::Experimental::CandidateValueType::kokkos_value_set;
-    variable_info.candidates = Kokkos::Tools::Experimental::make_candidate_set(
-        candidates.size(), candidates.data());
-    type_id = Kokkos::Tools::Experimental::declare_output_type(
-        name + "_team_size", variable_info);
+/** We're going to take in search space descriptions
+  * as nested maps, which aren't efficient to 
+  * iterate across by index. These are very similar
+  * to nested maps, but better for index-based lookup
+  */
+template<typename ValueType, typename ContainedType>
+struct ValueHierarchyNode;
+
+template<typename ValueType, typename ContainedType>
+struct ValueHierarchyNode {
+  std::vector<ValueType> root_values;
+  std::vector<ContainedType> sub_values; 
+  void add_root_value(const ValueType& in) noexcept {
+    root_values.push_back(in);
   }
+  void add_sub_container(const ContainedType& in){
+    sub_values.push_back(in);
+  }
+  const ValueType& get_root_value(const size_t index) const {
+    return root_values[index];
+  }
+  const ContainedType& get_sub_value(const size_t index) const {
+    return sub_values[index];
+  }
+};
+
+template<typename ValueType>
+struct ValueHierarchyNode <ValueType, void> {
+  std::vector<ValueType> root_values;
+  void add_root_value(const ValueType& in) noexcept {
+    root_values.push_back(in);
+  }
+  const ValueType& get_root_value(const size_t index) const {
+    return root_values[index];
+  }
+};
+
+/** For a given nested map type, we need a way to
+  * declare the equivalent ValueHierarchyNode
+  * structure
+  */
+
+template<class NestedMap>
+struct MapTypeConverter;
+
+// Vectors are our lowest-level, no nested values
+template<class T>
+struct MapTypeConverter < std::vector<T> > {
+   using type = ValueHierarchyNode<T,void>;
+};
+
+// Maps contain both the "root" types and sub-vectors
+template<class K, class V>
+struct MapTypeConverter < std::map<K,V> > {
+   using type = ValueHierarchyNode<K, typename MapTypeConverter<V>::type>;
+};
+
+/**
+  * We also need to be able to construct a ValueHierarchyNode set from a
+  * map
+  */
+
+template<class NestedMap>
+struct ValueHierarchyConstructor;
+
+// Vectors are our lowest-level, no nested values. Just fill in the fundamental values
+template<class T>
+struct ValueHierarchyConstructor < std::vector<T> > {
+   using return_type = typename MapTypeConverter<std::vector<T>>::type;
+   static return_type build(const std::vector<T>& in){
+     return return_type { in };
+   }
+};
+
+// For maps, we need to fill in the fundamental values, and construct child nodes
+template<class K, class V>
+struct ValueHierarchyConstructor < std::map<K,V> > {
+   using return_type = typename MapTypeConverter<std::map<K,V>>::type;
+   static return_type build(const std::map<K,V>& in){
+     return_type node_to_build;
+     for(auto & entry : in){
+       node_to_build.add_root_value(entry.first);
+       node_to_build.add_sub_container(ValueHierarchyConstructor<V>::build(entry.second));
+     }
+     return node_to_build;
+   } 
+};
+
+
+/**
+  * We're going to be declaring a sparse multidimensional 
+  * tuning space as a set of nested maps. The innermost level
+  * will be a vector. The dimensionality of such a space is the number of
+  * maps + 1.
+  *
+  * The following templates implement such logic recursively
+  */
+template< class InspectForDepth >
+struct get_space_dimensionality;
+
+// The dimensionality of a vector is 1
+template<class T>
+struct get_space_dimensionality < std::vector<T> > {
+   static constexpr const int value = 1; 
+};
+
+// The dimensionality of a map is 1 (the map) plus the dimensionality
+// of the map's value type
+template<class K, class V>
+struct get_space_dimensionality < std::map<K,V> > {
+   static constexpr const int value = 1 + get_space_dimensionality<V>::value; 
+};
+
+/**
+  * This is the ugly part of this implementation: mapping a set of doubles in [0.0,1.0)
+  * into a point in this multidimensional space. We're going to implement this concept 
+  * recursively, building up a tuple at each level.
+  */
+
+// First, a helper to get the value in one dimension
+template<class Container>
+struct DimensionValueExtractor;
+
+// At any given level, just return your value at that level
+template<class RootType, class Subtype>
+struct DimensionValueExtractor<ValueHierarchyNode<RootType, Subtype>> {
+  static RootType get(const ValueHierarchyNode<RootType,Subtype>& dimension, double fraction_to_traverse) {
+    size_t index = dimension.root_values.size() * fraction_to_traverse;
+    return dimension.get_root_value(index);
+  }
+
+};
+
+/** Now we're going to do the full "get a point in the space".
+  * At a root level, we'll take in a ValueHierarchyNode and a set of doubles
+  * representing the value in [0.0,1.0) we want to pick
+  */
+
+// At the bottom level, we have one double and a base-level ValueHierarchyNode
+  
+template<class HierarchyNode, class... InterpolationIndices>
+struct GetMultidimensionalPoint;
+
+template<class ValueType>
+struct GetMultidimensionalPoint<ValueHierarchyNode<ValueType,void>, double> {
+  using node_type = ValueHierarchyNode<ValueType,void>;
+  using return_type = std::tuple<ValueType>;
+  static return_type build(const node_type& in, double index){
+    return std::make_tuple(DimensionValueExtractor<node_type>::get(in, index));
+  }
+};
+
+// At levels above the bottom, we tuple_cat the result of our child on the end of our own tuple
+template<class ValueType, class Subtype, class... Indices>
+struct GetMultidimensionalPoint<ValueHierarchyNode<ValueType,Subtype>, double, Indices...> {
+  using node_type = ValueHierarchyNode<ValueType,Subtype>;
+  using sub_tuple = typename  GetMultidimensionalPoint<Subtype,Indices...>::return_type;
+  using return_type = decltype(std::tuple_cat(std::declval<std::tuple<ValueType>>(),std::declval<sub_tuple>()));
+  static return_type build(const node_type& in, double fraction_to_traverse, Indices... indices) {
+    size_t index = in.sub_values.size() * fraction_to_traverse;
+    auto dimension_value = std::make_tuple(DimensionValueExtractor<node_type>::get(in, fraction_to_traverse));
+    return std::tuple_cat(dimension_value, GetMultidimensionalPoint<Subtype, Indices...>::build(in.get_sub_value(index),indices...));  
+  }
+};
+
+template<typename PointType, class ArrayType, size_t... Is>
+auto get_point_helper(const PointType& in, const ArrayType& indices, std::index_sequence<Is...>){
+  using helper = GetMultidimensionalPoint<PointType, decltype(std::get<Is>(std::declval<ArrayType>()).value.double_value)...>;
+  return helper::build(in, std::get<Is>(indices).value.double_value...);
+}
+
+template<typename PointType, typename ArrayType>
+struct GetPoint;
+
+template<typename PointType, size_t X>
+struct GetPoint<PointType, std::array<Kokkos::Tools::Experimental::VariableValue, X> > {
+  using index_set_type = std::array<Kokkos::Tools::Experimental::VariableValue, X>;
+  static auto build(const PointType& in, const index_set_type& indices){
+    return get_point_helper(in, indices, std::make_index_sequence<X>{});
+  }
+};
+
+template<typename PointType, typename ArrayType>
+auto get_point(const PointType& point, const ArrayType& indices){
+  return GetPoint<PointType,ArrayType>::build(point, indices);
+}
+
+} // Impl
+
+template<template<class...> class Container, size_t MaxDimensionSize = 100, class... TemplateArguments>
+class MultidimensionalSparseTuningProblem {
+public:
+  using ProblemSpaceInput = Container<TemplateArguments...>;
+  static constexpr const int space_dimensionality = Impl::get_space_dimensionality<ProblemSpaceInput>::value; 
+  static constexpr const size_t max_space_dimension_size = MaxDimensionSize;
+  static constexpr const double tuning_min = 0.0;
+  static constexpr const double tuning_max = 0.999;
+  static constexpr const double tuning_step = tuning_max / max_space_dimension_size;
+
+  using StoredProblemSpace = typename Impl::MapTypeConverter<ProblemSpaceInput>::type;
+  using HierarchyConstructor = typename Impl::ValueHierarchyConstructor<Container<TemplateArguments...>>;
+  
+  using ValueArray = std::array<Kokkos::Tools::Experimental::VariableValue, space_dimensionality>;
+private:
+  StoredProblemSpace m_space;
+  std::array<size_t,space_dimensionality> variable_ids;
+  size_t context; 
+public:
+
+  //using PointTuple = typename Impl::GetMultidimensionalPoint<StoredProblemSpace /** , doubles */ >::return_type;
+
+  MultidimensionalSparseTuningProblem(ProblemSpaceInput space, std::vector<std::string> names) : m_space(HierarchyConstructor::build(space)) {
+    assert(names.size() == space_dimensionality);
+    for(int x = 0; x < names.size() ; ++x){
+      VariableInfo info;
+      info.type = Kokkos::Tools::Experimental::ValueType::kokkos_value_double;
+      info.category = Kokkos::Tools::Experimental::StatisticalCategory::kokkos_value_interval;
+      info.valueQuantity = Kokkos::Tools::Experimental::CandidateValueType::kokkos_value_range;
+      info.candidates = Kokkos::Tools::Experimental::make_candidate_range(tuning_min,tuning_max, tuning_step, true, true);
+      variable_ids[x] = declare_output_type(names[x], info);
+    } 
+  }
+
+  auto begin(){
+    context = Kokkos::Tools::Experimental::get_new_context_id();
+    ValueArray values;
+    for(int x = 0 ; x<space_dimensionality; ++x){
+      values[x] = Kokkos::Tools::Experimental::make_variable_value(variable_ids[x], 0.0);
+    }
+    begin_context(context);
+    request_output_values(context, space_dimensionality, values.data());
+    return get_point(m_space, values);
+  }
+
+  auto end(){
+    end_context(context);
+  }
+
+}; 
+
+template<template<class...> class Container, size_t MaxDimensionSize = 100, class... TemplateArguments>
+auto make_multidimensional_sparse_tuning_problem(const Container<TemplateArguments...>& in,std::vector<std::string> names){
+  return MultidimensionalSparseTuningProblem<Container, MaxDimensionSize, TemplateArguments...>(in, names);
+}
+class TeamSizeTuner {
+ private:
+   using SpaceDescription = std::map<int64_t, std::vector<int64_t>>;
+   using TunerType = decltype(make_multidimensional_sparse_tuning_problem(std::declval<SpaceDescription>(), std::declval<std::vector<std::string>>()));
+   TunerType tuner;
+ public:
   TeamSizeTuner& operator=(const TeamSizeTuner& other) = default;
   TeamSizeTuner()                                      = default;
   TeamSizeTuner(const TeamSizeTuner& other)            = default;
@@ -123,21 +363,10 @@ class TeamSizeTuner {
      *    have one vector length
      *
      */
-    std::vector<int64_t> configuration_indices;
-
+    SpaceDescription space_description; 
+    
     if (policy.auto_vector_length()) {
       policy.impl_set_vector_length(1);  // TODO: find a heuristic
-    }
-
-    // in all cases, start by pushing the default
-    configuration_indices.push_back(0);  // default option index
-    if (policy.auto_team_size()) {       // case 1 or 3
-      auto default_team_size = team_size_recommended;
-      configurations.push_back(
-          std::make_pair(default_team_size, policy.vector_length()));
-    } else {  // case 2
-      configurations.push_back(
-          std::make_pair(policy.team_size(), policy.vector_length()));
     }
 
     auto max_vector_length = PolicyType::vector_length_max();
@@ -168,52 +397,41 @@ class TeamSizeTuner {
     } else {  // case 3, there's only one vector length to care about
       allowed_vector_lengths.push_back(policy.vector_length());
     }
-    int index = 1;
 
     for (const auto vector_length : allowed_vector_lengths) {
+      std::vector<int64_t> allowed_team_sizes;
       policy.impl_set_vector_length(vector_length);
       auto max_team_size = team_size_max;
       if (policy.auto_team_size()) {  // case 1 or 3, try all legal team sizes
         for (int team_size = max_team_size; team_size >= 1; team_size /= 2) {
-          configuration_indices.push_back(index++);
-          configurations.push_back(std::make_pair(team_size, vector_length));
+          allowed_team_sizes.push_back(team_size); 
         }
       } else {  // case 2, just try the provided team size
-        configuration_indices.push_back(index++);
-        configurations.push_back(
-            std::make_pair(policy.team_size(), vector_length));
+          allowed_team_sizes.push_back(policy.team_size());
       }
+      space_description[vector_length] = allowed_team_sizes;
     }
-
-    declare_output_type(name, configuration_indices);
+    tuner = make_multidimensional_sparse_tuning_problem(space_description, {std::string(name + "_vector_length"), std::string(name + "_team_size")});
     policy.impl_set_vector_length(initial_vector_length);
   }
 
   template <typename... Properties>
   void tune(Kokkos::TeamPolicy<Properties...>& policy,
             const size_t context_id) {
-    // index zero contains a reasonable default, so in the absence of
-    // a tool, this will return that reasonable default.
-    Kokkos::Tools::Experimental::VariableValue value_index =
-        Kokkos::Tools::Experimental::make_variable_value(type_id, 0);
-    Kokkos::Tools::Experimental::request_output_values(context_id, 1,
-                                                       &value_index);
-    int index          = value_index.value.int_value;
-    auto configuration = configurations[index];
-    auto team_size     = configuration.first;
-    auto vector_length = configuration.second;
+
+    auto configuration = tuner.begin();
+    auto team_size     = std::get<1>(configuration);
+    auto vector_length = std::get<0>(configuration);
     if (vector_length > 0) {
       policy.impl_set_team_size(team_size);
       policy.impl_set_vector_length(vector_length);
     }
   }
   void end(size_t context_id) {
-    Kokkos::Tools::Experimental::end_context(context_id);
+    tuner.end();
   }
 
  private:
-  size_t type_id;
-  std::vector<std::pair<int64_t, int64_t>> configurations;
 };
 
 }  // namespace Experimental
