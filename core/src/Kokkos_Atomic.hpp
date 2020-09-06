59f13d1f903288db9c48e34e3646a571252aad82
j8asic <j8asic@gmail.com>
2020-09-06 22:56:30 +0200

    clang++ compiles CUDA backend on Windows

--- core/src/Kokkos_Atomic.hpp
+++ core/src/Kokkos_Atomic.hpp
@@ -73,9 +73,14 @@
 #include <impl/Kokkos_Traits.hpp>
 
 //----------------------------------------------------------------------------
-#if defined(_WIN32)
-#define KOKKOS_ENABLE_WINDOWS_ATOMICS
 
+// Need to fix this for pure clang on windows
+#if defined(_WIN32)
+#if defined(KOKKOS_COMPILER_CLANG)
+#define KOKKOS_ENABLE_GNU_ATOMICS
+#else
+#define KOKKOS_ENABLE_WINDOWS_ATOMICS
+#endif
 #if defined(KOKKOS_ENABLE_CUDA)
 #define KOKKOS_ENABLE_CUDA_ATOMICS
 #if defined(KOKKOS_COMPILER_CLANG)
@@ -83,7 +88,7 @@
 #endif
 #endif
 
-#else
+#else // _WIN32
 #if defined(KOKKOS_ENABLE_CUDA)
 
 // Compiling NVIDIA device code, must use Cuda atomics:
