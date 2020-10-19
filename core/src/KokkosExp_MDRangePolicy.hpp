c0f4dc8a83b303ac50d598a0695af7b49e4f520f
Damien L-G <dalg24@gmail.com>
2020-10-19 15:46:34 -0400

    Fixup constexpr array construction helper with narrowing conversions
    Co-Authored-By: Daniel Arndt <arndtd@ornl.gov>

--- core/src/KokkosExp_MDRangePolicy.hpp
+++ core/src/KokkosExp_MDRangePolicy.hpp
@@ -114,14 +114,11 @@ struct Rank {
 namespace Impl {
 // NOTE prefer C array U[M] to std::initalizer_list<U> so that the number of
 // elements can be deduced (https://stackoverflow.com/q/40241370)
-// NOTE commented out constexpr specifier below to work around a GCC bug that
-// was fixed from version 7.3
-// (https://gcc.gnu.org/bugzilla/show_bug.cgi?id=80935)
 template <class Array, class U, std::size_t M>
-/*constexpr*/ Array to_array_potentially_narrowing(const U (&init)[M]) {
+constexpr Array to_array_potentially_narrowing(const U (&init)[M]) {
   using T = typename Array::value_type;
-  Array a;
-  constexpr std::size_t N = Array::size();
+  Array a{};
+  constexpr std::size_t N = a.size();
   static_assert(M <= N, "");
   auto* ptr = a.data();
   for (auto x : init)
