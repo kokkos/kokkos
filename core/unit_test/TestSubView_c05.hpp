2da65b777e2bb1dd6c6ac22bb10107b5e665ce3a
Daniel Arndt <arndtd@ornl.gov>
2020-10-20 14:01:36 -0400

    Fix SubView05

--- core/unit_test/TestSubView_c05.hpp
+++ core/unit_test/TestSubView_c05.hpp
@@ -48,7 +48,7 @@
 
 namespace Test {
 
-TEST(openmp, view_subview_2d_from_3d_atomic) {
+TEST(TEST_CATEGORY, view_subview_2d_from_3d_atomic) {
   TestViewSubview::test_2d_subview_3d<TEST_EXECSPACE,
                                       Kokkos::MemoryTraits<Kokkos::Atomic> >();
 }
