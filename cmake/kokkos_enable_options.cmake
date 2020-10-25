79ea54b0ecdebdd04aac7098b51d95a6d5c082e2
Christian Trott <crtrott@sandia.gov>
2020-10-24 22:32:15 -0700

    Add CMake Option for DESUL Atomics, fix some warnings

--- cmake/kokkos_enable_options.cmake
+++ cmake/kokkos_enable_options.cmake
@@ -67,6 +67,7 @@ KOKKOS_ENABLE_OPTION(DEBUG_BOUNDS_CHECK   OFF "Whether to use bounds checking - 
 KOKKOS_ENABLE_OPTION(COMPILER_WARNINGS    OFF "Whether to print all compiler warnings")
 KOKKOS_ENABLE_OPTION(PROFILING_LOAD_PRINT OFF "Whether to print information about which profiling tools got loaded")
 KOKKOS_ENABLE_OPTION(TUNING               OFF "Whether to create bindings for tuning tools")
+KOKKOS_ENABLE_OPTION(IMPL_DESUL_ATOMICS   OFF "Whether to use desul based atomics - option only during beta")
 KOKKOS_ENABLE_OPTION(AGGRESSIVE_VECTORIZATION OFF "Whether to aggressively vectorize loops")
 KOKKOS_ENABLE_OPTION(LAUNCH_COMPILER      ON  "Whether to potentially use the launch compiler")
 
