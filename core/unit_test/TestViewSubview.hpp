5e86a31f0a71dbc2238bcdfa00bd405e17c7a880
Daniel Arndt <arndtd@ornl.gov>
2020-10-29 09:55:55 -0400

    Disable SubView tests rather in core/unit_test/CMakeLists.txt

--- core/unit_test/TestViewSubview.hpp
+++ core/unit_test/TestViewSubview.hpp
@@ -165,8 +165,6 @@ struct fill_2D {
 
 template <class Layout, class Space>
 void test_auto_1d() {
-  // FIXME_SYCL requires MDRange policy
-#ifndef KOKKOS_ENABLE_SYCL
   using mv_type   = Kokkos::View<double**, Layout, Space>;
   using size_type = typename mv_type::size_type;
 
@@ -232,7 +230,6 @@ void test_auto_1d() {
       }
     }
   }
-#endif
 }
 
 template <class LD, class LS, class Space>
@@ -1998,8 +1995,6 @@ void test_1d_assign() {
 
 template <class Space, class MemTraits = void>
 void test_2d_subview_3d() {
-  // FIXME_SYCL requires MDRange policy
-#ifndef KOKKOS_ENABLE_SYCL
   Impl::test_2d_subview_3d_impl_layout<Space, Kokkos::LayoutRight,
                                        Kokkos::LayoutRight, Kokkos::LayoutRight,
                                        MemTraits>();
@@ -2015,33 +2010,26 @@ void test_2d_subview_3d() {
   Impl::test_2d_subview_3d_impl_layout<Space, Kokkos::LayoutStride,
                                        Kokkos::LayoutStride, Kokkos::LayoutLeft,
                                        MemTraits>();
-#endif
 }
 
 template <class Space, class MemTraits = void>
 void test_3d_subview_5d_right() {
-  // FIXME_SYCL requires MDRange policy
-#ifndef KOKKOS_ENABLE_SYCL
   Impl::test_3d_subview_5d_impl_layout<Space, Kokkos::LayoutStride,
                                        Kokkos::LayoutRight, Kokkos::LayoutRight,
                                        MemTraits>();
   Impl::test_3d_subview_5d_impl_layout<Space, Kokkos::LayoutStride,
                                        Kokkos::LayoutStride,
                                        Kokkos::LayoutRight, MemTraits>();
-#endif
 }
 
 template <class Space, class MemTraits = void>
 void test_3d_subview_5d_left() {
-  // FIXME_SYCL requires MDRange policy
-#ifndef KOKKOS_ENABLE_SYCL
   Impl::test_3d_subview_5d_impl_layout<Space, Kokkos::LayoutStride,
                                        Kokkos::LayoutLeft, Kokkos::LayoutLeft,
                                        MemTraits>();
   Impl::test_3d_subview_5d_impl_layout<Space, Kokkos::LayoutStride,
                                        Kokkos::LayoutStride, Kokkos::LayoutLeft,
                                        MemTraits>();
-#endif
 }
 
 template <class Space, class MemTraits = void>
