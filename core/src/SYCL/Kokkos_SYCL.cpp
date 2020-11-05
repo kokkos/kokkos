0aa76bc2d7dd1cb0c8bd312efa5c5ca1594b136e
Daniel Arndt <arndtd@ornl.gov>
2020-11-05 12:59:23 -0500

    Enable custom reduction tests for SYCL

--- core/src/SYCL/Kokkos_SYCL.cpp
+++ core/src/SYCL/Kokkos_SYCL.cpp
@@ -82,8 +82,9 @@ SYCL::SYCL() : m_space_instance(&Impl::SYCLInternal::singleton()) {
 }
 
 int SYCL::concurrency() {
-  // FIXME_SYCL
-  return 1;
+  // FIXME_SYCL We need a value larger than 1 here for some tests to pass,
+  // clearly this is true but not the roght value
+  return 2;
 }
 
 bool SYCL::impl_is_initialized() {
