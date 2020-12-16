4aef11c472ce5de9fc232e8e15a149c10ec7851c
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2020-12-16 13:52:46 -0800

    SKIP_TRIBITS + ARGS
    - Added SKIP_TRIBITS options to KOKKOS_ADD_TEST
    - Changes ARGS processing in KOKKOS_ADD_TEST
    - ARGS processing in KOKKOS_ADD_TEST migrated to KOKKOS_ADD_EXECUTABLE_AND_TEST
    - Use EXIT_SUCCESS instead of EXIT_FAILURE
    - Call Kokkos::finalize before exiting after tools help

--- core/src/impl/Kokkos_Core.cpp
+++ core/src/impl/Kokkos_Core.cpp
@@ -277,7 +277,8 @@ void initialize_profiling(const InitArguments& args) {
     if (!Kokkos::Profiling::printHelp(args.tool_args)) {
       std::cerr << "Tool has not provided a help message" << std::endl;
     }
-    std::exit(EXIT_FAILURE);
+    ::Kokkos::finalize();
+    std::exit(EXIT_SUCCESS);
   }
   Kokkos::Profiling::parseArgs(args.tool_args);
 }
