ce2171433add02a0cd1480824dc15dbc622c1a56
Daniel Arndt <arndtd@ornl.gov>
2021-02-11 17:42:54 -0500

    Avoid copying policies to SYCL kernels

--- core/src/SYCL/Kokkos_SYCL_Parallel_Range.hpp
+++ core/src/SYCL/Kokkos_SYCL_Parallel_Range.hpp
@@ -73,11 +73,10 @@ class Kokkos::Impl::ParallelFor<FunctorType, ExecPolicy,
 
     q.submit([functor, policy](sycl::handler& cgh) {
       sycl::range<1> range(policy.end() - policy.begin());
+      const auto begin = policy.begin();
 
       cgh.parallel_for(range, [=](sycl::item<1> item) {
-        const typename Policy::index_type id =
-            static_cast<typename Policy::index_type>(item.get_linear_id()) +
-            policy.begin();
+        const typename Policy::index_type id = item.get_linear_id() + begin;
         if constexpr (std::is_same<WorkTag, void>::value)
           functor(id);
         else
@@ -128,7 +127,26 @@ class Kokkos::Impl::ParallelFor<FunctorType, Kokkos::MDRangePolicy<Traits...>,
   using WorkTag          = typename Policy::work_tag;
 
   const FunctorType m_functor;
-  const Policy m_policy;
+  // MDRangePolicy is not trivially copyable. Hence, replicate the data we
+  // really need in DeviceIterateTile in a trivially copyable struct.
+  const struct BarePolicy {
+    using index_type = typename Policy::index_type;
+
+    BarePolicy(const Policy& policy)
+        : m_lower(policy.m_lower),
+          m_upper(policy.m_upper),
+          m_tile(policy.m_tile),
+          m_tile_end(policy.m_tile_end),
+          m_num_tiles(policy.m_num_tiles) {}
+
+    const typename Policy::point_type m_lower;
+    const typename Policy::point_type m_upper;
+    const typename Policy::tile_type m_tile;
+    const typename Policy::point_type m_tile_end;
+    const typename Policy::index_type m_num_tiles;
+    static constexpr Iterate inner_direction = Policy::inner_direction;
+  } m_policy;
+  const Kokkos::Experimental::SYCL& m_space;
 
   sycl::nd_range<3> compute_ranges() const {
     const auto& m_tile     = m_policy.m_tile;
@@ -185,21 +203,20 @@ class Kokkos::Impl::ParallelFor<FunctorType, Kokkos::MDRangePolicy<Traits...>,
   template <typename Functor>
   void sycl_direct_launch(const Functor& functor) const {
     // Convenience references
-    const Kokkos::Experimental::SYCL& space = m_policy.space();
     Kokkos::Experimental::Impl::SYCLInternal& instance =
-        *space.impl_internal_space_instance();
+        *m_space.impl_internal_space_instance();
     sycl::queue& q = *instance.m_queue;
 
-    space.fence();
+    m_space.fence();
 
     if (m_policy.m_num_tiles == 0) return;
 
-    auto& policy = m_policy;
+    const BarePolicy bare_policy(m_policy);
 
-    q.submit([functor, this, policy](sycl::handler& cgh) {
+    q.submit([functor, this, bare_policy](sycl::handler& cgh) {
       const auto range = compute_ranges();
 
-      cgh.parallel_for(range, [functor, policy](sycl::nd_item<3> item) {
+      cgh.parallel_for(range, [functor, bare_policy](sycl::nd_item<3> item) {
         const index_type local_x    = item.get_local_id(0);
         const index_type local_y    = item.get_local_id(1);
         const index_type local_z    = item.get_local_id(2);
@@ -210,15 +227,15 @@ class Kokkos::Impl::ParallelFor<FunctorType, Kokkos::MDRangePolicy<Traits...>,
         const index_type n_global_y = item.get_group_range(1);
         const index_type n_global_z = item.get_group_range(2);
 
-        Kokkos::Impl::DeviceIterateTile<Policy::rank, Policy, Functor,
+        Kokkos::Impl::DeviceIterateTile<Policy::rank, BarePolicy, Functor,
                                         typename Policy::work_tag>(
-            policy, functor, {n_global_x, n_global_y, n_global_z},
+            bare_policy, functor, {n_global_x, n_global_y, n_global_z},
             {global_x, global_y, global_z}, {local_x, local_y, local_z})
             .exec_range();
       });
     });
 
-    space.fence();
+    m_space.fence();
   }
 
  public:
@@ -226,9 +243,8 @@ class Kokkos::Impl::ParallelFor<FunctorType, Kokkos::MDRangePolicy<Traits...>,
 
   void execute() const {
     Kokkos::Experimental::Impl::SYCLInternal::IndirectKernelMem&
-        indirectKernelMem = m_policy.space()
-                                .impl_internal_space_instance()
-                                ->m_indirectKernelMem;
+        indirectKernelMem =
+            m_space.impl_internal_space_instance()->m_indirectKernelMem;
 
     const auto functor_wrapper = Experimental::Impl::make_sycl_function_wrapper<
         std::reference_wrapper<FunctorType>>(m_functor, indirectKernelMem);
@@ -242,7 +258,9 @@ class Kokkos::Impl::ParallelFor<FunctorType, Kokkos::MDRangePolicy<Traits...>,
   ~ParallelFor()                        = default;
 
   ParallelFor(const FunctorType& arg_functor, const Policy& arg_policy)
-      : m_functor(arg_functor), m_policy(arg_policy) {}
+      : m_functor(arg_functor),
+        m_policy(arg_policy),
+        m_space(arg_policy.space()) {}
 };
 
 #endif  // KOKKOS_SYCL_PARALLEL_RANGE_HPP_
