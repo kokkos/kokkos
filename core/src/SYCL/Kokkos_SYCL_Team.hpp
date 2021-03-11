f5de7c063289f38a84988018ba0e520a16bc2ee6
Daniel Arndt <arndtd@ornl.gov>
2021-03-11 20:31:57 +0000

    Make prescan more similar to algorithm used in RangePolicy::parallel_scan

--- core/src/SYCL/Kokkos_SYCL_Team.hpp
+++ core/src/SYCL/Kokkos_SYCL_Team.hpp
@@ -184,48 +184,39 @@ class SYCLTeamMember {
 
   // FIXME_SYCL move somewhere else and combine with other places that do
   // parallel_scan
-  // Exclusive scan returning the total sum, compare
-  // https://developer.nvidia.com/gpugems/gpugems3/part-vi-gpu-computing/chapter-39-parallel-prefix-sum-scan-cuda
+  // Exclusive scan returning the itotal sum.
+  // n is required to be a power of two and
+  // temp must point to an array containing the data to be processed
+  // The accumulated value is returned.
   template <typename Type>
   static Type prescan(sycl::nd_item<2> m_item, Type* temp, int n) {
-    int thid   = m_item.get_local_id(0);
-    int offset = 1;
+    int thid = m_item.get_local_id(0);
 
-    for (int d = n >> 1; d > 0; d >>= 1)  // build sum in place up the tree
-    {
-      if (thid < d) {
-        int ai = offset * (2 * thid + 1) - 1;
-        int bi = offset * (2 * thid + 2) - 1;
-
-        temp[bi] += temp[ai];
-      }
+    // First do a reduction saving intermediate results
+    for (int stride = 1; stride < n; stride <<= 1) {
+      auto idx = 2 * stride * (thid + 1) - 1;
+      if (idx < n) temp[idx] += temp[idx - stride];
       m_item.barrier(sycl::access::fence_space::local_space);
-
-      offset *= 2;
     }
 
     Type total_sum = temp[n - 1];
     m_item.barrier(sycl::access::fence_space::local_space);
-    if (thid == 0) {
-      temp[n - 1] = 0;
-    }  // clear the last element
 
-    for (int d = 1; d < n; d *= 2)  // traverse down tree & build scan
-    {
-      offset >>= 1;
-      m_item.barrier(sycl::access::fence_space::local_space);
+    // clear the last element so we get an exclusive scan
+    if (thid == 0) temp[n - 1] = 0;
+    m_item.barrier(sycl::access::fence_space::local_space);
 
-      if (thid < d) {
-        int ai = offset * (2 * thid + 1) - 1;
-        int bi = offset * (2 * thid + 2) - 1;
-
-        Type t   = temp[ai];
-        temp[ai] = temp[bi];
-        temp[bi] += t;
+    // Now add the intermediate results to the remaining items again
+    for (int stride = n / 2; stride > 0; stride >>= 1) {
+      auto idx = 2 * stride * (thid + 1) - 1;
+      if (idx < n) {
+        Type dummy         = temp[idx - stride];
+        temp[idx - stride] = temp[idx];
+        temp[idx] += dummy;
       }
+      m_item.barrier(sycl::access::fence_space::local_space);
     }
 
-    m_item.barrier(sycl::access::fence_space::local_space);
     return total_sum;
   }
 
