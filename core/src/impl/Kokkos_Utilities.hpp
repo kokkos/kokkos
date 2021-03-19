44e4262c0619b317aadda7928db7e4455c604fe1
Damien L-G <dalg24@gmail.com>
2021-03-19 13:51:33 -0400

    Add Kokkos::Impl::void_t
    Utility metafunction that maps a sequence of any types to the type void

--- core/src/impl/Kokkos_Utilities.hpp
+++ core/src/impl/Kokkos_Utilities.hpp
@@ -65,6 +65,14 @@ struct identity {
 template <typename T>
 using identity_t = typename identity<T>::type;
 
+#if defined(__cpp_lib_void_t)
+// since C++17
+using std::void_t;
+#else
+template <class...>
+using void_t = void;
+#endif
+
 //==============================================================================
 // <editor-fold desc="remove_cvref_t"> {{{1
 
