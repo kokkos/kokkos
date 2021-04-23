8d24230c6b448de68c163410c31d2813b75d556c
David Poliakoff <david.poliakoff@gmail.com>
2021-04-23 11:19:01 -0600

    init/fini

--- core/unit_test/tools/TestBuiltinTuners.cpp
+++ core/unit_test/tools/TestBuiltinTuners.cpp
@@ -50,57 +50,61 @@ struct TestTeamFunctor {
 struct TestMDFunctor {
   KOKKOS_FUNCTION void operator()(const int, const int) const {}
 };
-int main() {
-  Kokkos::TeamPolicy<ExecSpace> teamp(1, Kokkos::AUTO, Kokkos::AUTO);
-  Kokkos::MDRangePolicy<Kokkos::Rank<2>> mdp({0, 0}, {1, 1});
-  Kokkos::Tools::Experimental::TeamSizeTuner team_tune_this(
-      "team_tuner", teamp, TestTeamFunctor{}, Kokkos::ParallelForTag{},
-      Kokkos::Tools::Impl::Impl::SimpleTeamSizeCalculator{});
+int main(int argc, char* argv[]) {
+  Kokkos::initialize(argc, argv);
+  {
+    Kokkos::TeamPolicy<ExecSpace> teamp(1, Kokkos::AUTO, Kokkos::AUTO);
+    Kokkos::MDRangePolicy<Kokkos::Rank<2>> mdp({0, 0}, {1, 1});
+    Kokkos::Tools::Experimental::TeamSizeTuner team_tune_this(
+        "team_tuner", teamp, TestTeamFunctor{}, Kokkos::ParallelForTag{},
+        Kokkos::Tools::Impl::Impl::SimpleTeamSizeCalculator{});
 
-  Kokkos::Tools::Experimental::MDRangeTuner<2> md_tune_this(
-      "md_tuner", mdp, TestMDFunctor{}, Kokkos::ParallelForTag{},
-      Kokkos::Tools::Impl::Impl::SimpleTeamSizeCalculator{});
+    Kokkos::Tools::Experimental::MDRangeTuner<2> md_tune_this(
+        "md_tuner", mdp, TestMDFunctor{}, Kokkos::ParallelForTag{},
+        Kokkos::Tools::Impl::Impl::SimpleTeamSizeCalculator{});
 
-  std::vector<int> options{1, 2, 3, 4, 5};
+    std::vector<int> options{1, 2, 3, 4, 5};
 
-  auto new_team_tuner = team_tune_this.combine("options", options);
-  auto new_md_tuner   = md_tune_this.combine("options", options);
-  using namespace Kokkos::Tools::Experimental;
-  VariableInfo info;
-  info.category      = StatisticalCategory::kokkos_value_categorical;
-  info.valueQuantity = CandidateValueType::kokkos_value_unbounded;
-  info.type          = ValueType::kokkos_value_string;
-  size_t input       = declare_input_type("kernel", info);
-  VariableValue team_kernel_value = make_variable_value(input, "abs");
-  VariableValue md_kernel_value   = make_variable_value(input, "abs");
-  size_t kernel_context           = get_new_context_id();
-  begin_context(kernel_context);
-  set_input_values(kernel_context, 1, &team_kernel_value);
-  for (int x = 0; x < 10000; ++x) {
-    auto config = new_md_tuner.begin();
-    int option  = std::get<0>(config);
-    (void)option;
-    int tile_x = std::get<1>(config);
-    int tile_y = std::get<2>(config);
-    Kokkos::parallel_for("mdrange",
-                         Kokkos::MDRangePolicy<Kokkos::Rank<2>>(
-                             {0, 0}, {1, 1}, {tile_x, tile_y}),
-                         TestMDFunctor{});
-    new_md_tuner.end();
+    auto new_team_tuner = team_tune_this.combine("options", options);
+    auto new_md_tuner   = md_tune_this.combine("options", options);
+    using namespace Kokkos::Tools::Experimental;
+    VariableInfo info;
+    info.category      = StatisticalCategory::kokkos_value_categorical;
+    info.valueQuantity = CandidateValueType::kokkos_value_unbounded;
+    info.type          = ValueType::kokkos_value_string;
+    size_t input       = declare_input_type("kernel", info);
+    VariableValue team_kernel_value = make_variable_value(input, "abs");
+    VariableValue md_kernel_value   = make_variable_value(input, "abs");
+    size_t kernel_context           = get_new_context_id();
+    begin_context(kernel_context);
+    set_input_values(kernel_context, 1, &team_kernel_value);
+    for (int x = 0; x < 10000; ++x) {
+      auto config = new_md_tuner.begin();
+      int option  = std::get<0>(config);
+      (void)option;
+      int tile_x = std::get<1>(config);
+      int tile_y = std::get<2>(config);
+      Kokkos::parallel_for("mdrange",
+                           Kokkos::MDRangePolicy<Kokkos::Rank<2>>(
+                               {0, 0}, {1, 1}, {tile_x, tile_y}),
+                           TestMDFunctor{});
+      new_md_tuner.end();
+    }
+    end_context(kernel_context);
+    begin_context(kernel_context);
+    set_input_values(kernel_context, 1, &md_kernel_value);
+    for (int x = 0; x < 10000; ++x) {
+      auto config = new_team_tuner.begin();
+      int option  = std::get<0>(config);
+      (void)option;
+      int team   = std::get<1>(config);
+      int vector = std::get<2>(config);
+      Kokkos::parallel_for("mdrange",
+                           Kokkos::TeamPolicy<ExecSpace>(1, team, vector),
+                           TestTeamFunctor{});
+      new_team_tuner.end();
+    }
+    end_context(kernel_context);
   }
-  end_context(kernel_context);
-  begin_context(kernel_context);
-  set_input_values(kernel_context, 1, &md_kernel_value);
-  for (int x = 0; x < 10000; ++x) {
-    auto config = new_team_tuner.begin();
-    int option  = std::get<0>(config);
-    (void)option;
-    int team   = std::get<1>(config);
-    int vector = std::get<2>(config);
-    Kokkos::parallel_for("mdrange",
-                         Kokkos::TeamPolicy<ExecSpace>(1, team, vector),
-                         TestTeamFunctor{});
-    new_team_tuner.end();
-  }
-  end_context(kernel_context);
+  Kokkos::finalize();
 }
