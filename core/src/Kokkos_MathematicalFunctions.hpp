fe1737cdf8c48622b12223d20dfcda66fe152332
Damien L-G <dalg24@gmail.com>
2021-06-04 14:59:50 -0400

    Attempt to unify absolute value implementation

--- core/src/Kokkos_MathematicalFunctions.hpp
+++ core/src/Kokkos_MathematicalFunctions.hpp
@@ -247,25 +247,32 @@ namespace Experimental {
 #endif
 
 // Basic operations
-// clang-format off
-#ifdef KOKKOS_ENABLE_SYCL
-KOKKOS_INLINE_FUNCTION int abs(int n) { return sycl::abs(n); }
-KOKKOS_INLINE_FUNCTION long abs(long n) { return sycl::abs(n); }
-KOKKOS_INLINE_FUNCTION long long abs(long long n) { return sycl::abs(n); }
-// NOTE sycl::abs(genfloat) -> genfloat math function not available
-KOKKOS_INLINE_FUNCTION float abs(float x) { return sycl::fabs(x); }
-KOKKOS_INLINE_FUNCTION double abs(double x) { return sycl::fabs(x); }
-#else
-KOKKOS_INLINE_FUNCTION int abs(int n) { using std::abs; return abs(n); }
-KOKKOS_INLINE_FUNCTION long abs(long n) { using std::abs; return abs(n); }
-KOKKOS_INLINE_FUNCTION long long abs(long long n) { using std::abs; return abs(n); }
-KOKKOS_INLINE_FUNCTION float abs(float x) { using std::abs; return abs(x); }
-KOKKOS_INLINE_FUNCTION double abs(double x) { using std::abs; return abs(x); }
-#if !(defined(KOKKOS_ENABLE_CUDA) || defined(KOKKOS_ENABLE_HIP) || defined(KOKKOS_ENABLE_OPENMPTARGET))
-KOKKOS_INLINE_FUNCTION long double abs(long double x) { using std::abs; return abs(x); }
+KOKKOS_INLINE_FUNCTION int abs(int n) {
+  using KOKKOS_IMPL_MATH_FUNCTIONS_NAMESPACE::abs;
+  return abs(n);
+}
+KOKKOS_INLINE_FUNCTION long abs(long n) {
+  using KOKKOS_IMPL_MATH_FUNCTIONS_NAMESPACE::abs;
+  return abs(n);
+}
+KOKKOS_INLINE_FUNCTION long long abs(long long n) {
+  using KOKKOS_IMPL_MATH_FUNCTIONS_NAMESPACE::abs;
+  return abs(n);
+}
+KOKKOS_INLINE_FUNCTION float abs(float x) {
+  using KOKKOS_IMPL_MATH_FUNCTIONS_NAMESPACE::abs;
+  return abs(x);
+}
+KOKKOS_INLINE_FUNCTION double abs(double x) {
+  using KOKKOS_IMPL_MATH_FUNCTIONS_NAMESPACE::abs;
+  return abs(x);
+}
+#if defined(KOKKOS_IMPL_MATH_FUNCTIONS_HAVE_LONG_DOUBLE_OVERLOADS)
+KOKKOS_INLINE_FUNCTION long double abs(long double x) {
+  using KOKKOS_IMPL_MATH_FUNCTIONS_NAMESPACE::abs;
+  return abs(x);
+}
 #endif
-#endif
-// clang-format on
 KOKKOS_IMPL_MATH_UNARY_FUNCTION(fabs)
 KOKKOS_IMPL_MATH_BINARY_FUNCTION(fmod)
 KOKKOS_IMPL_MATH_BINARY_FUNCTION(remainder)
