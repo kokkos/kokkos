de5caf650188848b9887c8b02de2c9785f7cc93e
Daniel Arndt <arndtd@ornl.gov>
2021-07-14 18:06:44 -0400

    add sub_group barrier

--- core/src/SYCL/Kokkos_SYCL_Parallel_Reduce.hpp
+++ core/src/SYCL/Kokkos_SYCL_Parallel_Reduce.hpp
@@ -77,6 +77,7 @@ void subgroup_reduction(const sycl::nd_item<dim>& item,
     auto* tmp = sg.shuffle_down(local_mem, stride);
     if (id_in_sg < std::min(stride, max_index - stride))
       ValueJoin::join(selected_reducer, local_mem, tmp);
+    sg.barrier();
   }
 }
 
