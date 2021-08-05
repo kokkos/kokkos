43b89e436f4f2916db5d74dda920b27e56e96a72
Evan Harvey <eharvey@sandia.gov>
2021-08-05 10:04:12 -0600

    Disable point_t reduction test on OPENMPTARGET

--- core/unit_test/TestReducers_d.hpp
+++ core/unit_test/TestReducers_d.hpp
@@ -93,11 +93,12 @@ TEST(TEST_CATEGORY, reducers_int8_t) {
   TestReducers<ThisTestType, TEST_EXECSPACE>::test_prod(4);
 }
 
-#if !defined(KOKKOS_ENABLE_HIP)
-// TODO: resolve: "Kokkos_HIP_Vectorization.hpp:80:15: error: call to
+#if !defined(KOKKOS_ENABLE_HIP) && !defined(KOKKOS_ENABLE_OPENMPTARGET)
+// TODO - resolve: "Kokkos_HIP_Vectorization.hpp:80:15: error: call to
 //                 implicitly-deleted default constructor of 'conv_type'
 //                   conv_type tmp_in;"
 //
+// TODO - resolve:  4: [  FAILED  ] openmptarget.reducers_point_t (1 ms)
 TEST(TEST_CATEGORY, reducers_point_t) {
   using ThisTestType = point_t;
 
@@ -106,6 +107,6 @@ TEST(TEST_CATEGORY, reducers_point_t) {
   TestReducers<ThisTestType, TEST_EXECSPACE>::test_sum(3);
   TestReducers<ThisTestType, TEST_EXECSPACE>::test_sum(4);
 }
-#endif  // !KOKKOS_ENABLE_HIP
+#endif  // !KOKKOS_ENABLE_HIP && !KOKKOS_ENABLE_OPENMPTARGET
 
 }  // namespace Test
