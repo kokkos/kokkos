30adda85a234ac962f27da93ca2754052311a5af
Mikael Simberg <simberg@cscs.ch>
2021-08-17 10:54:58 +0200

    Fix HPX fence label

--- core/src/HPX/Kokkos_HPX_Task.hpp
+++ core/src/HPX/Kokkos_HPX_Task.hpp
@@ -209,7 +209,9 @@ class TaskQueueSpecializationConstrained<
     task_queue.scheduler = &scheduler;
     Kokkos::Impl::dispatch_execute_task(&task_queue,
                                         Kokkos::Experimental::HPX());
-    Kokkos::Experimental::HPX().fence()"Kokkos::Impl::TaskQueueSpecializationConstrained::execute: fence after task execution";
+    Kokkos::Experimental::HPX().fence(
+        "Kokkos::Impl::TaskQueueSpecialization<SimpleTask>::execute: fence "
+        "after task execution");
   }
 
   // Must provide task queue execution function
