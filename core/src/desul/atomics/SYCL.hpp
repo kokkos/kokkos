2a7dff5ee500c9b280bbe6df3db5f69515be0a7b
Damien L-G <dalg24@gmail.com>
2021-09-24 15:52:56 -0400

    Use macros to form SYCL's atomic_fetch_oper overload set

--- core/src/desul/atomics/SYCL.hpp
+++ core/src/desul/atomics/SYCL.hpp
@@ -9,114 +9,48 @@ SPDX-License-Identifier: (BSD-3-Clause)
 #define DESUL_ATOMICS_SYCL_HPP_
 
 #ifdef DESUL_HAVE_SYCL_ATOMICS
+
 #include "desul/atomics/Common.hpp"
 
 namespace desul {
-namespace Impl {
-template<class T>
-struct is_sycl_atomic_type {
-  static constexpr bool value = std::is_same<T, int>::value ||
-                                std::is_same<T, unsigned int>::value ||
-				std::is_same<T, long>::value ||
-				std::is_same<T, unsigned long>::value ||
-				std::is_same<T, long long>::value ||
-                                std::is_same<T, unsigned long long int>::value ||
-				std::is_same<T, float>::value ||
-				std::is_same<T, double>::value;
-};
-} // Impl
 
-// Atomic Add
-template<class T, class MemoryOrder/*, class MemoryScope*/>
-inline
-typename std::enable_if<Impl::is_sycl_atomic_type<T>::value,T>::type
-atomic_fetch_add(T* dest, T val, MemoryOrder, MemoryScopeDevice) {
-  Impl::sycl_atomic_ref<
-    T,
-    MemoryOrder,
-    MemoryScopeDevice>
-  dest_ref(*dest);
-  return dest_ref.fetch_add(val);
-}
+#define DESUL_IMPL_SYCL_ATOMIC_FETCH_OPER(OPER, TYPE)                        \
+  template <class MemoryOrder, class MemoryScope>                            \
+  TYPE atomic_fetch_##OPER(TYPE* dest, TYPE val, MemoryOrder, MemoryScope) { \
+    Impl::sycl_atomic_ref<TYPE, MemoryOrder, MemoryScope> dest_ref(*dest);   \
+    return dest_ref.fetch_##OPER(val);                                       \
+  }
 
-// Atomic Sub
-template<class T, class MemoryOrder/*, class MemoryScope*/>
-inline
-typename std::enable_if<Impl::is_sycl_atomic_type<T>::value,T>::type
-atomic_fetch_sub(T* dest, T val, MemoryOrder, MemoryScopeDevice) {
-  Impl::sycl_atomic_ref<
-    T,
-    MemoryOrder,
-    MemoryScopeDevice>
-  dest_ref(*dest);
-  return dest_ref.fetch_sub(val);
-}
+#define DESUL_IMPL_SYCL_ATOMIC_FETCH_OPER_INTEGRAL(OPER) \
+  DESUL_IMPL_SYCL_ATOMIC_FETCH_OPER(OPER, int)           \
+  DESUL_IMPL_SYCL_ATOMIC_FETCH_OPER(OPER, unsigned int)  \
+  DESUL_IMPL_SYCL_ATOMIC_FETCH_OPER(OPER, long)          \
+  DESUL_IMPL_SYCL_ATOMIC_FETCH_OPER(OPER, unsigned long) \
+  DESUL_IMPL_SYCL_ATOMIC_FETCH_OPER(OPER, long long)     \
+  DESUL_IMPL_SYCL_ATOMIC_FETCH_OPER(OPER, unsigned long long)
 
-// Atomic Max
-template<class T, class MemoryOrder/*, class MemoryScope*/>
-inline
-typename std::enable_if<Impl::is_sycl_atomic_type<T>::value,T>::type
-atomic_fetch_max(T* dest, T val, MemoryOrder, MemoryScopeDevice) {
-  Impl::sycl_atomic_ref<
-    T,
-    MemoryOrder,
-    MemoryScopeDevice>
-  dest_ref(*dest);
-  return dest_ref.fetch_max(val);
-}
+#define DESUL_IMPL_SYCL_ATOMIC_FETCH_OPER_FLOATING_POINT(OPER) \
+  DESUL_IMPL_SYCL_ATOMIC_FETCH_OPER(OPER, float)               \
+  DESUL_IMPL_SYCL_ATOMIC_FETCH_OPER(OPER, double)
 
-// Atomic Min
-template<class T, class MemoryOrder/*, class MemoryScope*/>
-inline
-typename std::enable_if<Impl::is_sycl_atomic_type<T>::value,T>::type
-atomic_fetch_min(T* dest, T val, MemoryOrder, MemoryScopeDevice) {
-  Impl::sycl_atomic_ref<
-    T,
-    MemoryOrder,
-    MemoryScopeDevice>
-  dest_ref(*dest);
-  return dest_ref.fetch_min(val);
-}
+DESUL_IMPL_SYCL_ATOMIC_FETCH_OPER_INTEGRAL(add)
+DESUL_IMPL_SYCL_ATOMIC_FETCH_OPER_INTEGRAL(sub)
+DESUL_IMPL_SYCL_ATOMIC_FETCH_OPER_INTEGRAL(and)
+DESUL_IMPL_SYCL_ATOMIC_FETCH_OPER_INTEGRAL(or)
+DESUL_IMPL_SYCL_ATOMIC_FETCH_OPER_INTEGRAL(xor)
+DESUL_IMPL_SYCL_ATOMIC_FETCH_OPER_INTEGRAL(min)
+DESUL_IMPL_SYCL_ATOMIC_FETCH_OPER_INTEGRAL(max)
 
-// Atomic And
-template<class T, class MemoryOrder/*, class MemoryScope*/>
-inline
-typename std::enable_if<Impl::is_sycl_atomic_type<T>::value,T>::type
-atomic_fetch_and(T* dest, T val, MemoryOrder, MemoryScopeDevice) {
-  Impl::sycl_atomic_ref<
-    T,
-    MemoryOrder,
-    MemoryScopeDevice>
-  dest_ref(*dest);
-  return dest_ref.fetch_and(val);
-}
+DESUL_IMPL_SYCL_ATOMIC_FETCH_OPER_FLOATING_POINT(add)
+DESUL_IMPL_SYCL_ATOMIC_FETCH_OPER_FLOATING_POINT(sub)
+DESUL_IMPL_SYCL_ATOMIC_FETCH_OPER_FLOATING_POINT(min)
+DESUL_IMPL_SYCL_ATOMIC_FETCH_OPER_FLOATING_POINT(max)
 
-// Atomic XOR
-template<class T, class MemoryOrder/*, class MemoryScope*/>
-inline
-typename std::enable_if<Impl::is_sycl_atomic_type<T>::value,T>::type
-atomic_fetch_xor(T* dest, T val, MemoryOrder, MemoryScopeDevice) {
-  Impl::sycl_atomic_ref<
-    T,
-    MemoryOrder,
-    MemoryScopeDevice>
-  dest_ref(*dest);
-  return dest_ref.fetch_xor(val);
-}
+#undef DESUL_IMPL_SYCL_ATOMIC_FETCH_OPER_FLOATING_POINT
+#undef DESUL_IMPL_SYCL_ATOMIC_FETCH_OPER_INTEGRAL
+#undef DESUL_IMPL_SYCL_ATOMIC_FETCH_OPER
 
-// Atomic OR
-template<class T, class MemoryOrder/*, class MemoryScope*/>
-inline
-typename std::enable_if<Impl::is_sycl_atomic_type<T>::value,T>::type
-atomic_fetch_or(T* dest, T val, MemoryOrder, MemoryScopeDevice) {
-  Impl::sycl_atomic_ref<
-    T,
-    MemoryOrder,
-    MemoryScopeDevice>
-  dest_ref(*dest);
-  return dest_ref.fetch_or(val);
-}
+}  // namespace desul
 
-} // desul
 #endif  // DESUL_HAVE_SYCL_ATOMICS
 #endif  // DESUL_ATOMICS_SYCL_HPP_
