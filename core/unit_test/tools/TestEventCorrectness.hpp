7c60c188615586f93796674bffb74af33addcbf1
David Poliakoff <david.poliakoff@gmail.com>
2021-10-04 15:43:30 -0600

    Format

--- core/unit_test/tools/TestEventCorrectness.hpp
+++ core/unit_test/tools/TestEventCorrectness.hpp
@@ -472,7 +472,7 @@ TEST(defaultdevicetype, raw_allocation) {
         }
         /**
          * Note: this is broken in develop, need to fix in a followup
-         * 
+         *
         if (free.name != "dogs") {
           return MatchDiagnostic{false, {"No match on free name"}};
         }
@@ -502,7 +502,7 @@ TEST(defaultdevicetype, view) {
         }
         /**
          * Note: this is broken in develop, need to fix in a followup
-         * 
+         *
         if (free.name != "dogs") {
           return MatchDiagnostic{false, {"No match on free name"}};
         }
@@ -581,24 +581,36 @@ TEST(defaultdevicetype, tuning_sequence) {
   listen_tool_events(Config::DisableAll(), Config::EnableTuning());
   size_t input_id, output_id;
   Kokkos::Tools::Experimental::VariableInfo input_info;
-        input_info.type = Kokkos::Tools::Experimental::ValueType::kokkos_value_int64; 
-        input_info.category = Kokkos::Tools::Experimental::StatisticalCategory::kokkos_value_categorical; 
-        input_info.valueQuantity = Kokkos::Tools::Experimental::CandidateValueType::kokkos_value_unbounded;
+  input_info.type = Kokkos::Tools::Experimental::ValueType::kokkos_value_int64;
+  input_info.category = Kokkos::Tools::Experimental::StatisticalCategory::
+      kokkos_value_categorical;
+  input_info.valueQuantity =
+      Kokkos::Tools::Experimental::CandidateValueType::kokkos_value_unbounded;
   Kokkos::Tools::Experimental::VariableInfo output_info = input_info;
-        output_info.valueQuantity = Kokkos::Tools::Experimental::CandidateValueType::kokkos_value_set;
-        std::vector<int64_t> values {1,2,3,4,5};
-        output_info.candidates = Kokkos::Tools::Experimental::make_candidate_set(values.size(), values.data());
-              
+  output_info.valueQuantity =
+      Kokkos::Tools::Experimental::CandidateValueType::kokkos_value_set;
+  std::vector<int64_t> values{1, 2, 3, 4, 5};
+  output_info.candidates = Kokkos::Tools::Experimental::make_candidate_set(
+      values.size(), values.data());
+
   auto success = validate_event_set(
       [&]() {
-        input_id = Kokkos::Tools::Experimental::declare_input_type("input.dogs", input_info);
-        output_id = Kokkos::Tools::Experimental::declare_output_type("output.dogs", output_info);
+        input_id = Kokkos::Tools::Experimental::declare_input_type("input.dogs",
+                                                                   input_info);
+        output_id = Kokkos::Tools::Experimental::declare_output_type(
+            "output.dogs", output_info);
         auto next_context = Kokkos::Tools::Experimental::get_new_context_id();
         Kokkos::Tools::Experimental::begin_context(next_context);
-        Kokkos::Tools::Experimental::VariableValue feature_value = Kokkos::Tools::Experimental::make_variable_value(input_id, int64_t(0));
-        Kokkos::Tools::Experimental::VariableValue tuning_value = Kokkos::Tools::Experimental::make_variable_value(output_id, int64_t(1));
-        Kokkos::Tools::Experimental::set_input_values(next_context, 1, &feature_value);
-        Kokkos::Tools::Experimental::request_output_values(next_context, 1, &tuning_value);
+        Kokkos::Tools::Experimental::VariableValue feature_value =
+            Kokkos::Tools::Experimental::make_variable_value(input_id,
+                                                             int64_t(0));
+        Kokkos::Tools::Experimental::VariableValue tuning_value =
+            Kokkos::Tools::Experimental::make_variable_value(output_id,
+                                                             int64_t(1));
+        Kokkos::Tools::Experimental::set_input_values(next_context, 1,
+                                                      &feature_value);
+        Kokkos::Tools::Experimental::request_output_values(next_context, 1,
+                                                           &tuning_value);
         Kokkos::Tools::Experimental::end_context(next_context);
       },
       [&](DeclareInputTypeEvent input, DeclareOutputTypeEvent output) {
@@ -608,15 +620,13 @@ TEST(defaultdevicetype, tuning_sequence) {
         if (output.variable_id != output_id) {
           return MatchDiagnostic{false, {"No match on input id"}};
         }
-        if( output.info.candidates.set.size != 5) {
-          return MatchDiagnostic{false, {"Candidates not properly passed through tuning system"}};
-          
+        if (output.info.candidates.set.size != 5) {
+          return MatchDiagnostic{
+              false, {"Candidates not properly passed through tuning system"}};
         }
         return MatchDiagnostic{true};
       },
-      [=](BeginContextEvent) {
-        return MatchDiagnostic{true};
-      },
+      [=](BeginContextEvent) { return MatchDiagnostic{true}; },
       [&](RequestOutputValuesEvent value_request) {
         if (value_request.inputs[0].metadata->type != input_info.type) {
           return MatchDiagnostic{false, {"No match on input in request"}};
@@ -626,10 +636,7 @@ TEST(defaultdevicetype, tuning_sequence) {
         }
         return MatchDiagnostic{true};
       },
-      [=](EndContextEvent) {
-        return MatchDiagnostic{true};
-      }
-      );
+      [=](EndContextEvent) { return MatchDiagnostic{true}; });
   ASSERT_TRUE(success);
 }
 #endif
