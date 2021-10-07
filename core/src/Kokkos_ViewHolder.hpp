1097796edaf4a893efe66f2c95c4033f4034f766
Nicolas Morales <nmmoral@sandia.gov>
2021-10-06 18:15:56 -0700

    ViewHolder: move to a value model

--- core/src/Kokkos_ViewHolder.hpp
+++ core/src/Kokkos_ViewHolder.hpp
@@ -92,68 +92,88 @@ make_unmanaged_view_like(View<DataType, Properties...> view,
       view.rank_dynamic > 6 ? view.extent(6) : KOKKOS_IMPL_CTOR_DEFAULT_ARG,
       view.rank_dynamic > 7 ? view.extent(7) : KOKKOS_IMPL_CTOR_DEFAULT_ARG);
 }
-}  // namespace Impl
 
-struct ViewMetadata {
-  size_t span;
-  bool span_is_contiguous;
-  void *data;
-  std::string label;
-  // virtual ConstViewHolderBase *clone() const = 0;
-  size_t data_type_size;
-  bool is_hostspace;
-};
-
-class ConstViewHolderBase {
+class ConstViewHolderImplBase {
  public:
-  virtual ~ConstViewHolderBase() = default;
+  virtual ~ConstViewHolderImplBase() = default;
 
-  size_t span() const { return m_metadata.span; }
-  bool span_is_contiguous() const { return m_metadata.span_is_contiguous; }
-  // const void *data() const ;
-  void *data() const { return m_metadata.data; }
-  std::string label() const { return m_metadata.label; }
+  size_t span() const { return m_span; }
+  bool span_is_contiguous() const { return m_span_is_contiguous; }
+  const void *data() const { return m_data; }
+  std::string label() const { return m_label; }
 
-  size_t data_type_size() const { return m_metadata.data_type_size; }
-  bool is_hostspace() const noexcept { return m_metadata.is_hostspace; }
+  size_t data_type_size() const { return m_data_type_size; }
+  bool is_hostspace() const noexcept { return m_is_hostspace; }
 
   virtual void deep_copy_to_buffer(unsigned char *buff) = 0;
+  virtual ConstViewHolderImplBase *clone() const             = 0;
 
  protected:
 
-  explicit ConstViewHolderBase( ViewMetadata &&md )
-    : m_metadata( std::move( md ) )
+    ConstViewHolderImplBase( std::size_t span, bool span_is_contiguous,
+                      const void *data, std::string label, std::size_t data_type_size,
+                      bool is_hostspace )
+                      : m_span( span ), m_span_is_contiguous( span_is_contiguous ),
+                      m_data( data ), m_label( std::move( label ) ),
+                      m_data_type_size( data_type_size ), m_is_hostspace( is_hostspace )
+                      {}
+
+ private:
+
+  size_t m_span = 0;
+  bool m_span_is_contiguous = false;
+  const void *m_data = nullptr;
+  std::string m_label;
+  size_t m_data_type_size = 0;
+  bool m_is_hostspace = false;
+};
+
+class ViewHolderImplBase {
+ public:
+  virtual ~ViewHolderImplBase() = default;
+
+  size_t span() const { return m_span; }
+  bool span_is_contiguous() const { return m_span_is_contiguous; }
+  void *data() const { return m_data; }
+  std::string label() const { return m_label; }
+
+  size_t data_type_size() const { return m_data_type_size; }
+  bool is_hostspace() const noexcept { return m_is_hostspace; }
+
+  virtual void deep_copy_to_buffer(unsigned char *buff) = 0;
+  virtual void deep_copy_from_buffer(unsigned char *buff) = 0;
+  virtual ViewHolderImplBase *clone() const                    = 0;
+
+ protected:
+
+  ViewHolderImplBase( std::size_t span, bool span_is_contiguous,
+                     void *data, std::string label, std::size_t data_type_size,
+                     bool is_hostspace )
+      : m_span( span ), m_span_is_contiguous( span_is_contiguous ),
+        m_data( data ), m_label( std::move( label ) ),
+        m_data_type_size( data_type_size ), m_is_hostspace( is_hostspace )
   {}
 
  private:
 
-  ViewMetadata m_metadata;
-};
-
-class ViewHolderBase : public ConstViewHolderBase {
- public:
-  virtual ~ViewHolderBase() = default;
-
-  // virtual void *data() = 0;
-  // virtual ViewHolderBase *clone() const = 0;
-  virtual void deep_copy_from_buffer(unsigned char *buff) = 0;
-
- protected:
-
-  explicit ViewHolderBase( ViewMetadata &&md )
-    : ConstViewHolderBase( std::move( md ) )
-  {}
+  size_t m_span = 0;
+  bool m_span_is_contiguous = false;
+  void *m_data = nullptr;
+  std::string m_label;
+  size_t m_data_type_size = 0;
+  bool m_is_hostspace = false;
 };
 
 template <typename View, typename Enable = void>
-class ViewHolder : public ViewHolderBase {
+class ViewHolderImpl : public ViewHolderImplBase {
  public:
-  virtual ~ViewHolder() = default;
+  virtual ~ViewHolderImpl() = default;
 
-  explicit ViewHolder(const View &view)
-      : ViewHolderBase(ViewMetadata{m_view.span(), m_view.span_is_contiguous(), m_view.data(),
-                   m_view.label(), sizeof(typename View::value_type),
-                   std::is_same<typename View::memory_space, HostSpace>::value}),
+  explicit ViewHolderImpl(const View &view)
+      : ViewHolderImplBase(
+        view.span(), view.span_is_contiguous(), view.data(),
+        view.label(), sizeof(typename View::value_type),
+            std::is_same<typename View::memory_space, HostSpace>::value),
         m_view(view) {}
 
   void deep_copy_to_buffer(unsigned char *buff) override {
@@ -166,21 +186,24 @@ class ViewHolder : public ViewHolderBase {
     deep_copy(m_view, unmanaged);
   }
 
+  ViewHolderImpl *clone() const override { return new ViewHolderImpl(m_view); }
+
  private:
   View m_view;
 };
 
 template <class View>
-class ViewHolder<View, typename std::enable_if<std::is_const<
-                           typename View::value_type>::value>::type>
-    : public ConstViewHolderBase {
+class ViewHolderImpl<View, typename std::enable_if<std::is_const<
+                               typename View::value_type>::value>::type>
+    : public ConstViewHolderImplBase {
  public:
-  virtual ~ViewHolder() = default;
+  virtual ~ViewHolderImpl() = default;
 
-  explicit ViewHolder(const View &view)
-      : ConstViewHolderBase(ViewMetadata{m_view.span(), m_view.span_is_contiguous(), m_view.data(),
-                   m_view.label(), sizeof(typename View::value_type),
-                   std::is_same<typename View::memory_space, HostSpace>::value}),
+  explicit ViewHolderImpl(const View &view)
+      : ConstViewHolderImplBase(
+          view.span(), view.span_is_contiguous(), view.data(),
+          view.label(), sizeof(typename View::value_type),
+            std::is_same<typename View::memory_space, HostSpace>::value),
         m_view(view) {}
 
   void deep_copy_to_buffer(unsigned char *buff) override {
@@ -188,9 +211,103 @@ class ViewHolder<View, typename std::enable_if<std::is_const<
     deep_copy(unmanaged, m_view);
   }
 
+  ViewHolderImpl *clone() const override { return new ViewHolderImpl(m_view); }
+
  private:
   View m_view;
 };
+}  // namespace Impl
+
+namespace Experimental {
+
+class ConstViewHolder {
+ public:
+
+  ConstViewHolder() = default;
+
+  ConstViewHolder( const ConstViewHolder &other )
+  : m_impl( other.m_impl ? other.m_impl->clone() : nullptr )
+  {}
+
+  ConstViewHolder( ConstViewHolder &&other ) {
+    std::swap(m_impl, other.m_impl);
+  }
+
+  ConstViewHolder &operator=( const ConstViewHolder &other )
+  {
+    m_impl = std::unique_ptr<Impl::ConstViewHolderImplBase>(other.m_impl ? other.m_impl->clone() : nullptr);
+    return *this;
+  }
+
+  ConstViewHolder &operator=( ConstViewHolder &&other ) {
+    std::swap(m_impl, other.m_impl);
+    return *this;
+  }
+
+  const void *data() const { return m_impl ? m_impl->data() : nullptr; }
+
+ private:
+
+  template< typename V >
+  friend auto make_view_holder(const V &view);
+
+  template <typename... Args>
+  explicit ConstViewHolder(const View<Args...> &view)
+    : m_impl(std::make_unique<Impl::ViewHolderImpl<View<Args...>>>(view)) {
+  }
+
+  std::unique_ptr<Impl::ConstViewHolderImplBase> m_impl;
+};
+
+class ViewHolder {
+ public:
+
+  ViewHolder() = default;
+
+  ViewHolder( const ViewHolder &other )
+    : m_impl( other.m_impl ? other.m_impl->clone() : nullptr )
+  {}
+
+  ViewHolder( ViewHolder &&other ) {
+    std::swap(m_impl, other.m_impl);
+  }
+
+  ViewHolder &operator=( const ViewHolder &other )
+  {
+    m_impl = std::unique_ptr<Impl::ViewHolderImplBase>(other.m_impl ? other.m_impl->clone() : nullptr);
+    return *this;
+  }
+
+  ViewHolder &operator=( ViewHolder &&other ) {
+    std::swap(m_impl, other.m_impl);
+    return *this;
+  }
+
+  void *data() const { return m_impl ? m_impl->data() : nullptr; }
+
+ private:
+
+  template< typename V >
+  friend auto make_view_holder(const V &view);
+
+  template <typename... Args>
+  explicit ViewHolder(const View<Args...> &view)
+  : m_impl(std::make_unique<Impl::ViewHolderImpl<View<Args...>>>(view)) {
+
+  }
+
+  std::unique_ptr<Impl::ViewHolderImplBase> m_impl;
+};
+
+template< typename V >
+auto make_view_holder( const V &view ) {
+  using holder_type = typename std::conditional<std::is_const<
+      typename V::value_type >::value, ConstViewHolder, ViewHolder >::type;
+  return holder_type( view );
+}
+
+
+}  // namespace Experimental
 
 }  // namespace Kokkos
 
