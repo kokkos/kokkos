83be64065a7905221809e31e102e01331cebde21
Christian Trott <crtrott@sandia.gov>
2021-10-27 08:51:50 -0600

    Merge pull request #4465 from ndellingwood/disable-dep-warns-intel
    Kokkos_Macros: guard deprecation macros for intel<=1900
    
    (cherry picked from commit cb20666043e90b2e7cea81c9bd6a3f742da2f3fb)

--- core/src/Kokkos_Macros.hpp
+++ core/src/Kokkos_Macros.hpp
@@ -535,7 +535,11 @@
 #define KOKKOS_DEPRECATED_TRAILING_ATTRIBUTE
 #endif
 
-#if defined(KOKKOS_ENABLE_DEPRECATION_WARNINGS) && !defined(__NVCC__)
+// Guard intel compiler version <= 1900
+// intel error #2651: attribute does not apply to any entity
+// using <deprecated_type> KOKKOS_DEPRECATED = ...
+#if defined(KOKKOS_ENABLE_DEPRECATION_WARNINGS) && !defined(__NVCC__) && \
+    (KOKKOS_COMPILER_INTEL <= 1900)
 #define KOKKOS_DEPRECATED [[deprecated]]
 #define KOKKOS_DEPRECATED_WITH_COMMENT(comment) [[deprecated(comment)]]
 #else
