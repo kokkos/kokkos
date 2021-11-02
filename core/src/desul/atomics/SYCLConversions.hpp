5293248710719772a969cc690006b0b1856d32a4
Daniel Arndt <arndtd@ornl.gov>
2021-11-02 17:39:47 -0400

    Use desul indentation

--- core/src/desul/atomics/SYCLConversions.hpp
+++ core/src/desul/atomics/SYCLConversions.hpp
@@ -1,4 +1,4 @@
-/* 
+/*
 Copyright (c) 2019, Lawrence Livermore National Security, LLC
 and DESUL project contributors. See the COPYRIGHT file for details.
 Source: https://github.com/desul/desul
@@ -26,46 +26,58 @@ namespace sycl_sync_and_atomics = ::sycl;
 #endif
 
 template <bool extended_namespace>
-using sycl_memory_order = std::conditional_t<extended_namespace, sycl_sync_and_atomics::memory_order, sycl::memory_order>;
+using sycl_memory_order = std::conditional_t<extended_namespace,
+                                             sycl_sync_and_atomics::memory_order,
+                                             sycl::memory_order>;
 template <bool extended_namespace>
-using sycl_memory_scope = std::conditional_t<extended_namespace, sycl_sync_and_atomics::memory_scope, sycl::memory_scope>;
+using sycl_memory_scope = std::conditional_t<extended_namespace,
+                                             sycl_sync_and_atomics::memory_scope,
+                                             sycl::memory_scope>;
 
-template <class MemoryOrder, bool extended_namespace=true>
+template <class MemoryOrder, bool extended_namespace = true>
 struct DesulToSYCLMemoryOrder;
 template <bool extended_namespace>
 struct DesulToSYCLMemoryOrder<MemoryOrderSeqCst, extended_namespace> {
-  static constexpr sycl_memory_order<extended_namespace> value = sycl_memory_order<extended_namespace>::seq_cst;
+  static constexpr sycl_memory_order<extended_namespace> value =
+      sycl_memory_order<extended_namespace>::seq_cst;
 };
 template <bool extended_namespace>
 struct DesulToSYCLMemoryOrder<MemoryOrderAcquire, extended_namespace> {
-  static constexpr sycl_memory_order<extended_namespace> value = sycl_memory_order<extended_namespace>::acquire;
+  static constexpr sycl_memory_order<extended_namespace> value =
+      sycl_memory_order<extended_namespace>::acquire;
 };
 template <bool extended_namespace>
 struct DesulToSYCLMemoryOrder<MemoryOrderRelease, extended_namespace> {
-  static constexpr sycl_memory_order<extended_namespace> value = sycl_memory_order<extended_namespace>::release;
+  static constexpr sycl_memory_order<extended_namespace> value =
+      sycl_memory_order<extended_namespace>::release;
 };
 template <bool extended_namespace>
 struct DesulToSYCLMemoryOrder<MemoryOrderAcqRel, extended_namespace> {
-  static constexpr sycl_memory_order<extended_namespace> value = sycl_memory_order<extended_namespace>::acq_rel;
+  static constexpr sycl_memory_order<extended_namespace> value =
+      sycl_memory_order<extended_namespace>::acq_rel;
 };
 template <bool extended_namespace>
 struct DesulToSYCLMemoryOrder<MemoryOrderRelaxed, extended_namespace> {
-  static constexpr sycl_memory_order<extended_namespace> value = sycl_memory_order<extended_namespace>::relaxed;
+  static constexpr sycl_memory_order<extended_namespace> value =
+      sycl_memory_order<extended_namespace>::relaxed;
 };
 
-template <class MemoryScope, bool extended_namespace=true>
+template <class MemoryScope, bool extended_namespace = true>
 struct DesulToSYCLMemoryScope;
 template <bool extended_namespace>
 struct DesulToSYCLMemoryScope<MemoryScopeCore, extended_namespace> {
-  static constexpr sycl_memory_scope<extended_namespace> value = sycl_memory_scope<extended_namespace>::work_group;
+  static constexpr sycl_memory_scope<extended_namespace> value =
+      sycl_memory_scope<extended_namespace>::work_group;
 };
 template <bool extended_namespace>
 struct DesulToSYCLMemoryScope<MemoryScopeDevice, extended_namespace> {
-  static constexpr sycl_memory_scope<extended_namespace> value = sycl_memory_scope<extended_namespace>::device;
+  static constexpr sycl_memory_scope<extended_namespace> value =
+      sycl_memory_scope<extended_namespace>::device;
 };
 template <bool extended_namespace>
 struct DesulToSYCLMemoryScope<MemoryScopeSystem, extended_namespace> {
-  static constexpr sycl_memory_scope<extended_namespace> value = sycl_memory_scope<extended_namespace>::system;
+  static constexpr sycl_memory_scope<extended_namespace> value =
+      sycl_memory_scope<extended_namespace>::system;
 };
 
 template <class T, class MemoryOrder, class MemoryScope>
