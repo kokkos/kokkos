3e53f2a5db6a55ade8bcbcf1b1da29444eb25d73
Damien L-G <dalg24@gmail.com>
2021-12-06 16:58:44 -0500

    Deprecate log2(unsigned) -> int

--- core/src/impl/Kokkos_Spinwait.cpp
+++ core/src/impl/Kokkos_Spinwait.cpp
@@ -65,7 +65,7 @@ void host_thread_yield(const uint32_t i, const WaitMode mode) {
   static constexpr uint32_t sleep_limit = 1 << 13;
   static constexpr uint32_t yield_limit = 1 << 12;
 
-  const int c = Kokkos::log2(i);
+  const int c = int_log2(i);
 
   if (WaitMode::ROOT != mode) {
     if (sleep_limit < i) {
