69cf846eaf25fdf1edd2608a4b0b9994494e6cbd
Christian Trott <crtrott@sandia.gov>
2021-12-09 08:39:56 -0700

    Unify GPU-like exemption to desul GCC/MSVC atomic enable

--- core/src/desul/atomics/Macros.hpp
+++ core/src/desul/atomics/Macros.hpp
@@ -11,25 +11,27 @@ SPDX-License-Identifier: (BSD-3-Clause)
 
 // Macros
 
+#if (!defined(__CUDA_ARCH__) || !defined(__NVCC__)) &&  \
+    (!defined(__HIP_DEVICE_COMPILE) || !defined(__HIP_PLATFORM_HCC__)) && \
+     !defined(__SYCL_DEVICE_ONLY__) && \
+     !defined(DESUL_HAVE_OPENMP_ATOMICS) && \
+     !defined(DESUL_HAVE_SERIAL_ATOMICS)
+#define DESUL_IMPL_HAVE_GCC_OR_MSVC_ATOMICS
+#endif
+
 // ONLY use GNUC atomics if not compiling for the device
 // and we didn't explicitly say to use OPENMP or SERIAL atomics
-#if defined(__GNUC__) && \
-    (!defined(__CUDA_ARCH__) || !defined(__NVCC__)) && \
-    (!defined(__HIP_DEVICE_COMPILE) || !defined(__HIP_PLATFORM_HCC__)) && \
-    !defined(__SYCL_DEVICE_ONLY__) && \
-    !defined(DESUL_HAVE_OPENMP_ATOMICS) && \
-    !defined(DESUL_HAVE_SERIAL_ATOMICS)
+#if defined(__GNUC__) && defined(DESUL_IMPL_HAVE_GCC_OR_MSVC_ATOMICS)
 #define DESUL_HAVE_GCC_ATOMICS
 #endif
 
 // Equivalent to above: if we are compiling for the device we
 // need to use CUDA/HIP/SYCL atomics instead of MSVC atomics
-#if defined(_MSC_VER)  && \
-    (!defined(__CUDA_ARCH__) || !defined(__NVCC__)) && \
-    (!defined(__HIP_DEVICE_COMPILE) || !defined(__HIP_PLATFORM_HCC__)) && \
-    !defined(SYCL_LANGUAGE_VERSION)
+#if defined(_MSC_VER)  && defined(DESUL_IMPL_HAVE_GCC_OR_MSVC_ATOMICS)
 #define DESUL_HAVE_MSVC_ATOMICS
 #endif
+
+#undef DESUL_IMPL_HAVE_GCC_OR_MSVC_ATOMICS
 
 #ifdef __CUDACC__
 #define DESUL_HAVE_CUDA_ATOMICS
@@ -43,7 +45,8 @@ SPDX-License-Identifier: (BSD-3-Clause)
 #define DESUL_HAVE_SYCL_ATOMICS
 #endif
 
-#if defined(__CUDA_ARCH__) || defined(__HIP_DEVICE_COMPILE__) || defined(__SYCL_DEVICE_ONLY__)
+#if defined(__CUDA_ARCH__) || defined(__HIP_DEVICE_COMPILE__) || \
+    defined(__SYCL_DEVICE_ONLY__)
 #define DESUL_HAVE_GPU_LIKE_PROGRESS
 #endif
 
