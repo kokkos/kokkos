0d6fd46c205da3a3e426f1e4b0c53f95c5b9490e
Daniel Arndt <arndtd@ornl.gov>
2022-01-12 23:29:31 +0000

    Fixunordered map

--- containers/src/Kokkos_UnorderedMap.hpp
+++ containers/src/Kokkos_UnorderedMap.hpp
@@ -711,8 +711,8 @@ class UnorderedMap {
 
 #ifdef KOKKOS_ENABLE_DEPRECATED_CODE_3
   template <typename Dummy = value_type>
-  KOKKOS_FORCEINLINE_FUNCTION KOKKOS_DEPRECATED_WITH_COMMENT(
-      "Calling value_at for value_type==void is deprecated!")
+  KOKKOS_DEPRECATED_WITH_COMMENT(
+      "Calling value_at for value_type==void is deprecated!") KOKKOS_FORCEINLINE_FUNCTION
       std::enable_if_t<
           std::is_void<Dummy>::value,  // is_set
           std::conditional_t<has_const_value, impl_value_type,
