cea57784f367bcac10d1d7b71ff6dd9d60cb3408
Eric Phipps <etphipp@sandia.gov>
2022-02-08 14:45:22 -0700

    Kokkos:  Allow overloads of create_mirror_view_and_copy()
    The default implementation doesn't work with some Sacado/Stokhos scalar
    types, so restrict it to ViewTraits::specialize == void as usual to
    allow overloads in these packages.

--- core/src/Kokkos_CopyViews.hpp
+++ core/src/Kokkos_CopyViews.hpp
@@ -3427,6 +3427,7 @@ create_mirror_view_and_copy(
     const Space&, const Kokkos::View<T, P...>& src,
     std::string const& name = "",
     typename std::enable_if<
+        std::is_same<typename ViewTraits<T, P...>::specialize, void>::value &&
         !Impl::MirrorViewType<Space, T, P...>::is_same_memspace>::type* =
         nullptr) {
   using Mirror      = typename Impl::MirrorViewType<Space, T, P...>::view_type;
