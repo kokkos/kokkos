f01326ed87d27e763e4231a2a621f4c1777e677c
Daniel Arndt <arndtd@ornl.gov>
2022-02-18 15:38:18 -0500

    Use the actual league_size in another place

--- core/src/OpenMPTarget/Kokkos_OpenMPTarget_Parallel.hpp
+++ core/src/OpenMPTarget/Kokkos_OpenMPTarget_Parallel.hpp
@@ -1054,7 +1054,7 @@ struct ParallelReduceSpecialize<FunctorType, TeamPolicyInternal<PolicyArgs...>,
 
     // Allocate scratch per active thread.
     OpenMPTargetExec::resize_scratch(1, 0, value_count * sizeof(ValueType),
-                                     std::numeric_limits<int64_t>::max());
+                                     league_size);
     void* scratch_ptr = OpenMPTargetExec::get_scratch_ptr();
 
     // Enter this loop if the functor has an `init`
