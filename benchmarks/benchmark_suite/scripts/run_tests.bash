abd369fee18ed752230e803022a72c13a8bce6e9
Cezary Skrzyński <cezary.skrzynski@ng-analytics.com>
2022-02-21 12:25:56 +0100

    Restore run_tests.bash script

--- benchmarks/benchmark_suite/scripts/run_tests.bash
+++ benchmarks/benchmark_suite/scripts/run_tests.bash
@@ -3,9 +3,15 @@
 # BytesAndFlops
 cd build/bytes_and_flops
 
-BAF_EXE=bytes_and_flops.exe
-TEAM_SIZE=1
+USE_CUDA=`grep "_CUDA" KokkosCore_config.h | wc -l`
 
+if [[ ${USE_CUDA} > 0 ]]; then
+  BAF_EXE=bytes_and_flops.cuda
+  TEAM_SIZE=256
+else
+  BAF_EXE=bytes_and_flops.exe
+  TEAM_SIZE=1
+fi
 
 BAF_PERF_1=`./${BAF_EXE} 2 100000 1024 1 1 1 1 ${TEAM_SIZE} 6000 | awk '{print $12/174.5}'`
 BAF_PERF_2=`./${BAF_EXE} 2 100000 1024 16 1 8 64 ${TEAM_SIZE} 6000 | awk '{print $14/1142.65}'`
