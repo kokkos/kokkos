2d61d31a8194c2b9b4289cdeed0a5ec34bee32c1
Dan Ibanez <daibane@sandia.gov>
2022-05-11 15:15:46 -0600

    draft Kokkos_Simd.hpp

---
+++ simd/src/Kokkos_Simd.hpp
@@ -0,0 +1,30 @@
+#ifndef KOKKOS_SIMD_HPP
+#define KOKKOS_SIMD_HPP
+
+#include <Kokkos_Simd_Common.hpp>
+
+#include <Kokkos_Simd_Scalar.hpp>
+
+#ifdef __AVX512F__
+#include <Kokkos_Simd_AVX512.hpp>
+#endif
+
+namespace simd_abi {
+
+#if defined(__AVX512F__)
+using host_native = avx512_fixed_size<8>;
+#else
+using host_native = scalar;
+#endif
+
+#if defined(KOKKOS_ENABLE_CUDA) || defined(KOKKOS_ENABLE_HIP)
+using device_native = scalar;
+#else
+using device_native = host_native;
+#endif
+
+using native = host_native;
+
+}
+
+#endif
