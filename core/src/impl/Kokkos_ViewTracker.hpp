fea0f2ee3c0370c775ce88ceeb0529acdfaa8ab3
Christian Trott <crtrott@sandia.gov>
2022-05-16 15:33:39 -0600

    Fix ViewTracker self assignment from different type
    Only really can be triggered via reinterpret_casting a view

--- core/src/impl/Kokkos_ViewTracker.hpp
+++ core/src/impl/Kokkos_ViewTracker.hpp
@@ -91,6 +91,9 @@ struct ViewTracker {
 
   template <class RT, class... RP>
   KOKKOS_INLINE_FUNCTION void assign(const View<RT, RP...>& vt) noexcept {
+    if (reinterpret_cast<void*>(this) ==
+        reinterpret_cast<const void*>(&vt.m_track))
+      return;
     KOKKOS_IF_ON_HOST((
         if (view_traits::is_managed && Kokkos::Impl::SharedAllocationRecord<
                                            void, void>::tracking_enabled()) {
