eb4ac66298393f39ca63e75c47c3306f729a8821
Daniel Arndt <arndtd@ornl.gov>
2022-05-23 10:05:34 -0400

    [ci skip] Comment on hostspace_fence(exec)

--- core/src/Kokkos_HostSpace.hpp
+++ core/src/Kokkos_HostSpace.hpp
@@ -282,6 +282,10 @@ struct ZeroMemset<typename HostSpace::execution_space, DT, DP...> {
   ZeroMemset(const typename HostSpace::execution_space& exec,
              const View<DT, DP...>& dst,
              typename View<DT, DP...>::const_value_type&) {
+    // Host spaces, except for HPX, are synchronous and we need to fence for HPX
+    // since we can't properly enqueue a std::memset otherwise.
+    // We can't use exec.fence() directly since we don't have a full definition
+    // of HostSpace here.
     hostspace_fence(exec);
     using ValueType = typename View<DT, DP...>::value_type;
     std::memset(dst.data(), 0, sizeof(ValueType) * dst.size());
