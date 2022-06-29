ad3bf4bd3a888acba1552a44e5767bb72b89a167
Dong Hun Lee <donlee@sandia.gov>
2022-06-29 17:28:40 -0600

    Fix default inner direction incorrectly being set

--- core/src/HIP/Kokkos_HIP_Team.hpp
+++ core/src/HIP/Kokkos_HIP_Team.hpp
@@ -600,7 +600,7 @@ KOKKOS_INLINE_FUNCTION auto MDThreadVectorRange(
   static constexpr Kokkos::Iterate inner_direction =
       InnerDirection == Kokkos::Iterate::Default
           ? Kokkos::layout_iterate_type_selector<
-                array_layout>::outer_iteration_pattern
+                array_layout>::inner_iteration_pattern
           : InnerDirection;
   using iType = std::common_type_t<Ns...>;
 
@@ -631,7 +631,7 @@ KOKKOS_INLINE_FUNCTION auto MDTeamVectorRange(Impl::HIPTeamMember const& member,
   static constexpr Kokkos::Iterate inner_direction =
       InnerDirection == Kokkos::Iterate::Default
           ? Kokkos::layout_iterate_type_selector<
-                array_layout>::outer_iteration_pattern
+                array_layout>::inner_iteration_pattern
           : InnerDirection;
   using iType = std::common_type_t<Ns...>;
 
