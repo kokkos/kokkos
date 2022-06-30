495959a83c2da2ca0e81e01da75016b835ae734b
Bruno Turcksin <bruno.turcksin@gmail.com>
2022-06-30 16:09:42 +0000

    Remove useless macro guard

--- core/src/Serial/Kokkos_Serial_Task.cpp
+++ core/src/Serial/Kokkos_Serial_Task.cpp
@@ -43,7 +43,7 @@
 */
 
 #include <Kokkos_Macros.hpp>
-#if defined(KOKKOS_ENABLE_SERIAL) && defined(KOKKOS_ENABLE_TASKDAG)
+#if defined(KOKKOS_ENABLE_TASKDAG)
 
 #include <Kokkos_Core.hpp>
 
@@ -63,5 +63,4 @@ template class TaskQueue<Kokkos::Serial, typename Kokkos::Serial::memory_space>;
 
 #else
 void KOKKOS_CORE_SRC_IMPL_SERIAL_TASK_PREVENT_LINK_ERROR() {}
-#endif /* #if defined( KOKKOS_ENABLE_SERIAL ) && defined( \
-          KOKKOS_ENABLE_TASKDAG ) */
+#endif /* #if defined( KOKKOS_ENABLE_TASKDAG ) */
