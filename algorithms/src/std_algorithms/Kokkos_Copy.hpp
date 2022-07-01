cc95fcc63e49ba1748e0e68d0b5c93a54029a57c
Francesco Rizzi <fnrizzi@sandia.gov>
2022-07-01 13:54:27 +0200

    std_algos: teams level copy

--- algorithms/src/std_algorithms/Kokkos_Copy.hpp
+++ algorithms/src/std_algorithms/Kokkos_Copy.hpp
@@ -17,28 +17,37 @@
 #ifndef KOKKOS_STD_ALGORITHMS_COPY_HPP
 #define KOKKOS_STD_ALGORITHMS_COPY_HPP
 
+#include "./impl/Kokkos_IsTeamHandle.hpp"
 #include "impl/Kokkos_CopyCopyN.hpp"
 #include "Kokkos_BeginEnd.hpp"
 
 namespace Kokkos {
 namespace Experimental {
 
+//
+// overload set accepting execution space
+//
 template <class ExecutionSpace, class InputIterator, class OutputIterator>
-OutputIterator copy(const ExecutionSpace& ex, InputIterator first,
-                    InputIterator last, OutputIterator d_first) {
+std::enable_if_t< ::Kokkos::is_execution_space<ExecutionSpace>::value,
+                  OutputIterator>
+copy(const ExecutionSpace& ex, InputIterator first, InputIterator last,
+     OutputIterator d_first) {
   return Impl::copy_impl("Kokkos::copy_iterator_api_default", ex, first, last,
                          d_first);
 }
 
 template <class ExecutionSpace, class InputIterator, class OutputIterator>
-OutputIterator copy(const std::string& label, const ExecutionSpace& ex,
-                    InputIterator first, InputIterator last,
-                    OutputIterator d_first) {
+std::enable_if_t< ::Kokkos::is_execution_space<ExecutionSpace>::value,
+                  OutputIterator>
+copy(const std::string& label, const ExecutionSpace& ex, InputIterator first,
+     InputIterator last, OutputIterator d_first) {
   return Impl::copy_impl(label, ex, first, last, d_first);
 }
 
 template <class ExecutionSpace, class DataType1, class... Properties1,
-          class DataType2, class... Properties2>
+          class DataType2, class... Properties2,
+          std::enable_if_t< ::Kokkos::is_execution_space<ExecutionSpace>::value,
+                            int> = 0>
 auto copy(const ExecutionSpace& ex,
           const ::Kokkos::View<DataType1, Properties1...>& source,
           ::Kokkos::View<DataType2, Properties2...>& dest) {
@@ -51,7 +60,9 @@ auto copy(const ExecutionSpace& ex,
 }
 
 template <class ExecutionSpace, class DataType1, class... Properties1,
-          class DataType2, class... Properties2>
+          class DataType2, class... Properties2,
+          std::enable_if_t< ::Kokkos::is_execution_space<ExecutionSpace>::value,
+                            int> = 0>
 auto copy(const std::string& label, const ExecutionSpace& ex,
           const ::Kokkos::View<DataType1, Properties1...>& source,
           ::Kokkos::View<DataType2, Properties2...>& dest) {
@@ -61,6 +72,33 @@ auto copy(const std::string& label, const ExecutionSpace& ex,
   namespace KE = ::Kokkos::Experimental;
   return Impl::copy_impl(label, ex, KE::cbegin(source), KE::cend(source),
                          KE::begin(dest));
+}
+
+//
+// overload set accepting execution space
+//
+template <class TeamHandleType, class InputIterator, class OutputIterator>
+KOKKOS_FUNCTION std::enable_if_t<Impl::is_team_handle<TeamHandleType>::value,
+                                 OutputIterator>
+copy(const TeamHandleType& teamHandle, InputIterator first, InputIterator last,
+     OutputIterator d_first) {
+  return Impl::copy_team_impl(teamHandle, first, last, d_first);
+}
+
+template <
+    class TeamHandleType, class DataType1, class... Properties1,
+    class DataType2, class... Properties2,
+    std::enable_if_t<Impl::is_team_handle<TeamHandleType>::value, int> = 0>
+KOKKOS_FUNCTION auto copy(
+    const TeamHandleType& teamHandle,
+    const ::Kokkos::View<DataType1, Properties1...>& source,
+    ::Kokkos::View<DataType2, Properties2...>& dest) {
+  Impl::static_assert_is_admissible_to_kokkos_std_algorithms(source);
+  Impl::static_assert_is_admissible_to_kokkos_std_algorithms(dest);
+
+  namespace KE = ::Kokkos::Experimental;
+  return Impl::copy_team_impl(teamHandle, KE::cbegin(source), KE::cend(source),
+                              KE::begin(dest));
 }
 
 }  // namespace Experimental
