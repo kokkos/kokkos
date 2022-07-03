5b68031181eff71878e07c4419e87a0f93a5c79b
Dan Ibanez <daibane@sandia.gov>
2022-07-03 13:08:53 -0600

    replace host/device SIMD with exec space trait
    and provide ISO-TS-compatible `native` which
    is the trait for DefaultExecutionSpace

--- simd/src/Kokkos_SIMD.hpp
+++ simd/src/Kokkos_SIMD.hpp
@@ -58,22 +58,85 @@ namespace Experimental {
 
 namespace simd_abi {
 
+namespace Impl {
+
 #if defined(KOKKOS_ARCH_AVX512XEON)
 using host_native = avx512_fixed_size<8>;
 #else
 using host_native   = scalar;
 #endif
 
-#if defined(KOKKOS_ENABLE_CUDA) || defined(KOKKOS_ENABLE_HIP) || \
-    defined(KOKKOS_ENABLE_SYCL) || defined(KOKKOS_ENABLE_OPENMPTARGET)
-using device_native = scalar;
-#else
-using device_native = host_native;
+template <class T>
+struct ForSpace;
+
+#ifdef KOKKOS_ENABLE_SERIAL
+template <> 
+struct ForSpace<Kokkos::Serial> {
+  using type = host_native;
+};
 #endif
 
-using native = host_native;
+#ifdef KOKKOS_ENABLE_CUDA
+template <> 
+struct ForSpace<Kokkos::Cuda> {
+  using type = scalar;
+};
+#endif
+
+#ifdef KOKKOS_ENABLE_THREADS
+template <>
+struct ForSpace<Kokkos::Threads> {
+  using type = host_native;
+};
+#endif
+
+#ifdef KOKKOS_ENABLE_HPX
+template <>
+struct ForSpace<Kokkos::Experimental::HPX> {
+  using type = scalar;
+};
+#endif
+
+#ifdef KOKKOS_ENABLE_OPENMP
+template <>
+struct ForSpace<Kokkos::OpenMP> {
+  using type = host_native;
+};
+#endif
+
+#ifdef KOKKOS_ENABLE_OPENMPTARGET
+template <>
+struct ForSpace<Kokkos::Experimental::OpenMPTarget> {
+  using type = scalar;
+};
+#endif
+
+#ifdef KOKKOS_ENABLE_HIP
+template <>
+struct ForSpace<Kokkos::Experimental::HIP> {
+  using type = scalar;
+};
+#endif
+
+#ifdef KOKKOS_ENABLE_SYCL
+template <>
+struct ForSpace<Kokkos::Experimental::SYCL> {
+  using type = scalar;
+};
+#endif
+
+}
+
+template <class Space>
+using ForSpace = typename Impl::ForSpace<typename Space::execution_space>::type;
+
+template <class T>
+using native = ForSpace<Kokkos::DefaultExecutionSpace>;
 
 }  // namespace simd_abi
+
+template<class T>
+using native_simd = simd<T, simd_abi::native<T>>;
 
 namespace Impl {
 
@@ -89,11 +152,6 @@ using host_abi_set  = abi_set<simd_abi::scalar>;
 using device_abi_set = abi_set<simd_abi::scalar>;
 
 }
-
-template <class T>
-using device_simd = simd<T, simd_abi::device_native>;
-template <class T>
-using device_simd_mask = simd_mask<T, simd_abi::device_native>;
 
 }  // namespace Experimental
 }  // namespace Kokkos
