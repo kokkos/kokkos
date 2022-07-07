a18793c2f7ca7873cec3c867d715c9b2bf60557b
Francesco Rizzi <fnrizzi@sandia.gov>
2022-07-07 10:51:32 +0200

    formatting

--- algorithms/unit_tests/TestStdAlgorithmsTeamTransform.cpp
+++ algorithms/unit_tests/TestStdAlgorithmsTeamTransform.cpp
@@ -52,19 +52,17 @@ namespace TeamTransform {
 namespace KE = Kokkos::Experimental;
 
 template <class ValueType>
-struct TimesTwoUnaryOp
-{
+struct TimesTwoUnaryOp {
   KOKKOS_INLINE_FUNCTION
-  ValueType operator()(const ValueType & val) const {
+  ValueType operator()(const ValueType& val) const {
     return val * static_cast<ValueType>(2);
   }
 };
 
 template <class ValueType>
-struct AddValuesBinaryOp
-{
+struct AddValuesBinaryOp {
   KOKKOS_INLINE_FUNCTION
-  ValueType operator()(const ValueType & a, const ValueType & b) const {
+  ValueType operator()(const ValueType& a, const ValueType& b) const {
     return a + b;
   }
 };
@@ -75,8 +73,7 @@ struct TestFunctorA {
   ViewDestType m_dest_view;
   int m_api_pick;
 
-  TestFunctorA(const ViewFromType viewFrom,
-	       const ViewDestType viewDest,
+  TestFunctorA(const ViewFromType viewFrom, const ViewDestType viewDest,
                int apiPick)
       : m_from_view(viewFrom), m_dest_view(viewDest), m_api_pick(apiPick) {}
 
@@ -90,42 +87,36 @@ struct TestFunctorA {
         Kokkos::subview(m_dest_view, myRowIndex, Kokkos::ALL());
 
     using value_type = typename ViewFromType::value_type;
-    if (m_api_pick == 0)
-    {
-      auto it = KE::transform(member,
-			      KE::begin(myRowViewFrom), KE::end(myRowViewFrom),
-			      KE::begin(myRowViewDest),
-			      TimesTwoUnaryOp<value_type>());
-      (void) it;
-    } else if (m_api_pick == 1)
-    {
+    if (m_api_pick == 0) {
+      auto it = KE::transform(member, KE::begin(myRowViewFrom),
+                              KE::end(myRowViewFrom), KE::begin(myRowViewDest),
+                              TimesTwoUnaryOp<value_type>());
+      (void)it;
+    } else if (m_api_pick == 1) {
       auto it = KE::transform(member, myRowViewFrom, myRowViewDest,
-			      TimesTwoUnaryOp<value_type>());
-      (void) it;
+                              TimesTwoUnaryOp<value_type>());
+      (void)it;
     }
   }
 };
 
-template <
-  class ViewFromType1, class ViewFromType2,
-  class ViewDestType, class MemberType
-  >
+template <class ViewFromType1, class ViewFromType2, class ViewDestType,
+          class MemberType>
 struct TestFunctorB {
   ViewFromType1 m_from_view1;
   ViewFromType2 m_from_view2;
   ViewDestType m_dest_view;
   int m_api_pick;
 
-  TestFunctorB(const ViewFromType1 viewFrom1,
-	       const ViewFromType2 viewFrom2,
-	       const ViewDestType viewDest,
-               int apiPick)
-      : m_from_view1(viewFrom1), m_from_view2(viewFrom2),
-	m_dest_view(viewDest), m_api_pick(apiPick) {}
+  TestFunctorB(const ViewFromType1 viewFrom1, const ViewFromType2 viewFrom2,
+               const ViewDestType viewDest, int apiPick)
+      : m_from_view1(viewFrom1),
+        m_from_view2(viewFrom2),
+        m_dest_view(viewDest),
+        m_api_pick(apiPick) {}
 
   KOKKOS_INLINE_FUNCTION
-  void operator()(const MemberType& member) const
-  {
+  void operator()(const MemberType& member) const {
     const auto myRowIndex = member.league_rank();
 
     auto myRowViewFrom1 =
@@ -136,27 +127,23 @@ struct TestFunctorB {
         Kokkos::subview(m_dest_view, myRowIndex, Kokkos::ALL());
 
     using value_type = typename ViewFromType1::value_type;
-    if (m_api_pick == 0)
-    {
-      auto it = KE::transform(member,
-			      KE::begin(myRowViewFrom1), KE::end(myRowViewFrom1),
-			      KE::begin(myRowViewFrom2), KE::begin(myRowViewDest),
-			      AddValuesBinaryOp<value_type>());
-      (void) it;
-    } else if (m_api_pick == 1)
-    {
+    if (m_api_pick == 0) {
+      auto it = KE::transform(
+          member, KE::begin(myRowViewFrom1), KE::end(myRowViewFrom1),
+          KE::begin(myRowViewFrom2), KE::begin(myRowViewDest),
+          AddValuesBinaryOp<value_type>());
+      (void)it;
+    } else if (m_api_pick == 1) {
       auto it = KE::transform(member, myRowViewFrom1, myRowViewFrom2,
-			      myRowViewDest,
-			      AddValuesBinaryOp<value_type>());
-      (void) it;
+                              myRowViewDest, AddValuesBinaryOp<value_type>());
+      (void)it;
     }
   }
 };
 
 template <class Tag, class ValueType>
-void test_A(std::size_t num_teams, std::size_t num_cols,
-	    int apiId, int opSwitch)
-{
+void test_A(std::size_t num_teams, std::size_t num_cols, int apiId,
+            int opSwitch) {
   /* description: */
 
   Kokkos::Random_XorShift64_Pool<Kokkos::DefaultExecutionSpace> pool(12371);
@@ -171,40 +158,38 @@ void test_A(std::size_t num_teams, std::size_t num_cols,
   using team_member_type = typename policy_type::member_type;
   policy_type policy(num_teams, Kokkos::AUTO());
 
-  if (opSwitch == 0)
-  {
+  if (opSwitch == 0) {
     auto v2 = create_view<ValueType>(Tag{}, num_teams, num_cols, "v2");
-    using functor_type = TestFunctorA<decltype(v), decltype(v2), team_member_type>;
+    using functor_type =
+        TestFunctorA<decltype(v), decltype(v2), team_member_type>;
     functor_type fnc(v, v2, apiId);
     Kokkos::parallel_for(policy, fnc);
 
     // check
-    auto v_h = create_host_space_copy(v);
+    auto v_h  = create_host_space_copy(v);
     auto v2_h = create_host_space_copy(v2);
     for (std::size_t i = 0; i < v2_h.extent(0); ++i) {
       for (std::size_t j = 0; j < v2_h.extent(1); ++j) {
-	EXPECT_TRUE( v2_h(i,j) == static_cast<ValueType>(2)*v_h(i,j) );
+        EXPECT_TRUE(v2_h(i, j) == static_cast<ValueType>(2) * v_h(i, j));
       }
     }
-  }
-  else
-  {
+  } else {
     auto v2 = create_view<ValueType>(Tag{}, num_teams, num_cols, "v2");
     Kokkos::fill_random(v2, pool, 0, 15);
 
     auto v3 = create_view<ValueType>(Tag{}, num_teams, num_cols, "v3");
-    using functor_type = TestFunctorB<decltype(v), decltype(v2),
-				      decltype(v3), team_member_type>;
+    using functor_type =
+        TestFunctorB<decltype(v), decltype(v2), decltype(v3), team_member_type>;
     functor_type fnc(v, v2, v3, apiId);
     Kokkos::parallel_for(policy, fnc);
 
     // check
-    auto v_h = create_host_space_copy(v);
+    auto v_h  = create_host_space_copy(v);
     auto v2_h = create_host_space_copy(v2);
     auto v3_h = create_host_space_copy(v3);
     for (std::size_t i = 0; i < v3_h.extent(0); ++i) {
       for (std::size_t j = 0; j < v3_h.extent(1); ++j) {
-	EXPECT_TRUE( v3_h(i,j) == v_h(i,j) + v2_h(i,j) );
+        EXPECT_TRUE(v3_h(i, j) == v_h(i, j) + v2_h(i, j));
       }
     }
   }
@@ -215,7 +200,7 @@ void run_all_scenarios(int opSwitch) {
   for (int num_teams : team_sizes_to_test) {
     for (const auto& numCols : {0, 1, 2, 13, 101, 1444, 51153}) {
       for (int apiId : {0, 1}) {
-	test_A<Tag, ValueType>(num_teams, numCols, apiId, opSwitch);
+        test_A<Tag, ValueType>(num_teams, numCols, apiId, opSwitch);
       }
     }
   }
