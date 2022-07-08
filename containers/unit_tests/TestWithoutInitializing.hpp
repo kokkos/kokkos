c3aea33bbf5eb2db2721a109ce0148b5143f6b38
Daniel Arndt <arndtd@ornl.gov>
2022-07-08 18:33:43 -0400

    Remove a couple more fence exclusions

--- containers/unit_tests/TestWithoutInitializing.hpp
+++ containers/unit_tests/TestWithoutInitializing.hpp
@@ -161,10 +161,6 @@ TEST(TEST_CATEGORY, realloc_exec_space_dualview) {
   auto success = validate_absence(
       [&]() { Kokkos::realloc(Kokkos::view_alloc(TEST_EXECSPACE{}), v, 8); },
       [&](BeginFenceEvent event) {
-        if ((event.descriptor().find("Debug Only Check for Execution Error") !=
-             std::string::npos) ||
-            (event.descriptor().find("HostSpace fence") != std::string::npos))
-          return MatchDiagnostic{false};
         return MatchDiagnostic{true, {"Found fence event!"}};
       });
   ASSERT_TRUE(success);
@@ -263,8 +259,6 @@ TEST(TEST_CATEGORY, realloc_exec_space_dynrankview) {
         outer_view2 = inner_view;
       },
       [&](BeginFenceEvent event) {
-        if (event.descriptor().find("HostSpace fence") != std::string::npos)
-          return MatchDiagnostic{false};
         return MatchDiagnostic{true, {"Found fence event!"}};
       });
   ASSERT_TRUE(success);
@@ -398,10 +392,6 @@ TEST(TEST_CATEGORY, realloc_exec_space_scatterview) {
         Kokkos::realloc(Kokkos::view_alloc(TEST_EXECSPACE{}), inner_view, 10);
       },
       [&](BeginFenceEvent event) {
-        if ((event.descriptor().find("Debug Only Check for Execution Error") !=
-             std::string::npos) ||
-            (event.descriptor().find("HostSpace fence") != std::string::npos))
-          return MatchDiagnostic{false};
         return MatchDiagnostic{true, {"Found fence event!"}};
       });
   ASSERT_TRUE(success);
