d73bbed7191a61d3322be94a7e01354ba0653d4c
Damien L-G <dalg24@gmail.com>
2022-07-18 19:43:12 -0400

    Fail to compile when deprecated code is off

--- core/src/traits/Kokkos_WorkTagTrait.hpp
+++ core/src/traits/Kokkos_WorkTagTrait.hpp
@@ -109,7 +109,10 @@ struct WorkTagTrait : TraitSpecificationBase<WorkTagTrait> {
   //   we should benchmark this assumption if it becomes a problem.
   template <class T>
   using trait_matches_specification = std::integral_constant<
-      bool, !std::is_void<T>::value && /*is_empty<T>::value &&*/
+      bool, !std::is_void<T>::value &&
+#ifndef KOKKOS_ENABLE_DEPRECATED_CODE_3
+                std::is_empty<T>::value &&
+#endif
                 !type_list_any<_trait_matches_spec_predicate<T>::template apply,
                                _exec_policy_traits_without_work_tag>::value>;
 };
