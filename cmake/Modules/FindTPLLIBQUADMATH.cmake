f1eae639c3efacf4b6837111fcd5eca38da18f97
Daniel Arndt <arndtd@ornl.gov>
2022-07-28 11:56:26 -0400

    libraries -> link directories

--- cmake/Modules/FindTPLLIBQUADMATH.cmake
+++ cmake/Modules/FindTPLLIBQUADMATH.cmake
@@ -1,5 +1,5 @@
-# gcc bundles libquadmath and doesn't need any extra libraries or include
-# directories (which would not be contained in CMake's search paths anyway).
+# gcc bundles libquadmath and doesn't need any extra link or include directories
+# (which would not be contained in CMake's search paths anyway).
 # Hence, try if the compiler supports libquadmath natively first before doing
 # the standard package search.
 SET(CMAKE_REQUIRED_LIBRARIES "quadmath")
