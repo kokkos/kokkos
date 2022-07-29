2b820b14b1aeee13571793aca9c607e19b73985c
Daniel Arndt <arndtd@ornl.gov>
2022-07-29 14:29:13 -0400

    Update build system

--- cmake/kokkos_pick_cxx_std.cmake
+++ cmake/kokkos_pick_cxx_std.cmake
@@ -1,10 +1,9 @@
 # From CMake 3.10 documentation
 
 #This can run at any time
-KOKKOS_OPTION(CXX_STANDARD "" STRING "[[DEPRECATED - USE CMAKE_CXX_STANDARD INSTEAD]] The C++ standard for Kokkos to use: 14, 17, or 20. If empty, this will default to CMAKE_CXX_STANDARD. If both CMAKE_CXX_STANDARD and Kokkos_CXX_STANDARD are empty, this will default to 14")
+KOKKOS_OPTION(CXX_STANDARD "" STRING "[[DEPRECATED - USE CMAKE_CXX_STANDARD INSTEAD]] The C++ standard for Kokkos to use: 17 or 20. If empty, this will default to CMAKE_CXX_STANDARD. If both CMAKE_CXX_STANDARD and Kokkos_CXX_STANDARD are empty, this will default to 17")
 
 # Set CXX standard flags
-SET(KOKKOS_ENABLE_CXX14 OFF)
 SET(KOKKOS_ENABLE_CXX17 OFF)
 SET(KOKKOS_ENABLE_CXX20 OFF)
 SET(KOKKOS_ENABLE_CXX23 OFF)
@@ -13,7 +12,7 @@ IF (KOKKOS_CXX_STANDARD)
 ENDIF()
 
 IF (NOT CMAKE_CXX_STANDARD)
-  SET(KOKKOS_CXX_STANDARD "14")
+  SET(KOKKOS_CXX_STANDARD "17")
 ELSE()
   SET(KOKKOS_CXX_STANDARD ${CMAKE_CXX_STANDARD})
 ENDIF()
