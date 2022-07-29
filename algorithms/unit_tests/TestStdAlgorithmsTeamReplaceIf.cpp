02925b58b4c3aa3b2087242cb7e2843a74914320
Francesco Rizzi <fnrizzi@sandia.gov>
2022-07-29 17:35:50 -0500

    std_algos: improve replace_if

--- algorithms/unit_tests/TestStdAlgorithmsTeamReplaceIf.cpp
+++ algorithms/unit_tests/TestStdAlgorithmsTeamReplaceIf.cpp
@@ -52,97 +52,132 @@ namespace TeamReplaceIf {
 namespace KE = Kokkos::Experimental;
 
 template <class ValueType>
-struct IsGreaterThanValueFunctor {
+struct GreaterThanValueFunctor {
   ValueType m_val;
 
   KOKKOS_INLINE_FUNCTION
-  IsGreaterThanValueFunctor(ValueType val) : m_val(val) {}
+  GreaterThanValueFunctor(ValueType val) : m_val(val) {}
 
   KOKKOS_INLINE_FUNCTION
   bool operator()(ValueType val) const { return (val > m_val); }
 };
 
-template <class ViewType, class MemberType, class ReplaceIfUnaryOpType>
+template <class ViewType, class MemberType, class ValueType,
+          class ReplaceIfUnaryOpType>
 struct TestFunctorA {
   ViewType m_view;
+  ValueType m_threshold;
+  ValueType m_newVal;
   int m_api_pick;
 
-  TestFunctorA(const ViewType view, int apiPick)
-      : m_view(view), m_api_pick(apiPick) {}
+  TestFunctorA(const ViewType view, ValueType threshold, ValueType newVal,
+               int apiPick)
+      : m_view(view),
+        m_threshold(threshold),
+        m_newVal(newVal),
+        m_api_pick(apiPick) {}
 
   KOKKOS_INLINE_FUNCTION
   void operator()(const MemberType& member) const {
     const auto myRowIndex = member.league_rank();
     auto myRowView        = Kokkos::subview(m_view, myRowIndex, Kokkos::ALL());
 
+    ReplaceIfUnaryOpType op(m_threshold);
     if (m_api_pick == 0) {
-      KE::replace_if(member, KE::begin(myRowView), KE::end(myRowView),
-                     ReplaceIfUnaryOpType(151), 1);
+      KE::replace_if(member, KE::begin(myRowView), KE::end(myRowView), op,
+                     m_newVal);
     } else if (m_api_pick == 1) {
-      KE::replace_if(member, myRowView, ReplaceIfUnaryOpType(151), 1);
+      KE::replace_if(member, myRowView, op, m_newVal);
     }
   }
 };
 
 template <class Tag, class ValueType>
-void test_A(std::size_t num_teams, std::size_t num_cols, int apiId) {
+void test_A(std::size_t numTeams, std::size_t numCols, int apiId) {
   /* description:
      use a rank-2 view randomly filled with values between 0 and 523
-     and then we run a team-level replace_if where the values that
-     are greater than 151 are replaced with 1.
+     and run a team-level replace_if where the values strictly greater
+     than a threshold are replaced with a new value.
    */
+  const auto threshold = static_cast<ValueType>(151);
+  const auto newVal    = static_cast<ValueType>(1);
 
-  // v constructed on memory space associated with default exespace
-  auto v = create_view<ValueType>(Tag{}, num_teams, num_cols, "v");
+  // construct in memory space associated with default exespace
+  auto dataView = create_view<ValueType>(Tag{}, numTeams, numCols, "dataView");
 
-  // v might not deep copyable so to modify it on the host
-  auto v_dc   = create_deep_copyable_compatible_view_with_same_extent(v);
-  auto v_dc_h = create_mirror_view(Kokkos::HostSpace(), v_dc);
+  // dataView might not deep copyable (e.g. strided layout) so to modify it
+  // on the host we make a new one that is for sure deep copyable
+  auto dataView_dc =
+      create_deep_copyable_compatible_view_with_same_extent(dataView);
+  auto dataView_dc_h = create_mirror_view(Kokkos::HostSpace(), dataView_dc);
 
+  // randomly fill the view with values between 0 and 523
   Kokkos::Random_XorShift64_Pool<Kokkos::DefaultHostExecutionSpace> pool(12371);
-  Kokkos::fill_random(v_dc_h, pool, 0, 523);
-
-  std::vector<std::size_t> rowIndOfTargetElements;
-  std::vector<std::size_t> colIndOfTargetElements;
-  for (std::size_t i = 0; i < v_dc_h.extent(0); ++i) {
-    for (std::size_t j = 0; j < v_dc_h.extent(1); ++j) {
-      if (v_dc_h(i, j) > static_cast<ValueType>(151)) {
-        rowIndOfTargetElements.push_back(i);
-        colIndOfTargetElements.push_back(j);
+  Kokkos::fill_random(dataView_dc_h, pool, 0, 523);
+  // figure out which elements are strictly greater than the threshold
+  std::vector<std::size_t> targetElementsLinearizedIds;
+  for (std::size_t i = 0; i < dataView_dc_h.extent(0); ++i) {
+    for (std::size_t j = 0; j < dataView_dc_h.extent(1); ++j) {
+      if (dataView_dc_h(i, j) > threshold) {
+        targetElementsLinearizedIds.push_back(i * numCols + j);
       }
     }
   }
 
-  // copy to v_dc and then to v
-  Kokkos::deep_copy(v_dc, v_dc_h);
-  CopyFunctorRank2<decltype(v_dc), decltype(v)> F1(v_dc, v);
-  Kokkos::parallel_for("copy", v.extent(0) * v.extent(1), F1);
+  // copy to dataView_dc and then to dataView
+  Kokkos::deep_copy(dataView_dc, dataView_dc_h);
+  CopyFunctorRank2<decltype(dataView_dc), decltype(dataView)> F1(dataView_dc,
+                                                                 dataView);
+  Kokkos::parallel_for("copy", dataView.extent(0) * dataView.extent(1), F1);
 
   // launch kernel
   using space_t          = Kokkos::DefaultExecutionSpace;
   using policy_type      = Kokkos::TeamPolicy<space_t>;
   using team_member_type = typename policy_type::member_type;
-  policy_type policy(num_teams, Kokkos::AUTO());
+  policy_type policy(numTeams, Kokkos::AUTO());
 
-  using bop_t        = IsGreaterThanValueFunctor<ValueType>;
-  using functor_type = TestFunctorA<decltype(v), team_member_type, bop_t>;
-  functor_type fnc(v, apiId);
+  using bop_t = GreaterThanValueFunctor<ValueType>;
+  using functor_type =
+      TestFunctorA<decltype(dataView), team_member_type, ValueType, bop_t>;
+  functor_type fnc(dataView, threshold, newVal, apiId);
   Kokkos::parallel_for(policy, fnc);
 
-  // check
-  auto v2_h = create_host_space_copy(v);
-  for (std::size_t k = 0; k < rowIndOfTargetElements.size(); ++k) {
-    EXPECT_TRUE(v2_h(rowIndOfTargetElements[k], colIndOfTargetElements[k]) ==
-                static_cast<ValueType>(1));
+  // make a copy on host (generic to handle case view is not deep-copyable)
+  // and check that the right elements now have the new value and the rest is
+  // unchanged.
+  auto dataView2_h = create_host_space_copy(dataView);
+  for (auto k : targetElementsLinearizedIds) {
+    const std::size_t i = k / numCols;
+    const std::size_t j = k % numCols;
+    EXPECT_TRUE(dataView2_h(i, j) == newVal);
   }
+
+  // figure out which are the unchanged elements
+  std::vector<std::size_t> allIndices(numTeams * numCols);
+  std::iota(allIndices.begin(), allIndices.end(), 0);
+  std::vector<std::size_t> unchanged(allIndices.size());
+  // sort because set_difference requires that
+  std::sort(targetElementsLinearizedIds.begin(),
+            targetElementsLinearizedIds.end());
+  auto bound = std::set_difference(allIndices.cbegin(), allIndices.cend(),
+                                   targetElementsLinearizedIds.cbegin(),
+                                   targetElementsLinearizedIds.cend(),
+                                   unchanged.begin());
+
+  auto verify = [numCols, dataView2_h, dataView_dc_h](const std::size_t& k) {
+    const std::size_t i = k / numCols;
+    const std::size_t j = k % numCols;
+    EXPECT_TRUE(dataView2_h(i, j) == dataView_dc_h(i, j));
+  };
+  std::for_each(unchanged.begin(), bound, verify);
 }
 
 template <class Tag, class ValueType>
 void run_all_scenarios() {
-  for (int num_teams : team_sizes_to_test) {
-    for (const auto& numCols : {0, 1, 2, 13, 101, 1444, 51153}) {
+  for (int numTeams : team_sizes_to_test) {
+    for (const auto& numCols : {0, 1, 2, 13, 101, 1444, 8153}) {
       for (int apiId : {0, 1}) {
-        test_A<Tag, ValueType>(num_teams, numCols, apiId);
+        test_A<Tag, ValueType>(numTeams, numCols, apiId);
       }
     }
   }
