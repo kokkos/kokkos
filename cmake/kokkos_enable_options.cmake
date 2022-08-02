6a627ff4678d7410701f5fea9f0a54651f507413
Cezary Skrzyński <cezary.skrzynski@ng-analytics.com>
2022-08-02 20:29:59 +0200

    Add Kokkos_ENABLE_PERF_TESTS option
    Add Kokkos_ENABLE_PERF_TESTS option (default OFF). Only look for
    `benchmark` lib when the performance tests are enabled.

--- cmake/kokkos_enable_options.cmake
+++ cmake/kokkos_enable_options.cmake
@@ -40,6 +40,7 @@ KOKKOS_ENABLE_OPTION(DEPRECATION_WARNINGS ON "Whether to emit deprecation warnin
 KOKKOS_ENABLE_OPTION(HIP_RELOCATABLE_DEVICE_CODE  OFF "Whether to enable relocatable device code (RDC) for HIP")
 KOKKOS_ENABLE_OPTION(HPX_ASYNC_DISPATCH   OFF "Whether HPX supports asynchronous dispatch")
 KOKKOS_ENABLE_OPTION(TESTS         OFF  "Whether to build the unit tests")
+KOKKOS_ENABLE_OPTION(PERF_TESTS    OFF  "Whether to build the perf tests")
 KOKKOS_ENABLE_OPTION(EXAMPLES      OFF  "Whether to build the examples")
 STRING(TOUPPER "${CMAKE_BUILD_TYPE}" UPPERCASE_CMAKE_BUILD_TYPE)
 IF(UPPERCASE_CMAKE_BUILD_TYPE STREQUAL "DEBUG")
