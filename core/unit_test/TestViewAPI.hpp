9b324a021bbf50c802b01a2786b88876633b0261
Seyong Lee <lees2@ornl.gov>
2022-08-18 15:52:17 -0400

    Apply suggestions from code review
    Update FIXME_OPENACC comments as suggested.
    
    Co-authored-by: Damien L-G <dalg24+github@gmail.com>

--- core/unit_test/TestViewAPI.hpp
+++ core/unit_test/TestViewAPI.hpp
@@ -1386,9 +1386,7 @@ class TestViewAPI {
       Kokkos::deep_copy(h, d);
     }
 
-#if !defined(KOKKOS_ENABLE_OPENACC)
-    // FIXME_OPENACC: this can be removed when MDRange parallel_for is
-    // implemented.
+#if !defined(KOKKOS_ENABLE_OPENACC)  // FIXME_OPENACC MDRange parallel_for not implemented
     // Check Deep Copy of two empty 2D views
     {
       Kokkos::View<double * [3], Kokkos::LayoutRight> d;
