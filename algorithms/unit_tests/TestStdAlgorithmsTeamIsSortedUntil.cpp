8f427285fed1902bc3e5ec461034ec5586acc498
Francesco Rizzi <fnrizzi@sandia.gov>
2022-08-29 10:05:00 +0200

    formatting

--- algorithms/unit_tests/TestStdAlgorithmsTeamIsSortedUntil.cpp
+++ algorithms/unit_tests/TestStdAlgorithmsTeamIsSortedUntil.cpp
@@ -59,43 +59,38 @@ struct UnifDist<int> {
   std::mt19937 m_gen;
   dist_type m_dist;
 
-  UnifDist(int a, int b, std::size_t seedIn) : m_dist(a, b) { m_gen.seed(seedIn); }
+  UnifDist(int a, int b, std::size_t seedIn) : m_dist(a, b) {
+    m_gen.seed(seedIn);
+  }
 
   int operator()() { return m_dist(m_gen); }
 };
 
 template <class ViewType, class DistancesViewType>
-struct TestFunctorA
-{
+struct TestFunctorA {
   ViewType m_view;
   DistancesViewType m_distancesView;
   int m_apiPick;
 
-  TestFunctorA(const ViewType view, const
-	       DistancesViewType distancesView,
-	       int apiPick)
-      : m_view(view),
-        m_distancesView(distancesView),
-        m_apiPick(apiPick) {}
+  TestFunctorA(const ViewType view, const DistancesViewType distancesView,
+               int apiPick)
+      : m_view(view), m_distancesView(distancesView), m_apiPick(apiPick) {}
 
   template <class MemberType>
-  KOKKOS_INLINE_FUNCTION void operator()(const MemberType& member) const
-  {
+  KOKKOS_INLINE_FUNCTION void operator()(const MemberType& member) const {
     const auto leagueRank = member.league_rank();
     const auto myRowIndex = leagueRank;
     auto myRowView        = Kokkos::subview(m_view, myRowIndex, Kokkos::ALL());
 
     using value_type = typename ViewType::value_type;
     if (m_apiPick == 0) {
-      auto it = KE::is_sorted_until(member,
-				    KE::cbegin(myRowView),
-				    KE::cend(myRowView));
+      auto it = KE::is_sorted_until(member, KE::cbegin(myRowView),
+                                    KE::cend(myRowView));
 
       Kokkos::single(Kokkos::PerTeam(member), [=]() {
         m_distancesView(myRowIndex) = KE::distance(KE::cbegin(myRowView), it);
       });
-    }
-    else if (m_apiPick == 1) {
+    } else if (m_apiPick == 1) {
       auto it = KE::is_sorted_until(member, myRowView);
       Kokkos::single(Kokkos::PerTeam(member), [=]() {
         m_distancesView(myRowIndex) = KE::distance(KE::begin(myRowView), it);
@@ -103,10 +98,9 @@ struct TestFunctorA
     }
 
     else if (m_apiPick == 2) {
-      auto it = KE::is_sorted_until(member,
-				    KE::cbegin(myRowView),
-				    KE::cend(myRowView),
-				    CustomLessThanComparator<value_type>{});
+      auto it = KE::is_sorted_until(member, KE::cbegin(myRowView),
+                                    KE::cend(myRowView),
+                                    CustomLessThanComparator<value_type>{});
 
       Kokkos::single(Kokkos::PerTeam(member), [=]() {
         m_distancesView(myRowIndex) = KE::distance(KE::cbegin(myRowView), it);
@@ -114,7 +108,8 @@ struct TestFunctorA
     }
 
     else if (m_apiPick == 3) {
-      auto it = KE::is_sorted_until(member, myRowView, CustomLessThanComparator<value_type>{});
+      auto it = KE::is_sorted_until(member, myRowView,
+                                    CustomLessThanComparator<value_type>{});
       Kokkos::single(Kokkos::PerTeam(member), [=]() {
         m_distancesView(myRowIndex) = KE::distance(KE::begin(myRowView), it);
       });
@@ -124,8 +119,7 @@ struct TestFunctorA
 
 template <class LayoutTag, class ValueType>
 void test_A(std::size_t numTeams, std::size_t numCols, int apiId,
-            const std::string& sIn)
-{
+            const std::string& sIn) {
   /* description:
      use a rank-2 view and run a team-level is_sorted_until
      for various trivial and non trivial scenarios
@@ -146,12 +140,11 @@ void test_A(std::size_t numTeams, std::size_t numCols, int apiId,
       create_deep_copyable_compatible_view_with_same_extent(dataView);
   auto dataView_dc_h = create_mirror_view(Kokkos::HostSpace(), dataView_dc);
 
-  if (sIn == "trivialEmpty" or sIn == "trivialOneElement"){
+  if (sIn == "trivialEmpty" or sIn == "trivialOneElement") {
     // do not do anything
   }
 
-  else if (sIn == "nontrivialUntilLast")
-  {
+  else if (sIn == "nontrivialUntilLast") {
     for (std::size_t i = 0; i < dataView_dc_h.extent(0); ++i) {
       for (std::size_t j = 0; j < dataView_dc_h.extent(1); ++j) {
         dataView_dc_h(i, j) = ValueType(j);
@@ -159,10 +152,10 @@ void test_A(std::size_t numTeams, std::size_t numCols, int apiId,
     }
   }
 
-  else if (sIn == "nontrivialRandom")
-  {
+  else if (sIn == "nontrivialRandom") {
     // randomly fill the view
-    Kokkos::Random_XorShift64_Pool<Kokkos::DefaultHostExecutionSpace> pool(452377);
+    Kokkos::Random_XorShift64_Pool<Kokkos::DefaultHostExecutionSpace> pool(
+        452377);
     // make the range tight so that we have low likelihood of having
     // sorted data by chance for small num of cols
     Kokkos::fill_random(dataView_dc_h, pool, ValueType(113), ValueType(120));
@@ -175,17 +168,16 @@ void test_A(std::size_t numTeams, std::size_t numCols, int apiId,
        this allows us to exercise that the algorithm returns
        the larest sorted interval starting from 0
     */
-    assert( numCols > 10 );
+    assert(numCols > 10);
     const std::size_t midPoint = numCols / 2;
 
     UnifDist<int> randPoolA(0, midPoint, 3432779);
-    for (std::size_t i = 0; i < dataView_dc_h.extent(0); ++i)
-    {
+    for (std::size_t i = 0; i < dataView_dc_h.extent(0); ++i) {
       const std::size_t a = randPoolA();
-      for (std::size_t j = 0; j < a; ++j){
+      for (std::size_t j = 0; j < a; ++j) {
         dataView_dc_h(i, j) = ValueType(j);
       }
-      for (std::size_t j = a+3; j < numCols; ++j){
+      for (std::size_t j = a + 3; j < numCols; ++j) {
         dataView_dc_h(i, j) = ValueType(j);
       }
     }
@@ -215,18 +207,17 @@ void test_A(std::size_t numTeams, std::size_t numCols, int apiId,
   // -----------------------------------------------
   // check
   // -----------------------------------------------
-  auto distancesView_h   = create_host_space_copy(distancesView);
+  auto distancesView_h = create_host_space_copy(distancesView);
   for (std::size_t i = 0; i < dataView_dc_h.extent(0); ++i) {
     auto myRow = Kokkos::subview(dataView_dc_h, i, Kokkos::ALL());
 
     std::size_t stdDistance = 0;
     if (apiId <= 1) {
-      auto it = std::is_sorted_until(KE::cbegin(myRow), KE::cend(myRow));
+      auto it     = std::is_sorted_until(KE::cbegin(myRow), KE::cend(myRow));
       stdDistance = KE::distance(KE::cbegin(myRow), it);
     } else {
-      auto it = std::is_sorted_until(KE::cbegin(myRow),
-				     KE::cend(myRow),
-				     CustomLessThanComparator<ValueType>{});
+      auto it     = std::is_sorted_until(KE::cbegin(myRow), KE::cend(myRow),
+                                     CustomLessThanComparator<ValueType>{});
       stdDistance = KE::distance(KE::cbegin(myRow), it);
     }
     EXPECT_EQ(stdDistance, distancesView_h(i));
@@ -238,9 +229,7 @@ void test_A(std::size_t numTeams, std::size_t numCols, int apiId,
 }
 
 template <class LayoutTag, class ValueType>
-void run_all_scenarios(const std::string & name,
-		       const std::vector<int> & cols)
-{
+void run_all_scenarios(const std::string& name, const std::vector<int>& cols) {
   for (int numTeams : teamSizesToTest) {
     for (const auto& numCols : cols) {
 #if not defined KOKKOS_ENABLE_OPENMPTARGET
@@ -254,9 +243,8 @@ void run_all_scenarios(const std::string & name,
   }
 }
 
-TEST(std_algorithms_is_sorted_until_team_test, test_trivialA)
-{
-  const std::string name = "trivialEmpty";
+TEST(std_algorithms_is_sorted_until_team_test, test_trivialA) {
+  const std::string name      = "trivialEmpty";
   const std::vector<int> cols = {0};
 
   run_all_scenarios<DynamicTag, double>(name, cols);
@@ -264,28 +252,24 @@ TEST(std_algorithms_is_sorted_until_team_test, test_trivialA)
   run_all_scenarios<StridedThreeRowsTag, int>(name, cols);
 }
 
-TEST(std_algorithms_is_sorted_until_team_test, test_trivialB)
-{
-  const std::string name = "trivialOneElement";
+TEST(std_algorithms_is_sorted_until_team_test, test_trivialB) {
+  const std::string name      = "trivialOneElement";
   const std::vector<int> cols = {1};
   run_all_scenarios<DynamicTag, double>(name, cols);
   run_all_scenarios<StridedTwoRowsTag, double>(name, cols);
   run_all_scenarios<StridedThreeRowsTag, int>(name, cols);
 }
 
-TEST(std_algorithms_is_sorted_until_team_test, test_nontrivialA)
-{
-  const std::string name = "nontrivialUntilLast";
+TEST(std_algorithms_is_sorted_until_team_test, test_nontrivialA) {
+  const std::string name      = "nontrivialUntilLast";
   const std::vector<int> cols = {13, 101, 1444, 5153};
   run_all_scenarios<DynamicTag, double>(name, cols);
   run_all_scenarios<StridedTwoRowsTag, double>(name, cols);
   run_all_scenarios<StridedThreeRowsTag, int>(name, cols);
 }
 
-
-TEST(std_algorithms_is_sorted_until_team_test, test_nontrivialB)
-{
-  const std::string name = "nontrivialRandom";
+TEST(std_algorithms_is_sorted_until_team_test, test_nontrivialB) {
+  const std::string name      = "nontrivialRandom";
   const std::vector<int> cols = {13, 101, 1444, 5153};
   run_all_scenarios<DynamicTag, double>(name, cols);
   run_all_scenarios<StridedTwoRowsTag, double>(name, cols);
