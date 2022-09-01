f95e6faf8ca2a057da9a631e0b4d488863f1404a
Daniel Arndt <arndtd@ornl.gov>
2022-09-01 16:47:50 -0400

    Silence warnings about valueView being unused

--- algorithms/src/Kokkos_NestedSort.hpp
+++ algorithms/src/Kokkos_NestedSort.hpp
@@ -92,6 +92,9 @@ KOKKOS_INLINE_FUNCTION void sort_nested_impl(const TeamMember& t,
                                              const ValueViewType& valueView,
                                              const Comparator& comp,
                                              const NestedRange<useTeamLevel>) {
+  // Silence warnings about valueView being unused
+  if constexpr (std::is_same_v<ValueViewType, std::nullptr_t>) (void)valueView;
+
   using SizeType  = typename KeyViewType::size_type;
   using KeyType   = typename KeyViewType::non_const_value_type;
   using Range     = NestedRange<useTeamLevel>;
