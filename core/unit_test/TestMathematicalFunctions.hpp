b730a74585b1c1bca8a28979011886afa55ad8f5
Dan Ibanez <daibane@sandia.gov>
2022-09-06 12:22:50 -0600

    add test for Kokkos::fma

--- core/unit_test/TestMathematicalFunctions.hpp
+++ core/unit_test/TestMathematicalFunctions.hpp
@@ -446,6 +446,7 @@ DEFINE_BINARY_FUNCTION_EVAL(copysign, 1);
 
 #ifndef KOKKOS_MATHEMATICAL_FUNCTIONS_SKIP_1
 DEFINE_TERNARY_FUNCTION_EVAL(hypot, 2);
+DEFINE_TERNARY_FUNCTION_EVAL(fma, 2);
 #endif
 
 #undef DEFINE_TERNARY_FUNCTION_EVAL
@@ -713,6 +714,15 @@ TEST(TEST_CATEGORY, mathematical_functions_power_functions) {
   do_test_math_ternary_function<TEST_EXECSPACE, kk3_hypot>(2, 3.f, 4.);
 #ifdef MATHEMATICAL_FUNCTIONS_HAVE_LONG_DOUBLE_OVERLOADS
   do_test_math_ternary_function<TEST_EXECSPACE, kk3_hypot>(2.l, 3.l, 4.l);
+#endif
+}
+
+TEST(TEST_CATEGORY, mathematical_functions_fma) {
+  do_test_math_ternary_function<TEST_EXECSPACE, kk3_fma>(2.f, 3.f, 4.f);
+  do_test_math_ternary_function<TEST_EXECSPACE, kk3_fma>(2., 3., 4.);
+  do_test_math_ternary_function<TEST_EXECSPACE, kk3_fma>(2, 3.f, 4.);
+#ifdef MATHEMATICAL_FUNCTIONS_HAVE_LONG_DOUBLE_OVERLOADS
+  do_test_math_ternary_function<TEST_EXECSPACE, kk3_fma>(2.l, 3.l, 4.l);
 #endif
 }
 
