3a60b0ab2f2840f116055b0d66157048246f4150
Jakub Strzebonski <jakub.strzebonski@ng-analytics.com>
2022-09-09 12:14:10 +0200

    Implement all_of, any_of, none_of

--- algorithms/src/std_algorithms/Kokkos_AllOf.hpp
+++ algorithms/src/std_algorithms/Kokkos_AllOf.hpp
@@ -23,41 +23,74 @@
 namespace Kokkos {
 namespace Experimental {
 
+//
+// overload set accepting execution space
+//
 template <class ExecutionSpace, class InputIterator, class Predicate>
-bool all_of(const ExecutionSpace& ex, InputIterator first, InputIterator last,
-            Predicate predicate) {
-  return Impl::all_of_impl("Kokkos::all_of_iterator_api_default", ex, first,
-                           last, predicate);
+std::enable_if_t< ::Kokkos::is_execution_space<ExecutionSpace>::value, bool>
+all_of(const ExecutionSpace& ex, InputIterator first, InputIterator last,
+       Predicate predicate) {
+  return Impl::all_of_exespace_impl("Kokkos::all_of_iterator_api_default", ex,
+                                    first, last, predicate);
 }
 
 template <class ExecutionSpace, class InputIterator, class Predicate>
-bool all_of(const std::string& label, const ExecutionSpace& ex,
-            InputIterator first, InputIterator last, Predicate predicate) {
-  return Impl::all_of_impl(label, ex, first, last, predicate);
+std::enable_if_t< ::Kokkos::is_execution_space<ExecutionSpace>::value, bool>
+all_of(const std::string& label, const ExecutionSpace& ex, InputIterator first,
+       InputIterator last, Predicate predicate) {
+  return Impl::all_of_exespace_impl(label, ex, first, last, predicate);
 }
 
 template <class ExecutionSpace, class DataType, class... Properties,
           class Predicate>
-bool all_of(const ExecutionSpace& ex,
-            const ::Kokkos::View<DataType, Properties...>& v,
-            Predicate predicate) {
+std::enable_if_t< ::Kokkos::is_execution_space<ExecutionSpace>::value, bool>
+all_of(const ExecutionSpace& ex,
+       const ::Kokkos::View<DataType, Properties...>& v, Predicate predicate) {
   Impl::static_assert_is_admissible_to_kokkos_std_algorithms(v);
 
   namespace KE = ::Kokkos::Experimental;
-  return Impl::all_of_impl("Kokkos::all_of_view_api_default", ex, KE::cbegin(v),
-                           KE::cend(v), std::move(predicate));
+  return Impl::all_of_exespace_impl("Kokkos::all_of_view_api_default", ex,
+                                    KE::cbegin(v), KE::cend(v),
+                                    std::move(predicate));
 }
 
 template <class ExecutionSpace, class DataType, class... Properties,
           class Predicate>
-bool all_of(const std::string& label, const ExecutionSpace& ex,
-            const ::Kokkos::View<DataType, Properties...>& v,
-            Predicate predicate) {
+std::enable_if_t< ::Kokkos::is_execution_space<ExecutionSpace>::value, bool>
+all_of(const std::string& label, const ExecutionSpace& ex,
+       const ::Kokkos::View<DataType, Properties...>& v, Predicate predicate) {
   Impl::static_assert_is_admissible_to_kokkos_std_algorithms(v);
 
   namespace KE = ::Kokkos::Experimental;
-  return Impl::all_of_impl(label, ex, KE::cbegin(v), KE::cend(v),
-                           std::move(predicate));
+  return Impl::all_of_exespace_impl(label, ex, KE::cbegin(v), KE::cend(v),
+                                    std::move(predicate));
+}
+
+//
+// overload set accepting a team handle
+// Note: for now omit the overloads accepting a label
+// since they cause issues on device because of the string allocation.
+//
+template <class TeamHandleType, class InputIterator, class Predicate>
+KOKKOS_FUNCTION
+    std::enable_if_t<Impl::is_team_handle<TeamHandleType>::value, bool>
+    all_of(const TeamHandleType& teamHandle, InputIterator first,
+           InputIterator last, Predicate predicate) {
+  return Impl::all_of_team_impl(teamHandle, first, last, predicate);
+}
+
+template <class TeamHandleType, class DataType, class... Properties,
+          class Predicate>
+KOKKOS_FUNCTION
+    std::enable_if_t<Impl::is_team_handle<TeamHandleType>::value, bool>
+    all_of(const TeamHandleType& teamHandle,
+           const ::Kokkos::View<DataType, Properties...>& v,
+           Predicate predicate) {
+  Impl::static_assert_is_admissible_to_kokkos_std_algorithms(v);
+
+  namespace KE = ::Kokkos::Experimental;
+  return Impl::all_of_team_impl(teamHandle, KE::cbegin(v), KE::cend(v),
+                                std::move(predicate));
 }
 
 }  // namespace Experimental
---
+++ algorithms/unit_tests/TestStdAlgorithmsTeamAllOf.cpp
@@ -0,0 +1,178 @@
+/*
+//@HEADER
+// ************************************************************************
+//
+//                        Kokkos v. 3.0
+//       Copyright (2020) National Technology & Engineering
+//               Solutions of Sandia, LLC (NTESS).
+//
+// Under the terms of Contract DE-NA0003525 with NTESS,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY NTESS "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL NTESS OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact Christian R. Trott crtrott@sandia.gov)
+//
+// ************************************************************************
+//@HEADER
+*/
+
+#include <TestStdAlgorithmsCommon.hpp>
+#include <algorithm>
+#include <cstddef>
+#include <functional>
+#include <numeric>
+#include "gtest/gtest.h"
+#include "std_algorithms/Kokkos_BeginEnd.hpp"
+
+namespace Test {
+namespace stdalgos {
+namespace TestAllOf {
+
+namespace KE = Kokkos::Experimental;
+
+template <class ValueType>
+struct GreaterThanValueFunctor {
+  ValueType m_val;
+
+  KOKKOS_INLINE_FUNCTION
+  GreaterThanValueFunctor(ValueType val) : m_val(val) {}
+
+  KOKKOS_INLINE_FUNCTION
+  bool operator()(ValueType val) const { return (val > m_val); }
+};
+
+template <class SourceViewType, class DestViewType, class UnaryOp>
+struct TestFunctorA {
+  SourceViewType m_sourceView;
+  DestViewType m_destView;
+  int m_apiPick;
+  UnaryOp m_unaryOp;
+
+  TestFunctorA(const SourceViewType sourceView, const DestViewType destView,
+               int apiPick, UnaryOp unaryOp)
+      : m_sourceView(sourceView),
+        m_destView(destView),
+        m_apiPick(apiPick),
+        m_unaryOp(std::move(unaryOp)) {}
+
+  template <class MemberType>
+  KOKKOS_INLINE_FUNCTION void operator()(const MemberType& member) const {
+    const auto myRowIndex = member.league_rank();
+
+    auto myRowViewFrom =
+        Kokkos::subview(m_sourceView, myRowIndex, Kokkos::ALL());
+
+    switch (m_apiPick) {
+      case 0: {
+        const bool result = KE::all_of(member, KE::cbegin(myRowViewFrom),
+                                       KE::cend(myRowViewFrom), m_unaryOp);
+        Kokkos::single(Kokkos::PerTeam(member),
+                       [=]() { m_destView(myRowIndex) = result; });
+        break;
+      }
+
+      case 1: {
+        const bool result = KE::all_of(member, myRowViewFrom, m_unaryOp);
+        Kokkos::single(Kokkos::PerTeam(member),
+                       [=]() { m_destView(myRowIndex) = result; });
+        break;
+        break;
+      }
+    }
+  }
+};
+
+template <class LayoutTag, class ValueType>
+void test_A(std::size_t numTeams, std::size_t numCols, int apiId) {
+  /* description:
+     use a rank-2 view randomly filled with values,
+     and run a team-level all_of
+   */
+
+  // -----------------------------------------------
+  // prepare data
+  // -----------------------------------------------
+  // create a view in the memory space associated with default exespace
+  // with as many rows as the number of teams and fill it with random
+  // values from an arbitrary range.
+  constexpr auto lowerBound = ValueType{5};
+  constexpr auto upperBound = ValueType{523};
+  Kokkos::pair bounds{lowerBound, upperBound};
+  auto [sourceView, sourceViewBeforeOp_h] = create_random_view_and_host_clone(
+      LayoutTag{}, numTeams, numCols, bounds, "sourceView");
+
+  // -----------------------------------------------
+  // launch kokkos kernel
+  // -----------------------------------------------
+  using space_t = Kokkos::DefaultExecutionSpace;
+  Kokkos::TeamPolicy<space_t> policy(numTeams, Kokkos::AUTO());
+
+  // create the destination view
+  Kokkos::View<bool*> destView("destView", numTeams);
+
+  GreaterThanValueFunctor unaryPred{lowerBound - 1};
+
+  // use CTAD for functor
+  TestFunctorA fnc(sourceView, destView, apiId, unaryPred);
+  Kokkos::parallel_for(policy, fnc);
+
+  // -----------------------------------------------
+  // run cpp-std kernel and check
+  // -----------------------------------------------
+  auto destView_h = create_host_space_copy(destView);
+
+  for (std::size_t i = 0; i < sourceView.extent(0); ++i) {
+    auto rowFrom = Kokkos::subview(sourceViewBeforeOp_h, i, Kokkos::ALL());
+    const bool result =
+        std::all_of(KE::begin(rowFrom), KE::end(rowFrom), unaryPred);
+    EXPECT_EQ(result, destView_h(i));
+    break;
+  }
+}
+
+template <class LayoutTag, class ValueType>
+void run_all_scenarios() {
+  for (int numTeams : teamSizesToTest) {
+    for (const auto& numCols : {0, 1, 2, 13, 101, 1444, 8153}) {
+      for (int apiId : {0, 1}) {
+        test_A<LayoutTag, ValueType>(numTeams, numCols, apiId);
+      }
+    }
+  }
+}
+
+TEST(std_algorithms_all_of_team_test, test) {
+  run_all_scenarios<DynamicTag, double>();
+  run_all_scenarios<StridedTwoRowsTag, int>();
+  run_all_scenarios<StridedThreeRowsTag, unsigned>();
+}
+
+}  // namespace TestAllOf
+}  // namespace stdalgos
+}  // namespace Test
