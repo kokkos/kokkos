2920b4ab269dae372e2bb9e7f6520fdb64b938fb
Daniel Arndt <arndtd@ornl.gov>
2022-09-12 07:38:47 -0600

    Remove ok_id

--- core/src/HIP/Kokkos_HIP.cpp
+++ core/src/HIP/Kokkos_HIP.cpp
@@ -71,50 +71,41 @@ void HIP::impl_initialize(InitializationSettings const& settings) {
   // Need at least a GPU device
   int hipDevCount;
   KOKKOS_IMPL_HIP_SAFE_CALL(hipGetDeviceCount(&hipDevCount));
-  const bool ok_id = 0 <= hip_device_id && hip_device_id < hipDevCount;
 
-  if (ok_id) {
-    Impl::HIPInternal::m_hipDev = hip_device_id;
-    KOKKOS_IMPL_HIP_SAFE_CALL(hipGetDeviceProperties(
-        &Impl::HIPInternal::m_deviceProp, hip_device_id));
-    const auto& hipProp = Impl::HIPInternal::m_deviceProp;
-    KOKKOS_IMPL_HIP_SAFE_CALL(hipSetDevice(hip_device_id));
+  Impl::HIPInternal::m_hipDev = hip_device_id;
+  KOKKOS_IMPL_HIP_SAFE_CALL(
+      hipGetDeviceProperties(&Impl::HIPInternal::m_deviceProp, hip_device_id));
+  const auto& hipProp = Impl::HIPInternal::m_deviceProp;
+  KOKKOS_IMPL_HIP_SAFE_CALL(hipSetDevice(hip_device_id));
 
-    // number of multiprocessors
-    Impl::HIPInternal::m_multiProcCount = hipProp.multiProcessorCount;
+  // number of multiprocessors
+  Impl::HIPInternal::m_multiProcCount = hipProp.multiProcessorCount;
 
-    //----------------------------------
-    // Maximum number of warps,
-    // at most one warp per thread in a warp for reduction.
-    Impl::HIPInternal::m_maxWarpCount =
-        hipProp.maxThreadsPerBlock / Impl::HIPTraits::WarpSize;
-    if (Impl::HIPTraits::WarpSize < Impl::HIPInternal::m_maxWarpCount) {
-      Impl::HIPInternal::m_maxWarpCount = Impl::HIPTraits::WarpSize;
-    }
-    int constexpr WordSize              = sizeof(size_type);
-    Impl::HIPInternal::m_maxSharedWords = hipProp.sharedMemPerBlock / WordSize;
-
-    //----------------------------------
-    // Maximum number of blocks
-    Impl::HIPInternal::m_maxBlock[0] = hipProp.maxGridSize[0];
-    Impl::HIPInternal::m_maxBlock[1] = hipProp.maxGridSize[1];
-    Impl::HIPInternal::m_maxBlock[2] = hipProp.maxGridSize[2];
-
-    // theoretically, we can get 40 WF's / CU, but only can sustain 32
-    // see
-    // https://github.com/ROCm-Developer-Tools/HIP/blob/a0b5dfd625d99af7e288629747b40dd057183173/vdi/hip_platform.cpp#L742
-    Impl::HIPInternal::m_maxWavesPerCU = 32;
-    Impl::HIPInternal::m_shmemPerSM = hipProp.maxSharedMemoryPerMultiProcessor;
-    Impl::HIPInternal::m_maxShmemPerBlock = hipProp.sharedMemPerBlock;
-    Impl::HIPInternal::m_maxThreadsPerSM =
-        Impl::HIPInternal::m_maxWavesPerCU * Impl::HIPTraits::WarpSize;
-  } else {
-    std::ostringstream msg;
-    msg << "Kokkos::HIP::initialize(" << hip_device_id
-        << ") FAILED : Device identifier out of range "
-        << "[0.." << hipDevCount - 1 << "]";
-    Kokkos::Impl::throw_runtime_exception(msg.str());
+  //----------------------------------
+  // Maximum number of warps,
+  // at most one warp per thread in a warp for reduction.
+  Impl::HIPInternal::m_maxWarpCount =
+      hipProp.maxThreadsPerBlock / Impl::HIPTraits::WarpSize;
+  if (Impl::HIPTraits::WarpSize < Impl::HIPInternal::m_maxWarpCount) {
+    Impl::HIPInternal::m_maxWarpCount = Impl::HIPTraits::WarpSize;
   }
+  int constexpr WordSize              = sizeof(size_type);
+  Impl::HIPInternal::m_maxSharedWords = hipProp.sharedMemPerBlock / WordSize;
+
+  //----------------------------------
+  // Maximum number of blocks
+  Impl::HIPInternal::m_maxBlock[0] = hipProp.maxGridSize[0];
+  Impl::HIPInternal::m_maxBlock[1] = hipProp.maxGridSize[1];
+  Impl::HIPInternal::m_maxBlock[2] = hipProp.maxGridSize[2];
+
+  // theoretically, we can get 40 WF's / CU, but only can sustain 32
+  // see
+  // https://github.com/ROCm-Developer-Tools/HIP/blob/a0b5dfd625d99af7e288629747b40dd057183173/vdi/hip_platform.cpp#L742
+  Impl::HIPInternal::m_maxWavesPerCU = 32;
+  Impl::HIPInternal::m_shmemPerSM    = hipProp.maxSharedMemoryPerMultiProcessor;
+  Impl::HIPInternal::m_maxShmemPerBlock = hipProp.sharedMemPerBlock;
+  Impl::HIPInternal::m_maxThreadsPerSM =
+      Impl::HIPInternal::m_maxWavesPerCU * Impl::HIPTraits::WarpSize;
 
   // Init the array for used for arbitrarily sized atomics
   initialize_host_hip_lock_arrays();
