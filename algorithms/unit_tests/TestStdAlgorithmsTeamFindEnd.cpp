a3a910894aa6f5e360d59b7492caea402450a8d5
Jakub Strzebonski <jakub.strzebonski@ng-analytics.com>
2022-09-15 12:11:09 +0200

    Rework find_end test

--- algorithms/unit_tests/TestStdAlgorithmsTeamFindEnd.cpp
+++ algorithms/unit_tests/TestStdAlgorithmsTeamFindEnd.cpp
@@ -58,20 +58,21 @@ struct EqualFunctor {
   }
 };
 
-template <class DataViewType, class SearchedViewType, class DistancesViewType,
-          class BinaryOpType>
+template <class DataViewType, class SearchedSequncesViewType,
+          class DistancesViewType, class BinaryOpType>
 struct TestFunctorA {
   DataViewType m_dataView;
-  SearchedViewType m_searchedView;
+  SearchedSequncesViewType m_searchedSequncesView;
   DistancesViewType m_distancesView;
   BinaryOpType m_binaryOp;
   int m_apiPick;
 
-  TestFunctorA(const DataViewType dataView, const SearchedViewType searchedView,
+  TestFunctorA(const DataViewType dataView,
+               const SearchedSequncesViewType searchedSequncesView,
                const DistancesViewType distancesView, BinaryOpType binaryOp,
                int apiPick)
       : m_dataView(dataView),
-        m_searchedView(searchedView),
+        m_searchedSequncesView(searchedSequncesView),
         m_distancesView(distancesView),
         m_binaryOp(std::move(binaryOp)),
         m_apiPick(apiPick) {}
@@ -80,14 +81,14 @@ struct TestFunctorA {
   KOKKOS_INLINE_FUNCTION void operator()(const MemberType& member) const {
     const auto myRowIndex = member.league_rank();
     auto myRowViewFrom = Kokkos::subview(m_dataView, myRowIndex, Kokkos::ALL());
-    auto myRowSearchedView =
-        Kokkos::subview(m_searchedView, myRowIndex, Kokkos::ALL());
+    auto myRowSearchedSeqView =
+        Kokkos::subview(m_searchedSequncesView, myRowIndex, Kokkos::ALL());
 
     switch (m_apiPick) {
       case 0: {
         auto it = KE::find_end(
             member, KE::cbegin(myRowViewFrom), KE::cend(myRowViewFrom),
-            KE::cbegin(myRowSearchedView), KE::cend(myRowSearchedView));
+            KE::cbegin(myRowSearchedSeqView), KE::cend(myRowSearchedSeqView));
         Kokkos::single(Kokkos::PerTeam(member), [=]() {
           m_distancesView(myRowIndex) =
               KE::distance(KE::cbegin(myRowViewFrom), it);
@@ -97,7 +98,7 @@ struct TestFunctorA {
       }
 
       case 1: {
-        auto it = KE::find_end(member, myRowViewFrom, myRowSearchedView);
+        auto it = KE::find_end(member, myRowViewFrom, myRowSearchedSeqView);
         Kokkos::single(Kokkos::PerTeam(member), [=]() {
           m_distancesView(myRowIndex) =
               KE::distance(KE::begin(myRowViewFrom), it);
@@ -107,10 +108,10 @@ struct TestFunctorA {
       }
 
       case 2: {
-        auto it =
-            KE::find_end(member, KE::cbegin(myRowViewFrom),
-                         KE::cend(myRowViewFrom), KE::cbegin(myRowSearchedView),
-                         KE::cend(myRowSearchedView), m_binaryOp);
+        auto it = KE::find_end(member, KE::cbegin(myRowViewFrom),
+                               KE::cend(myRowViewFrom),
+                               KE::cbegin(myRowSearchedSeqView),
+                               KE::cend(myRowSearchedSeqView), m_binaryOp);
         Kokkos::single(Kokkos::PerTeam(member), [=]() {
           m_distancesView(myRowIndex) =
               KE::distance(KE::cbegin(myRowViewFrom), it);
@@ -120,8 +121,8 @@ struct TestFunctorA {
       }
 
       case 3: {
-        auto it =
-            KE::find_end(member, myRowViewFrom, myRowSearchedView, m_binaryOp);
+        auto it = KE::find_end(member, myRowViewFrom, myRowSearchedSeqView,
+                               m_binaryOp);
         Kokkos::single(Kokkos::PerTeam(member), [=]() {
           m_distancesView(myRowIndex) =
               KE::distance(KE::begin(myRowViewFrom), it);
@@ -134,7 +135,8 @@ struct TestFunctorA {
 };
 
 template <class LayoutTag, class ValueType>
-void test_A(std::size_t numTeams, std::size_t numCols, int apiId) {
+void test_A(const bool sequencesExist, std::size_t numTeams,
+            std::size_t numCols, int apiId) {
   /* description:
      use a rank-2 view randomly filled with values,
      and run a team-level find_end
@@ -146,26 +148,37 @@ void test_A(std::size_t numTeams, std::size_t numCols, int apiId) {
   // create a view in the memory space associated with default exespace
   // with as many rows as the number of teams and fill it with random
   // values from an arbitrary range.
+  const ValueType lowerBound{5}, upperBound{523};
   auto [dataView, dataViewBeforeOp_h] = create_random_view_and_host_clone(
-      LayoutTag{}, numTeams, numCols,
-      Kokkos::pair{ValueType(5), ValueType(523)}, "dataView");
+      LayoutTag{}, numTeams, numCols, Kokkos::pair{lowerBound, upperBound},
+      "dataView");
 
-  // create a view that stores a sequence to found in dataView. It is filled
-  // base on dataView context, to allow find_end to actually find anything
-  Kokkos::View<ValueType**> searchedView("destView", numTeams, numCols / 4);
-  auto searchedView_h = create_host_space_copy(searchedView);
+  // create a view that stores a sequence to found in dataView. If
+  // sequencesExist == true it is filled base on dataView context, to allow
+  // find_end to actually find anything. If sequencesExist == false it is filled
+  // with random values greater than upperBound
+  const auto halfCols    = (numCols + 1) / 2;
+  const auto quarterCols = halfCols / 2;
 
-  for (std::size_t i = 0; i < searchedView_h.extent(0); ++i) {
-    for (std::size_t j = 0; j < searchedView_h.extent(1); ++j) {
-      if (dataViewBeforeOp_h.extent(1) >= searchedView_h.extent(1) + 4) {
-        searchedView_h(i, j) = dataViewBeforeOp_h(i, j + 4);
-      } else {
-        searchedView_h(i, j) = dataViewBeforeOp_h(i, j);
+  Kokkos::View<ValueType**> searchedSequncesView(
+      "searchedSequncesView", numTeams, halfCols - quarterCols);
+  auto searchedSequncesView_h = create_host_space_copy(searchedSequncesView);
+
+  if (sequencesExist) {
+    for (std::size_t i = 0; i < searchedSequncesView_h.extent(0); ++i) {
+      for (std::size_t js = 0, jd = quarterCols; jd < halfCols; ++js, ++jd) {
+        searchedSequncesView_h(i, js) = dataViewBeforeOp_h(i, jd);
       }
     }
+  } else {
+    using rand_pool =
+        Kokkos::Random_XorShift64_Pool<Kokkos::DefaultHostExecutionSpace>;
+    rand_pool pool(lowerBound * upperBound);
+    Kokkos::fill_random(searchedSequncesView_h, pool, upperBound,
+                        upperBound * 2);
   }
 
-  Kokkos::deep_copy(searchedView, searchedView_h);
+  Kokkos::deep_copy(searchedSequncesView, searchedSequncesView_h);
 
   // -----------------------------------------------
   // launch kokkos kernel
@@ -182,7 +195,8 @@ void test_A(std::size_t numTeams, std::size_t numCols, int apiId) {
   EqualFunctor<ValueType> binaryOp;
 
   // use CTAD for functor
-  TestFunctorA fnc(dataView, searchedView, distancesView, binaryOp, apiId);
+  TestFunctorA fnc(dataView, searchedSequncesView, distancesView, binaryOp,
+                   apiId);
   Kokkos::parallel_for(policy, fnc);
 
   // -----------------------------------------------
@@ -191,49 +205,73 @@ void test_A(std::size_t numTeams, std::size_t numCols, int apiId) {
   auto distancesView_h = create_host_space_copy(distancesView);
 
   for (std::size_t i = 0; i < dataView.extent(0); ++i) {
-    auto rowFrom     = Kokkos::subview(dataViewBeforeOp_h, i, Kokkos::ALL());
-    auto rowSearched = Kokkos::subview(searchedView_h, i, Kokkos::ALL());
+    auto rowFrom = Kokkos::subview(dataViewBeforeOp_h, i, Kokkos::ALL());
+    const auto rowFromBegin = KE::begin(rowFrom);
+    const auto rowFromEnd   = KE::end(rowFrom);
+
+    auto rowSearchedSeq =
+        Kokkos::subview(searchedSequncesView_h, i, Kokkos::ALL());
+
+    std::size_t stdDistance;
+    const std::size_t beginEndDistance = KE::distance(rowFromBegin, rowFromEnd);
 
     switch (apiId) {
       case 0:
       case 1: {
-        auto it = std::find_end(KE::begin(rowFrom), KE::end(rowFrom),
-                                KE::begin(rowSearched), KE::end(rowSearched));
-        const std::size_t stdDistance = KE::distance(KE::begin(rowFrom), it);
-        EXPECT_EQ(stdDistance, distancesView_h(i));
+        auto it =
+            std::find_end(rowFromBegin, rowFromEnd, KE::begin(rowSearchedSeq),
+                          KE::end(rowSearchedSeq));
+        stdDistance = KE::distance(rowFromBegin, it);
 
         break;
       }
 
       case 2:
       case 3: {
-        auto it = std::find_end(KE::begin(rowFrom), KE::end(rowFrom),
-                                KE::begin(rowSearched), KE::end(rowSearched),
-                                binaryOp);
-        const std::size_t stdDistance = KE::distance(KE::begin(rowFrom), it);
-        EXPECT_EQ(stdDistance, distancesView_h(i));
+        auto it =
+            std::find_end(rowFromBegin, rowFromEnd, KE::begin(rowSearchedSeq),
+                          KE::end(rowSearchedSeq), binaryOp);
+        stdDistance = KE::distance(rowFromBegin, it);
 
         break;
       }
     }
+
+    if (sequencesExist) {
+      EXPECT_LT(stdDistance, beginEndDistance);
+    } else {
+      EXPECT_EQ(stdDistance, beginEndDistance);
+    }
+
+    EXPECT_EQ(stdDistance, distancesView_h(i));
   }
 }
 
 template <class LayoutTag, class ValueType>
-void run_all_scenarios() {
+void run_all_scenarios(const bool sequencesExist) {
   for (int numTeams : teamSizesToTest) {
-    for (const auto& numCols : {0, 1, 2, 13, 101, 1444, 8153}) {
+    for (const auto& numCols : {1, 2, 13, 101, 1444, 8153}) {
       for (int apiId : {0, 1, 2, 3}) {
-        test_A<LayoutTag, ValueType>(numTeams, numCols, apiId);
+        test_A<LayoutTag, ValueType>(sequencesExist, numTeams, numCols, apiId);
       }
     }
   }
 }
 
-TEST(std_algorithms_find_end_team_test, test) {
-  run_all_scenarios<DynamicTag, double>();
-  run_all_scenarios<StridedTwoRowsTag, int>();
-  run_all_scenarios<StridedThreeRowsTag, unsigned>();
+TEST(std_algorithms_find_end_team_test, sequences_exist) {
+  constexpr bool sequencesExist = true;
+
+  run_all_scenarios<DynamicTag, double>(sequencesExist);
+  run_all_scenarios<StridedTwoRowsTag, int>(sequencesExist);
+  run_all_scenarios<StridedThreeRowsTag, unsigned>(sequencesExist);
+}
+
+TEST(std_algorithms_find_end_team_test, sequences_do_not_exist) {
+  constexpr bool sequencesExist = false;
+
+  run_all_scenarios<DynamicTag, double>(sequencesExist);
+  run_all_scenarios<StridedTwoRowsTag, int>(sequencesExist);
+  run_all_scenarios<StridedThreeRowsTag, unsigned>(sequencesExist);
 }
 
 }  // namespace TeamFindEnd
