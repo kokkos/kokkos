c911d41bd118db62ab461f4829ae1d9a7b7a0c89
Jakub Strzebonski <jakub.strzebonski@ng-analytics.com>
2022-09-16 07:32:45 +0200

    Implement search

--- algorithms/src/std_algorithms/Kokkos_Search.hpp
+++ algorithms/src/std_algorithms/Kokkos_Search.hpp
@@ -23,24 +23,32 @@
 namespace Kokkos {
 namespace Experimental {
 
+//
+// overload set accepting execution space
+//
+
 // overload set 1: no binary predicate passed
 template <class ExecutionSpace, class IteratorType1, class IteratorType2>
-IteratorType1 search(const ExecutionSpace& ex, IteratorType1 first,
-                     IteratorType1 last, IteratorType2 s_first,
-                     IteratorType2 s_last) {
-  return Impl::search_impl("Kokkos::search_iterator_api_default", ex, first,
-                           last, s_first, s_last);
+std::enable_if_t< ::Kokkos::is_execution_space<ExecutionSpace>::value,
+                  IteratorType1>
+search(const ExecutionSpace& ex, IteratorType1 first, IteratorType1 last,
+       IteratorType2 s_first, IteratorType2 s_last) {
+  return Impl::search_exespace_impl("Kokkos::search_iterator_api_default", ex,
+                                    first, last, s_first, s_last);
 }
 
 template <class ExecutionSpace, class IteratorType1, class IteratorType2>
-IteratorType1 search(const std::string& label, const ExecutionSpace& ex,
-                     IteratorType1 first, IteratorType1 last,
-                     IteratorType2 s_first, IteratorType2 s_last) {
-  return Impl::search_impl(label, ex, first, last, s_first, s_last);
+std::enable_if_t< ::Kokkos::is_execution_space<ExecutionSpace>::value,
+                  IteratorType1>
+search(const std::string& label, const ExecutionSpace& ex, IteratorType1 first,
+       IteratorType1 last, IteratorType2 s_first, IteratorType2 s_last) {
+  return Impl::search_exespace_impl(label, ex, first, last, s_first, s_last);
 }
 
 template <class ExecutionSpace, class DataType1, class... Properties1,
-          class DataType2, class... Properties2>
+          class DataType2, class... Properties2,
+          std::enable_if_t< ::Kokkos::is_execution_space<ExecutionSpace>::value,
+                            int> = 0>
 auto search(const ExecutionSpace& ex,
             const ::Kokkos::View<DataType1, Properties1...>& view,
             const ::Kokkos::View<DataType2, Properties2...>& s_view) {
@@ -48,13 +56,15 @@ auto search(const ExecutionSpace& ex,
   Impl::static_assert_is_admissible_to_kokkos_std_algorithms(s_view);
 
   namespace KE = ::Kokkos::Experimental;
-  return Impl::search_impl("Kokkos::search_view_api_default", ex,
-                           KE::begin(view), KE::end(view), KE::begin(s_view),
-                           KE::end(s_view));
+  return Impl::search_exespace_impl("Kokkos::search_view_api_default", ex,
+                                    KE::begin(view), KE::end(view),
+                                    KE::begin(s_view), KE::end(s_view));
 }
 
 template <class ExecutionSpace, class DataType1, class... Properties1,
-          class DataType2, class... Properties2>
+          class DataType2, class... Properties2,
+          std::enable_if_t< ::Kokkos::is_execution_space<ExecutionSpace>::value,
+                            int> = 0>
 auto search(const std::string& label, const ExecutionSpace& ex,
             const ::Kokkos::View<DataType1, Properties1...>& view,
             const ::Kokkos::View<DataType2, Properties2...>& s_view) {
@@ -62,31 +72,37 @@ auto search(const std::string& label, const ExecutionSpace& ex,
   Impl::static_assert_is_admissible_to_kokkos_std_algorithms(s_view);
 
   namespace KE = ::Kokkos::Experimental;
-  return Impl::search_impl(label, ex, KE::begin(view), KE::end(view),
-                           KE::begin(s_view), KE::end(s_view));
+  return Impl::search_exespace_impl(label, ex, KE::begin(view), KE::end(view),
+                                    KE::begin(s_view), KE::end(s_view));
 }
 
 // overload set 2: binary predicate passed
 template <class ExecutionSpace, class IteratorType1, class IteratorType2,
           class BinaryPredicateType>
-IteratorType1 search(const ExecutionSpace& ex, IteratorType1 first,
-                     IteratorType1 last, IteratorType2 s_first,
-                     IteratorType2 s_last, const BinaryPredicateType& pred) {
-  return Impl::search_impl("Kokkos::search_iterator_api_default", ex, first,
-                           last, s_first, s_last, pred);
+std::enable_if_t< ::Kokkos::is_execution_space<ExecutionSpace>::value,
+                  IteratorType1>
+search(const ExecutionSpace& ex, IteratorType1 first, IteratorType1 last,
+       IteratorType2 s_first, IteratorType2 s_last,
+       const BinaryPredicateType& pred) {
+  return Impl::search_exespace_impl("Kokkos::search_iterator_api_default", ex,
+                                    first, last, s_first, s_last, pred);
 }
 
 template <class ExecutionSpace, class IteratorType1, class IteratorType2,
           class BinaryPredicateType>
-IteratorType1 search(const std::string& label, const ExecutionSpace& ex,
-                     IteratorType1 first, IteratorType1 last,
-                     IteratorType2 s_first, IteratorType2 s_last,
-                     const BinaryPredicateType& pred) {
-  return Impl::search_impl(label, ex, first, last, s_first, s_last, pred);
+std::enable_if_t< ::Kokkos::is_execution_space<ExecutionSpace>::value,
+                  IteratorType1>
+search(const std::string& label, const ExecutionSpace& ex, IteratorType1 first,
+       IteratorType1 last, IteratorType2 s_first, IteratorType2 s_last,
+       const BinaryPredicateType& pred) {
+  return Impl::search_exespace_impl(label, ex, first, last, s_first, s_last,
+                                    pred);
 }
 
 template <class ExecutionSpace, class DataType1, class... Properties1,
-          class DataType2, class... Properties2, class BinaryPredicateType>
+          class DataType2, class... Properties2, class BinaryPredicateType,
+          std::enable_if_t< ::Kokkos::is_execution_space<ExecutionSpace>::value,
+                            int> = 0>
 auto search(const ExecutionSpace& ex,
             const ::Kokkos::View<DataType1, Properties1...>& view,
             const ::Kokkos::View<DataType2, Properties2...>& s_view,
@@ -95,13 +111,15 @@ auto search(const ExecutionSpace& ex,
   Impl::static_assert_is_admissible_to_kokkos_std_algorithms(s_view);
 
   namespace KE = ::Kokkos::Experimental;
-  return Impl::search_impl("Kokkos::search_view_api_default", ex,
-                           KE::begin(view), KE::end(view), KE::begin(s_view),
-                           KE::end(s_view), pred);
+  return Impl::search_exespace_impl("Kokkos::search_view_api_default", ex,
+                                    KE::begin(view), KE::end(view),
+                                    KE::begin(s_view), KE::end(s_view), pred);
 }
 
 template <class ExecutionSpace, class DataType1, class... Properties1,
-          class DataType2, class... Properties2, class BinaryPredicateType>
+          class DataType2, class... Properties2, class BinaryPredicateType,
+          std::enable_if_t< ::Kokkos::is_execution_space<ExecutionSpace>::value,
+                            int> = 0>
 auto search(const std::string& label, const ExecutionSpace& ex,
             const ::Kokkos::View<DataType1, Properties1...>& view,
             const ::Kokkos::View<DataType2, Properties2...>& s_view,
@@ -110,8 +128,67 @@ auto search(const std::string& label, const ExecutionSpace& ex,
   Impl::static_assert_is_admissible_to_kokkos_std_algorithms(s_view);
 
   namespace KE = ::Kokkos::Experimental;
-  return Impl::search_impl(label, ex, KE::begin(view), KE::end(view),
-                           KE::begin(s_view), KE::end(s_view), pred);
+  return Impl::search_exespace_impl(label, ex, KE::begin(view), KE::end(view),
+                                    KE::begin(s_view), KE::end(s_view), pred);
+}
+
+//
+// overload set accepting a team handle
+// Note: for now omit the overloads accepting a label
+// since they cause issues on device because of the string allocation.
+//
+
+// overload set 1: no binary predicate passed
+template <class TeamHandleType, class IteratorType1, class IteratorType2>
+KOKKOS_FUNCTION
+    std::enable_if_t<Impl::is_team_handle<TeamHandleType>::value, IteratorType1>
+    search(const TeamHandleType& teamHandle, IteratorType1 first,
+           IteratorType1 last, IteratorType2 s_first, IteratorType2 s_last) {
+  return Impl::search_team_impl(teamHandle, first, last, s_first, s_last);
+}
+
+template <
+    class TeamHandleType, class DataType1, class... Properties1,
+    class DataType2, class... Properties2,
+    std::enable_if_t<Impl::is_team_handle<TeamHandleType>::value, int> = 0>
+KOKKOS_FUNCTION auto search(
+    const TeamHandleType& teamHandle,
+    const ::Kokkos::View<DataType1, Properties1...>& view,
+    const ::Kokkos::View<DataType2, Properties2...>& s_view) {
+  Impl::static_assert_is_admissible_to_kokkos_std_algorithms(view);
+  Impl::static_assert_is_admissible_to_kokkos_std_algorithms(s_view);
+
+  namespace KE = ::Kokkos::Experimental;
+  return Impl::search_team_impl(teamHandle, KE::begin(view), KE::end(view),
+                                KE::begin(s_view), KE::end(s_view));
+}
+
+// overload set 2: binary predicate passed
+template <class TeamHandleType, class IteratorType1, class IteratorType2,
+          class BinaryPredicateType>
+KOKKOS_FUNCTION
+    std::enable_if_t<Impl::is_team_handle<TeamHandleType>::value, IteratorType1>
+    search(const TeamHandleType& teamHandle, IteratorType1 first,
+           IteratorType1 last, IteratorType2 s_first, IteratorType2 s_last,
+           const BinaryPredicateType& pred) {
+  return Impl::search_team_impl(teamHandle, first, last, s_first, s_last, pred);
+}
+
+template <
+    class TeamHandleType, class DataType1, class... Properties1,
+    class DataType2, class... Properties2, class BinaryPredicateType,
+    std::enable_if_t<Impl::is_team_handle<TeamHandleType>::value, int> = 0>
+KOKKOS_FUNCTION auto search(
+    const TeamHandleType& teamHandle,
+    const ::Kokkos::View<DataType1, Properties1...>& view,
+    const ::Kokkos::View<DataType2, Properties2...>& s_view,
+    const BinaryPredicateType& pred) {
+  Impl::static_assert_is_admissible_to_kokkos_std_algorithms(view);
+  Impl::static_assert_is_admissible_to_kokkos_std_algorithms(s_view);
+
+  namespace KE = ::Kokkos::Experimental;
+  return Impl::search_team_impl(teamHandle, KE::begin(view), KE::end(view),
+                                KE::begin(s_view), KE::end(s_view), pred);
 }
 
 }  // namespace Experimental
---
+++ algorithms/unit_tests/TestStdAlgorithmsTeamSearch.cpp
@@ -0,0 +1,290 @@
+/*
+//@HEADER
+// ************************************************************************
+//
+//                        Kokkos v. 3.0
+//       Copyright (2020) National Technology & Engineering
+//               Solutions of Sandia, LLC (NTESS).
+//
+// Under the terms of Contract DE-NA0003525 with NTESS,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY NTESS "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL NTESS OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact Christian R. Trott (crtrott@sandia.gov)
+//
+// ************************************************************************
+//@HEADER
+*/
+
+#include <TestStdAlgorithmsCommon.hpp>
+
+#include <algorithm>
+#include <cmath>
+
+namespace Test {
+namespace stdalgos {
+namespace TeamSearch {
+
+namespace KE = Kokkos::Experimental;
+
+template <class ValueType>
+struct EqualFunctor {
+  KOKKOS_INLINE_FUNCTION
+  bool operator()(const ValueType& lhs, const ValueType& rhs) const {
+    return lhs == rhs;
+  }
+};
+
+template <class DataViewType, class SearchedSequencesViewType,
+          class DistancesViewType, class BinaryOpType>
+struct TestFunctorA {
+  DataViewType m_dataView;
+  SearchedSequencesViewType m_searchedSequencesView;
+  DistancesViewType m_distancesView;
+  BinaryOpType m_binaryOp;
+  int m_apiPick;
+
+  TestFunctorA(const DataViewType dataView,
+               const SearchedSequencesViewType searchedSequencesView,
+               const DistancesViewType distancesView, BinaryOpType binaryOp,
+               int apiPick)
+      : m_dataView(dataView),
+        m_searchedSequencesView(searchedSequencesView),
+        m_distancesView(distancesView),
+        m_binaryOp(std::move(binaryOp)),
+        m_apiPick(apiPick) {}
+
+  template <class MemberType>
+  KOKKOS_INLINE_FUNCTION void operator()(const MemberType& member) const {
+    const auto myRowIndex = member.league_rank();
+    auto myRowViewFrom = Kokkos::subview(m_dataView, myRowIndex, Kokkos::ALL());
+    auto myRowSearchedSeqView =
+        Kokkos::subview(m_searchedSequencesView, myRowIndex, Kokkos::ALL());
+
+    switch (m_apiPick) {
+      case 0: {
+        auto it = KE::search(
+            member, KE::cbegin(myRowViewFrom), KE::cend(myRowViewFrom),
+            KE::cbegin(myRowSearchedSeqView), KE::cend(myRowSearchedSeqView));
+        Kokkos::single(Kokkos::PerTeam(member), [=]() {
+          m_distancesView(myRowIndex) =
+              KE::distance(KE::cbegin(myRowViewFrom), it);
+        });
+
+        break;
+      }
+
+      case 1: {
+        auto it = KE::search(member, myRowViewFrom, myRowSearchedSeqView);
+        Kokkos::single(Kokkos::PerTeam(member), [=]() {
+          m_distancesView(myRowIndex) =
+              KE::distance(KE::begin(myRowViewFrom), it);
+        });
+
+        break;
+      }
+
+      case 2: {
+        auto it = KE::search(member, KE::cbegin(myRowViewFrom),
+                             KE::cend(myRowViewFrom),
+                             KE::cbegin(myRowSearchedSeqView),
+                             KE::cend(myRowSearchedSeqView), m_binaryOp);
+        Kokkos::single(Kokkos::PerTeam(member), [=]() {
+          m_distancesView(myRowIndex) =
+              KE::distance(KE::cbegin(myRowViewFrom), it);
+        });
+
+        break;
+      }
+
+      case 3: {
+        auto it =
+            KE::search(member, myRowViewFrom, myRowSearchedSeqView, m_binaryOp);
+        Kokkos::single(Kokkos::PerTeam(member), [=]() {
+          m_distancesView(myRowIndex) =
+              KE::distance(KE::begin(myRowViewFrom), it);
+        });
+
+        break;
+      }
+    }
+  }
+};
+
+template <class LayoutTag, class ValueType>
+void test_A(const bool sequencesExist, std::size_t numTeams,
+            std::size_t numCols, int apiId) {
+  /* description:
+     use a rank-2 view randomly filled with values,
+     and run a team-level search
+   */
+
+  // -----------------------------------------------
+  // prepare data
+  // -----------------------------------------------
+  // create a view in the memory space associated with default exespace
+  // with as many rows as the number of teams and fill it with random
+  // values from an arbitrary range.
+  const ValueType lowerBound{5}, upperBound{523};
+  auto [dataView, dataViewBeforeOp_h] = create_random_view_and_host_clone(
+      LayoutTag{}, numTeams, numCols, Kokkos::pair{lowerBound, upperBound},
+      "dataView");
+
+  // create a view that stores a sequence to found in dataView. If
+  // sequencesExist == true it is filled base on dataView content, to allow
+  // search to actually find anything. If sequencesExist == false it is filled
+  // with random values greater than upperBound
+  const std::size_t halfCols = (numCols > 1) ? ((numCols + 1) / 2) : (1);
+  const std::size_t seqSize  = (numCols > 1) ? (std::log2(numCols)) : (1);
+
+  Kokkos::View<ValueType**> searchedSequncesView("searchedSequncesView",
+                                                 numTeams, seqSize);
+  auto searchedSequncesView_h = create_host_space_copy(searchedSequncesView);
+
+  if (sequencesExist) {
+    const std::size_t dataBegin = halfCols - seqSize;
+    for (std::size_t i = 0; i < searchedSequncesView_h.extent(0); ++i) {
+      for (std::size_t js = 0, jd = dataBegin; js < seqSize; ++js, ++jd) {
+        searchedSequncesView_h(i, js) = dataViewBeforeOp_h(i, jd);
+      }
+    }
+  } else {
+    using rand_pool =
+        Kokkos::Random_XorShift64_Pool<Kokkos::DefaultHostExecutionSpace>;
+    rand_pool pool(lowerBound * upperBound);
+    Kokkos::fill_random(searchedSequncesView_h, pool, upperBound,
+                        upperBound * 2);
+  }
+
+  Kokkos::deep_copy(searchedSequncesView, searchedSequncesView_h);
+
+  // -----------------------------------------------
+  // launch kokkos kernel
+  // -----------------------------------------------
+  using space_t = Kokkos::DefaultExecutionSpace;
+  Kokkos::TeamPolicy<space_t> policy(numTeams, Kokkos::AUTO());
+
+  // search returns an iterator so to verify that it is correct each team stores
+  // the distance of the returned iterator from the beginning of the interval
+  // that team operates on and then we check that these distances match the std
+  // result
+  Kokkos::View<std::size_t*> distancesView("distancesView", numTeams);
+
+  EqualFunctor<ValueType> binaryOp;
+
+  // use CTAD for functor
+  TestFunctorA fnc(dataView, searchedSequncesView, distancesView, binaryOp,
+                   apiId);
+  Kokkos::parallel_for(policy, fnc);
+
+  // -----------------------------------------------
+  // run cpp-std kernel and check
+  // -----------------------------------------------
+  auto distancesView_h = create_host_space_copy(distancesView);
+
+  for (std::size_t i = 0; i < dataView.extent(0); ++i) {
+    auto rowFrom = Kokkos::subview(dataViewBeforeOp_h, i, Kokkos::ALL());
+    auto rowSearchedSeq =
+        Kokkos::subview(searchedSequncesView_h, i, Kokkos::ALL());
+
+    const auto rowFromBegin     = KE::begin(rowFrom);
+    const auto rowFromEnd       = KE::end(rowFrom);
+    const auto rowSearchedBegin = KE::begin(rowSearchedSeq);
+    const auto rowSearchedEnd   = KE::end(rowSearchedSeq);
+
+    const std::size_t beginEndDistance = KE::distance(rowFromBegin, rowFromEnd);
+
+    switch (apiId) {
+      case 0:
+      case 1: {
+        auto it = std::search(rowFromBegin, rowFromEnd, rowSearchedBegin,
+                              rowSearchedEnd);
+        const std::size_t stdDistance = KE::distance(rowFromBegin, it);
+
+        if (sequencesExist) {
+          EXPECT_LT(distancesView_h(i), beginEndDistance);
+        } else {
+          EXPECT_EQ(distancesView_h(i), beginEndDistance);
+        }
+
+        EXPECT_EQ(stdDistance, distancesView_h(i));
+
+        break;
+      }
+
+      case 2:
+      case 3: {
+        auto it = std::search(rowFromBegin, rowFromEnd, rowSearchedBegin,
+                              rowSearchedEnd, binaryOp);
+        const std::size_t stdDistance = KE::distance(rowFromBegin, it);
+
+        if (sequencesExist) {
+          EXPECT_LT(distancesView_h(i), beginEndDistance);
+        } else {
+          EXPECT_EQ(distancesView_h(i), beginEndDistance);
+        }
+
+        EXPECT_EQ(stdDistance, distancesView_h(i));
+
+        break;
+      }
+    }
+  }
+}
+
+template <class LayoutTag, class ValueType>
+void run_all_scenarios(const bool sequencesExist) {
+  for (int numTeams : teamSizesToTest) {
+    for (const auto& numCols : {1, 2, 13, 101, 1444, 8153}) {
+      for (int apiId : {0, 1, 2, 3}) {
+        test_A<LayoutTag, ValueType>(sequencesExist, numTeams, numCols, apiId);
+      }
+    }
+  }
+}
+
+TEST(std_algorithms_search_team_test, sequences_exist) {
+  constexpr bool sequencesExist = true;
+
+  run_all_scenarios<DynamicTag, double>(sequencesExist);
+  run_all_scenarios<StridedTwoRowsTag, int>(sequencesExist);
+  run_all_scenarios<StridedThreeRowsTag, unsigned>(sequencesExist);
+}
+
+TEST(std_algorithms_search_team_test, sequences_do_not_exist) {
+  constexpr bool sequencesExist = false;
+
+  run_all_scenarios<DynamicTag, double>(sequencesExist);
+  run_all_scenarios<StridedTwoRowsTag, int>(sequencesExist);
+  run_all_scenarios<StridedThreeRowsTag, unsigned>(sequencesExist);
+}
+
+}  // namespace TeamSearch
+}  // namespace stdalgos
+}  // namespace Test
