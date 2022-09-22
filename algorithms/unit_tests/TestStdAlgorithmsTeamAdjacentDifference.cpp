a90b2df9024b924076ba9da87223471a0b2993ed
Jakub Strzebonski <jakub.strzebonski@ng-analytics.com>
2022-09-22 23:40:34 +0200

    Add missing break

--- algorithms/unit_tests/TestStdAlgorithmsTeamAdjacentDifference.cpp
+++ algorithms/unit_tests/TestStdAlgorithmsTeamAdjacentDifference.cpp
@@ -198,6 +198,7 @@ void test_A(std::size_t numTeams, std::size_t numCols, int apiId) {
 
         const std::size_t stdDistance = KE::distance(KE::begin(rowDest), it);
         EXPECT_EQ(stdDistance, distancesView_h(i));
+        break;
       }
     }
   }
