992202ac427ed238f8a1006f5b9af74b231d2729
Nicolas Morales <nmmoral@sandia.gov>
2022-09-28 17:04:54 -0700

    tests: move extents test to a subdirectory so we can begin better organizing view tests. Rename the test and make it compile-only

---
+++ core/unit_test/view/TestExtentsDatatypeConversion.cpp
@@ -0,0 +1,70 @@
+#include <Kokkos_Core.hpp>
+#include <type_traits>
+
+#ifdef KOKKOS_ENABLE_IMPL_MDSPAN
+
+namespace {
+// Conversion from DataType to extents
+// 0-rank view
+static_assert(std::is_same_v<typename Kokkos::Experimental::Impl::
+                                 ExtentsFromDataType<std::size_t, double>::type,
+                             std::experimental::extents<std::size_t>>);
+// Only dynamic
+static_assert(
+    std::is_same_v<typename Kokkos::Experimental::Impl::ExtentsFromDataType<
+                       std::size_t, double***>::type,
+                   std::experimental::extents<
+                       std::size_t, std::experimental::dynamic_extent,
+                       std::experimental::dynamic_extent,
+                       std::experimental::dynamic_extent>>);
+// Only static
+static_assert(std::is_same_v<
+              typename Kokkos::Experimental::Impl::ExtentsFromDataType<
+                  std::size_t, double[2][3][17]>::type,
+              std::experimental::extents<std::size_t, std::size_t{2},
+                                         std::size_t{3}, std::size_t{17}>>);
+// Both dynamic and static
+static_assert(
+    std::is_same_v<typename Kokkos::Experimental::Impl::ExtentsFromDataType<
+                       std::size_t, double** [3][2][8]>::type,
+                   std::experimental::extents<
+                       std::size_t, std::experimental::dynamic_extent,
+                       std::experimental::dynamic_extent, std::size_t { 3 },
+                       std::size_t{2}, std::size_t{8}>>);
+
+// Conversion from extents to DataType
+// 0-rank extents
+static_assert(
+    std::is_same_v<double,
+                   typename Kokkos::Experimental::Impl::DataTypeFromExtents<
+                       double, std::experimental::extents<std::size_t>>::type>);
+
+// only dynamic
+static_assert(std::is_same_v<
+              double****,
+              typename Kokkos::Experimental::Impl::DataTypeFromExtents<
+                  double, std::experimental::extents<
+                              std::size_t, std::experimental::dynamic_extent,
+                              std::experimental::dynamic_extent,
+                              std::experimental::dynamic_extent,
+                              std::experimental::dynamic_extent>>::type>);
+
+// only static
+static_assert(
+    std::is_same_v<
+        double[7][5][3],
+        typename Kokkos::Experimental::Impl::DataTypeFromExtents<
+            double, std::experimental::extents<std::size_t, 7, 5, 3>>::type>);
+
+// both dynamic and static
+static_assert(
+    std::is_same_v<
+        double*** [20][45],
+        typename Kokkos::Experimental::Impl::DataTypeFromExtents<
+            double, std::experimental::extents<
+                        std::size_t, std::experimental::dynamic_extent,
+                        std::experimental::dynamic_extent,
+                        std::experimental::dynamic_extent, 20, 45>>::type>);
+}  // namespace
+
+#endif  // KOKKOS_ENABLE_IMPL_MDSPAN
