fa9277745493bcda05a0c1f03ab7af6f4efe8438
Daniel Arndt <arndtd@ornl.gov>
2022-10-07 09:10:38 -0400

    Trilinos: Pass OpenMP flags instead of linking with the OpenMP target

--- cmake/kokkos_tpls.cmake
+++ cmake/kokkos_tpls.cmake
@@ -97,7 +97,13 @@ endif()
 
 IF (Kokkos_ENABLE_OPENMP)
   find_package(OpenMP REQUIRED)
-  KOKKOS_EXPORT_CMAKE_TPL(OpenMP REQUIRED)
+  # FIXME_TRILINOS Trilinos doesn't allow for Kokkos to use find_dependency
+  # so we just append the flags here instead of linking with the OpenMP target.
+  IF(KOKKOS_HAS_TRILINOS)
+    COMPILER_SPECIFIC_FLAGS(DEFAULT ${OpenMP_CXX_FLAGS})
+  ELSE()
+    KOKKOS_EXPORT_CMAKE_TPL(OpenMP REQUIRED)
+  ENDIF()
 ENDIF()
 
 #Convert list to newlines (which CMake doesn't always like in cache variables)
