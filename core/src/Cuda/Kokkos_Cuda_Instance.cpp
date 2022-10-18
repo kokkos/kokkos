f6634bcd39e9bc04966ecb5a28ecbf4b22310086
Daniel Arndt <6da@ornl.gov>
2022-10-18 11:07:51 -0400

    Add FIXME

--- core/src/Cuda/Kokkos_Cuda_Instance.cpp
+++ core/src/Cuda/Kokkos_Cuda_Instance.cpp
@@ -202,8 +202,8 @@ void cuda_internal_error_abort(cudaError e, const char *name, const char *file,
   if (file) {
     out << " " << file << ":" << line;
   }
-  // Call Kokkos::Impl::host_abort instead of Kokkos::abort to avoid a warning
-  // about Kokkos::abort returning in some cases.
+  // FIXME Call Kokkos::Impl::host_abort instead of Kokkos::abort to avoid a
+  // warning about Kokkos::abort returning in some cases.
   host_abort(out.str().c_str());
 }
 
