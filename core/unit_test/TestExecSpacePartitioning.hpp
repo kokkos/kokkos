1a12a5538480a4a5559a78fd2631036570cc9073
Bruno Turcksin <bruno.turcksin@gmail.com>
2022-10-26 19:28:46 +0000

    Fix multithreaded test

--- core/unit_test/TestExecSpacePartitioning.hpp
+++ core/unit_test/TestExecSpacePartitioning.hpp
@@ -68,8 +68,10 @@ void run_threaded_test(const Lambda1 l1, const Lambda2 l2) {
 #else
 template <class Lambda1, class Lambda2>
 void run_threaded_test(const Lambda1 l1, const Lambda2 l2) {
-  std::thread(std::move(l1));
-  std::thread(std::move(l2));
+  std::thread t1(std::move(l1));
+  std::thread t2(std::move(l2));
+  t1.join();
+  t2.join();
 }
 #endif
 
