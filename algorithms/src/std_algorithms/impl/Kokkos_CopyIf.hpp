04c72232b4ff4aad5399a900e2e292ac60955f41
Jakub Strzebonski <jakub.strzebonski@ng-analytics.com>
2022-10-27 21:05:39 +0200

    Try to enable parallel_scan in CopyIf impl for every backend

--- algorithms/src/std_algorithms/impl/Kokkos_CopyIf.hpp
+++ algorithms/src/std_algorithms/impl/Kokkos_CopyIf.hpp
@@ -127,7 +127,8 @@ KOKKOS_FUNCTION OutputIterator copy_if_team_impl(
   // but later on we have to revise all this to guard only
   // what really needs to be guarded
 
-#if defined(KOKKOS_ENABLE_CUDA) || defined(KOKKOS_ENABLE_HIP)
+  // #if defined(KOKKOS_ENABLE_CUDA) || defined(KOKKOS_ENABLE_HIP) ||
+  // defined(KOKKOS_ENABLE_SYCL)
   const auto num_elements = Kokkos::Experimental::distance(first, last);
   typename InputIterator::difference_type count = 0;
   ::Kokkos::parallel_scan(TeamThreadRange(teamHandle, 0, num_elements),
@@ -135,21 +136,21 @@ KOKKOS_FUNCTION OutputIterator copy_if_team_impl(
                           StdCopyIfFunctor(first, d_first, pred), count);
   teamHandle.team_barrier();
 
-#else
+  // #else
 
-  const std::size_t num_elements = Kokkos::Experimental::distance(first, last);
-  std::size_t count              = {};
-  if (teamHandle.team_rank() == 0) {
-    for (std::size_t i = 0; i < num_elements; ++i) {
-      const auto& myval = first[i];
-      if (pred(myval)) {
-        d_first[count++] = myval;
-      }
-    }
-  }
-  // no need for barrier because calling broadcast implicitly blocks
-  teamHandle.team_broadcast(count, 0);
-#endif
+  //   const std::size_t num_elements = Kokkos::Experimental::distance(first,
+  //   last); std::size_t count              = {}; if (teamHandle.team_rank() ==
+  //   0) {
+  //     for (std::size_t i = 0; i < num_elements; ++i) {
+  //       const auto& myval = first[i];
+  //       if (pred(myval)) {
+  //         d_first[count++] = myval;
+  //       }
+  //     }
+  //   }
+  //   // no need for barrier because calling broadcast implicitly blocks
+  //   teamHandle.team_broadcast(count, 0);
+  // #endif
 
   return d_first + count;
 }
