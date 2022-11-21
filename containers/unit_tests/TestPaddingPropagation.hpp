3bf486fb3ad31179c4c5a2167c3067de8e93b2b3
Daniel Arndt <arndtd@ornl.gov>
2022-11-21 08:32:17 -0500

    Simplify Containers test

--- containers/unit_tests/TestPaddingPropagation.hpp
+++ containers/unit_tests/TestPaddingPropagation.hpp
@@ -48,18 +48,6 @@
 #include <Kokkos_DynRankView.hpp>
 #include <Kokkos_Core.hpp>
 
-#define DV Kokkos::DualView
-#define DRV Kokkos::DynRankView
-
-#define PARAM_0
-#define PARAM_1 50
-#define PARAM_2 50, 1
-#define PARAM_3 50, 1, 50
-#define PARAM_4 50, 1, 1, 50
-#define PARAM_5 50, 1, 1, 1, 50
-#define PARAM_6 50, 1, 1, 1, 1, 50
-#define PARAM_7 50, 1, 1, 1, 1, 1, 50
-
 namespace Test {
 
 using DType   = int;
@@ -71,145 +59,97 @@ using DType_4 = DType ****;
 using DType_5 = DType *****;
 using DType_6 = DType ******;
 using DType_7 = DType *******;
+using DType_8 = DType ********;
 
 using pod = DType;
 
-#define TEST_VIEW_STRIDES(view1, view2, rank)             \
-  (view1.stride(0) == view2.stride(0) || rank < 1) &&     \
-      (view1.stride(1) == view2.stride(1) || rank < 2) && \
-      (view1.stride(2) == view2.stride(2) || rank < 3) && \
-      (view1.stride(3) == view2.stride(3) || rank < 4) && \
-      (view1.stride(4) == view2.stride(4) || rank < 5) && \
-      (view1.stride(5) == view2.stride(5) || rank < 6) && \
-      (view1.stride(6) == view2.stride(6) || rank < 7) && \
-      (view1.stride(7) == view2.stride(7) || rank < 8)
+template <typename View1, typename View2>
+bool test_view_strides(View1 &view1, View2 &view2, int rank) {
+  return (view1.stride(0) == view2.stride(0) || rank < 1) &&
+         (view1.stride(1) == view2.stride(1) || rank < 2) &&
+         (view1.stride(2) == view2.stride(2) || rank < 3) &&
+         (view1.stride(3) == view2.stride(3) || rank < 4) &&
+         (view1.stride(4) == view2.stride(4) || rank < 5) &&
+         (view1.stride(5) == view2.stride(5) || rank < 6) &&
+         (view1.stride(6) == view2.stride(6) || rank < 7) &&
+         (view1.stride(7) == view2.stride(7) || rank < 8);
+}
 
-#define TEST_VIEW_OFFSET_STRIDES(view, drv, rank)       \
-  (view.stride(0) == drv.stride_0() || rank < 1) &&     \
-      (view.stride(1) == drv.stride_1() || rank < 2) && \
-      (view.stride(2) == drv.stride_2() || rank < 3) && \
-      (view.stride(3) == drv.stride_3() || rank < 4) && \
-      (view.stride(4) == drv.stride_4() || rank < 5) && \
-      (view.stride(5) == drv.stride_5() || rank < 6) && \
-      (view.stride(6) == drv.stride_6() || rank < 7) && \
-      (view.stride(7) == drv.stride_7() || rank < 8)
-
-template <typename view_t, typename dt, typename layout, typename... Vals>
-std::enable_if_t<std::is_same<view_t, DV<dt, layout>>::value, bool>
-padding_assignment_and_copy(Vals... params) {
-  constexpr int rank = sizeof...(params);
-  bool result        = true;
-  auto alloc_prop =
-      Kokkos::view_alloc("vDim" + std::to_string(rank), Kokkos::AllowPadding);
-  view_t dv(alloc_prop, params...);
-  view_t dv_2(dv.d_view, dv.h_view);
-  result &= TEST_VIEW_STRIDES(dv.d_view, dv.h_view, rank);
-  result &= TEST_VIEW_STRIDES(dv_2.d_view, dv_2.h_view, rank);
-  return result;
+template <typename View, typename DRV>
+bool test_view_offset_strides(View &view, DRV &drv, int rank) {
+  return (view.stride(0) == drv.stride_0() || rank < 1) &&
+         (view.stride(1) == drv.stride_1() || rank < 2) &&
+         (view.stride(2) == drv.stride_2() || rank < 3) &&
+         (view.stride(3) == drv.stride_3() || rank < 4) &&
+         (view.stride(4) == drv.stride_4() || rank < 5) &&
+         (view.stride(5) == drv.stride_5() || rank < 6) &&
+         (view.stride(6) == drv.stride_6() || rank < 7) &&
+         (view.stride(7) == drv.stride_7() || rank < 8);
 }
 
 template <typename view_t, typename dt, typename layout, typename... Vals>
-std::enable_if_t<std::is_same<view_t, DRV<pod, layout>>::value &&
-                     ((std::is_same<dt, DType_0>::value ||
-                       std::is_same<dt, DType_1>::value) ||
-                      (std::is_same<layout, Kokkos::LayoutRight>::value &&
-                       std::is_same<dt, DType_2>::value)),
-                 bool>
-padding_assignment_and_copy(Vals... params) {
+bool padding_assignment_and_copy(Vals... params) {
   constexpr int rank = sizeof...(params);
-  bool result        = true;
   auto alloc_prop =
       Kokkos::view_alloc("vDim" + std::to_string(rank), Kokkos::AllowPadding);
-  Kokkos::View<dt, layout> v(alloc_prop, params...);
-  view_t drv(v);
-  // We do not propagate padding to DynRankView and we do not
-  // pad for this data type so strides should match
-  result &= TEST_VIEW_OFFSET_STRIDES(v, drv, rank);
-  return result;
+  if constexpr (std::is_same_v<view_t, Kokkos::DualView<dt, layout>>) {
+    view_t dv(alloc_prop, params...);
+    view_t dv_2(dv.d_view, dv.h_view);
+    bool result = test_view_strides(dv.d_view, dv.h_view, rank);
+    result &= test_view_strides(dv_2.d_view, dv_2.h_view, rank);
+    return result;
+  } else {
+    static_assert(std::is_same_v<view_t, Kokkos::DynRankView<pod, layout>>);
+    Kokkos::View<dt, layout> v(alloc_prop, params...);
+    view_t drv(v);
+    // We do not propagate padding to DynRankView and we only
+    // pad up to rank 2, so strides should only match in that case
+    if ((std::is_same_v<dt, DType_0> || std::is_same_v<dt, DType_1>) ||
+        (std::is_same_v<layout, Kokkos::LayoutRight> &&
+         std::is_same_v<dt, DType_2>))
+      return test_view_offset_strides(v, drv, rank);
+    else
+      return !test_view_offset_strides(v, drv, rank);
+  }
 }
 
-template <typename view_t, typename dt, typename layout, typename... Vals>
-std::enable_if_t<std::is_same<view_t, DRV<pod, layout>>::value &&
-                     (!(std::is_same<dt, DType_0>::value ||
-                        std::is_same<dt, DType_1>::value) &&
-                      !(std::is_same<layout, Kokkos::LayoutRight>::value &&
-                        std::is_same<dt, DType_2>::value)),
-                 bool>
-padding_assignment_and_copy(Vals... params) {
-  constexpr int rank = sizeof...(params);
-  bool result        = true;
-  auto alloc_prop =
-      Kokkos::view_alloc("vDim" + std::to_string(rank), Kokkos::AllowPadding);
-  Kokkos::View<dt, layout> v(alloc_prop, params...);
-  view_t drv(v);
-  // We do not propagate padding to DynRankView
-  // so strides should not match
-  result &= !(TEST_VIEW_OFFSET_STRIDES(v, drv, rank));
-  return result;
-}
+template <typename... Args>
+using DV = Kokkos::DualView<Args...>;
+
+template <typename... Args>
+using DRV = Kokkos::DynRankView<Args...>;
 
 template <typename layout>
 bool padding_assignment_and_copy_helper() {
   auto result = true;
-  result &= padding_assignment_and_copy<DV<DType_0, layout>, DType_0, layout>(
-      PARAM_0);
-  result &= padding_assignment_and_copy<DV<DType_1, layout>, DType_1, layout>(
-      PARAM_1);
-  result &= padding_assignment_and_copy<DV<DType_2, layout>, DType_2, layout>(
-      PARAM_2);
-  result &= padding_assignment_and_copy<DV<DType_3, layout>, DType_3, layout>(
-      PARAM_3);
-  result &= padding_assignment_and_copy<DV<DType_4, layout>, DType_4, layout>(
-      PARAM_4);
-  result &= padding_assignment_and_copy<DV<DType_5, layout>, DType_5, layout>(
-      PARAM_5);
-  result &= padding_assignment_and_copy<DV<DType_6, layout>, DType_6, layout>(
-      PARAM_6);
-  result &= padding_assignment_and_copy<DV<DType_7, layout>, DType_7, layout>(
-      PARAM_7);
-  result &=
-      padding_assignment_and_copy<DRV<pod, layout>, DType_0, layout>(PARAM_0);
-  result &=
-      padding_assignment_and_copy<DRV<pod, layout>, DType_1, layout>(PARAM_1);
-  result &=
-      padding_assignment_and_copy<DRV<pod, layout>, DType_2, layout>(PARAM_2);
-  result &=
-      padding_assignment_and_copy<DRV<pod, layout>, DType_3, layout>(PARAM_3);
-  result &=
-      padding_assignment_and_copy<DRV<pod, layout>, DType_4, layout>(PARAM_4);
-  result &=
-      padding_assignment_and_copy<DRV<pod, layout>, DType_5, layout>(PARAM_5);
-  result &=
-      padding_assignment_and_copy<DRV<pod, layout>, DType_6, layout>(PARAM_6);
-  result &=
-      padding_assignment_and_copy<DRV<pod, layout>, DType_7, layout>(PARAM_7);
+  // clang-format off
+  result &= padding_assignment_and_copy<DV<DType_0, layout>, DType_0, layout>();
+  result &= padding_assignment_and_copy<DV<DType_1, layout>, DType_1, layout>(50);
+  result &= padding_assignment_and_copy<DV<DType_2, layout>, DType_2, layout>(50, 1);
+  result &= padding_assignment_and_copy<DV<DType_3, layout>, DType_3, layout>(50, 1, 50);
+  result &= padding_assignment_and_copy<DV<DType_4, layout>, DType_4, layout>(50, 1, 1, 50);
+  result &= padding_assignment_and_copy<DV<DType_5, layout>, DType_5, layout>(50, 1, 1, 1, 50);
+  result &= padding_assignment_and_copy<DV<DType_6, layout>, DType_6, layout>(50, 1, 1, 1, 1, 50);
+  result &= padding_assignment_and_copy<DV<DType_7, layout>, DType_7, layout>(50, 1, 1, 1, 1, 1, 50);
+  result &= padding_assignment_and_copy<DV<DType_8, layout>, DType_8, layout>(50, 1, 1, 1, 1, 1, 1, 50);
+
+  result &= padding_assignment_and_copy<DRV<pod, layout>, DType_0, layout>();
+  result &= padding_assignment_and_copy<DRV<pod, layout>, DType_1, layout>(50);
+  result &= padding_assignment_and_copy<DRV<pod, layout>, DType_2, layout>(50, 1);
+  result &= padding_assignment_and_copy<DRV<pod, layout>, DType_3, layout>(50, 1, 50);
+  result &= padding_assignment_and_copy<DRV<pod, layout>, DType_4, layout>(50, 1, 1, 50);
+  result &= padding_assignment_and_copy<DRV<pod, layout>, DType_5, layout>(50, 1, 1, 1, 50);
+  result &= padding_assignment_and_copy<DRV<pod, layout>, DType_6, layout>(50, 1, 1, 1, 1, 50);
+  result &= padding_assignment_and_copy<DRV<pod, layout>, DType_7, layout>(50, 1, 1, 1, 1, 1, 50);
+  result &= padding_assignment_and_copy<DRV<pod, layout>, DType_8, layout>(50, 1, 1, 1, 1, 1, 1, 50);
+  // clang-format on
   return result;
 }
 
 TEST(TEST_CATEGORY, container_padding_propagation) {
   /*Test correct propagation in view assignements*/
-  auto result = true;
-  result &= padding_assignment_and_copy_helper<Kokkos::LayoutLeft>();
-  result &= padding_assignment_and_copy_helper<Kokkos::LayoutRight>();
-  ASSERT_TRUE(result);
-
-  // Padding is not allowed for LayoutStride
-  // using ls = Kokkos::LayoutStride;
+  ASSERT_TRUE(padding_assignment_and_copy_helper<Kokkos::LayoutLeft>());
+  ASSERT_TRUE(padding_assignment_and_copy_helper<Kokkos::LayoutRight>());
 }
-
-#undef PARAM_0
-#undef PARAM_1
-#undef PARAM_2
-#undef PARAM_3
-#undef PARAM_4
-#undef PARAM_5
-#undef PARAM_6
-#undef PARAM_7
-
-#undef TEST_VIEW_STRIDES
-#undef TEST_VIEW_OFFSET_STRIDES
-
-#undef DV
-#undef DRV
 
 }  // namespace Test
