aa20b2b31ddedf71d94da51b46e4494df9dcc7ce
Cezary Skrzyński <cezary.skrzynski@ng-analytics.com>
2022-12-29 22:16:02 +0100

    Avoid multiple `main()` definitions

--- core/perf_test/BenchmarkMain.cpp
+++ core/perf_test/BenchmarkMain.cpp
@@ -27,11 +27,16 @@
 #include "Benchmark_Context.hpp"
 #include <Kokkos_Core.hpp>
 
+#include "PerfTest_Category.hpp"
+
 int main(int argc, char** argv) {
   Kokkos::initialize(argc, argv);
   benchmark::Initialize(&argc, argv);
   benchmark::SetDefaultTimeUnit(benchmark::kSecond);
   KokkosBenchmark::add_benchmark_context(true);
+
+  (void)Test::command_line_num_args(argc);
+  (void)Test::command_line_arg(0, argv);
 
   benchmark::RunSpecifiedBenchmarks();
 
