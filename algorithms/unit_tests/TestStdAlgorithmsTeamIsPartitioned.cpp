5aa7bb13a5b6a44b2a5c10534b22edbd1b6fb682
Francesco Rizzi <fnrizzi@sandia.gov>
2023-01-02 16:35:50 +0100

    narrow down

--- algorithms/unit_tests/TestStdAlgorithmsTeamIsPartitioned.cpp
+++ algorithms/unit_tests/TestStdAlgorithmsTeamIsPartitioned.cpp
@@ -227,13 +227,13 @@ void run_all_scenarios(const std::string& name, const std::vector<int>& cols) {
   }
 }
 
-TEST(std_algorithms_is_partitioned_team_test, empty) {
-  const std::string name      = "trivialEmpty";
-  const std::vector<int> cols = {0};
-  run_all_scenarios<DynamicTag, double>(name, cols);
-  run_all_scenarios<StridedTwoRowsTag, double>(name, cols);
-  run_all_scenarios<StridedThreeRowsTag, int>(name, cols);
-}
+// TEST(std_algorithms_is_partitioned_team_test, empty) {
+//   const std::string name      = "trivialEmpty";
+//   const std::vector<int> cols = {0};
+//   run_all_scenarios<DynamicTag, double>(name, cols);
+//   run_all_scenarios<StridedTwoRowsTag, double>(name, cols);
+//   run_all_scenarios<StridedThreeRowsTag, int>(name, cols);
+// }
 
 TEST(std_algorithms_is_partitioned_team_test, all_true) {
   const std::string name      = "allTrue";
@@ -251,13 +251,13 @@ TEST(std_algorithms_is_partitioned_team_test, all_false) {
   run_all_scenarios<StridedThreeRowsTag, int>(name, cols);
 }
 
-TEST(std_algorithms_is_partitioned_team_test, random) {
-  const std::string name      = "random";
-  const std::vector<int> cols = {13, 101, 1444, 5153};
-  run_all_scenarios<DynamicTag, double>(name, cols);
-  run_all_scenarios<StridedTwoRowsTag, double>(name, cols);
-  run_all_scenarios<StridedThreeRowsTag, int>(name, cols);
-}
+// TEST(std_algorithms_is_partitioned_team_test, random) {
+//   const std::string name      = "random";
+//   const std::vector<int> cols = {13, 101, 1444, 5153};
+//   run_all_scenarios<DynamicTag, double>(name, cols);
+//   run_all_scenarios<StridedTwoRowsTag, double>(name, cols);
+//   run_all_scenarios<StridedThreeRowsTag, int>(name, cols);
+// }
 
 }  // namespace TeamIsPartitioned
 }  // namespace stdalgos
