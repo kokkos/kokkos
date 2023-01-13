05d62719bd873c65661b40b53cc8ae4397a07d5c
Damien L-G <dalg24@gmail.com>
2023-01-13 08:33:25 -0500

    Desul atomics configure library based what the user enabled

--- tpls/desul/include/desul/atomics/Macros.hpp
+++ tpls/desul/include/desul/atomics/Macros.hpp
@@ -9,7 +9,25 @@ SPDX-License-Identifier: (BSD-3-Clause)
 #ifndef DESUL_ATOMICS_MACROS_HPP_
 #define DESUL_ATOMICS_MACROS_HPP_
 
+#include <desul/atomics/Config.hpp>
+
 // Macros
+
+#if defined(DESUL_ATOMICS_ENABLE_CUDA) && defined(__CUDACC__)
+#define DESUL_HAVE_CUDA_ATOMICS
+#endif
+
+#if defined(DESUL_ATOMICS_ENABLE_HIP) && defined(__HIPCC__)
+#define DESUL_HAVE_HIP_ATOMICS
+#endif
+
+#if defined(DESUL_ATOMICS_ENABLE_SYCL) && defined(SYCL_LANGUAGE_VERSION)
+#define DESUL_HAVE_SYCL_ATOMICS
+#endif
+
+#if defined(DESUL_ATOMICS_ENABLE_OPENMP)
+#define DESUL_HAVE_OPENMP_ATOMICS
+#endif
 
 // ONLY use GNUC atomics if not explicitly say to use OpenMP atomics
 #if !defined(DESUL_HAVE_OPENMP_ATOMICS) && defined(__GNUC__)
@@ -21,20 +39,9 @@ SPDX-License-Identifier: (BSD-3-Clause)
 #define DESUL_HAVE_MSVC_ATOMICS
 #endif
 
-#ifdef __CUDACC__
-#define DESUL_HAVE_CUDA_ATOMICS
-#endif
-
-#ifdef __HIPCC__
-#define DESUL_HAVE_HIP_ATOMICS
-#endif
-
-#ifdef SYCL_LANGUAGE_VERSION
-#define DESUL_HAVE_SYCL_ATOMICS
-#endif
-
-#if defined(__CUDA_ARCH__) || defined(__HIP_DEVICE_COMPILE__) || \
-    defined(__SYCL_DEVICE_ONLY__)
+#if (defined(DESUL_ATOMICS_ENABLE_CUDA) && defined(__CUDA_ARCH__)) ||         \
+    (defined(DESUL_ATOMICS_ENABLE_HIP) && defined(__HIP_DEVICE_COMPILE__)) || \
+    (defined(DESUL_ATOMICS_ENABLE_SYCL) && defined(__SYCL_DEVICE_ONLY__))
 #define DESUL_HAVE_GPU_LIKE_PROGRESS
 #endif
 
@@ -62,7 +69,7 @@ SPDX-License-Identifier: (BSD-3-Clause)
 #define DESUL_IMPL_ESC_(...) DESUL_IMPL_VAN_##__VA_ARGS__
 #define DESUL_IMPL_VAN_DESUL_IMPL_ISH
 
-#if defined(__CUDACC__) && defined(__NVCOMPILER)
+#if (defined(DESUL_ATOMICS_ENABLE_CUDA) && defined(__CUDACC__)) && defined(__NVCOMPILER)
 #include <nv/target>
 #define DESUL_IF_ON_DEVICE(CODE) NV_IF_TARGET(NV_IS_DEVICE, CODE)
 #define DESUL_IF_ON_HOST(CODE) NV_IF_TARGET(NV_IS_HOST, CODE)
@@ -99,8 +106,9 @@ static constexpr bool desul_impl_omp_on_host() { return false; }
 #endif
 
 #if !defined(DESUL_IF_ON_HOST) && !defined(DESUL_IF_ON_DEVICE)
-#if defined(__CUDA_ARCH__) || defined(__HIP_DEVICE_COMPILE__) || \
-    defined(__SYCL_DEVICE_ONLY__)
+#if (defined(DESUL_ATOMICS_ENABLE_CUDA) && defined(__CUDA_ARCH__)) ||         \
+    (defined(DESUL_ATOMICS_ENABLE_HIP) && defined(__HIP_DEVICE_COMPILE__)) || \
+    (defined(DESUL_ATOMICS_ENABLE_SYCL) && defined(__SYCL_DEVICE_ONLY__))
 #define DESUL_IF_ON_DEVICE(CODE) \
   { DESUL_IMPL_STRIP_PARENS(CODE) }
 #define DESUL_IF_ON_HOST(CODE) \
