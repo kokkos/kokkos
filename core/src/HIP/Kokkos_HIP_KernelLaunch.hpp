d882b10d0fc9891bd7db1c3f0975c7909e33b2c2
Bruno Turcksin <bruno.turcksin@gmail.com>
2023-01-24 10:28:28 -0500

    [4.0.0.] Add parameter to force using GlobaLMemory launch mechanism using HIP (#5803)
    * Add parameter to force using GlobaLMemory launch mechanism using HIP
    
    * Force GlobalMemory launch for some Bessel tests when using ROCm 5.4
    
    * Fix tests when using ROCm 5.3
    
    * Skip test and add comment explaining why
    
    * Use GTEST_SKIP to skip test
    
    * Fix typo
    
    Co-authored-by: Damien L-G <dalg24+github@gmail.com>
    
    * Rename ImplForceGlobal to ImplForceGlobalLaunch
    
    Co-authored-by: Damien L-G <dalg24+github@gmail.com>

--- core/src/HIP/Kokkos_HIP_KernelLaunch.hpp
+++ core/src/HIP/Kokkos_HIP_KernelLaunch.hpp
@@ -127,6 +127,9 @@ struct DeduceHIPLaunchMechanism {
       light_weight = Kokkos::Experimental::WorkItemProperty::HintLightWeight;
   static constexpr Kokkos::Experimental::WorkItemProperty::HintHeavyWeight_t
       heavy_weight = Kokkos::Experimental::WorkItemProperty::HintHeavyWeight;
+  static constexpr Kokkos::Experimental::WorkItemProperty::
+      ImplForceGlobalLaunch_t force_global_launch =
+          Kokkos::Experimental::WorkItemProperty::ImplForceGlobalLaunch;
   static constexpr typename DriverType::Policy::work_item_property property =
       typename DriverType::Policy::work_item_property();
 
@@ -160,15 +163,17 @@ struct DeduceHIPLaunchMechanism {
   // Kal<F<CMU     CG  LCG C  C        CG  LG C  G    CG  CG C  C
   // CMU<F          G  LCG G  G         G  LG G  G     G  CG G  G
   static constexpr HIPLaunchMechanism launch_mechanism =
-      ((property & light_weight) == light_weight)
-          ? (sizeof(DriverType) < HIPTraits::KernelArgumentLimit
-                 ? HIPLaunchMechanism::LocalMemory
-                 : HIPLaunchMechanism::GlobalMemory)
-          : (((property & heavy_weight) == heavy_weight)
-                 ? (sizeof(DriverType) < HIPTraits::ConstantMemoryUsage
-                        ? HIPLaunchMechanism::ConstantMemory
-                        : HIPLaunchMechanism::GlobalMemory)
-                 : (default_launch_mechanism));
+      ((property & force_global_launch) == force_global_launch)
+          ? HIPLaunchMechanism::GlobalMemory
+          : ((property & light_weight) == light_weight)
+                ? (sizeof(DriverType) < HIPTraits::KernelArgumentLimit
+                       ? HIPLaunchMechanism::LocalMemory
+                       : HIPLaunchMechanism::GlobalMemory)
+                : (((property & heavy_weight) == heavy_weight)
+                       ? (sizeof(DriverType) < HIPTraits::ConstantMemoryUsage
+                              ? HIPLaunchMechanism::ConstantMemory
+                              : HIPLaunchMechanism::GlobalMemory)
+                       : (default_launch_mechanism));
 };
 
 template <typename DriverType, typename LaunchBounds,
