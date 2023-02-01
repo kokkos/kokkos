1d19328ed60cdaaa33b38eec71a6143caaab1ea8
Damien L-G <dalg24@gmail.com>
2023-02-01 16:25:15 -0500

    Desul atomics: SYCL lock arrays out of sync

--- tpls/desul/include/desul/atomics/Lock_Array_SYCL.hpp
+++ tpls/desul/include/desul/atomics/Lock_Array_SYCL.hpp
@@ -128,13 +128,26 @@ inline void unlock_address_sycl(void* ptr, MemoryScopeNode) {
   lock_node_ref.exchange(0);
 }
 #else
+
+template <typename /*AlwaysInt*/ = int>
+void init_lock_arrays_sycl(sycl::queue q) {
+  assert(false);
+}
+
+template <typename /*AlwaysInt*/ = int>
+void finalize_lock_arrays_sycl(sycl::queue q) {
+  assert(false);
+}
+
 inline bool lock_address_sycl(void*, MemoryScopeDevice) {
   assert(false);
+  // return true so that the CAS loops don't hang.
   return true;
 }
 
 inline bool lock_address_sycl(void*, MemoryScopeNode) {
   assert(false);
+  // return true so that the CAS loops don't hang.
   return true;
 }
 
