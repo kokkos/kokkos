b734750be15dd191caa25fbc0b90d90f287269d2
Damien L-G <dalg24@gmail.com>
2023-02-02 17:35:39 -0500

    Check that Kokkos::initialization_settings() is callable

---
+++ core/unit_test/TestQueryInitializationSettings.cpp
@@ -0,0 +1,27 @@
+//@HEADER
+// ************************************************************************
+//
+//                        Kokkos v. 4.0
+//       Copyright (2022) National Technology & Engineering
+//               Solutions of Sandia, LLC (NTESS).
+//
+// Under the terms of Contract DE-NA0003525 with NTESS,
+// the U.S. Government retains certain rights in this software.
+//
+// Part of Kokkos, under the Apache License v2.0 with LLVM Exceptions.
+// See https://kokkos.org/LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//@HEADER
+
+#include <gtest/gtest.h>
+
+#include "Kokkos_Core.hpp"
+
+namespace {
+TEST(defaultdevicetype, query_initialization_settings) {
+  // not doing much besides checking that the runtime function is callable
+  ASSERT_NO_THROW((void)Kokkos::initialization_settings(););
+}
+
+}  // namespace
