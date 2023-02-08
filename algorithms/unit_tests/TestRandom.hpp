45fb7bc27c014361cd3ed6477658e50563b0bda4
Vinh Dang <vqdang@sandia.gov>
2023-02-08 13:55:45 -0800

    Change threshold for float and complex<float> tests

--- algorithms/unit_tests/TestRandom.hpp
+++ algorithms/unit_tests/TestRandom.hpp
@@ -533,8 +533,14 @@ struct TestView_Normal {
               << ", variance_eps " << variance_eps << ", mean_expect " << mean
               << ", variance_expect " << stddev * stddev << ", mean "
               << mean_calc << ", variance " << var_calc << std::endl;
-    EXPECT_LE(mean_eps, 1e-2);
-    EXPECT_LE(variance_eps, 1e-2);
+    if (std::is_same<ScalarA, double>::value) {
+      EXPECT_LE(mean_eps, 1e-2);
+      EXPECT_LE(variance_eps, 1e-2);
+    }
+    else {
+      EXPECT_LE(mean_eps, 1e-1);
+      EXPECT_LE(variance_eps, 1e-1);
+    }
   }
 };
 
@@ -588,8 +594,14 @@ struct TestViewCmplx_Normal {
               << real(stddev) * real(stddev) + imag(stddev) * imag(stddev)
               << ", mean " << mean_calc << ", variance " << var_calc
               << std::endl;
-    EXPECT_LE(mean_eps, 1e-2);
-    EXPECT_LE(variance_eps, 1e-2);
+    if (std::is_same<ScalarA, Kokkos::complex<double>::value) {
+      EXPECT_LE(mean_eps, 1e-2);
+      EXPECT_LE(variance_eps, 1e-2);
+    }
+    else {
+      EXPECT_LE(mean_eps, 1e-1);
+      EXPECT_LE(variance_eps, 1e-1);
+    }
   }
 };
 
