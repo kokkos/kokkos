105b64607611c27e35782e43dfc1c46e7ab43e25
Dong Hun Lee <donlee@sandia.gov>
2023-02-14 17:14:55 -0700

    Addressing comments from reviews

--- core/src/impl/Kokkos_Combined_Reducer.hpp
+++ core/src/impl/Kokkos_Combined_Reducer.hpp
@@ -481,7 +481,7 @@ KOKKOS_INLINE_FUNCTION constexpr auto make_combined_reducer(
   return reducer_type(value,
                       _reducer_from_arg_t<Space, ReferencesOrViewsOrReducers>{
                           (ReferencesOrViewsOrReducers &&) args}...);
-  //---------------------------------------
+  //----------------------------------------
 }
 
 template <class Space, class Functor, class... ReferencesOrViewsOrReducers>
@@ -503,7 +503,7 @@ KOKKOS_INLINE_FUNCTION void parallel_reduce_combined_reducers_impl(
     BoundaryStructType const& boundaries, Functor const& functor,
     ReturnType1&& returnType1, ReturnType2&& returnType2,
     ReturnTypes&&... returnTypes) noexcept {
-  using mem_space_type = typename MemberType::scratch_memory_space;
+  using mem_space_type = DefaultExecutionSpace::memory_space;
 
   auto combined_value = Impl::make_combined_reducer_value<mem_space_type>(
       returnType1, returnType2, returnTypes...);
