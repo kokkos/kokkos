106a4a3633937f854e5304df55ebc950a0c57617
Damien L-G <dalg24@gmail.com>
2023-03-17 17:53:09 -0600

    Fixup NVIDIA GPU arch must be defined potentially for other backends as well

--- core/src/Kokkos_Macros.hpp
+++ core/src/Kokkos_Macros.hpp
@@ -55,6 +55,7 @@
 
 #ifndef KOKKOS_DONT_INCLUDE_CORE_CONFIG_H
 #include <KokkosCore_config.h>
+#include <Cuda/Kokkos_Cuda_NvidiaGpuArchitectures.hpp>
 #endif
 
 //----------------------------------------------------------------------------
