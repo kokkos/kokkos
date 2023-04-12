9d27b544fe28e73d96e3630b8f7cb59541132235
Daniel Arndt <arndtd@ornl.gov>
2023-04-12 20:30:14 +0000

    Fix CUDA

--- core/src/Cuda/Kokkos_Cuda.hpp
+++ core/src/Cuda/Kokkos_Cuda.hpp
@@ -244,30 +244,6 @@ struct DeviceTypeTraits<Cuda> {
 };
 }  // namespace Experimental
 }  // namespace Tools
-
-namespace Impl {
-
-template <class DT, class... DP>
-struct ZeroMemset<Kokkos::Cuda, DT, DP...> {
-  ZeroMemset(const Kokkos::Cuda& exec_space_instance,
-             const View<DT, DP...>& dst,
-             typename View<DT, DP...>::const_value_type&) {
-    KOKKOS_IMPL_CUDA_SAFE_CALL(cudaMemsetAsync(
-        dst.data(), 0,
-        dst.size() * sizeof(typename View<DT, DP...>::value_type),
-        exec_space_instance.cuda_stream()));
-    instance.impl_internal_space_instance()->m_internal_status =
-        Kokkos::Experimental::ExecutionSpaceStatus::submitted;
-  }
-
-  ZeroMemset(const View<DT, DP...>& dst,
-             typename View<DT, DP...>::const_value_type&) {
-    KOKKOS_IMPL_CUDA_SAFE_CALL(
-        cudaMemset(dst.data(), 0,
-                   dst.size() * sizeof(typename View<DT, DP...>::value_type)));
-  }
-};
-}  // namespace Impl
 }  // namespace Kokkos
 
 /*--------------------------------------------------------------------------*/
--- core/src/Cuda/Kokkos_Cuda_Instance.hpp
+++ core/src/Cuda/Kokkos_Cuda_Instance.hpp
@@ -211,7 +211,27 @@ class CudaInternal {
   void release_team_scratch_space(int scratch_pool_id);
 };
 
-}  // Namespace Impl
+template <class DT, class... DP>
+struct ZeroMemset<Kokkos::Cuda, DT, DP...> {
+  ZeroMemset(const Kokkos::Cuda& exec_space_instance,
+             const View<DT, DP...>& dst,
+             typename View<DT, DP...>::const_value_type&) {
+    KOKKOS_IMPL_CUDA_SAFE_CALL(cudaMemsetAsync(
+        dst.data(), 0,
+        dst.size() * sizeof(typename View<DT, DP...>::value_type),
+        exec_space_instance.cuda_stream()));
+    exec_space_instance.impl_internal_space_instance()->m_internal_status =
+        Kokkos::Experimental::ExecutionSpaceStatus::submitted;
+  }
+
+  ZeroMemset(const View<DT, DP...>& dst,
+             typename View<DT, DP...>::const_value_type&) {
+    KOKKOS_IMPL_CUDA_SAFE_CALL(
+        cudaMemset(dst.data(), 0,
+                   dst.size() * sizeof(typename View<DT, DP...>::value_type)));
+  }
+};
+}  // namespace Impl
 
 namespace Experimental {
 // Partitioning an Execution Space: expects space and integer arguments for
