a33f81d880e92865f82b43e9bde6fcde934f7e20
Arkadiusz Szczepkowicz <arek.szczepkowicz@ng-analytics.com>
2023-04-27 19:35:50 +0200

    #5635: Add UTs for parallel_scan with return value

--- core/unit_test/incremental/Test16_ParallelScan.hpp
+++ core/unit_test/incremental/Test16_ParallelScan.hpp
@@ -104,6 +104,164 @@ struct TestScan {
       ASSERT_EQ(h_data(i), upd);
     }
   }
+
+// Temporary: This condition will progressively be reduced when parallel_scan
+// with return value will be implemented for more backends.
+#if defined(KOKKOS_ENABLE_SERIAL) || defined(KOKKOS_ENABLE_OPENMP)
+#if !defined(KOKKOS_ENABLE_CUDA) && !defined(KOKKOS_ENABLE_HIP) &&             \
+    !defined(KOKKOS_ENABLE_OPENACC) && !defined(KOKKOS_ENABLE_SYCL) &&         \
+    !defined(KOKKOS_ENABLE_THREADS) && !defined(KOKKOS_ENABLE_OPENMPTARGET) && \
+    !defined(KOKKOS_ENABLE_HPX)
+  template <typename FunctorType>
+  void parallel_scan_retval() {
+    View_1D d_data("data", N);
+
+    // Initialize data.
+    Kokkos::parallel_for(
+        Kokkos::RangePolicy<ExecSpace>(0, N),
+        KOKKOS_LAMBDA(const int i) { d_data(i) = i * 0.5; });
+
+    // Exclusive parallel_scan call with return value
+    double return_val = 0;
+    Kokkos::parallel_scan(Kokkos::RangePolicy<ExecSpace>(0, N),
+                          FunctorType{d_data}, return_val);
+
+    // Copy back the data.
+    auto h_data =
+        Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), d_data);
+
+    // Check Correctness
+    ASSERT_EQ(h_data(0), 0.0);
+    value_type upd = h_data(0);
+    for (int i = 1; i < N; ++i) {
+      upd += (i - 1) * 0.5;
+      ASSERT_EQ(h_data(i), upd);
+    }
+    // Check return value
+    ASSERT_DOUBLE_EQ(return_val, 22.5);
+  }
+
+  template <typename FunctorType>
+  void parallel_scan_retval_team_thread_range() {
+    using team_policy = Kokkos::TeamPolicy<ExecSpace>;
+    using member_type = typename Kokkos::TeamPolicy<ExecSpace>::member_type;
+
+    View_1D d_data("data", N);
+    // Initialize data.
+    Kokkos::parallel_for(
+        Kokkos::RangePolicy<ExecSpace>(0, N),
+        KOKKOS_LAMBDA(const int i) { d_data(i) = i * 0.5; });
+
+    // Execute parallel_scan with return value
+    Kokkos::parallel_for(
+        "Team", team_policy(1, Kokkos::AUTO),
+        KOKKOS_LAMBDA(const member_type &team) {
+          const int n = team.league_rank();
+
+          double return_val = 0;
+          Kokkos::parallel_scan(Kokkos::TeamThreadRange(team, N),
+                                FunctorType{d_data}, return_val);
+
+          // Store output at first position
+          Kokkos::single(Kokkos::PerTeam(team),
+                         [&]() { d_data(n) = return_val; });
+        });
+
+    // Copy back the data
+    Kokkos::fence();
+    auto d_data_copy =
+        Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), d_data);
+
+    // Check Correctness
+    ASSERT_EQ(d_data_copy(0), 22.5);
+    value_type upd = 0;
+    for (int i = 1; i < N; ++i) {
+      upd += (i - 1) * 0.5;
+      ASSERT_EQ(d_data_copy(i), upd);
+    }
+  }
+
+  template <typename FunctorType>
+  void parallel_scan_retval_team_vector_range() {
+    using team_policy = Kokkos::TeamPolicy<ExecSpace>;
+    using member_type = typename Kokkos::TeamPolicy<ExecSpace>::member_type;
+
+    View_1D d_data("data", N);
+    // Initialize data.
+    Kokkos::parallel_for(
+        Kokkos::RangePolicy<ExecSpace>(0, N),
+        KOKKOS_LAMBDA(const int i) { d_data(i) = i * 0.5; });
+
+    // Execute parallel_scan with return value
+    Kokkos::parallel_for(
+        "Team", team_policy(1, Kokkos::AUTO),
+        KOKKOS_LAMBDA(const member_type &team) {
+          const int n = team.league_rank();
+
+          double return_val = 0;
+          Kokkos::parallel_scan(Kokkos::TeamVectorRange(team, N),
+                                FunctorType{d_data}, return_val);
+
+          // Store output at first position
+          Kokkos::single(Kokkos::PerTeam(team),
+                         [&]() { d_data(n) = return_val; });
+        });
+
+    // Copy back the data
+    Kokkos::fence();
+    auto d_data_copy =
+        Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), d_data);
+
+    // Check Correctness
+    ASSERT_EQ(d_data_copy(0), 22.5);
+    value_type upd = 0;
+    for (int i = 1; i < N; ++i) {
+      upd += (i - 1) * 0.5;
+      ASSERT_EQ(d_data_copy(i), upd);
+    }
+  }
+
+  template <typename FunctorType>
+  void parallel_scan_retval_thread_vector_range() {
+    using team_policy = Kokkos::TeamPolicy<ExecSpace>;
+    using member_type = typename Kokkos::TeamPolicy<ExecSpace>::member_type;
+
+    View_1D d_data("data", N);
+    // Initialize data.
+    Kokkos::parallel_for(
+        Kokkos::RangePolicy<ExecSpace>(0, N),
+        KOKKOS_LAMBDA(const int i) { d_data(i) = i * 0.5; });
+
+    // Execute parallel_scan with return value
+    Kokkos::parallel_for(
+        "Team", team_policy(1, Kokkos::AUTO),
+        KOKKOS_LAMBDA(const member_type &team) {
+          const int n = team.league_rank();
+
+          double return_val = 0;
+          Kokkos::parallel_scan(Kokkos::ThreadVectorRange(team, N),
+                                FunctorType{d_data}, return_val);
+
+          // Store output at first position
+          Kokkos::single(Kokkos::PerTeam(team),
+                         [&]() { d_data(n) = return_val; });
+        });
+
+    // Copy back the data
+    Kokkos::fence();
+    auto d_data_copy =
+        Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), d_data);
+
+    // Check Correctness
+    ASSERT_EQ(d_data_copy(0), 22.5);
+    value_type upd = 0;
+    for (int i = 1; i < N; ++i) {
+      upd += (i - 1) * 0.5;
+      ASSERT_EQ(d_data_copy(i), upd);
+    }
+  }
+#endif
+#endif
 };
 
 template <class ExecSpace>
@@ -151,5 +309,51 @@ TEST(TEST_CATEGORY, IncrTest_16_parallelscan) {
   test_total.parallel_scan<NonTrivialScanFunctor<TEST_EXECSPACE>>();
   test_total.parallel_scan<GenericExclusiveScanFunctor<TEST_EXECSPACE>>();
 }
+
+// Temporary: This condition will progressively be reduced when parallel_scan
+// with return value will be implemented for more backends.
+#if defined(KOKKOS_ENABLE_SERIAL) || defined(KOKKOS_ENABLE_OPENMP)
+#if !defined(KOKKOS_ENABLE_CUDA) && !defined(KOKKOS_ENABLE_HIP) &&             \
+    !defined(KOKKOS_ENABLE_OPENACC) && !defined(KOKKOS_ENABLE_SYCL) &&         \
+    !defined(KOKKOS_ENABLE_THREADS) && !defined(KOKKOS_ENABLE_OPENMPTARGET) && \
+    !defined(KOKKOS_ENABLE_HPX)
+TEST(TEST_CATEGORY, IncrTest_16_parallelscan_retval) {
+  TestScan<TEST_EXECSPACE> test;
+  test.parallel_scan_retval<TrivialScanFunctor<TEST_EXECSPACE>>();
+  test.parallel_scan_retval<NonTrivialScanFunctor<TEST_EXECSPACE>>();
+  test.parallel_scan_retval<GenericExclusiveScanFunctor<TEST_EXECSPACE>>();
+}
+
+TEST(TEST_CATEGORY, IncrTest_16_parallelscan_retval_team_thread_range) {
+  TestScan<TEST_EXECSPACE> test;
+  test.parallel_scan_retval_team_thread_range<
+      TrivialScanFunctor<TEST_EXECSPACE>>();
+  test.parallel_scan_retval_team_thread_range<
+      NonTrivialScanFunctor<TEST_EXECSPACE>>();
+  test.parallel_scan_retval_team_thread_range<
+      GenericExclusiveScanFunctor<TEST_EXECSPACE>>();
+}
+
+TEST(TEST_CATEGORY, IncrTest_16_parallelscan_retval_team_vector_range) {
+  TestScan<TEST_EXECSPACE> test;
+  test.parallel_scan_retval_team_vector_range<
+      TrivialScanFunctor<TEST_EXECSPACE>>();
+  test.parallel_scan_retval_team_vector_range<
+      NonTrivialScanFunctor<TEST_EXECSPACE>>();
+  test.parallel_scan_retval_team_vector_range<
+      GenericExclusiveScanFunctor<TEST_EXECSPACE>>();
+}
+
+TEST(TEST_CATEGORY, IncrTest_16_parallelscan_retval_thread_vector_range) {
+  TestScan<TEST_EXECSPACE> test;
+  test.parallel_scan_retval_thread_vector_range<
+      TrivialScanFunctor<TEST_EXECSPACE>>();
+  test.parallel_scan_retval_thread_vector_range<
+      NonTrivialScanFunctor<TEST_EXECSPACE>>();
+  test.parallel_scan_retval_thread_vector_range<
+      GenericExclusiveScanFunctor<TEST_EXECSPACE>>();
+}
+#endif
+#endif
 
 }  // namespace Test
