4eb1f1803e939e492b2836dbc9a0cf382891685a
Francesco Rizzi <fnrizzi@sandia.gov>
2023-05-05 09:51:40 -0600

    fix for nvhpc

--- algorithms/src/Kokkos_Sort.hpp
+++ algorithms/src/Kokkos_Sort.hpp
@@ -146,8 +146,12 @@ class BinSort {
         Kokkos::is_view<SrcViewType>::value,
         Kokkos::View<typename SrcViewType::const_data_type,
                      typename SrcViewType::array_layout,
-                     typename SrcViewType::device_type,
-                     Kokkos::MemoryTraits<Kokkos::RandomAccess> >,
+                     typename SrcViewType::device_type
+#if not defined(KOKKOS_COMPILER_NVHPC)  // FIXME_NVHPC
+                     ,
+                     Kokkos::MemoryTraits<Kokkos::RandomAccess>
+#endif
+                     >,
         typename SrcViewType::const_type>;
 
     using perm_view_type = typename PermuteViewType::const_type;
@@ -340,19 +344,6 @@ class BinSort {
         "The provided execution space must be able to access the memory space "
         "of the View argument!");
 
-    // FRIZZI: 04/2023:
-    // Originally the following alias also contained
-    // "typename ValuesViewType::array_layout", however this is wrong because
-    // when ValuesViewType is a view with LayoutStride, the
-    // constructor below for sorted_values fails at compile time since it needs
-    // a layout object, not just extents. To ensure this sort method works
-    // also on views with LayoutStride, we can make the scratch view a "regular"
-    // view since it is only used a temporary space for doing the proper
-    // ordering, and then copied back to overwrite values.
-    using scratch_view_type =
-        Kokkos::View<typename ValuesViewType::data_type,
-                     typename ValuesViewType::device_type>;
-
     const size_t len        = range_end - range_begin;
     const size_t values_len = values_range_end - values_range_begin;
     if (len != values_len) {
@@ -360,6 +351,9 @@ class BinSort {
           "BinSort::sort: values range length != permutation vector length");
     }
 
+    using scratch_view_type =
+        Kokkos::View<typename ValuesViewType::data_type,
+                     typename ValuesViewType::device_type>;
     scratch_view_type sorted_values(
         view_alloc(exec, WithoutInitializing,
                    "Kokkos::SortImpl::BinSortFunctor::sorted_values"),
