f39a062633be155cc452439cdd5cd1f6e757591d
Daniel Arndt <arndtd@ornl.gov>
2023-05-09 15:41:01 -0400

    Revert unnecessary changes in Test16_ParallelScan.hpp

--- core/unit_test/incremental/Test16_ParallelScan.hpp
+++ core/unit_test/incremental/Test16_ParallelScan.hpp
@@ -77,18 +77,16 @@ struct GenericExclusiveScanFunctor {
 template <class ExecSpace>
 struct TestScan {
   // 1D  View of double
-  using View_1D  = typename Kokkos::View<value_type *, ExecSpace>;
-  View_1D d_data = View_1D("data", N);
-
-  template <typename IndexType>
-  KOKKOS_FUNCTION void operator()(IndexType i) const {
-    d_data(i) = i * 0.5;
-  }
+  using View_1D = typename Kokkos::View<value_type *, ExecSpace>;
 
   template <typename FunctorType>
   void parallel_scan() {
+    View_1D d_data("data", N);
+
     // Initialize data.
-    Kokkos::parallel_for(Kokkos::RangePolicy<ExecSpace>(0, N), *this);
+    Kokkos::parallel_for(
+        Kokkos::RangePolicy<ExecSpace>(0, N),
+        KOKKOS_LAMBDA(const int i) { d_data(i) = i * 0.5; });
 
     // Exclusive parallel_scan call
     Kokkos::parallel_scan(Kokkos::RangePolicy<ExecSpace>(0, N),
