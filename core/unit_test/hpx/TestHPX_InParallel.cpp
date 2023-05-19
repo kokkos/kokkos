72ea42a7a663e1e67752d70b87faf188af9c2fee
Mikael Simberg <mikael.simberg@iki.fi>
2023-05-19 17:28:45 +0200

    Don't use gtest macros in parallel regions in HPX in_parallel test

--- core/unit_test/hpx/TestHPX_InParallel.cpp
+++ core/unit_test/hpx/TestHPX_InParallel.cpp
@@ -26,70 +26,93 @@ namespace {
 inline constexpr int n = 1 << 10;
 
 TEST(hpx, in_parallel_for_range_policy) {
+  Kokkos::View<bool *, Kokkos::Experimental::HPX> a("a", n);
+
   ASSERT_FALSE(Kokkos::Experimental::HPX::in_parallel());
 
   Kokkos::RangePolicy<Kokkos::Experimental::HPX> policy(0, n);
   Kokkos::parallel_for(
-      "parallel_for_range_policy", policy, KOKKOS_LAMBDA(const int) {
-        ASSERT_TRUE(Kokkos::Experimental::HPX::in_parallel());
+      "parallel_for_range_policy", policy, KOKKOS_LAMBDA(const int i) {
+        a(i) = Kokkos::Experimental::HPX::in_parallel();
       });
 
   ASSERT_FALSE(Kokkos::Experimental::HPX::in_parallel());
   Kokkos::fence();
   ASSERT_FALSE(Kokkos::Experimental::HPX::in_parallel());
+
+  for (int i = 0; i < n; ++i) {
+    ASSERT_TRUE(a(i));
+  }
 }
 
 TEST(hpx, in_parallel_for_mdrange_policy) {
+  Kokkos::View<bool *, Kokkos::Experimental::HPX> a("a", n);
+
   ASSERT_FALSE(Kokkos::Experimental::HPX::in_parallel());
 
   Kokkos::MDRangePolicy<Kokkos::Experimental::HPX, Kokkos::Rank<2>> policy(
       {0, 0}, {n, 1});
   Kokkos::parallel_for(
       "parallel_for_mdrange_policy", policy,
-      KOKKOS_LAMBDA(const int, const int) {
-        ASSERT_TRUE(Kokkos::Experimental::HPX::in_parallel());
+      KOKKOS_LAMBDA(const int i, const int) {
+        a(i) = Kokkos::Experimental::HPX::in_parallel();
       });
 
   ASSERT_FALSE(Kokkos::Experimental::HPX::in_parallel());
   Kokkos::fence();
   ASSERT_FALSE(Kokkos::Experimental::HPX::in_parallel());
+
+  for (int i = 0; i < n; ++i) {
+    ASSERT_TRUE(a(i));
+  }
 }
 
 TEST(hpx, in_parallel_for_team_policy) {
+  Kokkos::View<bool *, Kokkos::Experimental::HPX> a("a", n);
+
   ASSERT_FALSE(Kokkos::Experimental::HPX::in_parallel());
 
   Kokkos::TeamPolicy<Kokkos::Experimental::HPX> policy(n, 1);
   using member_type = decltype(policy)::member_type;
   Kokkos::parallel_for(
-      "parallel_for_team_policy", policy, KOKKOS_LAMBDA(const member_type &) {
-        ASSERT_TRUE(Kokkos::Experimental::HPX::in_parallel());
+      "parallel_for_team_policy", policy,
+      KOKKOS_LAMBDA(const member_type &handle) {
+        a(handle.league_rank()) = Kokkos::Experimental::HPX::in_parallel();
       });
 
   ASSERT_FALSE(Kokkos::Experimental::HPX::in_parallel());
   Kokkos::fence();
   ASSERT_FALSE(Kokkos::Experimental::HPX::in_parallel());
+
+  for (int i = 0; i < n; ++i) {
+    ASSERT_TRUE(a(i));
+  }
 }
 
 TEST(hpx, in_parallel_reduce_range_policy) {
-  Kokkos::View<int, Kokkos::Experimental::HPX> b("b");
+  Kokkos::View<int, Kokkos::Experimental::HPX> a("a");
 
   ASSERT_FALSE(Kokkos::Experimental::HPX::in_parallel());
 
   Kokkos::RangePolicy<Kokkos::Experimental::HPX> policy(0, n);
   Kokkos::parallel_reduce(
       "parallel_reduce_range_policy", policy,
-      KOKKOS_LAMBDA(const int, int &) {
-        ASSERT_TRUE(Kokkos::Experimental::HPX::in_parallel());
+      KOKKOS_LAMBDA(const int, int &x) {
+        if (!Kokkos::Experimental::HPX::in_parallel()) {
+          ++x;
+        }
       },
-      b);
+      a);
 
   ASSERT_FALSE(Kokkos::Experimental::HPX::in_parallel());
   Kokkos::fence();
   ASSERT_FALSE(Kokkos::Experimental::HPX::in_parallel());
+
+  ASSERT_EQ(a(), 0);
 }
 
 TEST(hpx, in_parallel_reduce_mdrange_policy) {
-  Kokkos::View<int, Kokkos::Experimental::HPX> b("b");
+  Kokkos::View<int, Kokkos::Experimental::HPX> a("a");
 
   ASSERT_FALSE(Kokkos::Experimental::HPX::in_parallel());
 
@@ -97,18 +120,22 @@ TEST(hpx, in_parallel_reduce_mdrange_policy) {
       {0, 0}, {n, 1});
   Kokkos::parallel_reduce(
       "parallel_reduce_mdrange_policy", policy,
-      KOKKOS_LAMBDA(const int, const int, int &) {
-        ASSERT_TRUE(Kokkos::Experimental::HPX::in_parallel());
+      KOKKOS_LAMBDA(const int, const int, int &x) {
+        if (!Kokkos::Experimental::HPX::in_parallel()) {
+          ++x;
+        }
       },
-      b);
+      a);
 
   ASSERT_FALSE(Kokkos::Experimental::HPX::in_parallel());
   Kokkos::fence();
   ASSERT_FALSE(Kokkos::Experimental::HPX::in_parallel());
+
+  ASSERT_EQ(a(), 0);
 }
 
 TEST(hpx, in_parallel_reduce_team_policy) {
-  Kokkos::View<int, Kokkos::Experimental::HPX> b("b");
+  Kokkos::View<int, Kokkos::Experimental::HPX> a("a");
 
   ASSERT_FALSE(Kokkos::Experimental::HPX::in_parallel());
 
@@ -116,31 +143,41 @@ TEST(hpx, in_parallel_reduce_team_policy) {
   using member_type = decltype(policy)::member_type;
   Kokkos::parallel_reduce(
       "parallel_reduce_team_policy", policy,
-      KOKKOS_LAMBDA(const member_type &, int &) {
-        ASSERT_TRUE(Kokkos::Experimental::HPX::in_parallel());
+      KOKKOS_LAMBDA(const member_type &, int &x) {
+        if (!Kokkos::Experimental::HPX::in_parallel()) {
+          ++x;
+        }
       },
-      b);
+      a);
 
   ASSERT_FALSE(Kokkos::Experimental::HPX::in_parallel());
   Kokkos::fence();
   ASSERT_FALSE(Kokkos::Experimental::HPX::in_parallel());
+
+  ASSERT_EQ(a(), 0);
 }
 
 TEST(hpx, in_parallel_scan_range_policy) {
-  Kokkos::View<int, Kokkos::Experimental::HPX> b("b");
+  Kokkos::View<int *, Kokkos::Experimental::HPX> a("a", n);
 
   ASSERT_FALSE(Kokkos::Experimental::HPX::in_parallel());
 
   Kokkos::RangePolicy<Kokkos::Experimental::HPX> policy(0, n);
   Kokkos::parallel_scan(
       "parallel_scan_range_policy", policy,
-      KOKKOS_LAMBDA(const int, int &, bool) {
-        ASSERT_TRUE(Kokkos::Experimental::HPX::in_parallel());
+      KOKKOS_LAMBDA(const int, int &x, bool) {
+        if (!Kokkos::Experimental::HPX::in_parallel()) {
+          ++x;
+        }
       },
-      b);
+      a);
 
   ASSERT_FALSE(Kokkos::Experimental::HPX::in_parallel());
   Kokkos::fence();
   ASSERT_FALSE(Kokkos::Experimental::HPX::in_parallel());
+
+  for (int i = 0; i < n; ++i) {
+    ASSERT_EQ(a(i), 0);
+  }
 }
 }  // namespace
