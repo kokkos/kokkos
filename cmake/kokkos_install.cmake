f9cfb743c70b26ed9d9bc8a9c95bd8303cec67ab
Daniel Arndt <arndtd@ornl.gov>
2023-05-25 16:20:35 -0400

    Also create symlinks for CMake configuration files to cmake_packages/Kokkos for TriBITS

--- cmake/kokkos_install.cmake
+++ cmake/kokkos_install.cmake
@@ -29,6 +29,12 @@ IF (NOT KOKKOS_HAS_TRILINOS AND NOT Kokkos_INSTALL_TESTING)
     DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/Kokkos)
   install(EXPORT KokkosTargets NAMESPACE Kokkos:: DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/Kokkos)
   export(EXPORT KokkosTargets NAMESPACE Kokkos:: FILE ${Kokkos_BINARY_DIR}/KokkosTargets.cmake)
+
+  # Required to be a TriBITS-compliant external package
+  file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/cmake_packages/Kokkos)
+  file(CREATE_LINK ${Kokkos_BINARY_DIR}/KokkosTargets.cmake ${CMAKE_BINARY_DIR}/cmake_packages/Kokkos/KokkosTargets.cmake SYMBOLIC)
+  file(CREATE_LINK ${Kokkos_BINARY_DIR}/KokkosConfig.cmake ${CMAKE_BINARY_DIR}/cmake_packages/Kokkos/KokkosConfig.cmake SYMBOLIC)
+  file(CREATE_LINK ${Kokkos_BINARY_DIR}/KokkosConfigCommon.cmake ${CMAKE_BINARY_DIR}/cmake_packages/Kokkos/KokkosConfigCommon.cmake SYMBOLIC)
 ELSE()
   CONFIGURE_FILE(cmake/KokkosConfigCommon.cmake.in ${Kokkos_BINARY_DIR}/KokkosConfigCommon.cmake @ONLY)
   file(READ ${Kokkos_BINARY_DIR}/KokkosConfigCommon.cmake KOKKOS_CONFIG_COMMON)
