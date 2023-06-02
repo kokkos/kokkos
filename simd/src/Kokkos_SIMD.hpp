17e63a7fa044b08564b1eccbc15115da7c557d83
Dong Hun Lee <donlee@sandia.gov>
2023-06-02 09:06:33 -0600

    Added float simd in NEON

--- simd/src/Kokkos_SIMD.hpp
+++ simd/src/Kokkos_SIMD.hpp
@@ -173,7 +173,7 @@ using data_type_set =
 #elif defined(__ARM_NEON)
 using host_abi_set = abi_set<simd_abi::scalar, simd_abi::neon_fixed_size<2>>;
 using data_type_set =
-    data_types<std::int32_t, std::int64_t, std::uint64_t, double>;
+    data_types<std::int32_t, std::int64_t, std::uint64_t, double, float>;
 #else
 using host_abi_set  = abi_set<simd_abi::scalar>;
 using data_type_set = data_types<std::int32_t, std::uint32_t, std::int64_t,
