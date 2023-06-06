a360df23fb5f6f1373f0998160eebfa6dca60fad
Daniel Arndt <arndtd@ornl.gov>
2023-06-06 15:24:33 +0000

    Add test case

--- containers/unit_tests/TestDynViewAPI_rank12345.hpp
+++ containers/unit_tests/TestDynViewAPI_rank12345.hpp
@@ -20,4 +20,43 @@ namespace Test {
 TEST(TEST_CATEGORY, dyn_rank_view_api_operator_rank12345) {
   TestDynViewAPI<double, TEST_EXECSPACE>::run_operator_test_rank12345();
 }
+
+template <typename SharedMemorySpace>
+void test_dyn_rank_view_resize() {
+  int n = 1000000;
+  Kokkos::DynRankView<double, SharedMemorySpace> device_view("device view", n);
+  auto device_view_copy = device_view;
+
+  Kokkos::resize(device_view, 2 * n);
+
+  for (int i = 0; i < 2 * n; ++i) device_view(i) = i + 1;
+
+  Kokkos::fence();
+
+  for (int i = 0; i < 2 * n; ++i) ASSERT_EQ(device_view(i), i + 1);
+}
+
+template <typename SharedMemorySpace>
+void test_dyn_rank_view_realloc() {
+  int n = 1000000;
+  Kokkos::DynRankView<double, SharedMemorySpace> device_view("device view", n);
+  auto device_view_copy = device_view;
+
+  Kokkos::realloc(device_view, 2 * n);
+
+  for (int i = 0; i < 2 * n; ++i) device_view(i) = i + 1;
+
+  Kokkos::fence();
+
+  for (int i = 0; i < 2 * n; ++i) ASSERT_EQ(device_view(i), i + 1);
+}
+
+TEST(TEST_CATEGORY, dyn_rank_view_resize_realloc) {
+  if constexpr (std::is_same_v<TEST_EXECSPACE, Kokkos::DefaultExecutionSpace>) {
+    test_dyn_rank_view_resize<Kokkos::SharedSpace>();
+    test_dyn_rank_view_realloc<Kokkos::SharedSpace>();
+  } else
+    GTEST_SKIP() << "skipping since we only test SharedSpace";
+}
+
 }  // namespace Test
