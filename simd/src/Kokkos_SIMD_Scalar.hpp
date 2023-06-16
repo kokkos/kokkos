7570f3694ddbb2d5b8550d9181d9d208a2db8b22
Dong Hun Lee <donlee@sandia.gov>
2023-06-15 21:09:09 -0600

    Changed from using Kokkos::abs usage to manual impl

--- simd/src/Kokkos_SIMD_Scalar.hpp
+++ simd/src/Kokkos_SIMD_Scalar.hpp
@@ -196,7 +196,7 @@ template <class T>
 template <class T>
 [[nodiscard]] KOKKOS_FORCEINLINE_FUNCTION simd<T, simd_abi::scalar> abs(
     simd<T, simd_abi::scalar> const& a) {
-  return simd<T, simd_abi::scalar>(Kokkos::abs<T>(static_cast<T>(a)));
+  return simd<T, simd_abi::scalar>(((a < 0) ? a * -1 : a));
 }
 
 template <class T>
