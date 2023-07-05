35c1e3c37402ff8505410456421145e0cd50812c
Dong Hun Lee <59181952+ldh4@users.noreply.github.com>
2023-07-05 11:22:51 -0600

    SIMD: Add abs() for all int types (#6069)
    * Added abs() for all simd types except for avx2 int64_t
    
    * Fixed abs() for AVX2 int64_t
    
    * Added abs() test for device side
    
    * Changed from using Kokkos::abs usage to manual impl
    
    * Removed [[nodiscard]] from abs() in AVX2
    
    * Added a comment about _mm256_abs_epi64 being not available in AVX2
    
    * Converted scalar abs() to use Kokkos::abs() to avoid spurious warnings from nvc++
    
    * Update simd/src/Kokkos_SIMD_Scalar.hpp based on a review
    
    Co-authored-by: Damien L-G <dalg24+github@gmail.com>
    
    * Fix scalar abs()
    
    ---------
    
    Co-authored-by: Damien L-G <dalg24+github@gmail.com>

--- simd/src/Kokkos_SIMD_AVX2.hpp
+++ simd/src/Kokkos_SIMD_AVX2.hpp
@@ -661,6 +661,13 @@ class simd<std::int32_t, simd_abi::avx2_fixed_size<4>> {
       _mm_add_epi32(static_cast<__m128i>(lhs), static_cast<__m128i>(rhs)));
 }
 
+KOKKOS_IMPL_HOST_FORCEINLINE_FUNCTION
+simd<std::int32_t, simd_abi::avx2_fixed_size<4>> abs(
+    simd<std::int32_t, simd_abi::avx2_fixed_size<4>> const& a) {
+  __m128i const rhs = static_cast<__m128i>(a);
+  return simd<std::int32_t, simd_abi::avx2_fixed_size<4>>(_mm_abs_epi32(rhs));
+}
+
 [[nodiscard]] KOKKOS_IMPL_HOST_FORCEINLINE_FUNCTION
     simd<std::int32_t, simd_abi::avx2_fixed_size<4>>
     condition(simd_mask<std::int32_t, simd_abi::avx2_fixed_size<4>> const& a,
@@ -782,6 +789,15 @@ class simd<std::int64_t, simd_abi::avx2_fixed_size<4>> {
               simd<std::int64_t, simd_abi::avx2_fixed_size<4>> const& rhs) {
   return simd<std::int64_t, simd_abi::avx2_fixed_size<4>>(
       _mm256_add_epi64(static_cast<__m256i>(lhs), static_cast<__m256i>(rhs)));
+}
+
+// Manually computing absolute values, because _mm256_abs_epi64
+// is not in AVX2; it's available in AVX512.
+KOKKOS_IMPL_HOST_FORCEINLINE_FUNCTION
+simd<std::int64_t, simd_abi::avx2_fixed_size<4>> abs(
+    simd<std::int64_t, simd_abi::avx2_fixed_size<4>> const& a) {
+  return simd<std::int64_t, simd_abi::avx2_fixed_size<4>>(
+      [&](std::size_t i) { return (a[i] < 0) ? -a[i] : a[i]; });
 }
 
 KOKKOS_IMPL_HOST_FORCEINLINE_FUNCTION
@@ -907,6 +923,12 @@ simd<std::int64_t, simd_abi::avx2_fixed_size<4>>::simd(
               simd<std::uint64_t, simd_abi::avx2_fixed_size<4>> const& rhs) {
   return simd<std::uint64_t, simd_abi::avx2_fixed_size<4>>(
       _mm256_sub_epi64(static_cast<__m256i>(lhs), static_cast<__m256i>(rhs)));
+}
+
+KOKKOS_IMPL_HOST_FORCEINLINE_FUNCTION
+simd<std::uint64_t, simd_abi::avx2_fixed_size<4>> abs(
+    simd<std::uint64_t, simd_abi::avx2_fixed_size<4>> const& a) {
+  return a;
 }
 
 KOKKOS_IMPL_HOST_FORCEINLINE_FUNCTION
