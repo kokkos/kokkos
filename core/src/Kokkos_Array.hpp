6bb872f464ab239ccfa656d40893fe6d42ba45bc
Nevin Liber <nliber@anl.gov>
2023-07-13 16:27:49 -0500

    Added CTAD for Array and to_Array

--- core/src/Kokkos_Array.hpp
+++ core/src/Kokkos_Array.hpp
@@ -320,6 +320,35 @@ struct Array<T, KOKKOS_INVALID_INDEX, Array<>::strided> {
       : m_elem(arg_ptr), m_size(arg_size), m_stride(arg_stride) {}
 };
 
+template <typename T, typename... Us>
+Array(T, Us...)->Array<T, 1 + sizeof...(Us)>;
+
+namespace Impl {
+
+template <class T, std::size_t N, std::size_t... I>
+constexpr Array<std::remove_cv_t<T>, N> to_Array_impl(
+    T (&a)[N], std::index_sequence<I...>) {
+  return {{a[I]...}};
+}
+
+template <class T, std::size_t N, std::size_t... I>
+constexpr Array<std::remove_cv_t<T>, N> to_Array_impl(
+    T(&&a)[N], std::index_sequence<I...>) {
+  return {{std::move(a[I])...}};
+}
+
+}  // namespace Impl
+
+template <class T, std::size_t N>
+constexpr auto to_Array(T (&a)[N]) {
+  return Impl::to_Array_impl(a, std::make_index_sequence<N>{});
+}
+
+template <class T, std::size_t N>
+constexpr auto to_Array(T(&&a)[N]) {
+  return Impl::to_Array_impl(std::move(a), std::make_index_sequence<N>{});
+}
+
 }  // namespace Kokkos
 
 //<editor-fold desc="Support for structured binding">
