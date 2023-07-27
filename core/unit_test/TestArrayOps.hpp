16f1081d8f284e75724538a6c150c7ad9090f874
Nevin ":-)" Liber <nliber+github@gmail.com>
2023-07-27 13:10:10 -0500

    WIP: Added some tests for contiguous Array

--- core/unit_test/TestArrayOps.hpp
+++ core/unit_test/TestArrayOps.hpp
@@ -98,6 +98,81 @@ TEST(TEST_CATEGORY, array_element_access_data) {
   ASSERT_EQ(c.data()[0], 3);
   ASSERT_EQ(c.data()[1], 5);
 }
+
+TEST(TEST_CATEGORY, array_T_0) {
+  using A = Kokkos::Array<int, 0>;
+  A a;
+
+  ASSERT_TRUE(a.empty());
+  ASSERT_EQ(a.size(), 0u);
+  ASSERT_EQ(a.max_size(), 0u);
+
+  ASSERT_EQ(a.data(), nullptr);
+
+  const A& c = a;
+  ASSERT_EQ(c.data(), nullptr);
+}
+
+TEST(TEST_CATEGORY, array_T_0_contiguous) {
+  int is[] = {3, 5};
+
+  using A =
+      Kokkos::Array<int, KOKKOS_INVALID_INDEX, Kokkos::Array<>::contiguous>;
+  A a(is, std::size(is));
+
+  ASSERT_TRUE(a.empty());
+  ASSERT_EQ(a.size(), std::size(is));
+  ASSERT_EQ(a.max_size(), std::size(is));
+
+  ASSERT_EQ(a[0], 3);
+  ASSERT_EQ(a[false], 3);
+  ASSERT_EQ(a[EZero], 3);
+  ASSERT_EQ(a[ScopedEnum::SEZero], 3);
+  ASSERT_EQ(a[ScopedEnumShort::SESZero], 3);
+
+  ASSERT_EQ(a[1], 5);
+  ASSERT_EQ(a[true], 5);
+  ASSERT_EQ(a[EOne], 5);
+  ASSERT_EQ(a[ScopedEnum::SEOne], 5);
+  ASSERT_EQ(a[ScopedEnumShort::SESOne], 5);
+
+  ASSERT_EQ(a.data(), is);
+
+  const A& c = a;
+
+  ASSERT_EQ(c[0], 3);
+  ASSERT_EQ(c[false], 3);
+  ASSERT_EQ(c[EZero], 3);
+  ASSERT_EQ(c[ScopedEnum::SEZero], 3);
+  ASSERT_EQ(c[ScopedEnumShort::SESZero], 3);
+
+  ASSERT_EQ(c[1], 5);
+  ASSERT_EQ(c[true], 5);
+  ASSERT_EQ(c[EOne], 5);
+  ASSERT_EQ(c[ScopedEnum::SEOne], 5);
+  ASSERT_EQ(c[ScopedEnumShort::SESOne], 5);
+
+  ASSERT_EQ(c.data(), is);
+
+  using B = Kokkos::Array<int, 1>;
+  B b{{7}};
+  a = b;
+
+  ASSERT_EQ(a.size(), std::size(is));
+  ASSERT_EQ(a.max_size(), std::size(is));
+  ASSERT_EQ(a[0], 7);
+  ASSERT_EQ(a[1], 5);
+
+  using D = Kokkos::Array<int, 4>;
+  D d{{11, 13, 17, 19}};
+  a = d;
+
+  ASSERT_EQ(a.size(), std::size(is));
+  ASSERT_EQ(a.max_size(), std::size(is));
+  ASSERT_EQ(a[0], 11);
+  ASSERT_EQ(a[1], 13);
+}
+
 struct SetOnMove {
   KOKKOS_INLINE_FUNCTION SetOnMove(int i_) : i(i_) {}
   KOKKOS_INLINE_FUNCTION operator int() const { return i; }
