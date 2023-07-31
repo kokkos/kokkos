b8482317e26c3ae0eb421eb4b5b95fe0d16a0743
Daniel Arndt <arndtd@ornl.gov>
2023-07-31 20:28:08 +0000

    Introduce constructor for multi-GPU support.

--- core/src/Cuda/Kokkos_Cuda_Instance.hpp
+++ core/src/Cuda/Kokkos_Cuda_Instance.hpp
@@ -102,7 +102,7 @@ class CudaInternal {
  public:
   using size_type = Cuda::size_type;
 
-  inline static int m_cudaDev = -1;
+  int m_cudaDev = -1;
 
   // Device Properties
   inline static int m_cudaArch                      = -1;
@@ -159,7 +159,7 @@ class CudaInternal {
     return nullptr != m_scratchSpace && nullptr != m_scratchFlags;
   }
 
-  void initialize(cudaStream_t stream, bool manage_stream);
+  void initialize(int cuda_devie, cudaStream_t stream, bool manage_stream);
   void finalize();
 
   void print_configuration(std::ostream&) const;
