c87e78a37f5e37d0ece1b1b7bbe6e55effb33041
Nevin ":-)" Liber <nliber+github@gmail.com>
2023-08-22 18:40:09 -0500

    Added unit tests for Kokkos::to_Array

--- core/unit_test/TestArray.cpp
+++ core/unit_test/TestArray.cpp
@@ -49,4 +49,28 @@ KOKKOS_FUNCTION constexpr bool test_array_structured_binding_support() {
 
 static_assert(test_array_structured_binding_support());
 
+KOKKOS_FUNCTION constexpr bool test_to_array() {
+  // copies a string literal
+  auto a1 = Kokkos::to_Array("foo");
+  static_assert(a1.size() == 4);
+
+  // deduces both element type and length
+  auto a2 = Kokkos::to_Array({0, 2, 1, 3});
+  static_assert(std::is_same_v<decltype(a2), Kokkos::Array<int, 4>>);
+
+  // deduces length with element type specified
+  // implicit conversion happens
+  auto a3 = Kokkos::to_Array<long>({0, 1, 3});
+  static_assert(std::is_same_v<decltype(a3), Kokkos::Array<long, 3>>);
+
+  auto a4 = Kokkos::to_Array<std::pair<int, float>>(
+      {{3, 0.0f}, {4, 0.1f}, {4, 0.1e23f}});
+  static_assert(a4.size() == 3);
+
+  return true;
+}
+
+static_assert(test_to_array());
+
+
 }  // namespace
--- core/unit_test/TestArrayOps.hpp
+++ core/unit_test/TestArrayOps.hpp
@@ -532,4 +532,76 @@ struct SetOnMove {
   int i = std::numeric_limits<int>::min();
 };
 
+TEST(TEST_CATEGORY, to_Array_lvalue) {
+  int array[] = {
+      2,
+      3,
+      5,
+      7,
+  };
+  int a_sum   = 0;  // sum of array elements
+  int som_sum = 0;  // sum of som_array elements
+  int ka_sum  = 0;  // sum of Kokkos::Array elements
+
+  Kokkos::parallel_reduce(
+      1,
+      KOKKOS_LAMBDA(int, int& asum, int& somsum, int& kasum) {
+        SetOnMove som_array[std::size(array)];
+        int i = 0;
+        for (auto& v : array) som_array[i++] = v;
+
+        auto ka = Kokkos::to_Array(som_array);
+        static_assert(std::is_same_v<
+                      Kokkos::Array<std::remove_extent_t<decltype(som_array)>,
+                                    std::size(array)>,
+                      decltype(ka)>);
+
+        for (size_t j = 0; j != ka.size(); ++j) {
+          asum += array[j];
+          somsum += som_array[j];
+          kasum += ka[j];
+        }
+      },
+      a_sum, som_sum, ka_sum);
+
+  ASSERT_EQ(som_sum, a_sum);
+  ASSERT_EQ(ka_sum, a_sum);
+}
+
+TEST(TEST_CATEGORY, to_Array_rvalue) {
+  int array[] = {
+      2,
+      3,
+      5,
+      7,
+  };
+  int a_sum   = 0;  // sum of array elements
+  int som_sum = 0;  // sum of som_array elements
+  int ka_sum  = 0;  // sum of Kokkos::Array elements
+
+  Kokkos::parallel_reduce(
+      1,
+      KOKKOS_LAMBDA(int, int& asum, int& somsum, int& kasum) {
+        SetOnMove som_array[std::size(array)];
+        int i = 0;
+        for (auto& v : array) som_array[i++] = v;
+
+        auto ka = Kokkos::to_Array(std::move(som_array));
+        static_assert(std::is_same_v<
+                      Kokkos::Array<std::remove_extent_t<decltype(som_array)>,
+                                    std::size(array)>,
+                      decltype(ka)>);
+
+        for (size_t j = 0; j != ka.size(); ++j) {
+          asum += array[j];
+          somsum += som_array[j];
+          kasum += ka[j];
+        }
+      },
+      a_sum, som_sum, ka_sum);
+
+  ASSERT_EQ(som_sum, -1 * static_cast<int>(std::size(array)));
+  ASSERT_EQ(ka_sum, a_sum);
+}
+
 }  // namespace
