0692166d8289ba8bf160f72bf953f8680cf0110d
Seyong Lee <lees2@ornl.gov>
2023-08-24 21:08:11 -0400

    Minor bug fix on OpenACC

--- tpls/desul/include/desul/atomics/Macros.hpp
+++ tpls/desul/include/desul/atomics/Macros.hpp
@@ -128,6 +128,7 @@ static constexpr bool desul_impl_omp_on_host() { return false; }
 #endif
 
 #if defined(DESUL_HAVE_OPENACC_ATOMICS)
+#include <openacc.h>
 #ifdef __NVCOMPILER
 //In NVHPC, acc_on_device() is not constexpr; therefore, the same
 //device atomic implementations are used for both host and device.
@@ -136,7 +137,6 @@ static constexpr bool desul_impl_omp_on_host() { return false; }
 #define DESUL_IF_ON_HOST(CODE) \
   {}
 #else
-#include <openacc.h>
 #define DESUL_IF_ON_DEVICE(CODE)                     \
   if constexpr (acc_on_device(acc_device_not_host)) {\
     DESUL_IMPL_STRIP_PARENS(CODE)                    \
