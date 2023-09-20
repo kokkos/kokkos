7d327fb7535d8b2d929bffdf64b4a2a688794bbf
Francesco Rizzi <fnrizzi@sandia.gov>
2023-09-20 18:28:49 +0200

    fix comment

--- algorithms/unit_tests/TestStdAlgorithmsTeamReduce.cpp
+++ algorithms/unit_tests/TestStdAlgorithmsTeamReduce.cpp
@@ -197,8 +197,7 @@ void test_A(std::size_t numTeams, std::size_t numCols, int apiId) {
     // libstdc++ as provided by GCC 8 does not have reduce, transform_reduce,
     // exclusive_scan, inclusive_scan, transform_exclusive_scan,
     // transform_inclusive_scan and for GCC 9.1, 9.2 fails to compile them for
-    // missing overload not accepting policy so use here simplified versions of
-    // them, only for testing purpose
+    // missing overload not accepting policy
 
 #if defined(_GLIBCXX_RELEASE) && (_GLIBCXX_RELEASE <= 9)
 #define reduce testing_reduce
--- algorithms/unit_tests/TestStdAlgorithmsTeamTransformReduce.cpp
+++ algorithms/unit_tests/TestStdAlgorithmsTeamTransformReduce.cpp
@@ -242,8 +242,7 @@ void test_A(std::size_t numTeams, std::size_t numCols, int apiId) {
 // libstdc++ as provided by GCC 8 does not have reduce, transform_reduce,
 // exclusive_scan, inclusive_scan, transform_exclusive_scan,
 // transform_inclusive_scan and for GCC 9.1, 9.2 fails to compile them for
-// missing overload not accepting policy so use here simplified versions of
-// them, only for testing purpose
+// missing overload not accepting policy
 #if defined(_GLIBCXX_RELEASE) && (_GLIBCXX_RELEASE <= 9)
 #define transform_reduce testing_transform_reduce
 #else
