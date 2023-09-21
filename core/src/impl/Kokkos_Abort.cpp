b528dabba092633376bd66defe4bd9e29f3baba5
Nevin ":-)" Liber <nliber+github@gmail.com>
2023-09-21 14:02:17 -0500

    Inlined traceback_callstack

--- core/src/impl/Kokkos_Abort.cpp
+++ core/src/impl/Kokkos_Abort.cpp
@@ -27,7 +27,7 @@
 namespace Kokkos {
 namespace Impl {
 
-void traceback_callstack(std::ostream &msg) {
+inline void traceback_callstack(std::ostream &msg) {
 #ifdef KOKKOS_IMPL_ENABLE_STACKTRACE
   msg << "\nBacktrace:\n";
   save_stacktrace();
