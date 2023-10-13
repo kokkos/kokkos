dadb200bffe4995ef3785cb9fd988f801b3d67b0
Daniel Arndt <arndtd@ornl.gov>
2023-10-13 14:41:20 -0400

    Use vector_reduce for Cuda, HIP, and SYCL

--- core/src/Kokkos_ExecPolicy.hpp
+++ core/src/Kokkos_ExecPolicy.hpp
@@ -1000,8 +1000,19 @@ KOKKOS_INLINE_FUNCTION void parallel_reduce(
     ThreadVectorMDRange<Rank, TeamHandle> const& policy, Lambda const& lambda,
     ReducerValueType& val) {
   Impl::md_parallel_impl<Rank>(policy, lambda, val);
-  // policy.team.vector_reduce(Kokkos::Sum<ReducerValueType, typename
-  // TeamHandle::execution_space>{val});
+  if constexpr (false
+#ifdef KOKKOS_ENABLE_CUDA
+                || std::is_same_v<TeamHandle::execution_space, Kokkos::Cuda>
+#elif defined(KOKKOS_ENABLE_HIP)
+                || std::is_same_v<TeamHandle::execution_space, Kokkos::HIP>
+#elif defined(KOKKOS_ENABLE_SYCL)
+                || std::is_same_v<TeamHandle::execution_space,
+                                  Kokkos::Experimental::SYCL>
+#endif
+  )
+    policy.team.vector_reduce(
+        Kokkos::Sum<ReducerValueType, typename TeamHandle::execution_space>{
+            val});
 }
 
 template <typename Rank, typename TeamHandle, typename Lambda>
@@ -1016,8 +1027,19 @@ KOKKOS_INLINE_FUNCTION void parallel_reduce(
     TeamVectorMDRange<Rank, TeamHandle> const& policy, Lambda const& lambda,
     ReducerValueType& val) {
   Impl::md_parallel_impl<Rank>(policy, lambda, val);
-  // policy.team.vector_reduce(Kokkos::Sum<ReducerValueType, typename
-  // TeamHandle::execution_space>{val});
+  if constexpr (false
+#ifdef KOKKOS_ENABLE_CUDA
+                || std::is_same_v<TeamHandle::execution_space, Kokkos::Cuda>
+#elif defined(KOKKOS_ENABLE_HIP)
+                || std::is_same_v<TeamHandle::execution_space, Kokkos::HIP>
+#elif defined(KOKKOS_ENABLE_SYCL)
+                || std::is_same_v<TeamHandle::execution_space,
+                                  Kokkos::Experimental::SYCL>
+#endif
+  )
+    policy.team.vector_reduce(
+        Kokkos::Sum<ReducerValueType, typename TeamHandle::execution_space>{
+            val});
   policy.team.team_reduce(
       Kokkos::Sum<ReducerValueType, typename TeamHandle::execution_space>{val});
 }
