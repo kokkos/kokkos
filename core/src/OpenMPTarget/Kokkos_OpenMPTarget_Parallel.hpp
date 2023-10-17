952f3ebe47e3498bdb5f0c522a77cef7faf49717
Daniel Arndt <arndtd@ornl.gov>
2023-10-17 07:46:42 -0400

    reducer is unused

--- core/src/OpenMPTarget/Kokkos_OpenMPTarget_Parallel.hpp
+++ core/src/OpenMPTarget/Kokkos_OpenMPTarget_Parallel.hpp
@@ -119,10 +119,11 @@ class OpenMPTargetExecTeamMember {
     team_reduce(reducer, reducer.reference());
   }
 
+  // FIXME_OPENMPTARGET this function currently ignores the reducer passed.
   template <typename ReducerType>
   KOKKOS_INLINE_FUNCTION std::enable_if_t<is_reducer<ReducerType>::value>
-  team_reduce(ReducerType const& reducer,
-              typename ReducerType::value_type& value) const noexcept {
+  team_reduce(ReducerType const&, typename ReducerType::value_type& value) const
+      noexcept {
 #pragma omp barrier
 
     using value_type = typename ReducerType::value_type;
