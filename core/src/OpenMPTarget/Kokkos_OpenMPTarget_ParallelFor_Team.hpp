e1a5d9ff7e41d2003cb1d888405609e8cc51b596
Rahulkumar Gayatri <rgayatri@lbl.gov>
2023-11-07 21:28:24 -0800

    OpenMPTarget: Rebase and use macros for pragma.

--- core/src/OpenMPTarget/Kokkos_OpenMPTarget_ParallelFor_Team.hpp
+++ core/src/OpenMPTarget/Kokkos_OpenMPTarget_ParallelFor_Team.hpp
@@ -22,6 +22,18 @@
 #include <Kokkos_Parallel.hpp>
 #include <OpenMPTarget/Kokkos_OpenMPTarget_Parallel.hpp>
 
+#define KOKKOS_IMPL_OPENMPTARGET_PRAGMA_HELPER(x) _Pragma(#x)
+#define KOKKOS_IMPL_OMPTARGET_PRAGMA(x) \
+  KOKKOS_IMPL_OPENMPTARGET_PRAGMA_HELPER(omp target x)
+
+// Use scratch memory extensions to request dynamic shared memory for the
+// right compiler/architecture combination.
+#ifdef KOKKOS_IMPL_OPENMPTARGET_LLVM_EXTENSIONS
+#define KOKKOS_IMPL_OMPX_DYN_CGROUP_MEM(N) ompx_dyn_cgroup_mem(N)
+#else
+#define KOKKOS_IMPL_OMPX_DYN_CGROUP_MEM(N)
+#endif
+
 namespace Kokkos {
 
 /** \brief  Inter-thread parallel_for. Executes lambda(iType i) for each
@@ -140,16 +152,10 @@ class ParallelFor<FunctorType, Kokkos::TeamPolicy<Properties...>,
 // guarantees that the number of teams specified in the `num_teams` clause is
 // always less than or equal to the maximum concurrently running teams.
 #if !defined(KOKKOS_IMPL_OPENMPTARGET_HIERARCHICAL_INTEL_GPU)
-      // Use scratch memory extensions to request dynamic shared memory for the
-      // right compiler/architecture combination.
-#if defined(KOKKOS_IMPL_OPENMPTARGET_LLVM_EXTENSIONS)
-#pragma omp target teams thread_limit(team_size) firstprivate(a_functor) \
-    num_teams(max_active_teams) is_device_ptr(scratch_ptr)               \
-        ompx_dyn_cgroup_mem(shmem_size_L0)
-#else
-#pragma omp target teams thread_limit(team_size) firstprivate(a_functor) \
-    num_teams(max_active_teams) is_device_ptr(scratch_ptr)
-#endif
+    KOKKOS_IMPL_OMPTARGET_PRAGMA(
+        teams thread_limit(team_size) firstprivate(a_functor)
+            num_teams(max_active_teams) is_device_ptr(scratch_ptr)
+                KOKKOS_IMPL_OMPX_DYN_CGROUP_MEM(shmem_size_L0))
 #pragma omp parallel
     {
       if (omp_get_num_teams() > max_active_teams)
