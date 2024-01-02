ac64be2678046922cb9a4893ab33d4f39650e8f7
Damien L-G <dalg24@gmail.com>
2024-01-02 13:09:48 -0500

    Make Profiling::ProfilingSection constructor explicit

--- core/src/Kokkos_Profiling_ProfileSection.hpp
+++ core/src/Kokkos_Profiling_ProfileSection.hpp
@@ -37,7 +37,7 @@ class [[nodiscard]] ProfilingSection {
 #if defined(__has_cpp_attribute) && __has_cpp_attribute(nodiscard) >= 201907
   [[nodiscard]]
 #endif
-  ProfilingSection(const std::string& sectionName) {
+  explicit ProfilingSection(const std::string& sectionName) {
     Kokkos::Profiling::createProfileSection(sectionName, &secID);
   }
 
