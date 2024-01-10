9b3e96b7a0f73cc5c6d220e5a6b695f2738b1111
Francesco Rizzi <fnrizzi@sandia.gov>
2024-01-10 20:10:12 +0100

    fix name

--- core/unit_test/TestReduce.hpp
+++ core/unit_test/TestReduce.hpp
@@ -627,38 +627,45 @@ TEST(TEST_CATEGORY, int_combined_reduce_mixed) {
 #endif
 
 #if defined(NDEBUG)
+// the following test is for the following issue:
+// https://github.com/kokkos/kokkos/issues/6517
+
 template <class T>
-struct EnableTestForIssue6517 : std::false_type {};
+struct EnableTestForReductionWithLargeIterationCount : std::false_type {};
 
 #ifdef KOKKOS_ENABLE_CUDA
 template <>
-struct EnableTestForIssue6517<Kokkos::Cuda> : std::true_type {};
+struct EnableTestForReductionWithLargeIterationCount<Kokkos::Cuda>
+    : std::true_type {};
 #endif
 #ifdef KOKKOS_ENABLE_HIP
 template <>
-struct EnableTestForIssue6517<Kokkos::HIP> : std::true_type {};
+struct EnableTestForReductionWithLargeIterationCount<Kokkos::HIP>
+    : std::true_type {};
 #endif
 #ifdef KOKKOS_ENABLE_SYCL
 template <>
-struct EnableTestForIssue6517<Kokkos::Experimental::SYCL> : std::true_type {};
+struct EnableTestForReductionWithLargeIterationCount<Kokkos::Experimental::SYCL>
+    : std::true_type {};
 #endif
 
-struct FunctorIssue6517 {
+struct FunctorReductionWithLargeIterationCount {
   KOKKOS_FUNCTION void operator()(const int64_t /*i*/, double& update) const {
     update += 1.0;
   }
 };
 
-TEST(TEST_CATEGORY, issue6517) {
-  if constexpr (!EnableTestForIssue6517<TEST_EXECSPACE>::value) {
+TEST(TEST_CATEGORY, reduction_with_large_iteration_count) {
+  if constexpr (!EnableTestForReductionWithLargeIterationCount<
+                    TEST_EXECSPACE>::value) {
     GTEST_SKIP() << "Disabling for host backends";
   }
 
   const int64_t N = pow(2LL, 39LL) - pow(2LL, 8LL) + 1;
   Kokkos::RangePolicy<TEST_EXECSPACE, Kokkos::IndexType<int64_t>> p(0, N);
   double nu = 0;
-  EXPECT_NO_THROW(
-      Kokkos::parallel_reduce("sample reduction", p, FunctorIssue6517(), nu));
+  EXPECT_NO_THROW(Kokkos::parallel_reduce(
+      "sample reduction", p, FunctorReductionWithLargeIterationCount(), nu));
   ASSERT_DOUBLE_EQ(nu, double(N));
 }
 #endif
