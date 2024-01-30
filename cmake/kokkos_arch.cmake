16ec5cca65ea1a2e6b997b8060ab8e9aaa358349
Daniel Arndt <arndtd@ornl.gov>
2024-01-30 17:03:22 -0500

    Move -D__STRICT_ANSI__ from linker to compiler flags

--- cmake/kokkos_arch.cmake
+++ cmake/kokkos_arch.cmake
@@ -778,31 +778,31 @@ IF (KOKKOS_ENABLE_OPENMPTARGET)
     )
   ELSE()
     COMPILER_SPECIFIC_OPTIONS(
-      IntelLLVM -fopenmp-targets=spir64_gen
+      IntelLLVM -fopenmp-targets=spir64_gen -D__STRICT_ANSI__
     )
     IF(KOKKOS_ARCH_INTEL_GEN9)
       COMPILER_SPECIFIC_LINK_OPTIONS(
-        IntelLLVM -fopenmp-targets=spir64_gen -Xopenmp-target-backend "-device gen9" -D__STRICT_ANSI__
+        IntelLLVM -fopenmp-targets=spir64_gen -Xopenmp-target-backend "-device gen9"
       )
     ELSEIF(KOKKOS_ARCH_INTEL_GEN11)
       COMPILER_SPECIFIC_LINK_OPTIONS(
-        IntelLLVM -fopenmp-targets=spir64_gen -Xopenmp-target-backend "-device gen11" -D__STRICT_ANSI__
+        IntelLLVM -fopenmp-targets=spir64_gen -Xopenmp-target-backend "-device gen11"
       )
     ELSEIF(KOKKOS_ARCH_INTEL_GEN12LP)
       COMPILER_SPECIFIC_LINK_OPTIONS(
-        IntelLLVM -fopenmp-targets=spir64_gen -Xopenmp-target-backend "-device gen12lp" -D__STRICT_ANSI__
+        IntelLLVM -fopenmp-targets=spir64_gen -Xopenmp-target-backend "-device gen12lp"
       )
     ELSEIF(KOKKOS_ARCH_INTEL_DG1)
       COMPILER_SPECIFIC_LINK_OPTIONS(
-        IntelLLVM -fopenmp-targets=spir64_gen -Xopenmp-target-backend "-device dg1" -D__STRICT_ANSI__
+        IntelLLVM -fopenmp-targets=spir64_gen -Xopenmp-target-backend "-device dg1"
       )
     ELSEIF(KOKKOS_ARCH_INTEL_XEHP)
       COMPILER_SPECIFIC_LINK_OPTIONS(
-        IntelLLVM -fopenmp-targets=spir64_gen -Xopenmp-target-backend "-device 12.50.4" -D__STRICT_ANSI__
+        IntelLLVM -fopenmp-targets=spir64_gen -Xopenmp-target-backend "-device 12.50.4"
       )
     ELSEIF(KOKKOS_ARCH_INTEL_PVC)
       COMPILER_SPECIFIC_LINK_OPTIONS(
-        IntelLLVM -fopenmp-targets=spir64_gen -Xopenmp-target-backend "-device 12.60.7" -D__STRICT_ANSI__
+        IntelLLVM -fopenmp-targets=spir64_gen -Xopenmp-target-backend "-device 12.60.7"
     )
     ENDIF()
   ENDIF()
