8652249658862f77b8c75d9effb10587ced5f5ce
Daniel Arndt <arndtd@ornl.gov>
2024-01-29 19:37:58 -0500

    KOKKOS_ASSERT that the subgroup range doesn't exceed 32

--- core/src/SYCL/Kokkos_SYCL_Team.hpp
+++ core/src/SYCL/Kokkos_SYCL_Team.hpp
@@ -146,6 +146,7 @@ class SYCLTeamMember {
     shuffle_combine(8);
     shuffle_combine(16);
     shuffle_combine(32);
+    KOKKOS_ASSERT(sub_group_range <= 32);
 #else
     for (unsigned int shift = 1; vector_range * shift < sub_group_range;
          shift <<= 1) {
