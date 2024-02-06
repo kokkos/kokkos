e704b22fe1dc0142eb21c8efa31f12980183395f
Christian Trott <crtrott@sandia.gov>
2024-02-06 09:34:28 -0700

    Fix whitespace

--- core/src/impl/Kokkos_Core.cpp
+++ core/src/impl/Kokkos_Core.cpp
@@ -691,7 +691,7 @@ void pre_initialize_internal(const Kokkos::InitializationSettings& settings) {
   declare_configuration_metadata("architecture", "CPU architecture",
                                  "AMD_ZEN3");
 #elif defined(KOKKOS_ARCH_RISCV_SG2042)
-  declare_configuration_metadata("architecture", "CPU architecture", 
+  declare_configuration_metadata("architecture", "CPU architecture",
                                  "SG2042 (RISC-V)")
 #else
   declare_configuration_metadata("architecture", "CPU architecture", "none");
