473cd53136d2f116626cb64face2383eb21f1913
Daniel Arndt <arndtd@ornl.gov>
2024-02-07 18:42:02 -0500

    Remove DummyPolicy

--- core/unit_test/TestOccupancyControlTrait.hpp
+++ core/unit_test/TestOccupancyControlTrait.hpp
@@ -18,12 +18,6 @@
 
 namespace {
 
-// Dummy policy for testing base class.
-template <class... Args>
-struct DummyPolicy : Kokkos::Impl::PolicyTraits<Args...> {
-  using execution_policy = DummyPolicy;
-};
-
 template <class... Properties>
 void test_policy_execution(const Kokkos::RangePolicy<Properties...>& policy) {
   Kokkos::parallel_for(policy, KOKKOS_LAMBDA(int){});
@@ -39,8 +33,6 @@ template <class... Properties>
 void test_policy_execution(const Kokkos::MDRangePolicy<Properties...>& policy) {
   Kokkos::parallel_for(policy, KOKKOS_LAMBDA(int, int){});
 }
-template <class... Properties>
-void test_policy_execution(const DummyPolicy<Properties...>&) {}
 
 template <class Policy>
 void test_prefer_desired_occupancy(Policy policy) {
@@ -56,19 +48,16 @@ void test_prefer_desired_occupancy(Policy policy) {
   // MaximizeOccupancy -> DesiredOccupancy
   auto const policy_with_occ =
       prefer(policy_still_no_occ, DesiredOccupancy{33});
-  EXPECT_EQ(policy_with_occ.impl_get_desired_occupancy().value(), 33);
   test_policy_execution(policy_with_occ);
 
   // DesiredOccupancy -> DesiredOccupancy
   auto const policy_change_occ = prefer(policy_with_occ, DesiredOccupancy{24});
-  EXPECT_EQ(policy_change_occ.impl_get_desired_occupancy().value(), 24);
   test_policy_execution(policy_change_occ);
 
   // DesiredOccupancy -> DesiredOccupancy w/ hint
   auto policy_with_occ_and_hint = Kokkos::Experimental::require(
       policy_change_occ,
       Kokkos::Experimental::WorkItemProperty::HintLightWeight);
-  EXPECT_EQ(policy_with_occ_and_hint.impl_get_desired_occupancy().value(), 24);
   test_policy_execution(policy_with_occ_and_hint);
 
   // DesiredOccupancy -> MaximizeOccupancy
@@ -81,7 +70,6 @@ void test_prefer_desired_occupancy(Policy policy) {
 // This test doesn't compile with CUDA on Windows
 #if !(defined(_WIN32) && defined(KOKKOS_ENABLE_CUDA))
 TEST(TEST_CATEGORY, occupancy_control) {
-  test_prefer_desired_occupancy(DummyPolicy<TEST_EXECSPACE>{});
   test_prefer_desired_occupancy(Kokkos::RangePolicy<TEST_EXECSPACE>(0, 1));
   test_prefer_desired_occupancy(
       Kokkos::TeamPolicy<TEST_EXECSPACE>{1, Kokkos::AUTO});
