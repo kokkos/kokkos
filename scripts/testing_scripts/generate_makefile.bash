95f70b3f4c843f0816fda8e824663667ebe3f460
Damien L-G <dalg24@gmail.com>
2024-02-07 18:44:36 -0500

    Remove support for memkind

--- scripts/testing_scripts/generate_makefile.bash
+++ scripts/testing_scripts/generate_makefile.bash
@@ -59,9 +59,6 @@ do
     --with-hwloc*)
       HWLOC_PATH="${key#*=}"
       ;;
-    --with-memkind*)
-      MEMKIND_PATH="${key#*=}"
-      ;;
     --arch*)
       KOKKOS_ARCH="${key#*=}"
       ;;
@@ -177,7 +174,6 @@ do
       echo "--with-gtest=/Path/To/Gtest:  Set path to gtest.  (Used in unit and performance"
       echo "                                tests.)"
       echo "--with-hwloc=/Path/To/Hwloc:  Set path to hwloc library."
-      echo "--with-memkind=/Path/To/MemKind:  Set path to memkind library."
       echo "--with-options=[OPT]:         Additional options to Kokkos:"
       echo "                                compiler_warnings"
       echo "                                aggressive_vectorization = add ivdep on loops"
@@ -267,11 +263,6 @@ fi
 if [ ${#HWLOC_PATH} -gt 0 ]; then
   KOKKOS_SETTINGS="${KOKKOS_SETTINGS} HWLOC_PATH=${HWLOC_PATH}"
   KOKKOS_USE_TPLS="${KOKKOS_USE_TPLS},hwloc"
-fi
-
-if [ ${#MEMKIND_PATH} -gt 0 ]; then
-  KOKKOS_SETTINGS="${KOKKOS_SETTINGS} MEMKIND_PATH=${MEMKIND_PATH}" 
-  KOKKOS_USE_TPLS="${KOKKOS_USE_TPLS},experimental_memkind"
 fi
 
 if [ ${#KOKKOS_USE_TPLS} -gt 0 ]; then
