3e405209dc52b6629e3c2e2ae9af10d5a435fa7b
Patrick Diehl <diehlpk@users.noreply.github.com>
2024-02-08 10:28:08 -0600

    Add support for RISCV and the Milk-V's Pioneer (#6773)
    Add RISC-V Milk-V's Pioneer to the arch list
    
    ---------
    
    Co-authored-by: Christian Trott <crtrott@sandia.gov>
    Co-authored-by: Damien L-G <dalg24@gmail.com>
    Co-authored-by: Damien L-G <dalg24+github@gmail.com>
    Co-authored-by: Daniel Arndt <arndtd@ornl.gov>

--- core/src/impl/Kokkos_Core.cpp
+++ core/src/impl/Kokkos_Core.cpp
@@ -678,6 +678,9 @@ void pre_initialize_internal(const Kokkos::InitializationSettings& settings) {
 #elif defined(KOKKOS_ARCH_AMD_ZEN3)
   declare_configuration_metadata("architecture", "CPU architecture",
                                  "AMD_ZEN3");
+#elif defined(KOKKOS_ARCH_RISCV_SG2042)
+  declare_configuration_metadata("architecture", "CPU architecture",
+                                 "SG2042 (RISC-V)")
 #else
   declare_configuration_metadata("architecture", "CPU architecture", "none");
 #endif
@@ -756,8 +759,8 @@ void pre_initialize_internal(const Kokkos::InitializationSettings& settings) {
   declare_configuration_metadata("architecture", "GPU architecture",
                                  "AMD_GFX908");
 #elif defined(KOKKOS_ARCH_AMD_GFX90A)
-  declare_configuration_metadata("architecture", "GPU architecture",
-                                 "AMD_GFX90A");
+      declare_configuration_metadata("architecture", "GPU architecture",
+                                     "AMD_GFX90A");
 #elif defined(KOKKOS_ARCH_AMD_GFX1030)
   declare_configuration_metadata("architecture", "GPU architecture",
                                  "AMD_GFX1030");
