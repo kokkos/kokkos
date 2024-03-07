07e2d4f10f95f73e43fb9d4faadf7d3dec03ddf7
Damien L-G <dalg24@gmail.com>
2024-03-07 17:56:02 -0500

    Fixup OffsetView out-of-bounds on device

--- containers/src/Kokkos_OffsetView.hpp
+++ containers/src/Kokkos_OffsetView.hpp
@@ -124,15 +124,7 @@ KOKKOS_INLINE_FUNCTION void offsetview_verify_operator_bounds(
                                              args...);
          Kokkos::Impl::throw_runtime_exception(std::string(buffer));))
 
-    KOKKOS_IF_ON_DEVICE((
-        /* Check #1: is there a SharedAllocationRecord?
-          (we won't use it, but if it is not there then there isn't
-           a corresponding SharedAllocationHeader containing a label).
-          This check should cover the case of Views that don't
-          have the Unmanaged trait but were initialized by pointer. */
-        if (tracker.has_record()) {
-          Kokkos::abort("OffsetView bounds error");  // FIXME
-        } else { Kokkos::abort("OffsetView bounds error"); }))
+    KOKKOS_IF_ON_DEVICE((Kokkos::abort("OffsetView bounds error");))
   }
 }
 
