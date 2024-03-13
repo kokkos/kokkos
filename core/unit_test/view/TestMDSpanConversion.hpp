84b7c3cc49d0368c77ecb9d602ab6c68e2475e44
Nicolas Morales <nmmoral@sandia.gov>
2024-03-12 17:33:56 -0700

    #6805: fix formatting

--- core/unit_test/view/TestMDSpanConversion.hpp
+++ core/unit_test/view/TestMDSpanConversion.hpp
@@ -83,7 +83,7 @@ struct TestViewMDSpanConversion {
   template <class MDSpanLayoutMapping, class ViewType>
   static void test_conversion_to_mdspan(
       const MDSpanLayoutMapping &ref_layout_mapping, ViewType v) {
-    using view_type = ViewType;
+    using view_type           = ViewType;
     using natural_mdspan_type = typename Kokkos::Impl::MDSpanViewTraits<
         typename view_type::traits>::mdspan_type;
 
