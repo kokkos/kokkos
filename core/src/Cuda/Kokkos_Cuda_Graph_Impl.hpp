ea68e86b5fbe65a9b99040f24cbe605fea7e16ab
Daniel Arndt <arndtd@ornl.gov>
2024-04-02 20:07:10 +0000

    Clean up some static_asserts

--- core/src/Cuda/Kokkos_Cuda_Graph_Impl.hpp
+++ core/src/Cuda/Kokkos_Cuda_Graph_Impl.hpp
@@ -117,10 +117,7 @@ struct GraphImpl<Kokkos::Cuda> {
   //  requires NodeImplPtr is a shared_ptr to specialization of GraphNodeImpl
   //  Also requires that the kernel has the graph node tag in it's policy
   void add_node(std::shared_ptr<NodeImpl> const& arg_node_ptr) {
-    static_assert(
-        NodeImpl::kernel_type::Policy::is_graph_kernel::value,
-        "Something has gone horribly wrong, but it's too complicated to "
-        "explain here.  Buy Daisy a coffee and she'll explain it to you.");
+    static_assert(NodeImpl::kernel_type::Policy::is_graph_kernel::value);
     KOKKOS_EXPECTS(bool(arg_node_ptr));
     // The Kernel launch from the execute() method has been shimmed to insert
     // the node into the graph
