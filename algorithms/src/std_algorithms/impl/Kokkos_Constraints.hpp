2acd84fc9a6ecbac2d115ff0266417f933e59170
Yuuichi Asahi <yuuichi.asahi@cea.fr>
2024-04-05 18:18:52 +0200

    add else if for the opposite case

--- algorithms/src/std_algorithms/impl/Kokkos_Constraints.hpp
+++ algorithms/src/std_algorithms/impl/Kokkos_Constraints.hpp
@@ -220,12 +220,15 @@ KOKKOS_INLINE_FUNCTION void expect_valid_range(IteratorType first,
 //
 template <typename IteratorType1, typename IteratorType2>
 KOKKOS_INLINE_FUNCTION void expect_no_overlap(IteratorType1 first,
-                                              IteratorType1 last, 
+                                              IteratorType1 last,
                                               IteratorType2 s_first,
                                               IteratorType2 s_last) {
-  if constexpr( std::is_constructible_v<IteratorType2, IteratorType1> ) {
+  if constexpr (std::is_constructible_v<IteratorType2, IteratorType1>) {
     IteratorType1 s_first1(s_first), s_last1(s_last);
     KOKKOS_EXPECTS(first < s_first1 && s_last1 < last);
+  } else if constexpr (std::is_constructible_v<IteratorType1, IteratorType2>) {
+    IteratorType2 first2(first), last2(last);
+    KOKKOS_EXPECTS(first2 < s_first && s_last < last2);
   }
 }
 
