3be3bc8403ec0f32e0f1a0c928d07ccee54917db
Yuuichi Asahi <yuuichi.asahi@cea.fr>
2024-04-12 13:54:34 +0200

    fix converting constructor in Kokkos::RandomAccessIterator

--- algorithms/src/std_algorithms/impl/Kokkos_RandomAccessIterator.hpp
+++ algorithms/src/std_algorithms/impl/Kokkos_RandomAccessIterator.hpp
@@ -61,7 +61,7 @@ class RandomAccessIterator< ::Kokkos::View<DataType, Args...> > {
 
   template <
       class ViewType,
-      std::enable_if_t<std::is_convertible_v<ViewType, view_type>, int> = 0>
+      std::enable_if_t<std::is_constructible_v<view_type, ViewType>, int> = 0>
   KOKKOS_FUNCTION RandomAccessIterator(
       const RandomAccessIterator<ViewType>& other)
       : m_view(other.m_view), m_current_index(other.m_current_index) {}
