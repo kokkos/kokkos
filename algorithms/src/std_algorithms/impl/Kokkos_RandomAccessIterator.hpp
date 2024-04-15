dbffaf396f27b5f936fe1c5727f244f988bbf4ba
Yuuichi Asahi <yuuichi.asahi@cea.fr>
2024-04-15 18:29:38 +0200

    On second thought `KOKKOS_IMPL_CONDITIONAL_EXPLICIT` is not such a good idea
    because it let user write code that would compile with C++17 but not
    with later standards.

--- algorithms/src/std_algorithms/impl/Kokkos_RandomAccessIterator.hpp
+++ algorithms/src/std_algorithms/impl/Kokkos_RandomAccessIterator.hpp
@@ -59,13 +59,29 @@ class RandomAccessIterator< ::Kokkos::View<DataType, Args...> > {
                                                 ptrdiff_t current_index)
       : m_view(view), m_current_index(current_index) {}
 
-  template <class OtherViewType,
-            std::enable_if_t<std::is_constructible_v<view_type, OtherViewType>,
-                             int> = 0>
-  KOKKOS_FUNCTION KOKKOS_IMPL_CONDITIONAL_EXPLICIT(
-      !std::is_convertible_v<OtherViewType, view_type>)
-      RandomAccessIterator(const RandomAccessIterator<OtherViewType>& other)
+#ifndef KOKKOS_ENABLE_CXX17  // C++20 and beyond
+  template <class OtherViewType>
+  requires(std::is_constructible_v<view_type, OtherViewType>) KOKKOS_FUNCTION
+      explicit(!std::is_convertible_v<OtherViewType, view_type>)
+          RandomAccessIterator(const RandomAccessIterator<OtherViewType>& other)
       : m_view(other.m_view), m_current_index(other.m_current_index) {}
+#else
+  template <
+      class OtherViewType,
+      std::enable_if_t<std::is_constructible_v<view_type, OtherViewType> &&
+                           !std::is_convertible_v<OtherViewType, view_type>,
+                       int> = 0>
+  KOKKOS_FUNCTION explicit RandomAccessIterator(
+      const RandomAccessIterator<OtherViewType>& other)
+      : m_view(other.m_view), m_current_index(other.m_current_index) {}
+
+  template <class OtherViewType,
+            std::enable_if_t<std::is_convertible_v<OtherViewType, view_type>,
+                             int> = 0>
+  KOKKOS_FUNCTION RandomAccessIterator(
+      const RandomAccessIterator<OtherViewType>& other)
+      : m_view(other.m_view), m_current_index(other.m_current_index) {}
+#endif
 
   KOKKOS_FUNCTION
   iterator_type& operator++() {
