72897646233424edcbbd11363620aa0228b3a7ec
Daniel Arndt <arndtd@ornl.gov>
2024-05-06 13:59:47 -0400

    Turn SYCL_RELOCATABLE_DEVICE_CODE off by default

--- cmake/kokkos_enable_options.cmake
+++ cmake/kokkos_enable_options.cmake
@@ -51,7 +51,7 @@ KOKKOS_ENABLE_OPTION(IMPL_NVHPC_AS_DEVICE_COMPILER OFF "Whether to allow nvc++ a
 KOKKOS_ENABLE_OPTION(DEPRECATED_CODE_4    ON "Whether code deprecated in major release 4 is available" )
 KOKKOS_ENABLE_OPTION(DEPRECATION_WARNINGS ON "Whether to emit deprecation warnings" )
 KOKKOS_ENABLE_OPTION(HIP_RELOCATABLE_DEVICE_CODE  OFF "Whether to enable relocatable device code (RDC) for HIP")
-KOKKOS_ENABLE_OPTION(SYCL_RELOCATABLE_DEVICE_CODE  ON "Whether to enable relocatable device code (RDC) for SYCL")
+KOKKOS_ENABLE_OPTION(SYCL_RELOCATABLE_DEVICE_CODE  OFF "Whether to enable relocatable device code (RDC) for SYCL")
 KOKKOS_ENABLE_OPTION(TESTS         OFF  "Whether to build the unit tests")
 KOKKOS_ENABLE_OPTION(BENCHMARKS    OFF  "Whether to build the benchmarks")
 KOKKOS_ENABLE_OPTION(EXAMPLES      OFF  "Whether to build the examples")
