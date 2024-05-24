bca7a65199faf460eb16dc700617b19c2aa9aca6
Nevin ":-)" Liber <nliber@anl.gov>
2024-05-23 20:52:22 -0500

    Added back to_Array tests

--- core/unit_test/TestArray.cpp
+++ core/unit_test/TestArray.cpp
@@ -160,4 +160,23 @@ constexpr bool test_array_specialization_kokkos_swap() {
 
 static_assert(test_array_specialization_kokkos_swap());
 
+KOKKOS_FUNCTION constexpr bool test_to_array() {
+  // copies a string literal
+  auto a1 = Kokkos::to_Array("foo");
+  static_assert(a1.size() == 4);
+
+  // deduces both element type and length
+  auto a2 = Kokkos::to_Array({0, 2, 1, 3});
+  static_assert(std::is_same_v<decltype(a2), Kokkos::Array<int, 4>>);
+
+  // deduces length with element type specified
+  // implicit conversion happens
+  auto a3 = Kokkos::to_Array<long>({0, 1, 3});
+  static_assert(std::is_same_v<decltype(a3), Kokkos::Array<long, 3>>);
+
+  return true;
+}
+
+static_assert(test_to_array());
+
 }  // namespace
