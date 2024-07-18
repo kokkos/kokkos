a71c6b61f3692468205afe456f28d445f3588d6e
Bruno Turcksin <bruno.turcksin@gmail.com>
2024-07-18 09:56:54 -0400

    Use hipMallocAsync when using HIP execution space

--- core/src/HIP/Kokkos_HIP_Space.cpp
+++ core/src/HIP/Kokkos_HIP_Space.cpp
@@ -51,35 +51,51 @@ static std::atomic<bool> is_first_hip_managed_allocation(true);
 
 namespace Kokkos {
 
-HIPSpace::HIPSpace() : m_device(HIP().hip_device()) {}
+HIPSpace::HIPSpace()
+    : m_device(HIP().hip_device()), m_stream(Kokkos::HIP().hip_stream()) {}
 
 HIPHostPinnedSpace::HIPHostPinnedSpace() {}
 
 HIPManagedSpace::HIPManagedSpace() : m_device(HIP().hip_device()) {}
 
+void* HIPSpace::allocate(const HIP& exec_space,
+                         const size_t arg_alloc_size) const {
+  return allocate(exec_space, "[unlabeled]", arg_alloc_size);
+}
+
+void* HIPSpace::allocate(const HIP& exec_space, const char* arg_label,
+                         const size_t arg_alloc_size,
+                         const size_t arg_logical_size) const {
+  return impl_allocate(exec_space.hip_stream(), arg_label, arg_alloc_size,
+                       arg_logical_size, true);
+}
+
 void* HIPSpace::allocate(const size_t arg_alloc_size) const {
   return allocate("[unlabeled]", arg_alloc_size);
 }
-void* HIPSpace::allocate(
 
-    const char* arg_label, const size_t arg_alloc_size,
-    const size_t arg_logical_size) const {
-  return impl_allocate(arg_label, arg_alloc_size, arg_logical_size);
+void* HIPSpace::allocate(const char* arg_label, const size_t arg_alloc_size,
+                         const size_t arg_logical_size) const {
+  return impl_allocate(m_stream, arg_label, arg_alloc_size, arg_logical_size,
+                       false);
 }
-void* HIPSpace::impl_allocate(
-    const char* arg_label, const size_t arg_alloc_size,
-    const size_t arg_logical_size,
-    const Kokkos::Tools::SpaceHandle arg_handle) const {
+
+void* HIPSpace::impl_allocate(const hipStream_t stream, const char* arg_label,
+                              const size_t arg_alloc_size,
+                              const size_t arg_logical_size,
+                              const bool stream_sync_only) const {
   void* ptr = nullptr;
 
-  auto const error_code = hipMalloc(&ptr, arg_alloc_size);
-  if (error_code != hipSuccess) {
-    // This is the only way to clear the last error, which we should do here
-    // since we're turning it into an exception here
-    (void)hipGetLastError();
-    Kokkos::Impl::throw_bad_alloc(name(), arg_alloc_size, arg_label);
+  KOKKOS_IMPL_HIP_SAFE_CALL(hipMallocAsync(&ptr, arg_alloc_size, stream));
+  if (stream_sync_only) {
+    KOKKOS_IMPL_HIP_SAFE_CALL(hipStreamSynchronize(stream));
+  } else {
+    KOKKOS_IMPL_HIP_SAFE_CALL(hipDeviceSynchronize());
   }
+
   if (Kokkos::Profiling::profileLibraryLoaded()) {
+    const Kokkos::Tools::SpaceHandle arg_handle =
+        Kokkos::Tools::make_space_handle(name());
     const size_t reported_size =
         (arg_logical_size > 0) ? arg_logical_size : arg_alloc_size;
     Kokkos::Profiling::allocateData(arg_handle, arg_label, ptr, reported_size);
