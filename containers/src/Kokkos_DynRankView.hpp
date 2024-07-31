6357ce5360d49b143f29ba5d2cdabeee1259d107
Nathan Ellingwood <ndellin@sandia.gov>
2024-07-31 17:37:36 -0600

    Update containers/src/Kokkos_DynRankView.hpp
    Co-authored-by: Daniel Arndt <arndtd@ornl.gov>

--- containers/src/Kokkos_DynRankView.hpp
+++ containers/src/Kokkos_DynRankView.hpp
@@ -2045,7 +2045,7 @@ inline auto create_mirror_view(
       return Kokkos::Impl::choose_create_mirror(src, arg_prop);
     }
   }
-#if defined(KOKKOS_COMPILER_INTEL) || defined(KOKKOS_COMPILER_NVCC)
+#if defined(KOKKOS_COMPILER_INTEL) || (defined(KOKKOS_COMPILER_NVCC) && KOKKOS_COMPILER_NVCC >= 1130)
   __builtin_unreachable();
 #endif
 }
