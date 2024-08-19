405a76cacc53234cd03057f2fd013f12d289e3dc
Seyong Lee <lees2@ornl.gov>
2024-08-19 12:05:26 -0400

    Apply clang-format

--- core/src/OpenACC/Kokkos_OpenACC_Traits.hpp
+++ core/src/OpenACC/Kokkos_OpenACC_Traits.hpp
@@ -23,20 +23,20 @@ namespace Kokkos::Experimental::Impl {
 
 struct OpenACC_Traits {
 #if defined(KOKKOS_IMPL_ARCH_NVIDIA_GPU)
-  static constexpr acc_device_t dev_type            = acc_device_nvidia;
-  static constexpr bool may_fallback_to_host        = false;
+  static constexpr acc_device_t dev_type     = acc_device_nvidia;
+  static constexpr bool may_fallback_to_host = false;
 #elif defined(KOKKOS_ARCH_AMD_GPU)
-  static constexpr acc_device_t dev_type            = acc_device_radeon;
-  static constexpr bool may_fallback_to_host        = false;
+  static constexpr acc_device_t dev_type     = acc_device_radeon;
+  static constexpr bool may_fallback_to_host = false;
 #if defined(KOKKOS_ARCH_AMD_GFX1030) || defined(KOKKOS_ARCH_AMD_GFX1100)
 #else
 #endif
 #elif defined(KOKKOS_ENABLE_OPENACC_FORCE_HOST_AS_DEVICE)
-  static constexpr acc_device_t dev_type            = acc_device_host;
-  static constexpr bool may_fallback_to_host        = true;
+  static constexpr acc_device_t dev_type     = acc_device_host;
+  static constexpr bool may_fallback_to_host = true;
 #else
-  static constexpr acc_device_t dev_type            = acc_device_not_host;
-  static constexpr bool may_fallback_to_host        = true;
+  static constexpr acc_device_t dev_type     = acc_device_not_host;
+  static constexpr bool may_fallback_to_host = true;
 #endif
 };
 
