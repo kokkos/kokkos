d575bdd34a8b72495b948d9711cc053a5ffaf8f0
Damien L-G <dalg24+github@gmail.com>
2024-08-22 09:10:47 -0400

    Fix format

--- containers/unit_tests/TestWithoutInitializing.hpp
+++ containers/unit_tests/TestWithoutInitializing.hpp
@@ -277,7 +277,7 @@ TEST(TEST_CATEGORY, resize_realloc_no_init_scatterview) {
   using namespace Kokkos::Test::Tools;
   listen_tool_events(Config::DisableAll(), Config::EnableKernels());
   Kokkos::Experimental::ScatterView<
-      int*** * [1][2][3], typename TEST_EXECSPACE::array_layout, TEST_EXECSPACE>
+      int**** [1][2][3], typename TEST_EXECSPACE::array_layout, TEST_EXECSPACE>
       bla("bla", 4, 5, 6, 7);
 
   auto success = validate_absence(
@@ -309,7 +309,7 @@ TEST(TEST_CATEGORY, resize_realloc_no_alloc_scatterview) {
   listen_tool_events(Config::DisableAll(), Config::EnableKernels(),
                      Config::EnableAllocs());
   Kokkos::Experimental::ScatterView<
-      int*** * [1][2][3], typename TEST_EXECSPACE::array_layout, TEST_EXECSPACE>
+      int**** [1][2][3], typename TEST_EXECSPACE::array_layout, TEST_EXECSPACE>
       bla("bla", 7, 6, 5, 4);
 
   auto success = validate_absence(
@@ -340,7 +340,7 @@ TEST(TEST_CATEGORY, resize_exec_space_scatterview) {
   listen_tool_events(Config::DisableAll(), Config::EnableFences(),
                      Config::EnableKernels());
   Kokkos::Experimental::ScatterView<
-      int*** * [1][2][3], typename TEST_EXECSPACE::array_layout, TEST_EXECSPACE>
+      int**** [1][2][3], typename TEST_EXECSPACE::array_layout, TEST_EXECSPACE>
       bla("bla", 7, 6, 5, 4);
 
   auto success = validate_absence(
