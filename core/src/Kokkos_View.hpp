20953f17c7bce2368c1ca22b9b439d58b577121d
Christian Trott <crtrott@sandia.gov>
2024-08-23 10:58:56 -0600

    Add new Kokkos_View.hpp to include Kokkos_ViewLegacy.hpp

---
+++ core/src/Kokkos_View.hpp
@@ -0,0 +1,27 @@
+//@HEADER
+// ************************************************************************
+//
+//                        Kokkos v. 4.0
+//       Copyright (2022) National Technology & Engineering
+//               Solutions of Sandia, LLC (NTESS).
+//
+// Under the terms of Contract DE-NA0003525 with NTESS,
+// the U.S. Government retains certain rights in this software.
+//
+// Part of Kokkos, under the Apache License v2.0 with LLVM Exceptions.
+// See https://kokkos.org/LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//@HEADER
+
+#ifndef KOKKOS_IMPL_PUBLIC_INCLUDE
+#include <Kokkos_Macros.hpp>
+static_assert(false,
+              "Including non-public Kokkos header files is not allowed.");
+#endif
+#ifndef KOKKOS_VIEW_HPP
+#define KOKKOS_VIEW_HPP
+
+#include <View/Kokkos_ViewLegacy.hpp>
+
+#endif /* KOKKOS_VIEW_HPP */
