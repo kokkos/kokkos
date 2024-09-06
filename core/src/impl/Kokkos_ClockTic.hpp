57c86dccae7680df4d789ccd2e8d085e2a9151ed
Daniel Arndt <arndtd@ornl.gov>
2024-09-06 16:02:40 -0400

    Indentation

--- core/src/impl/Kokkos_ClockTic.hpp
+++ core/src/impl/Kokkos_ClockTic.hpp
@@ -57,7 +57,7 @@ KOKKOS_IMPL_DEVICE_FUNCTION inline uint64_t clock_tic_device() noexcept {
   return clock64();
 
 // FIXME_SYCL We can only return something useful for Intel GPUs and with RDC
-#elif defined(KOKKOS_ENABLE_SYCL) &&                            \
+#elif defined(KOKKOS_ENABLE_SYCL) &&                       \
     defined(KOKKOS_ENABLE_SYCL_RELOCATABLE_DEVICE_CODE) && \
     defined(KOKKOS_ARCH_INTEL_GPU) && defined(__SYCL_DEVICE_ONLY__)
 
