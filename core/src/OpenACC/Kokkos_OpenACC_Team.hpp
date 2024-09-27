6c0f71eadd2c308b7d17e4df098e7a2ed2ffa132
Seyong Lee <lees2@ornl.gov>
2024-09-27 16:35:49 -0400

    Hide unused OpenACCTeamMember contructor as suggested by the code review.
    Signed-off-by: Seyong Lee <lees2@ornl.gov>

--- core/src/OpenACC/Kokkos_OpenACC_Team.hpp
+++ core/src/OpenACC/Kokkos_OpenACC_Team.hpp
@@ -125,6 +125,8 @@ class OpenACCTeamMember {
   // FIXME_OPENACC - 512(16*32) bytes at the begining of the scratch space
   // for each league is saved for reduction. It should actually be based on the
   // ValueType of the reduction variable.
+#if !defined(KOKKOS_ENABLE_OPENACC_FORCE_HOST_AS_DEVICE) || \
+    !defined(KOKKOS_ENABLE_OPENACC_COLLAPSE_HIERARCHICAL_CONSTRUCTS)
   OpenACCTeamMember(const int league_rank, const int league_size,
                     const int team_size,
                     const int vector_length)  // const TeamPolicyInternal<
@@ -141,7 +143,7 @@ class OpenACCTeamMember {
     m_team_rank = 0;
 #endif
   }
-
+#else
   OpenACCTeamMember(const int league_rank, const int league_size,
                     const int team_rank, const int team_size,
                     const int vector_length)
@@ -150,6 +152,7 @@ class OpenACCTeamMember {
         m_team_rank(team_rank),
         m_team_size(team_size),
         m_vector_length(vector_length) {}
+#endif
 
   static int team_reduce_size() { return TEAM_REDUCE_SIZE; }
 };
