89ff6250c5ffa056c56781deba15a46eac6a5aa6
Bruno Turcksin <bruno.turcksin@gmail.com>
2024-10-03 16:03:48 -0400

    Add named variables in the declaration of allocate

--- core/src/HIP/Kokkos_HIP_Space.hpp
+++ core/src/HIP/Kokkos_HIP_Space.hpp
@@ -77,8 +77,9 @@ class HIPSpace {
     return allocate(arg_label, arg_alloc_size, arg_logical_size);
   }
 #else
-  void* allocate(const HIP&, const size_t arg_alloc_size) const;
-  void* allocate(const HIP&, const char* arg_label, const size_t arg_alloc_size,
+  void* allocate(const HIP& exec_space, const size_t arg_alloc_size) const;
+  void* allocate(const HIP& exec_space, const char* arg_label,
+                 const size_t arg_alloc_size,
                  const size_t arg_logical_size = 0) const;
 #endif
   void* allocate(const size_t arg_alloc_size) const;
