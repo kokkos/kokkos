320b4d7ade3480f708e31a2fb6c8d67b245f51de
Christian Trott <crtrott@sandia.gov>
2024-10-10 12:40:50 -0600

    Add new View implementation
    Co-authored-by: Nicolas Morales <nmmoral@sandia.gov>

--- cmake/kokkos_enable_options.cmake
+++ cmake/kokkos_enable_options.cmake
@@ -103,6 +103,17 @@ mark_as_advanced(Kokkos_ENABLE_IMPL_MDSPAN)
 mark_as_advanced(Kokkos_ENABLE_MDSPAN_EXTERNAL)
 mark_as_advanced(Kokkos_ENABLE_IMPL_SKIP_COMPILER_MDSPAN)
 
+if(Kokkos_ENABLE_IMPL_MDSPAN)
+  set(VIEW_LEGACY_DEFAULT OFF)
+else()
+  set(VIEW_LEGACY_DEFAULT ON)
+endif()
+kokkos_enable_option(IMPL_VIEW_LEGACY ${VIEW_LEGACY_DEFAULT} "Whether to use the legacy implementation of View")
+mark_as_advanced(Kokkos_ENABLE_IMPL_VIEW_LEGACY)
+if(NOT Kokkos_ENABLE_IMPL_VIEW_LEGACY AND NOT Kokkos_ENABLE_IMPL_MDSPAN)
+  message(FATAL_ERROR "MDSpan must be enabled to use the new View implementation")
+endif()
+
 kokkos_enable_option(COMPLEX_ALIGN ON "Whether to align Kokkos::complex to 2*alignof(RealType)")
 
 if(KOKKOS_ENABLE_TESTS)
