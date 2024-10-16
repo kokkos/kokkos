d0244b0d8b8cacf59426f4440fc9b4ea647f8d61
Nicolas Morales <nmmoral@sandia.gov>
2024-10-16 11:08:06 -0400

    fix formatting
    Signed-off-by: Nicolas Morales <nmmoral@sandia.gov>

--- core/src/Kokkos_View.hpp
+++ core/src/Kokkos_View.hpp
@@ -210,7 +210,7 @@ class View : public Impl::BasicViewFromTraits<DataType, Properties...>::type {
            typename traits::device_type, typename traits::hooks_policy,
            typename traits::memory_traits>;
 
-  // Compatible HostMirror view 
+  // Compatible HostMirror view
   using host_mirror_type =
       View<typename traits::non_const_data_type, typename traits::array_layout,
            Device<DefaultHostExecutionSpace,
@@ -266,8 +266,8 @@ class View : public Impl::BasicViewFromTraits<DataType, Properties...>::type {
                                                     size_t>
   stride(iType r) const {
     // base class doesn't have constraint
-  // FIXME: Eventually we need to deprecate this behavior and just use
-  // BasicView implementation
+    // FIXME: Eventually we need to deprecate this behavior and just use
+    // BasicView implementation
     return base_t::stride(r);
   }
 
@@ -315,8 +315,7 @@ class View : public Impl::BasicViewFromTraits<DataType, Properties...>::type {
 
   KOKKOS_INLINE_FUNCTION
   const Kokkos::Impl::SharedAllocationTracker& impl_track() const {
-    if constexpr ( traits::is_managed )
-    {
+    if constexpr (traits::is_managed) {
       return base_t::data_handle().tracker();
     } else {
       return {};
