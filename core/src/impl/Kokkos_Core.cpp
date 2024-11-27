ebfa43c7a904353caffee4800ebbdc2c5e1e842e
Damien L-G <dalg24@gmail.com>
2024-11-27 09:24:58 -0500

    Format glitch

--- core/src/impl/Kokkos_Core.cpp
+++ core/src/impl/Kokkos_Core.cpp
@@ -747,14 +747,14 @@ void pre_initialize_internal(const Kokkos::InitializationSettings& settings) {
   declare_configuration_metadata("architecture", "GPU architecture",
                                  "HOPPER90");
 #elif defined(KOKKOS_ARCH_AMD_GFX906)
-      declare_configuration_metadata("architecture", "GPU architecture",
-                                     "AMD_GFX906");
+  declare_configuration_metadata("architecture", "GPU architecture",
+                                 "AMD_GFX906");
 #elif defined(KOKKOS_ARCH_AMD_GFX908)
       declare_configuration_metadata("architecture", "GPU architecture",
                                      "AMD_GFX908");
 #elif defined(KOKKOS_ARCH_AMD_GFX90A)
-  declare_configuration_metadata("architecture", "GPU architecture",
-                                 "AMD_GFX90A");
+      declare_configuration_metadata("architecture", "GPU architecture",
+                                     "AMD_GFX90A");
 #elif defined(KOKKOS_ARCH_AMD_GFX1030)
   declare_configuration_metadata("architecture", "GPU architecture",
                                  "AMD_GFX1030");
