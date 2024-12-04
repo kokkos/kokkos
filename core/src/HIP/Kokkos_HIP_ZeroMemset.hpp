c229388b67dd8c52c3da3e4e3a44bcf2627985e0
Carl Pearson <cwpears@sandia.gov>
2024-12-04 15:46:26 -0800

    hip: require xnack for AMD_GPU_GFX_942_APU
    Kokkos allows a user to wrap a `malloc`ed pointer into
    an unmanaged SharedSpace view on HIP, so ZeroMemset
    needs to work. Calling hipMemset on such a pointer
    is a runtime error in rocm <= 6.2.1 (and possibly later),
    so provide an alternate path that uses a GPU kernel
    to set the memory. For that to work, xnack must be enabled.
    
    In the Kokkos developer meeting 2024-12-11, it was decided
    that it was reasonable to require xnack for the MI300A, so
    error out of HIP initialize if its not enabled.
    
    Update the ZeroMemset test-case to use SharedSpace, as it was
    always intended to.
    
    Signed-off-by: Carl Pearson <cwpears@sandia.gov>

--- core/src/HIP/Kokkos_HIP_ZeroMemset.hpp
+++ core/src/HIP/Kokkos_HIP_ZeroMemset.hpp
@@ -29,9 +29,11 @@ void zero_with_hip_kernel(const HIP& exec_space, void* dst, size_t cnt);
 template <>
 struct ZeroMemset<HIP> {
   ZeroMemset(const HIP& exec_space, void* dst, size_t cnt) {
-    // in ROCm <= 6.2.0, hipMemsetAsync on a host-allocated pointer
-    // returns an invalid value error, but accessing the data via a
-    // GPU kernel works.
+    // We allow user on an AMD APU with unified memory to `malloc` and wrap that
+    // in an unmanaged SharedSpace view. In ROCm <= 6.2.1 (and possibly later),
+    // hipMemsetAsync on a host-allocated pointer returns an invalid value
+    // error, but accessing the data via a GPU kernel works as long as xnack is
+    // present and enabled (HSA_XNACK=1)
 #if defined(KOKKOS_IMPL_HIP_UNIFIED_MEMORY)
     zero_with_hip_kernel(exec_space, dst, cnt);
 #else
