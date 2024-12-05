196fb45e352390599f9bc88603015be8b1c74e9b
Daniel Arndt <arndtd@ornl.gov>
2024-12-05 15:37:59 -0500

    Attach build information

--- CTestRun.cmake
+++ CTestRun.cmake
@@ -4,6 +4,8 @@ set(CTEST_BINARY_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
 set(CTEST_SOURCE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
 
 set(CTEST_SUBMIT_URL https://my.cdash.org/submit.php?project=Kokkos)
+set(CTEST_NOTES_FILES "${CTEST_BINARY_DIRECTORY}/generated/Kokkos_Version_Info.cpp")
+message(STATUS "CTEST_NOTES_FILES: ${CTEST_NOTES_FILES}")
 
 ctest_start(Experimental)
 ctest_test(OUTPUT_JUNIT "${OUTPUT_JUNIT_FILE}")
