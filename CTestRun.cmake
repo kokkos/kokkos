9fe6e3b4311cfc736d1e7e00466415e6eb807bc2
Daniel Arndt <arndtd@ornl.gov>
2024-12-10 15:12:49 -0500

    Also record configuring and building

--- CTestRun.cmake
+++ CTestRun.cmake
@@ -1,17 +1,17 @@
 cmake_minimum_required(VERSION 3.20)
 
-set(CTEST_BINARY_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
+set(CTEST_BINARY_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/build)
 set(CTEST_SOURCE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
 
-set(CTEST_GIT_COMMAND git)
 set(CTEST_UPDATE_COMMAND git)
 set(CTEST_UPDATE_VERSION_ONLY 1)
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 
 set(CTEST_SUBMIT_URL https://my.cdash.org/submit.php?project=Kokkos)
-set(CTEST_NOTES_FILES "${CTEST_BINARY_DIRECTORY}/generated/Kokkos_Version_Info.cpp")
-message(STATUS "CTEST_NOTES_FILES: ${CTEST_NOTES_FILES}")
 
 ctest_start(Experimental)
 ctest_update()
+ctest_configure(OPTIONS "${CMAKE_OPTIONS}")
+ctest_build()
 ctest_test(OUTPUT_JUNIT "${OUTPUT_JUNIT_FILE}")
 ctest_submit()
