2d6c354bcd6c2f0bf9b3c91bb15f011fc9a4253d
Carl Pearson <cwpears@sandia.gov>
2024-12-18 08:24:06 -0800

    Remove Kokkos_IsXnack.hpp
    Signed-off-by: Carl Pearson <cwpears@sandia.gov>

--- core/src/HIP/Kokkos_HIP.cpp
+++ core/src/HIP/Kokkos_HIP.cpp
@@ -21,7 +21,6 @@
 #include <Kokkos_Core.hpp>
 #include <HIP/Kokkos_HIP.hpp>
 #include <HIP/Kokkos_HIP_Instance.hpp>
-#include <HIP/Kokkos_IsXnack.hpp>
 
 #include <impl/Kokkos_DeviceManagement.hpp>
 #include <impl/Kokkos_ExecSpaceManager.hpp>
