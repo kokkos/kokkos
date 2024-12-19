5ab882d10e742c84d7b0aa9a2567a6aa490dc49b
JBludau <jakob.bludau@tum.de>
2024-12-19 16:43:15 -0500

    bumped required version in CTestRun.cmake to allow JUNIT output

--- CTestRun.cmake
+++ CTestRun.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 3.16)
+cmake_minimum_required(VERSION 3.21) #JUNIT_FILE support requires 3.21
 
 set(CTEST_BINARY_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/build)
 set(CTEST_SOURCE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
