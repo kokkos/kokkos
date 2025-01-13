7324e8d4a787b394c1113266ab4865d3061451dd
Daniel Arndt <arndtd@ornl.gov>
2025-01-13 17:06:12 -0500

    Test CDash on HPSF GitLab CI

---
+++ CTestRun.cmake
@@ -0,0 +1,21 @@
+cmake_minimum_required(VERSION 3.16)
+
+set(CTEST_BINARY_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/build)
+set(CTEST_SOURCE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+
+set(CTEST_UPDATE_COMMAND git)
+set(CTEST_UPDATE_VERSION_ONLY 1)
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+
+file(REMOVE_RECURSE "${CTEST_BINARY_DIRECTORY}")
+
+ctest_start(${CDASH_MODEL})
+ctest_update()
+ctest_configure(OPTIONS "${CMAKE_OPTIONS}")
+ctest_build()
+if(OUTPUT_JUNIT_FILE)
+  ctest_test(OUTPUT_JUNIT "${OUTPUT_JUNIT_FILE}")
+else()
+  ctest_test()
+endif()
+ctest_submit()
