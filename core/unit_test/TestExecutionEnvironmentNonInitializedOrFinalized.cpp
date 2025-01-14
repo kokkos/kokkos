4120cc80e42a7cebf4868bd4cae0bd8446f8722d
Damien L-G <dalg24@gmail.com>
2025-01-14 18:03:04 -0500

    Fiddle some more with the matchers

--- core/unit_test/TestExecutionEnvironmentNonInitializedOrFinalized.cpp
+++ core/unit_test/TestExecutionEnvironmentNonInitializedOrFinalized.cpp
@@ -106,24 +106,26 @@ TEST_F(ExecutionEnvironmentNonInitializedOrFinalized_DeathTest, views) {
       },
       "Kokkos allocation \"v\" is being deallocated after Kokkos::finalize was "
       "called");
-  std::string matcher =
-#if defined(KOKKOS_ENABLE_CUDA) || defined(KOKKOS_ENABLE_HIP) || \
-    defined(KOKKOS_ENABLE_SYCL)
-      std::string("Kokkos::") + Kokkos::DefaultExecutionSpace::name() +
-      "::" + Kokkos::DefaultExecutionSpace::name() +
-      " instance constructor : ERROR device not initialized";
-#else
+  std::string matcher2 =
       "Constructing View and initializing data with uninitialized execution "
       "space";
+#if defined(KOKKOS_ENABLE_CUDA) || defined(KOKKOS_ENABLE_HIP) || \
+    defined(KOKKOS_ENABLE_SYCL)
+  std::string matcher1 = std::string("Kokkos::") +
+                         Kokkos::DefaultExecutionSpace::name() +
+                         "::" + Kokkos::DefaultExecutionSpace::name() +
+                         " instance constructor : ERROR device not initialized";
+#else
+  std::string matcher1 = matcher2;
 #endif
-  EXPECT_DEATH({ Kokkos::View<int*> v("v", 0); }, matcher);
+  EXPECT_DEATH({ Kokkos::View<int*> v("v", 0); }, matcher1);
   EXPECT_DEATH(
       {
         Kokkos::initialize();
         Kokkos::finalize();
         Kokkos::View<int*> v("v", 0);
       },
-      matcher);
+      matcher2);
 }
 
 }  // namespace
