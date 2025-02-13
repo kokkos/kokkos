764d93529f9bb306f71a17d7349a3cbfb562e7d1
Damien L-G <dalg24@gmail.com>
2025-02-13 09:49:32 -0500

    Revert "Revert "Merge pull request #7738 from dalg24/bugprone-unhandled-self-assignment"" (#7761)
    * Revert "Revert "Merge pull request #7738 from dalg24/bugprone-unhandled-self-assignment""
    
    * Do not return early in self-assignment for ROCm 5.5 and earlier
    
    * Fix guard
    
    ---------
    
    Co-authored-by: Bruno Turcksin <bruno.turcksin@gmail.com>

--- core/unit_test/TestMDRange.hpp
+++ core/unit_test/TestMDRange.hpp
@@ -3699,6 +3699,7 @@ struct TestMDRange_ReduceScalar {
     }
     KOKKOS_INLINE_FUNCTION
     void operator=(const Scalar &src) {
+      if (&src == this) return;
       for (int i = 0; i < 4; i++) v[i] = src.v[i];
     }
     KOKKOS_INLINE_FUNCTION
