f625c453e21f8fba3ab7aa2bf97520fa65794b84
Carl Pearson <cwpears@sandia.gov>
2025-02-20 10:28:04 -0700

    perf_test: [[maybe_unused]] ViewFill_Rank1Strided
    Static function not used in all perf_test TUs
    
    Signed-off-by: Carl Pearson <cwpears@sandia.gov>

--- core/perf_test/PerfTest_ViewFill.hpp
+++ core/perf_test/PerfTest_ViewFill.hpp
@@ -125,7 +125,7 @@ static void ViewFill_Raw(benchmark::State& state) {
   }
 }
 
-static void ViewFill_Rank1Strided(benchmark::State& state) {
+[[maybe_unused]] static void ViewFill_Rank1Strided(benchmark::State& state) {
   const int N8 = std::pow(state.range(0), 8);
 
   // allocate 2x the size since layout only has 1/2 the elements
