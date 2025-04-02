d9631b765887088ac0b74ed282bc0b1c9e378312
Damien L-G <dalg24@gmail.com>
2025-04-02 10:21:07 -0400

    Drop `Impl::int_log2` in favor of `bit_width` (#7943)
    * Prefer bit_width to Impl::int_log2
    * Prefer bit_width_builtin to int_log2 in Bitset
    * Drop Impl::int_log2()
    
    ---------
    
    Signed-off-by: Damien L-G <dalg24@gmail.com>

--- containers/src/Kokkos_Bitset.hpp
+++ containers/src/Kokkos_Bitset.hpp
@@ -273,7 +273,7 @@ class Bitset {
     block  = Impl::rotate_right(block, offset);
     return (((!(scan_direction & BIT_SCAN_REVERSE)
                   ? Impl::bit_scan_forward(block)
-                  : Impl::int_log2(block)) +
+                  : Experimental::bit_width_builtin(block) - 1) +
              offset) &
             block_mask) +
            block_start;
