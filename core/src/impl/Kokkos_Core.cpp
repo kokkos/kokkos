bfa5f185bad245d470a86870bbb282a065fbc626
Damien L-G <dalg24@gmail.com>
2025-04-24 15:45:47 -0400

    Apply Clang-Format
    Signed-off-by: Damien L-G <dalg24@gmail.com>

--- core/src/impl/Kokkos_Core.cpp
+++ core/src/impl/Kokkos_Core.cpp
@@ -745,14 +745,14 @@ void pre_initialize_internal(const Kokkos::InitializationSettings& settings) {
   declare_configuration_metadata("architecture", "GPU architecture",
                                  "BLACKWELL100");
 #elif defined(KOKKOS_ARCH_BLACKWELL120)
-  declare_configuration_metadata("architecture", "GPU architecture",
-                                 "BLACKWELL120");
+      declare_configuration_metadata("architecture", "GPU architecture",
+                                     "BLACKWELL120");
 #elif defined(KOKKOS_ARCH_AMD_GFX906)
       declare_configuration_metadata("architecture", "GPU architecture",
                                      "AMD_GFX906");
 #elif defined(KOKKOS_ARCH_AMD_GFX908)
-      declare_configuration_metadata("architecture", "GPU architecture",
-                                     "AMD_GFX908");
+  declare_configuration_metadata("architecture", "GPU architecture",
+                                 "AMD_GFX908");
 #elif defined(KOKKOS_ARCH_AMD_GFX90A)
   declare_configuration_metadata("architecture", "GPU architecture",
                                  "AMD_GFX90A");
