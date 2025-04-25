0a55025a8b5b01a00dfb321e472d5d08356a8061
Jakob Bludau <jakob.bludau@tum.de>
2025-04-25 00:38:33 -0400

    [DO NOT MERGE] use 128bit cas for >CC90
    Signed-off-by: Jakob Bludau <jakob.bludau@tum.de>

--- tpls/desul/include/desul/atomics/cuda/CUDA_asm_exchange.hpp
+++ tpls/desul/include/desul/atomics/cuda/CUDA_asm_exchange.hpp
@@ -2,5 +2,10 @@
 namespace desul {
 namespace Impl {
 #include <desul/atomics/cuda/cuda_cc7_asm_exchange.inc>
+
+#ifndef DESUL_CUDA_ARCH_IS_PRE_HOPPER
+// Hopper (CC90) and above has some 128 bit atomic support
+#include <desul/atomics/cuda/cuda_cc9_asm_exchange.inc>
+#endif
 }
 }  // namespace desul
