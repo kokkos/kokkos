b677862946a69aab833327befbffc254b86d411a
Jakob Bludau <jakob.bludau@tum.de>
2025-04-25 04:35:38 -0400

    guarding agains cuda<12.8
    Signed-off-by: Jakob Bludau <jakob.bludau@tum.de>

--- tpls/desul/include/desul/atomics/cuda/CUDA_asm_exchange.hpp
+++ tpls/desul/include/desul/atomics/cuda/CUDA_asm_exchange.hpp
@@ -1,9 +1,11 @@
 #include <limits>
 namespace desul {
 namespace Impl {
+#include <cuda.h>
+
 #include <desul/atomics/cuda/cuda_cc7_asm_exchange.inc>
 
-#ifndef DESUL_CUDA_ARCH_IS_PRE_HOPPER
+#if (CUDA_VERSION >= 12080) && not defined(DESUL_CUDA_ARCH_IS_PRE_HOPPER)
 // Hopper (CC90) and above has some 128 bit atomic support
 #include <desul/atomics/cuda/cuda_cc9_asm_exchange.inc>
 #endif
