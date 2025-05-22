3b70d065f1d4407f618d6c97c9824872b907a5d8
Dong Hun Lee <donlee@sandia.gov>
2025-05-21 21:11:42 -0600

    Fix simd reduction and reduction identity usage

--- simd/unit_tests/include/TestSIMD_Reductions.hpp
+++ simd/unit_tests/include/TestSIMD_Reductions.hpp
@@ -80,12 +80,16 @@ inline void host_check_all_reductions(const DataType (&args)[n]) {
   host_check_reduction_all_loaders<Abi>(reduce_max(), n, args);
   host_check_reduction_all_loaders<Abi>(reduce<std::plus<>>(), n, args);
   host_check_reduction_all_loaders<Abi>(reduce<std::multiplies<>>(), n, args);
+  host_check_reduction_all_loaders<Abi>(reduce<maximum>(), n, args);
+  host_check_reduction_all_loaders<Abi>(reduce<minimum>(), n, args);
 
   host_check_reduction_all_loaders<Abi>(masked_reduce_min(), n, args);
   host_check_reduction_all_loaders<Abi>(masked_reduce_max(), n, args);
   host_check_reduction_all_loaders<Abi>(masked_reduce<std::plus<>>(), n, args);
   host_check_reduction_all_loaders<Abi>(masked_reduce<std::multiplies<>>(), n,
                                         args);
+  host_check_reduction_all_loaders<Abi>(masked_reduce<maximum>(), n, args);
+  host_check_reduction_all_loaders<Abi>(masked_reduce<minimum>(), n, args);
 }
 
 template <typename Abi, typename DataType>
@@ -165,6 +169,8 @@ KOKKOS_INLINE_FUNCTION void device_check_all_reductions(
   device_check_reduction_all_loaders<Abi>(reduce_max(), n, args);
   device_check_reduction_all_loaders<Abi>(reduce<std::plus<>>(), n, args);
   device_check_reduction_all_loaders<Abi>(reduce<std::multiplies<>>(), n, args);
+  device_check_reduction_all_loaders<Abi>(reduce<maximum>(), n, args);
+  device_check_reduction_all_loaders<Abi>(reduce<minimum>(), n, args);
 
   device_check_reduction_all_loaders<Abi>(masked_reduce_min(), n, args);
   device_check_reduction_all_loaders<Abi>(masked_reduce_max(), n, args);
@@ -172,6 +178,8 @@ KOKKOS_INLINE_FUNCTION void device_check_all_reductions(
                                           args);
   device_check_reduction_all_loaders<Abi>(masked_reduce<std::multiplies<>>(), n,
                                           args);
+  device_check_reduction_all_loaders<Abi>(masked_reduce<maximum>(), n, args);
+  device_check_reduction_all_loaders<Abi>(masked_reduce<minimum>(), n, args);
 }
 
 template <typename Abi, typename DataType>
