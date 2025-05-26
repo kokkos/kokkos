6b81a0949ce4757a3cd740d68b23df6f7cbecaa6
Adrien Taberner <adrien.taberner@outlook.com>
2025-05-26 15:56:34 +0200

    split into multiple files
    Signed-off-by: Adrien Taberner <adrien.taberner@outlook.com>

---
+++ core/perf_test/PerfTest_ViewFirstTouch_Initialize.cpp
@@ -0,0 +1,39 @@
+//@HEADER
+// ************************************************************************
+//
+//                        Kokkos v. 4.0
+//       Copyright (2022) National Technology & Engineering
+//               Solutions of Sandia, LLC (NTESS).
+//
+// Under the terms of Contract DE-NA0003525 with NTESS,
+// the U.S. Government retains certain rights in this software.
+//
+// Part of Kokkos, under the Apache License v2.0 with LLVM Exceptions.
+// See https://kokkos.org/LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//@HEADER
+
+#include "PerfTest_ViewFirstTouch.hpp"
+
+namespace Test {
+
+BENCHMARK_TEMPLATE(ViewFirstTouch_Initialize, double)
+    ->ArgName("N")
+    ->RangeMultiplier(4)
+    ->Range(int64_t(1) << 4, int64_t(1) << 28)
+    ->UseManualTime();
+
+BENCHMARK_TEMPLATE(ViewFirstTouch_Initialize, float)
+    ->ArgName("N")
+    ->RangeMultiplier(4)
+    ->Range(int64_t(1) << 4, int64_t(1) << 28)
+    ->UseManualTime();
+
+BENCHMARK_TEMPLATE(ViewFirstTouch_Initialize, int)
+    ->ArgName("N")
+    ->RangeMultiplier(4)
+    ->Range(int64_t(1) << 4, int64_t(1) << 28)
+    ->UseManualTime();
+
+}  // namespace Test
