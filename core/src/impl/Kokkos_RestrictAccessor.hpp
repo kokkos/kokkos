010fce41b7300cd689655ebed45ff3ed7f77f853
Carl Pearson <cwpears@sandia.gov>
2025-06-05 08:58:47 -0600

    core/src/impl/Kokkos_RestrictAccessor.hpp
    Kokkos_RestrictAccessor.hpp: initial attempt
    
    Signed-off-by: Carl Pearson <cwpears@sandia.gov>

---
+++ core/src/impl/Kokkos_RestrictAccessor.hpp
@@ -0,0 +1,55 @@
+//@HEADER
+// ************************************************************************
+//
+//                        Kokkos v. 4.0
+//       Copyright (2022) National Technology & Engineering
+//               Solutions of Sandia, LLC (NTESS).
+//
+// Under the terms of Contract DE-NA0003525 with NTESS,
+// the U.S. Government retains certain rights in this software.
+//
+// Part of Kokkos, under the Apache License v2.0 with LLVM Exceptions.
+// See https://kokkos.org/LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//@HEADER
+
+#ifndef KOKKOS_RESTRICTACCESSOR_HPP
+#define KOKKOS_RESTRICTACCESSOR_HPP
+
+#include <Kokkos_Macros.hpp>
+
+namespace Kokkos::Impl {
+
+template <class ElementType>
+struct restrict_accessor {
+  using offset_policy    = restrict_accessor;
+  using element_type     = ElementType;
+  using reference        = ElementType& KOKKOS_RESTRICT;
+  using data_handle_type = ElementType* KOKKOS_RESTRICT;
+
+  KOKKOS_DEFAULTED_FUNCTION constexpr restrict_accessor() noexcept = default;
+
+  template <
+      class OtherElementType,
+      typename ::std::enable_if<
+          (std::is_convertible_v<OtherElementType (*)[], element_type (*)[]>),
+          int>::type = 0>
+  KOKKOS_INLINE_FUNCTION constexpr restrict_accessor(
+      restrict_accessor<OtherElementType>) noexcept {}
+
+  KOKKOS_INLINE_FUNCTION
+  constexpr data_handle_type offset(data_handle_type p,
+                                    size_t i) const noexcept {
+    return p + i;
+  }
+
+  KOKKOS_FORCEINLINE_FUNCTION
+  constexpr reference access(data_handle_type p, size_t i) const noexcept {
+    return p[i];
+  }
+};
+
+}  // namespace Kokkos::Impl
+
+#endif  // KOKKOS_RESTRICACCESSOR_HPP
