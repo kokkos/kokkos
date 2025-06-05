7fbfe1eb9b247d041b206d2e9cc0302bdc2bce50
tcclevenger <tccleve@sandia.gov>
2025-06-05 09:34:45 -0400

    Use OpenMP::concurrency instead of impl_ function
    Signed-off-by: tcclevenger <tccleve@sandia.gov>

--- core/unit_test/TestExecSpacePartitioning.hpp
+++ core/unit_test/TestExecSpacePartitioning.hpp
@@ -50,8 +50,8 @@ void check_distinctive([[maybe_unused]] ExecSpace exec1,
 #ifdef KOKKOS_ENABLE_OPENMP
   if constexpr (std::is_same_v<ExecSpace, Kokkos::OpenMP>) {
     ASSERT_NE(exec1, exec2);
-    ASSERT_EQ(ExecSpace().impl_thread_pool_size(),
-              exec1.impl_thread_pool_size() + exec2.impl_thread_pool_size());
+    ASSERT_EQ(ExecSpace().concurrency(),
+              exec1.concurrency() + exec2.concurrency());
   }
 #endif
 #ifdef KOKKOS_ENABLE_CUDA
