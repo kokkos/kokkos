036cc769b7cba9c45761fba6914a38287dd3ce73
Adrien Taberner <adrien.taberner@outlook.com>
2025-06-26 11:36:22 +0200

    Fix includes
    - No benchmark added to tests in debug mode
    - Takes concurrency into account in algorithm selection
    
    Signed-off-by: Adrien Taberner <adrien.taberner@outlook.com>

--- core/src/OpenMP/Kokkos_OpenMP_ZeroMemset.hpp
+++ core/src/OpenMP/Kokkos_OpenMP_ZeroMemset.hpp
@@ -17,10 +17,10 @@
 #ifndef KOKKOS_OPENMP_ZEROMEMSET_HPP
 #define KOKKOS_OPENMP_ZEROMEMSET_HPP
 
-#include <Kokkos_Macros.hpp>
 #include <OpenMP/Kokkos_OpenMP.hpp>
-#include <OpenMP/Kokkos_OpenMP_Instance.hpp>
 #include <impl/Kokkos_ZeroMemset_fwd.hpp>
+
+#include <cstring>
 
 namespace Kokkos {
 namespace Impl {
@@ -29,7 +29,7 @@ template <>
 struct ZeroMemset<OpenMP> {
   ZeroMemset(const OpenMP& exec_space, void* dst, size_t cnt) {
     constexpr size_t host_memset_limit = 1lu << 17;
-    if (cnt < host_memset_limit) {
+    if (cnt < host_memset_limit || exec_space.concurrency() < 4) {
       std::memset(dst, 0, cnt);
     } else {
       hostspace_parallel_zeromemset(exec_space, dst, cnt);
