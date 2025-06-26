95a6962a91404fc6c24431a860be6e15a0d4401d
tcclevenger <tccleve@sandia.gov>
2025-06-26 13:42:01 -0400

    Add test for subview construction
    This test fails with current develop, but is fixed in this PR
    
    Signed-off-by: tcclevenger <tccleve@sandia.gov>

---
+++ core/unit_test/TestSubView_c15.hpp
@@ -0,0 +1,28 @@
+//@HEADER
+// ************************************************************************
+//
+//                        Kokkos v. 4.0
+//       Copyright (2022) National Technology & Engineering
+//               Solutions of Sandia, LLC (NTESS).
+//
+// Under the terms of Contract DE-NA0003525 with NTESS,
+// the U.S. Government retains certain rights in this software.
+//
+// Part of Kokkos, under the Apache License v2.0 with LLVM Exceptions.
+// See https://kokkos.org/LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//@HEADER
+
+#ifndef KOKKOS_TEST_SUBVIEW_C15_HPP
+#define KOKKOS_TEST_SUBVIEW_C15_HPP
+#include <TestViewSubview.hpp>
+
+namespace Test {
+
+TEST(TEST_CATEGORY_DEATH, subview_constructor_types) {
+  TestViewSubview::test_subview_constructor_types();
+}
+
+}  // namespace Test
+#endif
