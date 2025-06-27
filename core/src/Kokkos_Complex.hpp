9d72c09b068f910a1fd076df759fb1d18438f5df
Nevin ":-)" Liber <nliber@anl.gov>
2025-06-27 17:19:24 -0500

    Improved operator/

--- core/src/Kokkos_Complex.hpp
+++ core/src/Kokkos_Complex.hpp
@@ -451,6 +451,16 @@ class
     return complex(lhs.real() * rhs.real() - lhs.imag() * rhs.imag(),
                    lhs.real() * rhs.imag() + lhs.imag() * rhs.real());
   }
+
+  KOKKOS_FUNCTION friend constexpr complex operator/(
+      const complex& lhs, const std::complex<RealType>& rhs) {
+    return lhs / complex(rhs);
+  }
+
+  KOKKOS_FUNCTION friend constexpr complex operator/(
+      const std::complex<RealType>& lhs, const complex& rhs) {
+    return complex(lhs) / rhs;
+  }
 };
 
 }  // namespace Kokkos
@@ -1082,21 +1092,70 @@ inline complex<RealType> exp(const std::complex<RealType>& c) {
                            std::exp(c.real()) * std::sin(c.imag()));
 }
 
+//! Binary operator / for complex.
+//      Returns: complex<T>(lhs) /= rhs
+//
+//  Note: not constexpr until C++23 (because fabs is not constexpr until C++23)
+template <class T>
+KOKKOS_FUNCTION constexpr complex<T> operator/(const complex<T>& lhs,
+                                               const complex<T>& rhs) {
+  // Scale (by the "1-norm" of rhs) to avoid unwarranted overflow.
+  // If the real part is +/-Inf and the imaginary part is -/+Inf,
+  // this won't change the result.
+  const T s = fabs(real(rhs)) + fabs(imag(rhs));
+
+  // If s is 0, then rhs is zero, so lhs/rhs == real(lhs)/0 + i*imag(lhs)/0.
+  // In that case, the relation lhs/rhs == (lhs/s) / (rhs/s) doesn't hold,
+  // because rhs/s is NaN.
+  if (s == 0.0) {
+    return complex<T>(real(lhs) / s, imag(lhs) / s);
+  } else {
+    const complex<T> lhs_scaled(real(lhs) / s, imag(lhs) / s);
+    const complex<T> rhs_conj_scaled(real(rhs) / s, -imag(rhs) / s);
+    const T rhs_scaled_abs =
+        real(rhs_conj_scaled) * real(rhs_conj_scaled) +
+        imag(rhs_conj_scaled) *
+            imag(rhs_conj_scaled);  // abs(rhs) == abs(conj(rhs))
+    complex<T> result = lhs_scaled * rhs_conj_scaled;
+    result /= rhs_scaled_abs;
+    return result;
+  }
+}
+
+template <class T>
+KOKKOS_FUNCTION constexpr complex<T> operator/(const complex<T>& lhs,
+                                               const T& rhs) {
+  return complex<T>(real(lhs) / rhs, imag(lhs) / rhs);
+}
+
+template <class T>
+KOKKOS_FUNCTION constexpr complex<T> operator/(const T& lhs,
+                                               const complex<T>& rhs) {
+  return complex<T>(lhs) / rhs;
+}
+
 //! Binary operator / for complex and real numbers
-template <class RealType1, class RealType2>
-KOKKOS_INLINE_FUNCTION complex<std::common_type_t<RealType1, RealType2>>
-operator/(const complex<RealType1>& x,
-          const RealType2& y) noexcept(noexcept(RealType1{} / RealType2{})) {
+template <class RealType1, class RealType2,
+          class = std::enable_if_t<Impl::is_noncv_floating_point_v<
+              std::common_type<RealType1, RealType2>>>>
+KOKKOS_DEPRECATED KOKKOS_FUNCTION
+    complex<std::common_type_t<RealType1, RealType2>>
+    operator/(const complex<RealType1>& x,
+              const RealType2& y) noexcept(noexcept(RealType1{} /
+                                                    RealType2{})) {
   return complex<std::common_type_t<RealType1, RealType2>>(real(x) / y,
                                                            imag(x) / y);
 }
 
 //! Binary operator / for complex.
-template <class RealType1, class RealType2>
-KOKKOS_INLINE_FUNCTION complex<std::common_type_t<RealType1, RealType2>>
-operator/(const complex<RealType1>& x,
-          const complex<RealType2>& y) noexcept(noexcept(RealType1{} /
-                                                         RealType2{})) {
+template <class RealType1, class RealType2,
+          class = std::enable_if_t<Impl::is_noncv_floating_point_v<
+              std::common_type<RealType1, RealType2>>>>
+KOKKOS_DEPRECATED KOKKOS_FUNCTION
+    complex<std::common_type_t<RealType1, RealType2>>
+    operator/(const complex<RealType1>& x,
+              const complex<RealType2>& y) noexcept(noexcept(RealType1{} /
+                                                             RealType2{})) {
   // Scale (by the "1-norm" of y) to avoid unwarranted overflow.
   // If the real part is +/-Inf and the imaginary part is -/+Inf,
   // this won't change the result.
@@ -1121,11 +1180,14 @@ operator/(const complex<RealType1>& x,
 }
 
 //! Binary operator / for complex and real numbers
-template <class RealType1, class RealType2>
-KOKKOS_INLINE_FUNCTION complex<std::common_type_t<RealType1, RealType2>>
-operator/(const RealType1& x,
-          const complex<RealType2>& y) noexcept(noexcept(RealType1{} /
-                                                         RealType2{})) {
+template <class RealType1, class RealType2,
+          class = std::enable_if_t<Impl::is_noncv_floating_point_v<
+              std::common_type<RealType1, RealType2>>>>
+KOKKOS_DEPRECATED KOKKOS_FUNCTION
+    complex<std::common_type_t<RealType1, RealType2>>
+    operator/(const RealType1& x,
+              const complex<RealType2>& y) noexcept(noexcept(RealType1{} /
+                                                             RealType2{})) {
   return complex<std::common_type_t<RealType1, RealType2>>(x) / y;
 }
 
