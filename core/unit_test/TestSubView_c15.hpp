c8092d99917e3446e7c4e7b433e1a5b8b8e48962
tcclevenger <tccleve@sandia.gov>
2025-07-01 11:49:25 -0400

    Fix formatting
    Signed-off-by: tcclevenger <tccleve@sandia.gov>

--- core/unit_test/TestSubView_c15.hpp
+++ core/unit_test/TestSubView_c15.hpp
@@ -48,8 +48,8 @@ TEST(TEST_CATEGORY_DEATH, view_subview_constructor_layout_compatibility) {
 
   Kokkos::View<int**, LL> a2("A2", 1, N);
   {
-    // Using subview dims (1, ALL), but the first dimension is stride 1. Any subview
-    // layout should be appropriate.
+    // Using subview dims (1, ALL), but the first dimension is stride 1. Any
+    // subview layout should be appropriate.
     (void)Kokkos::View<int*, LL>(a2, 1, Kokkos::ALL);
     (void)Kokkos::View<int*, LS>(a2, 1, Kokkos::ALL);
     (void)Kokkos::View<int*, LR>(a2, 1, Kokkos::ALL);
