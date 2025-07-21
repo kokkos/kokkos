375ef2f39d482a3c954dd6f4afe9f8db99217a1f
Dong Hun Lee <donlee@sandia.gov>
2025-07-21 09:26:36 -0600

    Attempt 3
    Signed-off-by: Dong Hun Lee <donlee@sandia.gov>

--- simd/unit_tests/include/SIMDTesting_Utilities.hpp
+++ simd/unit_tests/include/SIMDTesting_Utilities.hpp
@@ -46,48 +46,21 @@ class kokkos_checker {
   KOKKOS_INLINE_FUNCTION void truth(bool x) const {
     if (!x) Kokkos::abort("SIMD unit test truth condition failed on device");
   }
-
-#if defined(KOKKOS_IMPL_32BIT)
-  // This is needed to work around a bug where the comparison fails because it
-  // is done on the x87 fpu (which is the default for 32 bit gcc) in long
-  // double and a and b end up being different in long double but have the
-  // same value when casted to float or double. (see
-  // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=323#c109)
-  union EqualityCheck {
-    double d;
-    float f;
-    std::int64_t i64;
-    std::int32_t i32;
-  };
-
   template <class T>
-  KOKKOS_INLINE_FUNCTION void equality(T const& a, T const& b) const {
-    EqualityCheck v_a, v_b;
-    if constexpr (std::is_same_v<T, float>) {
-      v_a.f = a;
-      v_b.f = b;
-
-      std::int32_t a32 = v_a.i32;
-      std::int32_t b32 = v_b.i32;
-
-      if ((a32 ^ b32) != 0)
-        Kokkos::abort("SIMD unit test equality condition failed on device");
-    } else if constexpr (std::is_same_v<T, double>) {
-      v_a.d = a;
-      v_b.d = b;
-
-      std::int64_t a64 = v_a.i64;
-      std::int64_t b64 = v_b.i64;
-
-      if ((a64 ^ b64) != 0)
-        Kokkos::abort("SIMD unit test equality condition failed on device");
-    } else {
-      if (a != b)
-        Kokkos::abort("SIMD unit test equality condition failed on device");
-    }
+#if defined(KOKKOS_IMPL_32BIT)
+  KOKKOS_FUNCTION __attribute__((noinline)) void equality(T const& a,
+                                                          T const& b) const {
+    // This is needed to work around a bug where the comparison fails because it
+    // is done on the x87 fpu (which is the default for 32 bit gcc) in long
+    // double and a and b end up being different in long double but have the
+    // same value when casted to float or double. (see
+    // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=323#c109)
+    T const volatile* const pa = a;
+    T const volatile* const pb = b;
+    if (*pa != *pb)
+      Kokkos::abort("SIMD unit test equality condition failed on device");
   }
 #else
-  template <class T>
   KOKKOS_INLINE_FUNCTION void equality(T const& a, T const& b) const {
     if (a != b)
       Kokkos::abort("SIMD unit test equality condition failed on device");
