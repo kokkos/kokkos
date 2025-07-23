b086d0330e1d5174eaf1548a3bbc8c40f3b8704a
Nicolas Morales <nmmoral@sandia.gov>
2025-07-23 14:15:43 -0400

    #8233: add test for accessor arg passed explicitly into DynRankView and for create_mirror calls on customized DynRankViews
    Signed-off-by: Nicolas Morales <nmmoral@sandia.gov>

--- containers/unit_tests/TestDynRankView_ViewCustomization.hpp
+++ containers/unit_tests/TestDynRankView_ViewCustomization.hpp
@@ -203,4 +203,43 @@ TEST(TEST_CATEGORY, view_customization_extra_int_arg) {
         sizeof(double);
     ASSERT_EQ(shmem, expected_shmem_size);
   }
+  // With accessor arg and no label
+  {
+    // This shouldn't take the last argument given and make it the accessor arg
+    // as we provided it explicitly
+    // Note that with and without labels are two separate cases
+    view_t a(Kokkos::view_alloc(Kokkos::Impl::AccessorArg_t{5ul}), 3, 7, 11);
+    ASSERT_EQ(a.rank(), 3lu);
+    ASSERT_EQ(a.extent(0), 3lu);
+    ASSERT_EQ(a.extent(1), 7lu);
+    ASSERT_EQ(a.extent(2), 11lu);
+    ASSERT_EQ(a.accessor().size, 5lu);
+    ASSERT_EQ(a.accessor().stride, size_t(3 * 7 * 11));
+  }
+  // With accessor arg and label
+  {
+    // This shouldn't take the last argument given and make it the accessor arg
+    // as we provided it explicitly
+    // Note that with and without labels are two separate cases
+    view_t a(Kokkos::view_alloc("A", Kokkos::Impl::AccessorArg_t{5ul}), 3, 7, 11);
+    ASSERT_EQ(a.rank(), 3lu);
+    ASSERT_EQ(a.extent(0), 3lu);
+    ASSERT_EQ(a.extent(1), 7lu);
+    ASSERT_EQ(a.extent(2), 11lu);
+    ASSERT_EQ(a.accessor().size, 5lu);
+    ASSERT_EQ(a.accessor().stride, size_t(3 * 7 * 11));
+  }
+  // Create mirror
+  {
+    view_t a("A", 3, 7, 11, 5);
+
+    auto b = Kokkos::create_mirror(a);
+    ASSERT_EQ(b.rank(), 3lu);
+    ASSERT_EQ(b.extent(0), 3lu);
+    ASSERT_EQ(b.extent(1), 7lu);
+    ASSERT_EQ(b.extent(2), 11lu);
+    ASSERT_EQ(b.accessor().size, 5lu);
+    ASSERT_EQ(b.accessor().stride, size_t(3 * 7 * 11));
+
+  }
 }
