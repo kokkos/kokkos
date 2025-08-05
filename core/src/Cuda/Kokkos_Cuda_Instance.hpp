7a2faa0f0db718ceb4ecef5dbec1439ee77b15fe
Nuno Nobre <nuno.nobre@stfc.ac.uk>
2025-08-05 13:21:43 +0100

    Fix incompatibilities with CUDA 13 runtime API
    Signed-off-by: Nuno Nobre <nuno.nobre@stfc.ac.uk>

--- core/src/Cuda/Kokkos_Cuda_Instance.hpp
+++ core/src/Cuda/Kokkos_Cuda_Instance.hpp
@@ -222,7 +222,11 @@ class CudaInternal {
       cudaGraph_t graph, const cudaGraphNode_t* from, const cudaGraphNode_t* to,
       size_t numDependencies) const {
     set_cuda_device();
+#if CUDART_VERSION >= 13000
+    return cudaGraphAddDependencies(graph, from, to, NULL, numDependencies);
+#else
     return cudaGraphAddDependencies(graph, from, to, numDependencies);
+#endif
   }
 
   cudaError_t cuda_graph_add_empty_node_wrapper(
@@ -276,7 +280,12 @@ class CudaInternal {
   cudaError_t cuda_mem_prefetch_async_wrapper(const void* devPtr, size_t count,
                                               int dstDevice) const {
     set_cuda_device();
+#if CUDART_VERSION >= 13000
+    cudaMemLocation loc = {cudaMemLocationTypeDevice, dstDevice};
+    return cudaMemPrefetchAsync(devPtr, count, loc, 0, m_stream);
+#else
     return cudaMemPrefetchAsync(devPtr, count, dstDevice, m_stream);
+#endif
   }
 
   cudaError_t cuda_memcpy_wrapper(void* dst, const void* src, size_t count,
