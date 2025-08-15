697d346145e3e222dab21cae045dcf2ff9656ef4
Damien L-G <dalg24@gmail.com>
2025-08-15 15:34:33 -0400

    Make reduction_identity<[b]half_t>::fun() return [b]half_t
    instead of float.
    Drop the constexpr and the pointless forceinline.
    
    Signed-off-by: Damien L-G <dalg24@gmail.com>

--- core/src/Cuda/Kokkos_Cuda_Half_Conversion.hpp
+++ core/src/Cuda/Kokkos_Cuda_Half_Conversion.hpp
@@ -26,8 +26,7 @@
 #include <cuda_bf16.h>
 #endif
 
-namespace Kokkos {
-namespace Experimental {
+namespace Kokkos::Experimental {
 
 /************************** half conversions **********************************/
 KOKKOS_INLINE_FUNCTION
@@ -472,44 +471,27 @@ cast_from_bhalf(bhalf_t val) {
 #endif  // CUDA_VERSION >= 11010
 
 #undef KOKKOS_IMPL_NVIDIA_GPU_ARCH_SUPPORT_BHALF
-}  // namespace Experimental
+}  // namespace Kokkos::Experimental
 
 #if (CUDA_VERSION >= 11000)
 template <>
-struct reduction_identity<Kokkos::Experimental::bhalf_t> {
-  KOKKOS_FORCEINLINE_FUNCTION constexpr static float sum() noexcept {
-    return 0.0F;
-  }
-  KOKKOS_FORCEINLINE_FUNCTION constexpr static float prod() noexcept {
-    return 1.0F;
-  }
-  KOKKOS_FORCEINLINE_FUNCTION constexpr static float max() noexcept {
-    return -Kokkos::Experimental::infinity_v<float>;
-  }
-  KOKKOS_FORCEINLINE_FUNCTION constexpr static float min() noexcept {
-    return Kokkos::Experimental::infinity_v<float>;
-  }
+struct Kokkos::reduction_identity<Kokkos::Experimental::bhalf_t> {
+  static constexpr auto inf = Experimental::infinity_v<float>;
+  KOKKOS_FUNCTION static Experimental::bhalf_t sum() noexcept { return 0; }
+  KOKKOS_FUNCTION static Experimental::bhalf_t prod() noexcept { return 1; }
+  KOKKOS_FUNCTION static Experimental::bhalf_t max() noexcept { return -inf; }
+  KOKKOS_FUNCTION static Experimental::bhalf_t min() noexcept { return inf; }
 };
 #endif  // CUDA_VERSION >= 11000
 
-// use float as the return type for sum and prod since cuda_fp16.h
-// has no constexpr functions for casting to __half
 template <>
-struct reduction_identity<Kokkos::Experimental::half_t> {
-  KOKKOS_FORCEINLINE_FUNCTION constexpr static float sum() noexcept {
-    return 0.0F;
-  }
-  KOKKOS_FORCEINLINE_FUNCTION constexpr static float prod() noexcept {
-    return 1.0F;
-  }
-  KOKKOS_FORCEINLINE_FUNCTION constexpr static float max() noexcept {
-    return -Kokkos::Experimental::infinity_v<float>;
-  }
-  KOKKOS_FORCEINLINE_FUNCTION constexpr static float min() noexcept {
-    return Kokkos::Experimental::infinity_v<float>;
-  }
+struct Kokkos::reduction_identity<Kokkos::Experimental::half_t> {
+  static constexpr auto inf = Experimental::infinity_v<float>;
+  KOKKOS_FUNCTION static Experimental::half_t sum() noexcept { return 0; }
+  KOKKOS_FUNCTION static Experimental::half_t prod() noexcept { return 1; }
+  KOKKOS_FUNCTION static Experimental::half_t max() noexcept { return -inf; }
+  KOKKOS_FUNCTION static Experimental::half_t min() noexcept { return inf; }
 };
 
-}  // namespace Kokkos
 #endif  // KOKKOS_IMPL_CUDA_HALF_TYPE_DEFINED
 #endif
