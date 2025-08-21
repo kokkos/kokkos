6ffdeff09951c6267619ff29b56b9a25997a9673
Adrien Taberner <adrien.taberner@outlook.com>
2025-08-21 16:04:04 +0200

    [ci skip] Kokkos::array -> std::array
    Signed-off-by: Adrien Taberner <adrien.taberner@outlook.com>

--- core/src/KokkosExp_MDRangePolicy.hpp
+++ core/src/KokkosExp_MDRangePolicy.hpp
@@ -30,6 +30,7 @@ static_assert(false,
 #include <impl/KokkosExp_Host_IterateTile.hpp>
 #include <Kokkos_ExecPolicy.hpp>
 #include <type_traits>
+#include <array>
 #include <cmath>
 
 namespace Kokkos {
@@ -140,7 +141,7 @@ struct TileSizeProperties {
   int default_tile_size;
   int max_total_tile_size;
   // For GPU backends: hardware limits for block dimensions
-  Kokkos::Array<int, 3> max_threads_dimensions;
+  std::array<int, 3> max_threads_dimensions;
 };
 
 template <typename ExecutionSpace>
