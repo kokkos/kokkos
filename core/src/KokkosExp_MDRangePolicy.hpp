0b2ac63417afd1f0c19174d877135a83ddddd9bf
Adrien Taberner <adrien.taberner@outlook.com>
2025-08-28 10:41:59 +0200

    Restore [SYCL,Cuda,HIP]_MDRangePolicy.hpp to upstream state
    Signed-off-by: Adrien Taberner <adrien.taberner@outlook.com>
    
    Restore core/src/KokkosExp_MDRangePolicy.hpp
    
    Signed-off-by: Adrien Taberner <adrien.taberner@outlook.com>

--- core/src/KokkosExp_MDRangePolicy.hpp
+++ core/src/KokkosExp_MDRangePolicy.hpp
@@ -17,6 +17,7 @@ static_assert(false,
 #include <impl/KokkosExp_Host_IterateTile.hpp>
 #include <Kokkos_ExecPolicy.hpp>
 #include <type_traits>
+#include <array>
 #include <cmath>
 
 namespace Kokkos {
@@ -122,10 +123,12 @@ constexpr NVCC_WONT_LET_ME_CALL_YOU_Array to_array_potentially_narrowing(
 }
 
 struct TileSizeProperties {
-  int max_threads;
+  int max_threads;  // (per SM, CU)
   int default_largest_tile_size;
   int default_tile_size;
   int max_total_tile_size;
+  // For GPU backends: hardware limits for block dimensions
+  std::array<int, 3> max_threads_dimensions;
 };
 
 template <typename ExecutionSpace>
@@ -136,6 +139,9 @@ TileSizeProperties get_tile_size_properties(const ExecutionSpace&) {
   properties.default_largest_tile_size = 0;
   properties.default_tile_size         = 2;
   properties.max_total_tile_size       = std::numeric_limits<int>::max();
+  for (int i = 0; i < 3; ++i) {
+    properties.max_threads_dimensions[i] = std::numeric_limits<int>::max();
+  }
   return properties;
 }
 
