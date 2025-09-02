cbcfd0c665edc375d19a91dee99310b95b203da9
Damien L-G <dalg24@gmail.com>
2025-09-02 15:33:19 -0400

    Drop `KOKKOS_ATTRIBUTE_NODISCARD` macro
    Signed-off-by: Damien L-G <dalg24@gmail.com>

--- core/src/Kokkos_Macros.hpp
+++ core/src/Kokkos_Macros.hpp
@@ -668,8 +668,6 @@ static constexpr bool kokkos_omp_on_host() { return false; }
 #endif
 // clang-format on
 
-#define KOKKOS_ATTRIBUTE_NODISCARD [[nodiscard]]
-
 #if (defined(KOKKOS_COMPILER_GNU) || defined(KOKKOS_COMPILER_CLANG) ||         \
      defined(KOKKOS_COMPILER_INTEL_LLVM) || defined(KOKKOS_COMPILER_NVHPC)) && \
     !defined(_WIN32) && !defined(__ANDROID__)
