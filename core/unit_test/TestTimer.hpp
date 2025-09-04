5a09567756840eb8d530857df632fd293719f648
Damien L-G <dalg24@gmail.com>
2025-09-04 09:13:16 -0400

    Add test for Timer
    Signed-off-by: Damien L-G <dalg24@gmail.com>

---
+++ core/unit_test/TestTimer.hpp
@@ -0,0 +1,53 @@
+//@HEADER
+// ************************************************************************
+//
+//                        Kokkos v. 4.0
+//       Copyright (2022) National Technology & Engineering
+//               Solutions of Sandia, LLC (NTESS).
+//
+// Under the terms of Contract DE-NA0003525 with NTESS,
+// the U.S. Government retains certain rights in this software.
+//
+// Part of Kokkos, under the Apache License v2.0 with LLVM Exceptions.
+// See https://kokkos.org/LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//@HEADER
+
+#include <gtest/gtest.h>
+
+#include <Kokkos_Timer.hpp>
+
+#include <chrono>
+#include <thread>
+#include <type_traits>
+
+namespace {
+
+TEST(TEST_CATEGORY, timer) {
+  using namespace std::chrono_literals;
+
+  Kokkos::Timer t;
+  std::this_thread::sleep_for(5ms);
+  auto elapsed = t.seconds();
+  EXPECT_GE(elapsed, .005);
+  EXPECT_LE(elapsed, .01);
+
+  std::this_thread::sleep_for(10ms);
+  auto elapsed2 = t.seconds();
+  EXPECT_GE(elapsed2, .015);
+  EXPECT_LE(elapsed2, .02);
+
+  t.reset();
+  std::this_thread::sleep_for(5ms);
+  auto elapsed3 = t.seconds();
+  EXPECT_GE(elapsed3, .005);
+  EXPECT_LE(elapsed3, .01);
+}
+
+static_assert(!std::is_copy_constructible_v<Kokkos::Timer>);
+static_assert(!std::is_move_constructible_v<Kokkos::Timer>);
+static_assert(!std::is_copy_assignable_v<Kokkos::Timer>);
+static_assert(!std::is_move_assignable_v<Kokkos::Timer>);
+
+}  // namespace
