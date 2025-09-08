56a5e97ddfdd20644ee9c6954e5af7092a132ba5
Prateek Chawla <p.chawla@fz-juelich.de>
2025-09-08 14:03:24 +0200

    syntax fix for RVV architecture definitions
    Signed-off-by: Prateek Chawla <p.chawla@fz-juelich.de>

--- core/src/impl/Kokkos_Core.cpp
+++ core/src/impl/Kokkos_Core.cpp
@@ -656,11 +656,11 @@ void pre_initialize_internal(const Kokkos::InitializationSettings& settings) {
 #elif defined(KOKKOS_ARCH_AMD_ZEN5)
   declare_configuration_metadata("architecture", "CPU architecture", "AMD_ZEN5");
 #elif defined(KOKKOS_ARCH_RISCV_SG2042)
-  declare_configuration_metadata("architecture", "CPU architecture", "SG2042 (RISC-V)")
+  declare_configuration_metadata("architecture", "CPU architecture", "SG2042 (RISC-V)");
 #elif defined(KOKKOS_ARCH_RISCV_RVA22V)
-  declare_configuration_metadata("architecture", "CPU architecture", "RVA22V (RISC-V)")
+  declare_configuration_metadata("architecture", "CPU architecture", "RVA22V (RISC-V)");
 #elif defined(KOKKOS_ARCH_RISCV_U74MC)
-  declare_configuration_metadata("architecture", "CPU architecture", "U74MC (RISC-V)")
+  declare_configuration_metadata("architecture", "CPU architecture", "U74MC (RISC-V)");
 #else
   declare_configuration_metadata("architecture", "CPU architecture", "none");
 #endif
