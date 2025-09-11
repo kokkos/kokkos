ff323791f8f5180eba80d9720839b73501a47dc5
Damien L-G <dalg24@gmail.com>
2025-09-11 09:37:22 -0700

    Revert "cuda runtime is missing in rdc builds. Switching to this flag seems to tell some clang+CUDA combinations to include a runtime"
    This reverts commit 1eb681eabf10d5cd7c03a2dcd1e3549da7fa1549.

--- cmake/kokkos_arch.cmake
+++ cmake/kokkos_arch.cmake
@@ -827,7 +827,7 @@ endif()
 
 if(NOT KOKKOS_COMPILE_LANGUAGE STREQUAL CUDA)
   if(KOKKOS_ENABLE_CUDA_RELOCATABLE_DEVICE_CODE)
-    compiler_specific_flags(Clang -fcuda-rdc --offload-new-driver NVIDIA --relocatable-device-code=true)
+    compiler_specific_flags(Clang -fgpu-rdc --offload-new-driver NVIDIA --relocatable-device-code=true)
   endif()
 endif()
 
