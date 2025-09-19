0c0fb6660c1d8de2bb6348893f71207ff682fd4d
Damien L-G <dalg24@gmail.com>
2025-09-18 18:32:55 -0600

    Fixup cmake format
    Signed-off-by: Damien L-G <dalg24@gmail.com>

--- cmake/kokkos_enable_options.cmake
+++ cmake/kokkos_enable_options.cmake
@@ -141,7 +141,7 @@ if(Kokkos_ENABLE_IMPL_MDSPAN)
   # CUDA 12.9 has a bug that causes it to segfault when mdspan-based view is used:
   #   see https://github.com/kokkos/kokkos/issues/8126
   if(KOKKOS_CXX_COMPILER_ID STREQUAL NVIDIA AND KOKKOS_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 12.9
-         AND KOKKOS_CXX_COMPILER_VERSION VERSION_LESS 13
+     AND KOKKOS_CXX_COMPILER_VERSION VERSION_LESS 13
   )
     set(VIEW_LEGACY_DEFAULT ON)
   else()
