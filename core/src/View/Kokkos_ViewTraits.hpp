1b3052f9772b1e8135ba0834b3272aafabaf0fa9
Nicolas Morales <nmmoral@sandia.gov>
2025-09-23 13:15:55 -0400

    fix return of default customize_view_hooks in the case of hooks_policy being void
    Signed-off-by: Nicolas Morales <nmmoral@sandia.gov>

--- core/src/View/Kokkos_ViewTraits.hpp
+++ core/src/View/Kokkos_ViewTraits.hpp
@@ -590,7 +590,9 @@ struct RemoveAlignedMemoryTrait {
 template <class DataType, class... Properties>
 constexpr auto customize_view_hooks() {
   using traits_type = ViewTraits<DataType, Properties...>;
-  return typename traits_type::hooks_policy{};
+  if constexpr ( !std::is_void_v<typename traits_type::hooks_policy> ) {
+    return typename traits_type::hooks_policy{};
+  }
 }
 
 template <class DataType, class... Properties>
