07b649f06f1898ea6728879c00b21684f0563494
Nicolas Morales <nmmoral@sandia.gov>
2025-10-06 12:13:32 -0400

    switch to if constexpr rather than constraints
    Signed-off-by: Nicolas Morales <nmmoral@sandia.gov>

--- core/src/View/Kokkos_BasicView.hpp
+++ core/src/View/Kokkos_BasicView.hpp
@@ -494,38 +494,26 @@ class BasicView {
 
   template <class T>
   KOKKOS_INLINE_FUNCTION static bool is_in_bounds(size_t extent,
-                                                  const std::pair<T, T> arg)
-    requires(std::is_signed_v<T>)
-  {
-    return static_cast<std::size_t>(arg.second) <= extent && arg.first >= 0 &&
-           arg.first <= arg.second;
+                                                  const std::pair<T, T> arg) {
+    if constexpr (std::is_signed_v<T>) {
+      return static_cast<std::size_t>(arg.second) <= extent && arg.first >= 0 &&
+             arg.first <= arg.second;
+    } else {
+      return static_cast<std::size_t>(arg.second) <= extent &&
+             arg.first <= arg.second;
+    }
   }
 
   template <class T>
-  KOKKOS_INLINE_FUNCTION static bool is_in_bounds(size_t extent,
-                                                  const std::pair<T, T> arg)
-    requires(!std::is_signed_v<T>)
-  {
-    return static_cast<std::size_t>(arg.second) <= extent &&
-           arg.first <= arg.second;
-  }
-
-  template <class T>
-  KOKKOS_INLINE_FUNCTION static bool is_in_bounds(size_t extent,
-                                                  const Kokkos::pair<T, T> arg)
-    requires(std::is_signed_v<T>)
-  {
-    return static_cast<std::size_t>(arg.second) <= extent && arg.first >= 0 &&
-           arg.first <= arg.second;
-  }
-
-  template <class T>
-  KOKKOS_INLINE_FUNCTION static bool is_in_bounds(size_t extent,
-                                                  const Kokkos::pair<T, T> arg)
-    requires(!std::is_signed_v<T>)
-  {
-    return static_cast<std::size_t>(arg.second) <= extent &&
-           arg.first <= arg.second;
+  KOKKOS_INLINE_FUNCTION static bool is_in_bounds(
+      size_t extent, const Kokkos::pair<T, T> arg) {
+    if constexpr (std::is_signed_v<T>) {
+      return static_cast<std::size_t>(arg.second) <= extent && arg.first >= 0 &&
+             arg.first <= arg.second;
+    } else {
+      return static_cast<std::size_t>(arg.second) <= extent &&
+             arg.first <= arg.second;
+    }
   }
 
   KOKKOS_INLINE_FUNCTION static bool is_in_bounds(size_t /*extent*/,
