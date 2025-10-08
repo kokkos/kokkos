568584e06ec3e1cc7c0e30687728a8e9fa55f18e
Jakob Bludau <bludauj@ornl.gov>
2025-10-08 09:37:53 -0400

    rework 16Byte overload trigger
    Signed-off-by: Jakob Bludau <bludauj@ornl.gov>

--- tpls/desul/include/desul/atomics/cuda/CUDA_asm_exchange.hpp
+++ tpls/desul/include/desul/atomics/cuda/CUDA_asm_exchange.hpp
@@ -1,11 +1,4 @@
-#include <cuda.h>
-
 #include <limits>
-#if (CUDA_VERSION >= 12080) && not defined(DESUL_CUDA_ARCH_IS_PRE_HOPPER)
-// CUDA 12.8 introduced 128-bit compare and swap support on Hopper+ (compute
-// capability 9.x and higher)
-#define DESUL_HAVE_CUDA_128BIT_CAS
-#endif
 
 namespace desul {
 namespace Impl {
