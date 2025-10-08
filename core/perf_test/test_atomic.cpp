a5c01a818d48701159376ff81ff85d1e2e058af6
Jakob Bludau <bludauj@ornl.gov>
2025-10-08 14:17:24 -0400

    try if int128 works
    Signed-off-by: Jakob Bludau <bludauj@ornl.gov>

--- core/perf_test/test_atomic.cpp
+++ core/perf_test/test_atomic.cpp
@@ -393,4 +393,6 @@ BENCHMARK(Test_Atomic<unsigned long int>)->Arg(LOOP)->Iterations(10);
 BENCHMARK(Test_Atomic<unsigned long long int>)->Arg(LOOP)->Iterations(10);
 BENCHMARK(Test_Atomic<float>)->Arg(LOOP)->Iterations(10);
 BENCHMARK(Test_Atomic<double>)->Arg(LOOP)->Iterations(10);
-BENCHMARK(Test_Atomic<Kokkos::complex<double>>)->Arg(LOOP)->Iterations(10);
+#ifdef __SIZEOF_INT128__
+BENCHMARK(Test_Atomic<__int128>)->Arg(LOOP)->Iterations(10);
+#endif
