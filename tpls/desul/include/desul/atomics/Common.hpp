095432a459e3bea97bd8f5c545bba167dc43720c
Jakob Bludau <bludauj@ornl.gov>
2025-10-08 09:44:07 -0400

    undo accidental license change
    Signed-off-by: Jakob Bludau <bludauj@ornl.gov>

--- tpls/desul/include/desul/atomics/Common.hpp
+++ tpls/desul/include/desul/atomics/Common.hpp
@@ -1,10 +1,10 @@
 /*
- Copyright (c) 2019, Lawrence Livermore National Security, LLC
- and DESUL project contributors. See the COPYRIGHT file for details.
- Source: https://github.com/desul/desul
+Copyright (c) 2019, Lawrence Livermore National Security, LLC
+and DESUL project contributors. See the COPYRIGHT file for details.
+Source: https://github.com/desul/desul
 
- SPDX-License-Identifier: (BSD-3-Clause)
- */
+SPDX-License-Identifier: (BSD-3-Clause)
+*/
 #ifndef DESUL_ATOMICS_COMMON_HPP_
 #define DESUL_ATOMICS_COMMON_HPP_
 
