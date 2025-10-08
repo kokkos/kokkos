054f2a172d0218137d2d2e0bf7bd7509039cbebe
Jakob Bludau <bludauj@ornl.gov>
2025-10-08 13:02:57 -0400

    forgot to remove _is in OpenACC
    Signed-off-by: Jakob Bludau <bludauj@ornl.gov>

--- tpls/desul/include/desul/atomics/Compare_Exchange_OpenACC.hpp
+++ tpls/desul/include/desul/atomics/Compare_Exchange_OpenACC.hpp
@@ -19,8 +19,8 @@ namespace desul {
 namespace Impl {
 
 template <class T>
-inline constexpr bool device_atomic_is_always_lock_free<T, void> = (sizeof(T) == 4) ||
-                                                                   (sizeof(T) == 8);
+inline constexpr bool device_atomic_always_lock_free<T, void> = (sizeof(T) == 4) ||
+                                                                (sizeof(T) == 8);
 
 #ifdef __NVCOMPILER
 
