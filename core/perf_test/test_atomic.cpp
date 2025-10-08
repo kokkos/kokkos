d239ef309cb6ed9acd37929878edbf9c636f2c5a
Jakob Bludau <bludauj@ornl.gov>
2025-10-08 14:46:18 -0400

    reduce size in perf_test to 30'000 to not sandbag the CI
    Signed-off-by: Jakob Bludau <bludauj@ornl.gov>

--- core/perf_test/test_atomic.cpp
+++ core/perf_test/test_atomic.cpp
@@ -394,5 +394,5 @@ BENCHMARK(Test_Atomic<unsigned long long int>)->Arg(LOOP)->Iterations(10);
 BENCHMARK(Test_Atomic<float>)->Arg(LOOP)->Iterations(10);
 BENCHMARK(Test_Atomic<double>)->Arg(LOOP)->Iterations(10);
 #ifdef __SIZEOF_INT128__
-BENCHMARK(Test_Atomic<__int128>)->Arg(LOOP)->Iterations(10);
+BENCHMARK(Test_Atomic<__int128>)->Arg(30'000)->Iterations(10);
 #endif
