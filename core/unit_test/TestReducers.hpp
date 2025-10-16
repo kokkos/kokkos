3f7db5416d863afd9b800a8c618b293396e6a738
Seyong Lee <lees2@ornl.gov>
2025-10-15 23:07:56 -0400

    Apply suggestion from @dalg24
    Co-authored-by: Damien L-G <dalg24+github@gmail.com>
    Signed-off-by: Seyong Lee <lees2@ornl.gov>

--- core/unit_test/TestReducers.hpp
+++ core/unit_test/TestReducers.hpp
@@ -494,7 +494,8 @@ struct TestReducers {
     }
 
 // FIXME_OPENACC - custom reduction with TeamPolicy is not yet implemented.
-#if !defined(KOKKOS_ENABLE_OPENACC)
+#ifdef KOKKOS_ENABLE_OPENACC
+      if constexpr(!std::is_same_v<ExecSpace, Kokkos::Experimental::OpenACC>)
     test_sum_team_policy(N, f, reference_sum);
 #endif
 
