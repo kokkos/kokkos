8bcfe63ffecaa562cfb7be212a1c6fbcd884265f
JBludau <104908666+JBludau@users.noreply.github.com>
2025-11-17 14:36:39 -0500

    Update cmake/kokkos_compiler_id.cmake
    Co-authored-by: Daniel Arndt <arndtd@ornl.gov>
    Signed-off-by: JBludau <104908666+JBludau@users.noreply.github.com>

--- cmake/kokkos_compiler_id.cmake
+++ cmake/kokkos_compiler_id.cmake
@@ -97,7 +97,7 @@ if(Kokkos_ENABLE_HIP)
 
   #FIXME HIP
   if(KOKKOS_CXX_COMPILER_VERSION MATCHES "7\\.1")
-    message(FATAL_ERROR "Found Rocm 7.1 which leads to wrong code when used with Kokkos. Please use an older version")
+    message(FATAL_ERROR "Found ROCm 7.1 which leads to wrong code when used with Kokkos. Please use an older version")
   endif()
 endif()
 
