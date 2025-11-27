4322f89b2528fd33dd16553acbb5f9345b4e79ba
Jan Ciesko <jan.ciesko@gmail.com>
2025-11-26 17:54:45 -0700

    Move test to new file
    Signed-off-by: Jan Ciesko <jan.ciesko@gmail.com>

--- core/unit_test/view/TestConversionFromPointer.cpp
+++ core/unit_test/view/TestConversionFromPointer.cpp
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 // SPDX-FileCopyrightText: Copyright Contributors to the Kokkos project
 
+#include <Kokkos_Macros.hpp>
+#ifdef KOKKOS_ENABLE_EXPERIMENTAL_CXX20_MODULES
+import kokkos.core;
+#else
 #include <Kokkos_Core.hpp>
-#include <type_traits>
-
+#endif
 // Checking requirement of explict type conversion to View
 
 namespace {
