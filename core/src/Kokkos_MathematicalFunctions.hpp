4a2eb9d24bbcc8a4ccd47b4acfcde5070b8e5bf6
Jakob Bludau <bludauj@ornl.gov>
2025-12-01 13:22:54 -0500

    enable them in all backends
    Signed-off-by: Jakob Bludau <bludauj@ornl.gov>

--- core/src/Kokkos_MathematicalFunctions.hpp
+++ core/src/Kokkos_MathematicalFunctions.hpp
@@ -433,15 +433,15 @@ KOKKOS_IMPL_MATH_UNARY_FUNCTION(ceil)
 KOKKOS_IMPL_MATH_UNARY_FUNCTION(floor)
 KOKKOS_IMPL_MATH_UNARY_FUNCTION(trunc)
 KOKKOS_IMPL_MATH_UNARY_FUNCTION(round)
-// lround
-// llround
+KOKKOS_IMPL_MATH_UNARY_FUNCTION(lround)
+KOKKOS_IMPL_MATH_UNARY_FUNCTION(llround)
 // FIXME_SYCL not available as of current SYCL 2020 specification (revision 4)
 #ifndef KOKKOS_ENABLE_SYCL  // FIXME_SYCL
 KOKKOS_IMPL_MATH_UNARY_FUNCTION(nearbyint)
 #endif
-// rint
-// lrint
-// llrint
+KOKKOS_IMPL_MATH_UNARY_FUNCTION(rint)
+KOKKOS_IMPL_MATH_UNARY_FUNCTION(lrint)
+KOKKOS_IMPL_MATH_UNARY_FUNCTION(llrint)
 // Floating point manipulation functions
 // frexp
 // ldexp
