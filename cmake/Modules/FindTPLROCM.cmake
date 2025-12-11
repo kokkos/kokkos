874edc5192a509bf41bd20a0a36f0637959bd772
Jakob Bludau <104908666+JBludau@users.noreply.github.com>
2025-12-11 08:43:10 -0500

    Update cmake/Modules/FindTPLROCM.cmake
    Signed-off-by: Jakob Bludau <104908666+JBludau@users.noreply.github.com>

--- cmake/Modules/FindTPLROCM.cmake
+++ cmake/Modules/FindTPLROCM.cmake
@@ -1,10 +1,6 @@
 find_package(hip 6.2 REQUIRED)
 
 set(TPL_ROCM_LIBRARIES "")
-if(KOKKOS_ENABLE_COMPILE_AS_CMAKE_LANGUAGE)
-  set(TPL_ROCM_LIBRARIES hip::device)
-else()
-  set(TPL_ROCM_LIBRARIES hip::device)
-endif()
+set(TPL_ROCM_LIBRARIES hip::device)
 
 kokkos_create_imported_tpl(ROCM INTERFACE LINK_LIBRARIES ${TPL_ROCM_LIBRARIES})
