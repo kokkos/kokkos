799179065076ed405de4693c59228e8ed43d9d49
Jakob Bludau <bludauj@ornl.gov>
2025-12-15 10:26:32 -0500

    remove unnecessary variable
    Signed-off-by: Jakob Bludau <bludauj@ornl.gov>

--- cmake/Modules/FindTPLROCM.cmake
+++ cmake/Modules/FindTPLROCM.cmake
@@ -1,5 +1,3 @@
 find_package(hip REQUIRED PATHS ${ROCM_PATH} $ENV{ROCM_PATH})
 
-set(TPL_ROCM_LIBRARIES hip::device)
-
-kokkos_create_imported_tpl(ROCM INTERFACE LINK_LIBRARIES ${TPL_ROCM_LIBRARIES})
+kokkos_create_imported_tpl(ROCM INTERFACE LINK_LIBRARIES hip::device)
