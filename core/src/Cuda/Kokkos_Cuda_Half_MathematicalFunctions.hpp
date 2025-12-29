69b4672f3232684cd9fe0624e45596a5fc266de4
Jakob Bludau <bludauj@ornl.gov>
2025-12-29 17:47:43 -0500

    readd casts
    Signed-off-by: Jakob Bludau <bludauj@ornl.gov>

--- core/src/Cuda/Kokkos_Cuda_Half_MathematicalFunctions.hpp
+++ core/src/Cuda/Kokkos_Cuda_Half_MathematicalFunctions.hpp
@@ -126,10 +126,10 @@ KOKKOS_CUDA_HALF_AND_BHALF_UNARY_FUNCTION_IMPL(rint, hrint)
 // as the range of int is enough for any value half_t can take.
 // Thus we just cast to the required return type here
 KOKKOS_INLINE_FUNCTION long impl_lrint(Kokkos::Experimental::half_t x) {
-  return impl_rint(x);
+  return static_cast<long>(impl_rint(x));
 }
 KOKKOS_INLINE_FUNCTION long long impl_llrint(Kokkos::Experimental::half_t x) {
-  return impl_rint(x);
+  return static_cast<long long>(impl_rint(x));
 }
 // logb
 // nextafter
