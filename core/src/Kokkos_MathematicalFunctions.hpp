a770f6ecd37c9e442c80c3cb97ac6470bcb7f6e2
svquick <st200498@stud.uni-stuttgart.de>
2026-01-02 12:18:48 +0100

    [#8703] MathematicalFunctions: Add modf wrapper
    Signed-off-by: svquick <st200498@stud.uni-stuttgart.de>

--- core/src/Kokkos_MathematicalFunctions.hpp
+++ core/src/Kokkos_MathematicalFunctions.hpp
@@ -208,6 +208,20 @@ using promote_3_t = typename promote_3<T, U, V>::type;
     return FUNC(static_cast<Promoted>(x), static_cast<Promoted>(y));           \
   }
 
+#define KOKKOS_IMPL_MATH_BINARY_PTR_FUNCTION(FUNC)                             \
+  KOKKOS_INLINE_FUNCTION float FUNC(float x, float* y) {                       \
+    using KOKKOS_IMPL_MATH_FUNCTIONS_NAMESPACE::FUNC;                          \
+    return FUNC(x, y);                                                         \
+  }                                                                            \
+  KOKKOS_INLINE_FUNCTION double FUNC(double x, double* y) {                    \
+    using KOKKOS_IMPL_MATH_FUNCTIONS_NAMESPACE::FUNC;                          \
+    return FUNC(x, y);                                                         \
+  }                                                                            \
+  inline long double FUNC(long double x, long double* y) {                     \
+    using std::FUNC;                                                           \
+    return FUNC(x, y);                                                         \
+  } 
+
 #define KOKKOS_IMPL_MATH_TERNARY_INT_PTR_FUNCTION(FUNC)                        \
   KOKKOS_INLINE_FUNCTION float FUNC(float x, float y, int* z) {                \
     using KOKKOS_IMPL_MATH_FUNCTIONS_NAMESPACE::FUNC;                          \
@@ -482,6 +496,7 @@ KOKKOS_IMPL_MATH_UNARY_FUNCTION(nearbyint)
 // frexp
 // ldexp
 // modf
+KOKKOS_IMPL_MATH_BINARY_PTR_FUNCTION(modf)
 // scalbn
 // scalbln
 KOKKOS_IMPL_MATH_UNARY_INT_FUNCTION(ilogb)
@@ -508,6 +523,7 @@ KOKKOS_IMPL_MATH_UNARY_PREDICATE(signbit)
 #undef KOKKOS_IMPL_MATH_UNARY_INT_FUNCTION
 #undef KOKKOS_IMPL_MATH_UNARY_PREDICATE
 #undef KOKKOS_IMPL_MATH_BINARY_FUNCTION
+#undef KOKKOS_IMPL_MATH_BINARY_PTR_FUNCTION
 #undef KOKKOS_IMPL_MATH_TERNARY_FUNCTION
 #undef KOKKOS_IMPL_MATH_TERNARY_INT_PTR_FUNCTION
 
