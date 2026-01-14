c9d40adbaa42fc28c6aff96203a0d831a3546825
Jan Ciesko <jan.ciesko@gmail.com>
2026-01-14 14:39:51 -0700

    Address reviewer's feedback
    Signed-off-by: Jan Ciesko <jan.ciesko@gmail.com>

--- core/unit_test/view/TestViewEqualityOperator.hpp
+++ core/unit_test/view/TestViewEqualityOperator.hpp
@@ -13,13 +13,11 @@ import kokkos.core_impl;
 #include <cstddef>
 
 // Test View equality operators
-// View equivalence defined as matching value_type,,
+// View equivalence defined as matching value_type,
 // layout_type, memory_space, rank, span, data pointers and extents
 
 namespace {
 
-// TODO: Replace SFINAE with concepts when we have have ArrayLayout and
-// MemoryTraits
 template <class T, class Enable = void>
 struct Another;
 
@@ -31,9 +29,9 @@ struct Another<T, std::enable_if_t<Kokkos::is_array_layout_v<T>>> {
 
 template <class T>
 struct Another<T, std::enable_if_t<Kokkos::is_memory_traits_v<T>>> {
-  using type = std::conditional_t<std::is_same_v<T, Kokkos::MemoryRandomAccess>,
-                                  typename Kokkos::MemoryUnmanaged,
-                                  Kokkos::MemoryRandomAccess>;
+  using type =
+      std::conditional_t<std::is_same_v<T, Kokkos::MemoryRandomAccess>,
+                         Kokkos::MemoryUnmanaged, Kokkos::MemoryRandomAccess>;
 };
 
 template <class T>
@@ -58,12 +56,12 @@ void test_view_equality_operator() {
     using V_data_type_3 = Kokkos::View<const V::value_type, TEST_EXECSPACE>;
     using V_data_type_4 = Kokkos::View<const V::data_type, TEST_EXECSPACE>;
 
-    ASSERT_EQ(check_equal(V(), V_data_type_0()), false);
-    ASSERT_EQ(check_equal(V(), V_data_type_1()), false);
-    ASSERT_EQ(check_equal(V(), V_data_type_2()), false);
+    ASSERT_FALSE(check_equal(V(), V_data_type_0()));
+    ASSERT_FALSE(check_equal(V(), V_data_type_1()));
+    ASSERT_FALSE(check_equal(V(), V_data_type_2()));
     // Note: We do not enforce matching const'ness of data_type or value_type
-    ASSERT_EQ(check_equal(V(), V_data_type_3()), true);
-    ASSERT_EQ(check_equal(V(), V_data_type_4()), true);
+    ASSERT_TRUE(check_equal(V(), V_data_type_3()));
+    ASSERT_TRUE(check_equal(V(), V_data_type_4()));
   }
 
   {
@@ -75,30 +73,29 @@ void test_view_equality_operator() {
     using V_memory_space_type = Kokkos::View<T, Another<V::memory_space>::type>;
 
     // Note: We do not enforce Traits::memory_traits equality
-    ASSERT_EQ(check_equal(V(), V_memory_traits()), true);
-    ASSERT_EQ(check_equal(V(), V_layout_type()), false);
+    ASSERT_TRUE(check_equal(V(), V_memory_traits()));
+    ASSERT_FALSE(check_equal(V(), V_layout_type()));
 
     // Creating this View outside of the if constexpr works around
     // a CUDA link issue, where a defaulted ctor is not created properly
     auto v_mem_space = V_memory_space_type();
-    if constexpr (std::is_same_v<
-                      TEST_EXECSPACE::memory_space,
-                      Kokkos::DefaultHostExecutionSpace::memory_space>)
-      ASSERT_EQ(check_equal(V(), v_mem_space), true);
+    if (std::is_same_v<TEST_EXECSPACE::memory_space,
+                       Kokkos::DefaultHostExecutionSpace::memory_space>)
+      ASSERT_TRUE(check_equal(V(), v_mem_space));
     else
-      ASSERT_EQ(check_equal(V(), v_mem_space), false);
+      ASSERT_FALSE(check_equal(V(), v_mem_space));
   }
 
   {
     // Check for pointer equality
-    ASSERT_EQ(check_equal(V(), V()), true);  // nullptr is equal
+    ASSERT_TRUE(check_equal(V(), V()));  // nullptr is equal
     using V_1D = Kokkos::View<T*, TEST_EXECSPACE>;
-    using V_1D_unmanged =
+    using V_1D_unmanaged =
         Kokkos::View<V_1D::data_type, TEST_EXECSPACE, Kokkos::MemoryUnmanaged>;
     auto v_1D_0 = V_1D("v_1D_0", 3);
     auto v_1D_1 = V_1D("v_1D_1", 3);
-    ASSERT_EQ(check_equal(v_1D_0, v_1D_1), false);
-    ASSERT_EQ(check_equal(v_1D_0, V_1D_unmanged(v_1D_0.data(), 3)), true);
+    ASSERT_FALSE(check_equal(v_1D_0, v_1D_1));
+    ASSERT_TRUE(check_equal(v_1D_0, V_1D_unmanaged(v_1D_0.data(), 3)));
   }
 
   {
@@ -107,7 +104,7 @@ void test_view_equality_operator() {
     auto v_2D_1 =
         Kokkos::View<T[3][1], TEST_EXECSPACE, Kokkos::MemoryUnmanaged>(
             v_2D_0.data());
-    ASSERT_EQ(check_equal(v_2D_0, v_2D_1), false);
+    ASSERT_FALSE(check_equal(v_2D_0, v_2D_1));
   }
 
   {
@@ -117,10 +114,10 @@ void test_view_equality_operator() {
     auto v_2D_3 =
         Kokkos::View<T[3][3], TEST_EXECSPACE, Kokkos::MemoryUnmanaged>(
             v_2D_2.data());
-    ASSERT_EQ(check_equal(v_2D_2, v_2D_3), true);
+    ASSERT_TRUE(check_equal(v_2D_2, v_2D_3));
   }
 }
 
-TEST(TEST_CATEGORY, View_equality_operator) { test_view_equality_operator(); }
+TEST(TEST_CATEGORY, view_equality_operator) { test_view_equality_operator(); }
 
 }  // namespace
