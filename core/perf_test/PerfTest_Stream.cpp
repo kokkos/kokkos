a6834eaaca26faed829e4533cf613bf0434fc091
Prateek Chawla <p.chawla@fz-juelich.de>
2026-01-14 17:51:05 +0100

    make perftest allocation unconditional
    Signed-off-by: Prateek Chawla <p.chawla@fz-juelich.de>

--- core/perf_test/PerfTest_Stream.cpp
+++ core/perf_test/PerfTest_Stream.cpp
@@ -213,21 +213,17 @@ static void or_skip(benchmark::State& state) {
   }
 }
 
-// As of May 2025, 10^8 doubles is larger than caches, but not so large as
-// to be inconvenient. Also run 11^8 for a quick check of convergence.
-// This value is only used if the available memory is > 6GB, so the test defaults
-// to a low-memory mode using 9^8 and 10^8 elements.
-#ifdef KOKKOS_ENABLE_LARGE_MEM_TESTS
-constexpr static int base_val = 10;
-#else
-constexpr static int base_val = 9;
-#endif
+// We choose the case of 9^8 doubles to test with (which allocates ~1 GB), as
+// well as a quick test with 10^8 doubles (~2.4 GB) for checking convergence. As
+// of January 2026, This value is higher than caches but reasonable enough
+// to be safely allocated, even on low-memory devices. This needs to be
+// (potentially) periodically revisited.
 
 #define STREAM_ARGS(label)            \
   Name(label)                         \
       ->ArgName("N")                  \
-      ->Arg(base_val)                 \
-      ->Arg(base_val + 1)             \
+      ->Arg(9)                        \
+      ->Arg(10)                       \
       ->Unit(benchmark::kMillisecond) \
       ->UseManualTime()
 
