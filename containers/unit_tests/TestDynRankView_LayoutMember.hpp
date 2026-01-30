eebf88dd3b5f9ba2e9e6f00bc6ef9b5b22e55e5f
Damien L-G <dalg24@gmail.com>
2026-01-30 15:42:44 +0900

    Revert "Fix a View::layout() bug for LayoutRight and in DynRankView (#8838)"
    This reverts commit a01851467545f5afaa866974238fa147e75375e9.
    
    Signed-off-by: Damien L-G <dalg24@gmail.com>

--- containers/unit_tests/TestDynRankView_LayoutMember.hpp
+++ containers/unit_tests/TestDynRankView_LayoutMember.hpp
@@ -9,7 +9,6 @@ namespace {
 
 template <class Layout>
 void test_dyn_rank_view_layout_member() {
-  constexpr bool is_ll = std::is_same_v<Layout, Kokkos::LayoutLeft>;
   {
     Kokkos::DynRankView<int, Layout> a(
         Kokkos::View<int***, Layout>("A", 11, 7, 5));
@@ -18,9 +17,11 @@ void test_dyn_rank_view_layout_member() {
     ASSERT_EQ(l.dimension[1], 7lu);
     ASSERT_EQ(l.dimension[2], 5lu);
 #ifdef KOKKOS_ENABLE_IMPL_VIEW_LEGACY
-    ASSERT_TRUE((l.stride == is_ll ? 11lu : KOKKOS_INVALID_INDEX));
+    ASSERT_EQ(l.stride, KOKKOS_INVALID_INDEX);
 #else
-    ASSERT_TRUE((l.stride == is_ll ? 11lu : 5lu));
+    ASSERT_EQ(l.stride, (std::is_same_v<Layout, Kokkos::LayoutLeft>
+                             ? 11lu
+                             : KOKKOS_INVALID_INDEX));
 #endif
   }
   {
@@ -29,9 +30,10 @@ void test_dyn_rank_view_layout_member() {
     ASSERT_EQ(l.dimension[0], 7lu);
     ASSERT_EQ(l.dimension[1], 5lu);
 #ifdef KOKKOS_ENABLE_IMPL_VIEW_LEGACY
-    ASSERT_TRUE((l.stride == is_ll ? 7lu : KOKKOS_INVALID_INDEX));
+    ASSERT_EQ(l.stride, KOKKOS_INVALID_INDEX);
 #else
-    ASSERT_TRUE((l.stride == is_ll ? 7lu : 5lu));
+    ASSERT_EQ(l.stride,
+              (std::is_same_v<Layout, Kokkos::LayoutLeft> ? 7lu : 5lu));
 #endif
   }
 }
@@ -41,13 +43,4 @@ void test_dyn_rank_view_layout_member() {
 TEST(TEST_CATEGORY, dyn_rank_view_layout_member) {
   test_dyn_rank_view_layout_member<Kokkos::LayoutRight>();
   test_dyn_rank_view_layout_member<Kokkos::LayoutLeft>();
-}
-
-TEST(TEST_CATEGORY, dyn_rank_view_layout_bug) {
-  Kokkos::DynRankView<double, Kokkos::LayoutRight> a("A", 10, 11);
-  // The following call failed with an error with legacy View off
-  Kokkos::DynRankView<double, Kokkos::LayoutRight> b("B", a.layout());
-#ifndef KOKKOS_ENABLE_IMPL_VIEW_LEGACY
-  ASSERT_EQ(a.mapping(), b.mapping());
-#endif
 }
