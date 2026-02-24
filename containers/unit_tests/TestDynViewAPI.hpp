695ebd507ba90dd8acb11eb848312de5653e99b3
Jakob Bludau <bludauj@ornl.gov>
2026-02-24 15:37:54 -0500

    make guard specific to the versions we know to fail
    Signed-off-by: Jakob Bludau <bludauj@ornl.gov>

--- containers/unit_tests/TestDynViewAPI.hpp
+++ containers/unit_tests/TestDynViewAPI.hpp
@@ -696,7 +696,8 @@ class TestDynViewAPI {
 
   static void run_operator_test_rank67() {
     // FIXME_CLANG 22 The test triggers an internal compiler error in clang 22.
-#if !defined(KOKKOS_COMPILER_CLANG) || (KOKKOS_COMPILER_CLANG < 2200)
+#if !defined(KOKKOS_COMPILER_CLANG) || (KOKKOS_COMPILER_CLANG < 2200) || \
+    (KOKKOS_COMPILER_CLANG > 2210)
     TestViewOperator_LeftAndRight<int, device, 7>::testit(2, 3, 4, 2, 3, 4, 2);
 #endif
     TestViewOperator_LeftAndRight<int, device, 6>::testit(2, 3, 4, 2, 3, 4);
