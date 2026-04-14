6048003388b1a34586a4375e77c30cdfa77553b3
Malachi <malachi2@illinois.edu>
2026-04-14 13:51:30 -0600

    Update core/src/Kokkos_View.hpp
    Co-authored-by: Dong Hun Lee <59181952+ldh4@users.noreply.github.com>
    Signed-off-by: Malachi <malachi2@illinois.edu>

--- core/src/Kokkos_View.hpp
+++ core/src/Kokkos_View.hpp
@@ -330,7 +330,11 @@ class View : public Impl::BasicViewFromTraits<DataType, Properties...>::type {
   }
 
   KOKKOS_INLINE_FUNCTION constexpr int extent_int(size_t r) const {
+#ifdef KOKKOS_ENABLE_DEPRECATED_CODE_5
+    return static_cast<int>(base_t::extent(r));
+#else
     return static_cast<int>(extent(r));
+#endif
   }
   //----------------------------------------
   // Allow specializations to query their specialized map
