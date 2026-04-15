366aab570efff0e77471e8ac95521f0dce32c5ca
Nemanja Grujic <nemanja.grujic@nextsilicon.com>
2026-04-15 15:17:42 -0500

    nextsilicon: Enable printing from host
    Co-authored-by: Patrick Fasano <patrick.fasano@nextsilicon.com>
    Signed-off-by: Patrick Fasano <patrick.fasano@nextsilicon.com>

--- core/src/NextSilicon/Kokkos_NextSilicon_Printf.hpp
+++ core/src/NextSilicon/Kokkos_NextSilicon_Printf.hpp
@@ -6,12 +6,20 @@
 
 #include <Kokkos_Macros.hpp>
 
+#include <cstdio>
+
 namespace Kokkos {
 namespace Impl {
 
 template <typename... Args>
-void nextsilicon_printf(const char* /*format*/, Args... /*args*/) {
+void nextsilicon_printf(const char* format, Args... args) {
   // FIXME_NEXTSILICON: CS-515 tracks printing from device
+  if (!__next_is_in_handed_off_code()) {
+    if constexpr (sizeof...(Args) == 0)
+      ::printf("%s", format);
+    else
+      ::printf(format, args...);
+  }
 }
 
 }  // namespace Impl
