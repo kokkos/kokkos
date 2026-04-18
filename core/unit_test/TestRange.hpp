8c0f4b3d3bd8f20316c7894618b1958e860bd828
Stéphane Del Pino <35096584+delpinux@users.noreply.github.com>
2026-04-18 17:10:22 +0200

    Update core/unit_test/TestRange.hpp
    Co-authored-by: Christian Trott <crtrott@sandia.gov>
    Signed-off-by: Stéphane Del Pino <35096584+delpinux@users.noreply.github.com>
    Signed-off-by: Stephane Del Pino <stephane.delpino44@gmail.com>

--- core/unit_test/TestRange.hpp
+++ core/unit_test/TestRange.hpp
@@ -409,8 +409,8 @@ TEST(TEST_CATEGORY, large_parallel_for_reduce) {
                                Kokkos::HostSpace>) {
     GTEST_SKIP() << "Disabling for host backends";
   }
-#ifdef KOKKOS_ARCH_MAXWELL50
-  GTEST_SKIP() << "skipping for Maxwell50 devices (not enough memory)";
+#ifndef KOKKOS_ENABLE_LARGE_MEM_TESTS
+  GTEST_SKIP() << "skipping for GPUs with not enough memory";
 #endif
   test_large_parallel_for_reduce();
 }
