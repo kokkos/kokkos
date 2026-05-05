a00fd0ec61f7ecb7b2f0107f84edde5e0c580380
Adrien Taberner <adrien.taberner@cea.fr>
2026-05-05 17:54:25 +0200

    HIP(AMD APU): use ViewFill instead of ZeroMemset
    Signed-off-by: Adrien Taberner <adrien.taberner@cea.fr>

--- core/src/Kokkos_CopyViews.hpp
+++ core/src/Kokkos_CopyViews.hpp
@@ -902,6 +902,17 @@ inline std::enable_if_t<
 contiguous_fill_or_memset(
     const ExecutionSpace& exec_space, const View<DT, DP...>& dst,
     typename ViewTraits<DT, DP...>::const_value_type& value) {
+  // We allow user on an AMD APU with unified memory to `malloc` and wrap that
+  // in an unmanaged SharedSpace view. In ROCm <= 7.2.0 (and possibly later),
+  // hipMemsetAsync on a host-allocated pointer returns an hipErrorInvalidValue
+  // error, but accessing the data via a GPU kernel works as long as xnack is
+  // present and enabled (HSA_XNACK=1)
+#if defined(KOKKOS_IMPL_HIP_UNIFIED_MEMORY)
+  if constexpr (std::is_same_v<ExecutionSpace, Kokkos::HIP>) {
+    contiguous_fill(exec_space, dst, value);
+    return;
+  }
+#endif
   if (has_all_zero_bits(value))
     ZeroMemset(exec_space, dst.data(),
                dst.size() * sizeof(typename ViewTraits<DT, DP...>::value_type));
