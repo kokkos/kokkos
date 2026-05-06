438341969c8f9fd3fc75b1dbf2a86e6b22ed5296
Jan Ciesko <jan.ciesko@gmail.com>
2026-05-06 12:00:19 -0600

    Use [[maybe_unused]] attribute
    Signed-off-by: Jan Ciesko <jan.ciesko@gmail.com>

--- algorithms/src/std_algorithms/impl/Kokkos_Constraints.hpp
+++ algorithms/src/std_algorithms/impl/Kokkos_Constraints.hpp
@@ -235,13 +235,9 @@ KOKKOS_INLINE_FUNCTION void expect_no_overlap(
 template <typename DataType1, typename... Properties1, typename DataType2,
           typename... Properties2>
 KOKKOS_INLINE_FUNCTION void expect_equal_extents(
-    const ::Kokkos::View<DataType1, Properties1...>& a,
-    const ::Kokkos::View<DataType2, Properties2...>& b) {
-  // this is from no-op for release
+    [[maybe_unused]] const ::Kokkos::View<DataType1, Properties1...>& a,
+    [[maybe_unused]] const ::Kokkos::View<DataType2, Properties2...>& b) {
   KOKKOS_EXPECTS(a.extent(0) == b.extent(0));
-  // avoid compiler complaining when KOKKOS_EXPECTS is no-op
-  (void)a;
-  (void)b;
 }
 
 //
@@ -251,13 +247,9 @@ KOKKOS_INLINE_FUNCTION void expect_equal_extents(
 template <typename DataType1, typename... Properties1, typename DataType2,
           typename... Properties2>
 KOKKOS_INLINE_FUNCTION void expect_less_than_extents(
-    const ::Kokkos::View<DataType1, Properties1...>& a,
-    const ::Kokkos::View<DataType2, Properties2...>& b) {
-  // this is from no-op for release
+    [[maybe_unused]] const ::Kokkos::View<DataType1, Properties1...>& a,
+    [[maybe_unused]] const ::Kokkos::View<DataType2, Properties2...>& b) {
   KOKKOS_EXPECTS(a.extent(0) <= b.extent(0));
-  // avoid compiler complaining when KOKKOS_EXPECTS is no-op
-  (void)a;
-  (void)b;
 }
 
 }  // namespace Impl
