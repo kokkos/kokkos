d6847964ee62e14bcf78ac3f4be4f0ba4adca487
Nicolas Morales <nmmoral@sandia.gov>
2026-05-14 17:47:30 -0400

    begin work on openmp flattened mdrange

--- core/src/Serial/Kokkos_Serial_Parallel_MDRange.hpp
+++ core/src/Serial/Kokkos_Serial_Parallel_MDRange.hpp
@@ -12,20 +12,6 @@
 
 namespace Kokkos {
 namespace Impl {
-
-template <typename IndexType, IndexType End, typename ItegerSequence>
-struct make_reverse_integer_sequence_impl;
-
-template <typename IndexType, IndexType End, IndexType... Indices>
-struct make_reverse_integer_sequence_impl<
-    IndexType, End, std::integer_sequence<IndexType, Indices...>>
-    : std::type_identity<std::integer_sequence<IndexType, End - 1 - Indices...>> {};
-
-template <typename IndexType, IndexType N>
-using make_reverse_integer_sequence =
-    typename make_reverse_integer_sequence_impl<
-        IndexType, N, std::make_integer_sequence<IndexType, N>>::type;
-
 template <class MDRP, class Functor, class Tag>
   requires std::same_as<typename MDRP::execution_space, Kokkos::Serial>
 class FlatIterate<MDRP, Functor, Tag> {
