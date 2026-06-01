e1ff2912a5e1c4c689d29762bdfd188fa7bea0db
Vladislav Semykin <34096407+ViNN280801@users.noreply.github.com>
2026-06-01 18:24:46 +0300

    Update algorithms/src/Kokkos_Random.hpp
    Co-authored-by: Daniel Arndt <arndtd@ornl.gov>
    Signed-off-by: Vladislav Semykin <34096407+ViNN280801@users.noreply.github.com>

--- algorithms/src/Kokkos_Random.hpp
+++ algorithms/src/Kokkos_Random.hpp
@@ -1221,8 +1221,6 @@ class Random_XorShift1024 {
     // INT64_MAX.
     const uint64_t urange =
         static_cast<uint64_t>(end) - static_cast<uint64_t>(start);
-    if (urange <= static_cast<uint64_t>(MAX_RAND64))
-      return rand64(static_cast<int64_t>(urange)) + start;
     return static_cast<int64_t>(urand64(urange) + static_cast<uint64_t>(start));
   }
 
