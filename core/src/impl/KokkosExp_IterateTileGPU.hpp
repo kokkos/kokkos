ad49105ff4bd435698b2a17125d557e440511baa
Damien L-G <dalg24@gmail.com>
2026-06-02 09:11:08 -0400

    Drop `KOKKOS_IMPL_FORCEINLINE` attribute in `Impl::DeviceIterate`
    Signed-off-by: Damien L-G <dalg24@gmail.com>

--- core/src/impl/KokkosExp_IterateTileGPU.hpp
+++ core/src/impl/KokkosExp_IterateTileGPU.hpp
@@ -246,8 +246,7 @@ struct DeviceIterate {
   // \tparam RIdx rank index
   // \return Flat hardware thread ID (packed) or global index (unpacked)
   template <unsigned RIdx>
-  KOKKOS_IMPL_DEVICE_FUNCTION KOKKOS_IMPL_FORCEINLINE constexpr index_type
-  my_begin() const noexcept {
+  KOKKOS_IMPL_DEVICE_FUNCTION constexpr index_type my_begin() const noexcept {
     static_assert(RIdx < 6);
     if constexpr (Rank < 4) {
       // No packed index
@@ -284,8 +283,7 @@ struct DeviceIterate {
   // \tparam RIdx rank index
   // \return product of the extents (packed) or upper bound (unpacked)
   template <unsigned RIdx>
-  KOKKOS_IMPL_DEVICE_FUNCTION KOKKOS_IMPL_FORCEINLINE constexpr index_type
-  my_end() const noexcept {
+  KOKKOS_IMPL_DEVICE_FUNCTION constexpr index_type my_end() const noexcept {
     static_assert(RIdx < 6);
     if constexpr (is_packed_index<RIdx>()) {
       if constexpr (RIdx % 2 == 0) {
@@ -304,8 +302,7 @@ struct DeviceIterate {
   // \tparam RIdx rank index
   // \return The stride used for this rank index
   template <unsigned RIdx>
-  KOKKOS_IMPL_DEVICE_FUNCTION KOKKOS_IMPL_FORCEINLINE constexpr index_type
-  my_stride() const noexcept {
+  KOKKOS_IMPL_DEVICE_FUNCTION constexpr index_type my_stride() const noexcept {
     // revisit the need for static_cast<index_type>
     static_assert(RIdx < 6);
     if constexpr (Rank < 4) {
