bf4a529e896e6e9d835796fa435a8a8aaa11a78c
Damien L-G <dalg24@gmail.com>
2026-06-03 18:09:32 -0400

    Include test that actually use the function annotations
    Signed-off-by: Damien L-G <dalg24@gmail.com>

--- core/unit_test/TestFunctionAnnotationMacros.cpp
+++ core/unit_test/TestFunctionAnnotationMacros.cpp
@@ -39,10 +39,47 @@ static_assert(false, "KOKKOS_LAMBDA macro is not defined!");
 static_assert(false, "KOKKOS_CLASS_LAMBDA macro is not defined!");
 #endif
 
-#ifndef KOKKOS_FORCEINLINE_LAMBDA
-static_assert(false, "KOKKOS_FORCEINLINE_LAMBDA macro is not defined!");
-#endif
+namespace {
 
-#ifndef KOKKOS_CLASS_FORCEINLINE_LAMBDA
-static_assert(false, "KOKKOS_CLASS_FORCEINLINE_LAMBDA macro is not defined!");
-#endif
+#define KOKKOS_TEST_FUNCTION_ANNOTATION(ANNOTATION)                         \
+  struct Struct_##ANNOTATION {                                              \
+    int m_val = 0;                                                          \
+    static ANNOTATION constexpr int kokkos_function_static() { return 42; } \
+    ANNOTATION static constexpr int static_kokkos_function() { return 42; } \
+    ANNOTATION constexpr int kokkos_function() const { return m_val; }      \
+  };                                                                        \
+  ANNOTATION constexpr int Fun_##ANNOTATION() {                             \
+    Struct_##ANNOTATION fun{42};                                            \
+    return fun.kokkos_function();                                           \
+  }                                                                         \
+  static_assert(Struct_##ANNOTATION::static_kokkos_function() == 42);       \
+  static_assert(Struct_##ANNOTATION::kokkos_function_static() == 42);       \
+  static_assert(Fun_##ANNOTATION() == 42)
+
+KOKKOS_TEST_FUNCTION_ANNOTATION(KOKKOS_FUNCTION);
+KOKKOS_TEST_FUNCTION_ANNOTATION(KOKKOS_INLINE_FUNCTION);
+KOKKOS_TEST_FUNCTION_ANNOTATION(KOKKOS_FORCEINLINE_FUNCTION);
+
+template <class T>
+struct Foo {
+  KOKKOS_INLINE_FUNCTION_DELETED Foo(Foo const&)            = delete;
+  KOKKOS_INLINE_FUNCTION_DELETED Foo& operator-(Foo const&) = delete;
+  KOKKOS_DEFAULTED_FUNCTION Foo()                           = default;
+  T m_val;
+  Foo(T val) : m_val(val) {}
+};
+template <class T>
+KOKKOS_DEDUCTION_GUIDE Foo(T) -> Foo<T>;
+static_assert(sizeof(Foo(3.14)) > 0);
+
+struct Bar {
+  int m_val = 3;
+  KOKKOS_FUNCTION constexpr int fun() const {
+    auto dec = KOKKOS_LAMBDA(int x) { return 2 * x; };
+    auto lam = KOKKOS_CLASS_LAMBDA() { return m_val; };
+    return dec(lam());
+  }
+};
+static_assert(Bar().fun() == 6);
+
+}  // namespace
