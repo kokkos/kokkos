9d32b87573c95b13ad25948c9b5d5b1e4cc05fd8
Prateek Chawla <p.chawla@fz-juelich.de>
2026-07-07 16:25:00 +0200

    don't load hwm limiter i f large memtests are enabled
    Signed-off-by: Prateek Chawla <p.chawla@fz-juelich.de>

--- cmake/fake_tribits.cmake
+++ cmake/fake_tribits.cmake
@@ -95,7 +95,7 @@ function(KOKKOS_ADD_TEST)
   # It uses the rusage struct, so it will only work on Linux or Mac.
   if(CMAKE_SYSTEM_NAME STREQUAL "Linux" OR CMAKE_SYSTEM_NAME STREQUAL "Darwin")
     string(FIND "${TEST_NAME}" "DefaultInstance" index)
-    if((NOT TEST_TOOL) AND (${index} LESS 0))
+    if((NOT TEST_TOOL) AND (${index} LESS 0) AND (NOT KOKKOS_ENABLE_LARGE_MEM_TESTS))
       set_property(TEST ${TEST_NAME} APPEND PROPERTY ENVIRONMENT_MODIFICATION KOKKOS_TOOLS_LIBS=set:$<TARGET_FILE:kokkoshwmtracker-tool>)
     endif()
   endif()
