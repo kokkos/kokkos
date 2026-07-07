5573e51985c2891b0a3971720679c7bf1babb1b0
Prateek Chawla <p.chawla@fz-juelich.de>
2026-07-07 16:23:56 +0200

    remove unnecessary includes
    Signed-off-by: Prateek Chawla <p.chawla@fz-juelich.de>

--- core/unit_test/tools/TestExcessMemoryAllocation.cpp
+++ core/unit_test/tools/TestExcessMemoryAllocation.cpp
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 // SPDX-FileCopyrightText: Copyright Contributors to the Kokkos project
 
-#include <alloca.h>
 #include <gtest/gtest.h>
 #include <Kokkos_Core.hpp>
-#include "gtest/gtest.h"
 
 void allocate_large_view() {
   Kokkos::initialize();
