cece5bd83efbca8b591a24c886e99f4965e597dd
Prateek Chawla <p.chawla@fz-juelich.de>
2026-07-08 10:47:53 +0200

    Update core/unit_test/tools/TestExcessMemoryAllocation.cpp
    Co-authored-by: Trévis Morvany <63788850+tretre91@users.noreply.github.com>
    Signed-off-by: Prateek Chawla <p.chawla@fz-juelich.de>

--- core/unit_test/tools/TestExcessMemoryAllocation.cpp
+++ core/unit_test/tools/TestExcessMemoryAllocation.cpp
@@ -6,9 +6,11 @@
 
 void allocate_large_view() {
   Kokkos::initialize();
-  size_t very_large_size = 5 << 30;
-  Kokkos::View<double *, Kokkos::DefaultHostExecutionSpace> a("A",
+  {
+    size_t very_large_size = 5 << 30;
+    Kokkos::View<double *, Kokkos::DefaultHostExecutionSpace> a("A",
                                                               very_large_size);
+  }
   Kokkos::finalize();
 }
 
