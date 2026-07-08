082bb662f3d7aef2eecce304a4d3f9f186728904
Nathan Ellingwood <ndellin@sandia.gov>
2026-07-08 13:53:30 -0600

    remove Kokkos_ENABLE_IMPL_MDSPAN option
    Signed-off-by: Nathan Ellingwood <ndellin@sandia.gov>

--- cmake/kokkos_enable_options.cmake
+++ cmake/kokkos_enable_options.cmake
@@ -127,22 +127,14 @@ if(Kokkos_ENABLE_EXPERIMENTAL_CXX20_MODULES)
   endif()
 endif()
 
-kokkos_enable_option(IMPL_MDSPAN ON "Whether to enable mdspan support (internal use only)")
 kokkos_enable_option(MDSPAN_EXTERNAL OFF "Whether to use an external version of mdspan")
 kokkos_enable_option(
   IMPL_CHECK_POSSIBLY_BREAKING_LAYOUTS
   OFF
   "Whether to check for uses of LayoutRight that have an explicit stride that may have changed in the new View implementation."
 )
-mark_as_advanced(Kokkos_ENABLE_IMPL_MDSPAN)
 mark_as_advanced(Kokkos_ENABLE_MDSPAN_EXTERNAL)
 mark_as_advanced(IMPL_CHECK_POSSIBLY_BREAKING_LAYOUTS)
-
-if(Kokkos_ENABLE_IMPL_MDSPAN)
-  set(VIEW_LEGACY_DEFAULT OFF)
-else()
-  set(VIEW_LEGACY_DEFAULT ON)
-endif()
 
 kokkos_enable_option(COMPLEX_ALIGN ON "Whether to align Kokkos::complex to 2*alignof(RealType)")
 
