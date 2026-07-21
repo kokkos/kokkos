8a014808a7d8d209c2ab559bf2b36db416e88707
daxmawal <jeanfrancoismanutea@gmail.com>
2026-07-21 11:55:59 +0200

    Restore in args parameters [[maybe_unused]]
    Signed-off-by: daxmawal <jeanfrancoismanutea@gmail.com>

--- core/src/Cuda/Kokkos_CudaSpace.cpp
+++ core/src/Cuda/Kokkos_CudaSpace.cpp
@@ -347,7 +347,8 @@ void CudaSpace::deallocate(const char *arg_label, void *const arg_alloc_ptr,
 }
 void CudaSpace::impl_deallocate(
     const char *arg_label, void *const arg_alloc_ptr,
-    const size_t /*arg_alloc_size*/, const size_t arg_reported_size,
+    [[maybe_unused]] const size_t arg_alloc_size,
+    const size_t arg_reported_size,
     const Kokkos::Tools::SpaceHandle arg_handle) const {
   if (Kokkos::Profiling::profileLibraryLoaded()) {
     Kokkos::Profiling::deallocateData(arg_handle, arg_label, arg_alloc_ptr,
