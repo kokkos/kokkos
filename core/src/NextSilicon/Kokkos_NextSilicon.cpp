f306db776c9219e44094d6748cf59c8a2e8a2026
Carl Pearson <cwpears@sandia.gov>
2026-07-29 09:01:26 -0600

    nextsilicon: handle callbacks before and during initialize
    Signed-off-by: Carl Pearson <cwpears@sandia.gov>

--- core/src/NextSilicon/Kokkos_NextSilicon.cpp
+++ core/src/NextSilicon/Kokkos_NextSilicon.cpp
@@ -24,9 +24,15 @@ Kokkos::Experimental::NextSilicon::~NextSilicon() {
 
 void Kokkos::Experimental::NextSilicon::impl_initialize(
     InitializationSettings const& /*settings*/) {
+  // Kokkos::is_initialized is false still
+  // Run callbacks registered before Kokkos::initialize().
+  Kokkos::Impl::run_nextsilicon_initialization_callbacks();
+
   Impl::NextSiliconInternal::default_instance =
       Kokkos::Impl::HostSharedPtr(new Impl::NextSiliconInternal);
 
+  // Kokkos::is_initialized is false still
+  // Run any callbacks registered while creating the default instance.
   Kokkos::Impl::run_nextsilicon_initialization_callbacks();
 }
 
