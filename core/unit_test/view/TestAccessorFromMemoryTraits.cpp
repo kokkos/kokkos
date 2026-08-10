73ab4e5ee6b8d2230bb7b2b22d5a0199c5b21a15
Nathan Ellingwood <ndellin@sandia.gov>
2026-08-10 13:16:51 -0600

    Drop desul header include
    Fix split off into https://github.com/kokkos/kokkos/pull/9417
    
    Signed-off-by: Nathan Ellingwood <ndellin@sandia.gov>

--- core/unit_test/view/TestAccessorFromMemoryTraits.cpp
+++ core/unit_test/view/TestAccessorFromMemoryTraits.cpp
@@ -2,7 +2,6 @@
 // SPDX-FileCopyrightText: Copyright Contributors to the Kokkos project
 
 #include <Kokkos_Macros.hpp>
-#include <desul/atomics.hpp>
 #ifdef KOKKOS_ENABLE_EXPERIMENTAL_CXX20_MODULES
 import kokkos.core;
 import kokkos.core_impl;
