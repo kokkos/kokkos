fd1894c64884cd9c3004f359021881a94c51a65e
Damien L-G <dalg24+github@gmail.com>
2026-08-10 15:24:01 -0600

    Refactor includes in TestAccessorFromMemoryTraits.cpp
    Removed duplicate includes and cleaned up preprocessor directives.

--- core/unit_test/view/TestAccessorFromMemoryTraits.cpp
+++ core/unit_test/view/TestAccessorFromMemoryTraits.cpp
@@ -2,13 +2,6 @@
 // SPDX-FileCopyrightText: Copyright Contributors to the Kokkos project
 
 #include <Kokkos_Macros.hpp>
-#include <desul/atomics.hpp>
-#ifdef KOKKOS_ENABLE_EXPERIMENTAL_CXX20_MODULES
-import kokkos.core;
-import kokkos.core_impl;
-#else
-#include <Kokkos_Core.hpp>
-#include <Kokkos_Macros.hpp>
 #ifdef KOKKOS_ENABLE_EXPERIMENTAL_CXX20_MODULES
 import kokkos.core;
 import kokkos.core_impl;
