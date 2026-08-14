3a9ff7469f12cee44109d60f5617f41a3bc03cae
Damien L-G <dalg24@gmail.com>
2026-08-14 06:41:40 -0600

    Suppress bugprone-exception-escape warnings in work graph policy
    Signed-off-by: Damien L-G <dalg24@gmail.com>

--- core/src/Kokkos_WorkGraphPolicy.hpp
+++ core/src/Kokkos_WorkGraphPolicy.hpp
@@ -52,6 +52,7 @@ class WorkGraphPolicy : public Kokkos::Impl::PolicyTraits<Properties...> {
   graph_type const m_graph;
   ints_type m_queue;
 
+  // NOLINTBEGIN(bugprone-exception-escape)
   KOKKOS_INLINE_FUNCTION
   void push_work(const std::int32_t w) const noexcept {
     const std::int32_t N = m_graph.numRows();
@@ -165,6 +166,7 @@ class WorkGraphPolicy : public Kokkos::Impl::PolicyTraits<Properties...> {
 
     if (0 == count_queue[w]) push_work(w);
   }
+  // NOLINTEND(bugprone-exception-escape)
 
   execution_space space() const { return execution_space(); }
 
