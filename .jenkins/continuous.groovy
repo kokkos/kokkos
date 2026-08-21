0594ebdbbeff1f6efebe04a3b87b0f09710f7a7b
Damien L-G <dalg24@gmail.com>
2026-08-21 06:48:52 -0600

    Drop all -Wno-error=* flags
    Signed-off-by: Damien L-G <dalg24@gmail.com>

--- .jenkins/continuous.groovy
+++ .jenkins/continuous.groovy
@@ -238,7 +238,7 @@ pipeline {
                                 -DCMAKE_BUILD_TYPE=Release \
                                 -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
                                 -DCMAKE_CXX_COMPILER=$WORKSPACE/bin/nvcc_wrapper \
-                                -DCMAKE_CXX_FLAGS="-Werror --Werror=all-warnings -Xcudafe --diag_suppress=940 -Wno-error=undef -Wno-error=parentheses" \
+                                -DCMAKE_CXX_FLAGS="-Werror --Werror=all-warnings -Xcudafe --diag_suppress=940" \
                                 -DCMAKE_EXE_LINKER_FLAGS="-Xnvlink -suppress-stack-size-warning" \
                                 -DCMAKE_CXX_STANDARD=20 \
                                 -DKokkos_INSTALL_TESTING=ON \
@@ -663,7 +663,7 @@ pipeline {
                                 -DCMAKE_BUILD_TYPE=Debug \
                                 -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
                                 -DCMAKE_CXX_COMPILER=$WORKSPACE/bin/nvcc_wrapper \
-                                -DCMAKE_CXX_FLAGS="-Werror -Werror=all-warnings -Wno-error=undef -Wno-error=parentheses" \
+                                -DCMAKE_CXX_FLAGS="-Werror -Werror=all-warnings" \
                                 -DCMAKE_CXX_STANDARD=20 \
                                 -DKokkos_ARCH_NATIVE=ON \
                                 -DKokkos_ENABLE_COMPILER_WARNINGS=ON \
