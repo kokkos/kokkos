c7cd5226346ccc0a890770283f26e4ff513cca44
Seyong Lee <lees2@ornl.gov>
2026-08-21 11:01:17 -0400

    Apply suggestions from code review
    Co-authored-by: Mark Hoemmen <mhoemmen@users.noreply.github.com>
    Signed-off-by: Seyong Lee <lees2@ornl.gov>

--- core/unit_test/range_policy/TestRangePolicyExecutionTypes.hpp
+++ core/unit_test/range_policy/TestRangePolicyExecutionTypes.hpp
@@ -89,8 +89,8 @@ void test_self_similar_range_policy_computation() {
   // Call sum_views(ExecSpace):
   sum_views(Kokkos::DefaultExecutionSpace(), v_x, v_y);
 
-#if !(defined(KOKKOS_ENABLE_OPENACC) && (KOKKOS_COMPILER_NVHPC > 240500))
-  // FIXME_OPENACC: compiling below fails if NVHPC version > 24.5.
+#if !(defined(KOKKOS_ENABLE_OPENACC) && (KOKKOS_COMPILER_NVHPC > 240500) && (KOKKOS_COMPILER_NVHPC <= 260500))
+  // FIXME_OPENACC: compiling below is known to fail for 24.5 < NVHPC version <= 26.5
   // Call sum_views(TeamHandle)
   using team_t = typename Kokkos::TeamPolicy<>::member_type;
   Kokkos::parallel_for(
@@ -149,12 +149,12 @@ TEST(TEST_CATEGORY, self_similar_range_policy_runtime) {
 }
 
 TEST(TEST_CATEGORY, self_similar_range_policy_computation) {
-#if defined(KOKKOS_ENABLE_OPENACC) && (KOKKOS_COMPILER_NVHPC > 240500)
-  // FIXME_OPENACC: compiling below fails if NVHPC version > 24.5.
+#if defined(KOKKOS_ENABLE_OPENACC) && (KOKKOS_COMPILER_NVHPC > 240500) && (KOKKOS_COMPILER_NVHPC <= 260500)
+  // FIXME_OPENACC: compiling below is known to fail for 24.5 < NVHPC version <= 26.5.
   // Error behavior: a device kernel accesses a undefined global symbol.
   // Error message: parse use of undefined value '@_T1236_79002'
-  GTEST_SKIP() << "skipping since the OpenACC backend fails to comple this "
-                  "test if NVHPC version is higher than 24.5";
+  GTEST_SKIP() << "skipping since the OpenACC backend fails to compile this "
+                  "test if 24.5 < NVHPC version <= 26.5";
 #else
   test_self_similar_range_policy_computation();
 #endif
