69283eb47738b3368b6e5ef3dd2e28fd67543e43
Seyong Lee <lees2@ornl.gov>
2026-08-21 15:10:20 -0400

    clang-formatting.
    Signed-off-by: Seyong Lee <lees2@ornl.gov>

--- core/unit_test/range_policy/TestRangePolicyExecutionTypes.hpp
+++ core/unit_test/range_policy/TestRangePolicyExecutionTypes.hpp
@@ -89,9 +89,10 @@ void test_self_similar_range_policy_computation() {
   // Call sum_views(ExecSpace):
   sum_views(Kokkos::DefaultExecutionSpace(), v_x, v_y);
 
-#if !(defined(KOKKOS_ENABLE_OPENACC) && (KOKKOS_COMPILER_NVHPC > 240500) && (KOKKOS_COMPILER_NVHPC <= 260500))
-  // FIXME_OPENACC: compiling below is known to fail for 24.5 < NVHPC version <= 26.5
-  // Call sum_views(TeamHandle)
+#if !(defined(KOKKOS_ENABLE_OPENACC) && (KOKKOS_COMPILER_NVHPC > 240500) && \
+      (KOKKOS_COMPILER_NVHPC <= 260500))
+  // FIXME_OPENACC: compiling below is known to fail for 24.5 < NVHPC version
+  // <= 26.5 Call sum_views(TeamHandle)
   using team_t = typename Kokkos::TeamPolicy<>::member_type;
   Kokkos::parallel_for(
       "apxyFromTeam", Kokkos::TeamPolicy(num_teams, Kokkos::AUTO()),
@@ -149,10 +150,11 @@ TEST(TEST_CATEGORY, self_similar_range_policy_runtime) {
 }
 
 TEST(TEST_CATEGORY, self_similar_range_policy_computation) {
-#if defined(KOKKOS_ENABLE_OPENACC) && (KOKKOS_COMPILER_NVHPC > 240500) && (KOKKOS_COMPILER_NVHPC <= 260500)
-  // FIXME_OPENACC: compiling below is known to fail for 24.5 < NVHPC version <= 26.5.
-  // Error behavior: a device kernel accesses a undefined global symbol.
-  // Error message: parse use of undefined value '@_T1236_79002'
+#if defined(KOKKOS_ENABLE_OPENACC) && (KOKKOS_COMPILER_NVHPC > 240500) && \
+    (KOKKOS_COMPILER_NVHPC <= 260500)
+  // FIXME_OPENACC: compiling below is known to fail for 24.5 < NVHPC version
+  // <= 26.5. Error behavior: a device kernel accesses a undefined global
+  // symbol. Error message: parse use of undefined value '@_T1236_79002'
   GTEST_SKIP() << "skipping since the OpenACC backend fails to compile this "
                   "test if 24.5 < NVHPC version <= 26.5";
 #else
