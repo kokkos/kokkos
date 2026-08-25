db36a934f5093cc763894ddc340b4c0924171bad
Damien L-G <dalg24@gmail.com>
2026-08-24 19:55:40 -0600

    Ensure the template arg is an execution space
    Signed-off-by: Damien L-G <dalg24@gmail.com>

--- core/unit_test/TestSubView_c13.hpp
+++ core/unit_test/TestSubView_c13.hpp
@@ -13,7 +13,7 @@ TEST(TEST_CATEGORY, view_test_unmanaged_subview_reset) {
 
 #if !defined(KOKKOS_ENABLE_CUDA) || defined(KOKKOS_ENABLE_CUDA_CONSTEXPR)
 TEST(TEST_CATEGORY, view_subview_std_pair_in_kernel) {
-  TEST_EXECSPACE exec;
+  TEST_EXECSPACE::execution_space exec;
   (void)TestViewSubview::TestSubviewStdPairInKernel(exec);
 }
 #endif
