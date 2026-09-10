f5df64973993fa0d1009585aafef6af9d7e4af3c
Kevin Huck <Kevin.Huck@amd.com>
2026-09-10 14:22:34 -0700

    Fixing issue_9511 for crayCC compiler and HIP targets - also works with amdclang++.
    Signed-off-by: Kevin Huck <Kevin.Huck@amd.com>

--- core/src/HIP/Kokkos_HIP_ReduceScan.hpp
+++ core/src/HIP/Kokkos_HIP_ReduceScan.hpp
@@ -143,11 +143,27 @@ struct HIPReductionsFunctor<FunctorType, false> {
   {
     int const lane_id =
         (threadIdx.y * blockDim.x + threadIdx.x) % HIPTraits::WarpSize;
+// HIP added support for __syncwarp() in version 7.0
+#if HIP_VERSION_MAJOR >= 7
+    size_t mask =
+       width == HIPTraits::WarpSize
+           ? 0xffffffffffffffff
+           : ((1 << width) - 1)
+                 << ((threadIdx.y * blockDim.x + threadIdx.x) / width) * width;
+    __syncwarp(mask);
+#else
+    __syncthreads();
+#endif
     for (int delta = skip_vector ? blockDim.x : 1; delta < width; delta *= 2) {
       if (lane_id + delta < width && (lane_id % (delta * 2) == 0)) {
         functor.join(value, value + delta);
       }
     }
+#if HIP_VERSION_MAJOR >= 7
+    __syncwarp(mask);
+#else
+    __syncthreads();
+#endif
     *value = *(value - lane_id);
   }
 
