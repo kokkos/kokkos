36b43846d31c49779d6b6e50c866fa62def54150
Daniel Arndt <arndtd@ornl.gov>
2026-09-14 10:22:45 -0400

    KOKKOS_HALF_TYPE_DEFINED->KOKKOS_HAS_NATIVE_HALF_TYPE
    Signed-off-by: Daniel Arndt <arndtd@ornl.gov>

--- core/src/setup/Kokkos_Setup_HIP.hpp
+++ core/src/setup/Kokkos_Setup_HIP.hpp
@@ -49,8 +49,8 @@ static_assert(false,
 #define KOKKOS_IMPL_HIP_UNIFIED_MEMORY
 #endif
 
-#define KOKKOS_HALF_TYPE_DEFINED
-#define KOKKOS_BHALF_TYPE_DEFINED
+#define KOKKOS_HAS_NATIVE_HALF_TYPE
+#define KOKKOS_HAS_NATIVE_BHALF_TYPE
 
 #if (HIP_VERSION_MAJOR > 6 ||                               \
      (HIP_VERSION_MAJOR == 6 && HIP_VERSION_MINOR >= 4)) || \
