29834beba9147f067f8e47f621504e53456be560
Seyong Lee <lees2@ornl.gov>
2026-09-15 11:56:22 -0400

    Re-enable OpenACC unit tests fixed by this PR.
    Signed-off-by: Seyong Lee <lees2@ornl.gov>

--- core/unit_test/range_policy/TestRangePolicyExecutionTypes.hpp
+++ core/unit_test/range_policy/TestRangePolicyExecutionTypes.hpp
@@ -89,10 +89,7 @@ void test_self_similar_range_policy_computation() {
   // Call sum_views(ExecSpace):
   sum_views(TEST_EXECSPACE(), v_x, v_y);
 
-#if !(defined(KOKKOS_ENABLE_OPENACC) && (KOKKOS_COMPILER_NVHPC > 240500) && \
-      (KOKKOS_COMPILER_NVHPC <= 260500))
-  // FIXME_OPENACC: compiling below is known to fail for 24.5 < NVHPC version
-  // <= 26.5 Call sum_views(TeamHandle)
+  // Call sum_views(TeamHandle)
   using team_t = typename Kokkos::TeamPolicy<TEST_EXECSPACE>::member_type;
   Kokkos::parallel_for(
       "apxyFromTeam",
@@ -101,7 +98,6 @@ void test_self_similar_range_policy_computation() {
         sum_views(team, Kokkos::subview(M_x, team.league_rank(), Kokkos::ALL()),
                   Kokkos::subview(M_y, team.league_rank(), Kokkos::ALL()));
       });
-#endif
 
   // Check v_x
   size_t result = 0;
@@ -151,16 +147,7 @@ TEST(TEST_CATEGORY, self_similar_range_policy_runtime) {
 }
 
 TEST(TEST_CATEGORY, self_similar_range_policy_computation) {
-#if defined(KOKKOS_ENABLE_OPENACC) && (KOKKOS_COMPILER_NVHPC > 240500) && \
-    (KOKKOS_COMPILER_NVHPC <= 260500)
-  // FIXME_OPENACC: compiling below is known to fail for 24.5 < NVHPC version
-  // <= 26.5. Error behavior: a device kernel accesses a undefined global
-  // symbol. Error message: parse use of undefined value '@_T1236_79002'
-  GTEST_SKIP() << "skipping since the OpenACC backend fails to compile this "
-                  "test if 24.5 < NVHPC version <= 26.5";
-#else
   test_self_similar_range_policy_computation();
-#endif
 }
 
 }  // namespace Test
