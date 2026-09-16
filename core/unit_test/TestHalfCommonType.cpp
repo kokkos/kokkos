abafb091046ad178c462cb672e45fda1b76ce4de
Daniel Arndt <arndtd@ornl.gov>
2026-09-16 17:43:34 -0400

    Also check with std::float16_t and std::bfloat16_t
    Signed-off-by: Daniel Arndt <arndtd@ornl.gov>

--- core/unit_test/TestHalfCommonType.cpp
+++ core/unit_test/TestHalfCommonType.cpp
@@ -3,69 +3,67 @@
 
 #include <Kokkos_Half.hpp>
 
-static_assert(std::is_same_v<std::common_type_t<float, double>, double>);
+#if defined(__STDCPP_FLOAT16_T__) || defined(__STDCPP_BFLOAT16_T__)
+#include <stdfloat>
+#endif
 
 // clang-format off
 
-#if !KOKKOS_BHALF_T_IS_FLOAT
-using bfloat16 = Kokkos::Experimental::bhalf_t;
-static_assert(std::is_same_v<std::common_type_t<bfloat16, bfloat16>, bfloat16>);
-static_assert(std::is_same_v<std::common_type_t<bfloat16, float>, float>);
-static_assert(std::is_same_v<std::common_type_t<float, bfloat16>, float>);
-static_assert(std::is_same_v<std::common_type_t<bfloat16, double>, double>);
-static_assert(std::is_same_v<std::common_type_t<double, bfloat16>, double>);
-static_assert(std::is_same_v<std::common_type_t<bfloat16, long double>, long double>);
-static_assert(std::is_same_v<std::common_type_t<long double, bfloat16>, long double>);
-static_assert(std::is_same_v<std::common_type_t<float, double, bfloat16>, double>);
-static_assert(std::is_same_v<std::common_type_t<float, bfloat16, double>, double>);
-static_assert(std::is_same_v<std::common_type_t<bfloat16, double, float>, double>);
-static_assert(std::is_same_v<std::common_type_t<bfloat16, float, float>, float>);
-static_assert(std::is_same_v<std::common_type_t<bfloat16, float, bfloat16>, float>);
-static_assert(std::is_same_v<std::common_type_t<bfloat16, bfloat16, bfloat16>, bfloat16>);
+template <class T>
+void check_common_types() {
+static_assert(std::is_same_v<std::common_type_t<T, T>, T>);
+static_assert(std::is_same_v<std::common_type_t<T, float>, float>);
+static_assert(std::is_same_v<std::common_type_t<float, T>, float>);
+static_assert(std::is_same_v<std::common_type_t<T, double>, double>);
+static_assert(std::is_same_v<std::common_type_t<double, T>, double>);
+static_assert(std::is_same_v<std::common_type_t<T, long double>, long double>);
+static_assert(std::is_same_v<std::common_type_t<long double, T>, long double>);
+static_assert(std::is_same_v<std::common_type_t<float, double, T>, double>);
+static_assert(std::is_same_v<std::common_type_t<float, T, double>, double>);
+static_assert(std::is_same_v<std::common_type_t<T, double, float>, double>);
+static_assert(std::is_same_v<std::common_type_t<T, float, float>, float>);
+static_assert(std::is_same_v<std::common_type_t<T, float, T>, float>);
+static_assert(std::is_same_v<std::common_type_t<T, T, T>, T>);
 
-static_assert(std::is_same_v<std::common_type_t<bfloat16, int>, bfloat16>);
-static_assert(std::is_same_v<std::common_type_t<int, bfloat16>, bfloat16>);
-static_assert(std::is_same_v<std::common_type_t<bfloat16, long int>, bfloat16>);
-static_assert(std::is_same_v<std::common_type_t<long int, bfloat16>, bfloat16>);
-static_assert(std::is_same_v<std::common_type_t<bfloat16, long long int>, bfloat16>);
-static_assert(std::is_same_v<std::common_type_t<long long int, bfloat16>, bfloat16>);
-#endif
-
+static_assert(std::is_same_v<std::common_type_t<T, int>, T>);
+static_assert(std::is_same_v<std::common_type_t<int, T>, T>);
+static_assert(std::is_same_v<std::common_type_t<T, long int>, T>);
+static_assert(std::is_same_v<std::common_type_t<long int, T>, T>);
+static_assert(std::is_same_v<std::common_type_t<T, long long int>, T>);
+static_assert(std::is_same_v<std::common_type_t<long long int, T>, T>);
+}
 #if !KOKKOS_HALF_T_IS_FLOAT
-using float16 = Kokkos::Experimental::half_t;
-static_assert(std::is_same_v<std::common_type_t<float16, float16>, float16>);
-static_assert(std::is_same_v<std::common_type_t<float16, float>, float>);
-static_assert(std::is_same_v<std::common_type_t<float, float16>, float>);
-static_assert(std::is_same_v<std::common_type_t<float16, double>, double>);
-static_assert(std::is_same_v<std::common_type_t<double, float16>, double>);
-static_assert(std::is_same_v<std::common_type_t<float16, long double>, long double>);
-static_assert(std::is_same_v<std::common_type_t<long double, float16>, long double>);
-static_assert(std::is_same_v<std::common_type_t<float, double, float16>, double>);
-static_assert(std::is_same_v<std::common_type_t<float, float16, double>, double>);
-static_assert(std::is_same_v<std::common_type_t<float16, double, float>, double>);
-static_assert(std::is_same_v<std::common_type_t<float16, float, float>, float>);
-static_assert(std::is_same_v<std::common_type_t<float16, float, float16>, float>);
-static_assert(std::is_same_v<std::common_type_t<float16, float16, float16>, float16>);
-
-static_assert(std::is_same_v<std::common_type_t<float16, int>, float16>);
-static_assert(std::is_same_v<std::common_type_t<int, float16>, float16>);
-static_assert(std::is_same_v<std::common_type_t<float16, long int>, float16>);
-static_assert(std::is_same_v<std::common_type_t<long int, float16>, float16>);
-static_assert(std::is_same_v<std::common_type_t<float16, long long int>, float16>);
-static_assert(std::is_same_v<std::common_type_t<long long int, float16>, float16>);
+template void check_common_types<Kokkos::Experimental::half_t>();
+#endif
+#if defined(__STDCPP_FLOAT16_T__)
+template void check_common_types<std::float16_t>();
+#endif
+#if !KOKKOS_BHALF_T_IS_FLOAT
+template void check_common_types<Kokkos::Experimental::bhalf_t>();
+#endif
+#if defined(__STDCPP_BFLOAT16_T__)
+template void check_common_types<std::bfloat16_t>();
 #endif
 
-#if !KOKKOS_HALF_T_IS_FLOAT && !KOKKOS_BHALF_T_IS_FLOAT
-static_assert(std::is_same_v<std::common_type_t<bfloat16, float16>, float>);
-static_assert(std::is_same_v<std::common_type_t<float16, bfloat16>, float>);
-static_assert(std::is_same_v<std::common_type_t<bfloat16, float16, float>, float>);
-static_assert(std::is_same_v<std::common_type_t<float16, bfloat16, float>, float>);
+template<class float16, class bfloat16>
+inline void check_common_types_both() {
+//static_assert(std::is_same_v<std::common_type_t<bfloat16, float16>, float>);
+//static_assert(std::is_same_v<std::common_type_t<float16, bfloat16>, float>);
+//static_assert(std::is_same_v<std::common_type_t<bfloat16, float16, float>, float>);
+//static_assert(std::is_same_v<std::common_type_t<float16, bfloat16, float>, float>);
+static_assert(!std::is_convertible_v<float16, bfloat16>);
 static_assert(std::is_same_v<std::common_type_t<float, bfloat16, float16>, float>);
 static_assert(std::is_same_v<std::common_type_t<float16, float, bfloat16>, float>);
 static_assert(std::is_same_v<std::common_type_t<double, bfloat16, float16>, double>);
 static_assert(std::is_same_v<std::common_type_t<float16, double, bfloat16>, double>);
 static_assert(std::is_same_v<std::common_type_t<long double, bfloat16, float16>, long double>);
 static_assert(std::is_same_v<std::common_type_t<float16, long double, bfloat16>, long double>);
+}
+#if !KOKKOS_HALF_T_IS_FLOAT && !KOKKOS_BHALF_T_IS_FLOAT
+template void check_common_types_both<Kokkos::Experimental::half_t, Kokkos::Experimental::bhalf_t>();
+#endif
+#if defined(__STDCPP_FLOAT16_T__) && defined(__STDCPP_BFLOAT16_T__)
+template void check_common_types_both<std::float16_t, std::bfloat16_t>();
 #endif
 
 // clang-format on
