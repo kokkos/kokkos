377a3ff6638dec26985b00046be47e2e6d9f7478
Trévis Morvany <trevis.morvany@cea.fr>
2026-09-16 10:39:22 +0200

    Remove leftover intel classic related code
    Signed-off-by: Trévis Morvany <trevis.morvany@cea.fr>

--- core/src/View/Kokkos_ViewCtor.hpp
+++ core/src/View/Kokkos_ViewCtor.hpp
@@ -400,10 +400,6 @@ KOKKOS_FUNCTION const auto &get_property(
     return view_ctor_prop;
   }
 }
-#ifdef KOKKOS_IMPL_INTEL_BOGUS_MISSING_RETURN_STATEMENT_AT_END_OF_NON_VOID_FUNCTION
-#pragma warning(pop)
-#undef KOKKOS_IMPL_INTEL_BOGUS_MISSING_RETURN_STATEMENT_AT_END_OF_NON_VOID_FUNCTION
-#endif
 
 template <typename Tag, typename... P>
 KOKKOS_FUNCTION auto &get_property(ViewCtorProp<P...> &view_ctor_prop) {
