66f4cb25598f3bba4ff87144dce9a857054eb5fa
Prateek Chawla <p.chawla@fz-juelich.de>
2026-09-16 16:30:07 +0200

    remove dead code, and make allocations fail at 4 GiB
    Signed-off-by: Prateek Chawla <p.chawla@fz-juelich.de>

--- core/unit_test/tools/memory-hwm-tool.cpp
+++ core/unit_test/tools/memory-hwm-tool.cpp
@@ -45,8 +45,7 @@ extern "C" void kokkosp_allocate_data(const SpaceHandle handle,
   std::lock_guard<std::mutex> lock(m);
 
   if (strcmp(handle.name, "Host") == 0) {
-    if (strcmp(handle.name, "Host") != 0) return;
-    if (total_allocated.fetch_add(size) > WARNING_THRESHOLD) {
+    if (total_allocated.fetch_add(size) >= WARNING_THRESHOLD) {
       // terminate program
       fprintf(
           stderr,
