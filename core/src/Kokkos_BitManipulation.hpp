a3a8ab05c98ce5019e091edc406b3a5b0b81f5c9
Carl William Pearson <cwpears@sandia.gov>
2026-09-17 13:04:29 -0600

    {PopCount, CountrZero}:do_compute return int
    The public APIs return `int`.
    
    Signed-off-by: Carl William Pearson <cwpears@sandia.gov>

--- core/src/Kokkos_BitManipulation.hpp
+++ core/src/Kokkos_BitManipulation.hpp
@@ -203,7 +203,7 @@ struct CountlZero<constant_evaluated, /*device=*/false> {
 template <bool constant_evaluated, bool device>
 struct CountrZero {
   template <class T>
-  static KOKKOS_FUNCTION constexpr T do_compute(T x) noexcept {
+  static KOKKOS_FUNCTION constexpr int do_compute(T x) noexcept {
     using ::Kokkos::digits_v;
     return digits_v<T> -
            CountlZero<constant_evaluated, device>::do_compute(
@@ -216,7 +216,7 @@ struct CountrZero {
 template <>
 struct CountrZero</*constant_evaluated=*/false, /*device=*/true> {
   template <class T>
-  static KOKKOS_IMPL_DEVICE_FUNCTION T do_compute(T x) noexcept {
+  static KOKKOS_IMPL_DEVICE_FUNCTION int do_compute(T x) noexcept {
     using ::Kokkos::digits_v;
     if (x == 0) return digits_v<T>;
 #if defined(KOKKOS_ENABLE_CUDA) || defined(KOKKOS_ENABLE_HIP)
@@ -236,7 +236,7 @@ struct CountrZero</*constant_evaluated=*/false, /*device=*/true> {
 template <bool constant_evaluated>
 struct CountrZero<constant_evaluated, /*device=*/false> {
   template <class T>
-  static KOKKOS_FUNCTION constexpr T do_compute(T x) noexcept {
+  static KOKKOS_FUNCTION constexpr int do_compute(T x) noexcept {
     using ::Kokkos::digits_v;
     if (x == 0) return digits_v<T>;
     if constexpr (std::is_same_v<T, unsigned long long>) {
@@ -255,7 +255,7 @@ struct CountrZero<constant_evaluated, /*device=*/false> {
 template <bool constant_evaluated, bool device>
 struct PopCount {
   template <class T>
-  static KOKKOS_FUNCTION constexpr T do_compute(T x) noexcept {
+  static KOKKOS_FUNCTION constexpr int do_compute(T x) noexcept {
     int c = 0;
     for (; x != 0; x &= x - 1) {
       ++c;
@@ -269,7 +269,7 @@ struct PopCount {
 template <>
 struct PopCount</*constant_evaluated=*/false, /*device=*/true> {
   template <class T>
-  static KOKKOS_IMPL_DEVICE_FUNCTION T do_compute(T x) noexcept {
+  static KOKKOS_IMPL_DEVICE_FUNCTION int do_compute(T x) noexcept {
 #if defined(KOKKOS_ENABLE_CUDA) || defined(KOKKOS_ENABLE_HIP)
     if constexpr (sizeof(T) == sizeof(long long int)) return __popcll(x);
     return __popc(x);
@@ -286,7 +286,7 @@ struct PopCount</*constant_evaluated=*/false, /*device=*/true> {
 template <bool constant_evaluated>
 struct PopCount<constant_evaluated, /*device=*/false> {
   template <class T>
-  static KOKKOS_FUNCTION constexpr T do_compute(T x) noexcept {
+  static KOKKOS_FUNCTION constexpr int do_compute(T x) noexcept {
     if constexpr (std::is_same_v<T, unsigned long long>) {
       return __builtin_popcountll(x);
     } else if constexpr (std::is_same_v<T, unsigned long>) {
