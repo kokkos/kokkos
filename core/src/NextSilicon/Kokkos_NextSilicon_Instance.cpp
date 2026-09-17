nextsilicon/kokkos-public

NextSilicon: add scratch reduction buffer management to instance
Co-authored-by: Carl Pearson <cwpears@sandia.gov>
Signed-off-by: Patrick Fasano <patrick.fasano@nextsilicon.com>

core/src/NextSilicon/Kokkos_NextSilicon_Instance.cpp
@@ -6,6 +6,7 @@
 #endif
 
 #include <Kokkos_Core.hpp>
+#include <impl/Kokkos_Profiling.hpp>
 #include <mutex>
 #include <ostream>
 #include <cstdint>
@@ -48,6 +49,14 @@ std::byte *NextSiliconInternal::resize_functor_buffer(size_t requested) {
   requested = std::max(requested, MIN_FUNCTOR_BUFFER_SIZE);
 
   return functorBuffer_.ensure("functor heap buffer", requested);
+}
+
+std::byte *NextSiliconInternal::resize_reduce_partial_buffer(size_t requested) {
+  constexpr static size_t MIN_REDUCE_PARTIAL_BUFFER_SIZE =
+      4 * 1024 * 1024;  // 4 MB
+  requested = std::max(requested, MIN_REDUCE_PARTIAL_BUFFER_SIZE);
+
+  return reducePartialBuffer_.ensure("reduce partial buffer", requested);
 }
 
 std::lock_guard<std::mutex>

core/src/NextSilicon/Kokkos_NextSilicon_Instance.hpp
@@ -16,10 +16,16 @@
 #include <NextSilicon/Kokkos_NextSilicon_HeapBuffer.hpp>
 #include <NextSilicon/Kokkos_NextSilicon_PageAlignedData.hpp>
 
+namespace Kokkos::Experimental {
+// Forward declaration prevents cyclic dependency imports
+class NextSilicon;
+}  // namespace Kokkos::Experimental
+
 namespace Kokkos::Experimental::Impl {
 
 class NextSiliconInternal {
   Impl::NextSiliconHeapBuffer functorBuffer_;
+  Impl::NextSiliconHeapBuffer reducePartialBuffer_;
   ::Kokkos::Impl::PageAlignedData<std::mutex,
                                   ::Kokkos::Impl::PageLocation::Host>
       device_mutex_;
@@ -47,6 +53,8 @@ class NextSiliconInternal {
     return std::unique_ptr<Driver, decltype(deleter)>(
         new (buffer) Driver(driver), deleter);
   }
+
+  std::byte* resize_reduce_partial_buffer(size_t requested);
 
   void print_configuration(std::ostream& os) const;
 

kokkos/kokkos

core/src/NextSilicon/Kokkos_NextSilicon_Instance.cpp
@@ -6,6 +6,7 @@
 #endif
 
 #include <Kokkos_Core.hpp>
+#include <impl/Kokkos_Profiling.hpp>
 #include <mutex>
 #include <ostream>
 #include <cstdint>
@@ -47,7 +48,15 @@ std::byte *NextSiliconInternal::resize_functor_buffer(size_t requested) {
   constexpr static size_t MIN_FUNCTOR_BUFFER_SIZE = 4 * 1024 * 1024;  // 4 MB
   requested = std::max(requested, MIN_FUNCTOR_BUFFER_SIZE);
 
-  return functorBuffer_.ensure("functor heap buffer", requested);
+  return m_functor_buffer.ensure("functor heap buffer", requested);
+}
+
+std::byte *NextSiliconInternal::resize_reduce_partial_buffer(size_t requested) {
+  constexpr static size_t MIN_REDUCE_PARTIAL_BUFFER_SIZE =
+      4 * 1024 * 1024;  // 4 MB
+  requested = std::max(requested, MIN_REDUCE_PARTIAL_BUFFER_SIZE);
+
+  return m_reduce_partial_buffer.ensure("reduce partial buffer", requested);
 }
 
 std::lock_guard<std::mutex>

core/src/NextSilicon/Kokkos_NextSilicon_Instance.hpp
@@ -16,10 +16,16 @@
 #include <NextSilicon/Kokkos_NextSilicon_HeapBuffer.hpp>
 #include <NextSilicon/Kokkos_NextSilicon_PageAlignedData.hpp>
 
+namespace Kokkos::Experimental {
+// Forward declaration prevents cyclic dependency imports
+class NextSilicon;
+}  // namespace Kokkos::Experimental
+
 namespace Kokkos::Experimental::Impl {
 
 class NextSiliconInternal {
-  Impl::NextSiliconHeapBuffer functorBuffer_;
+  Impl::NextSiliconHeapBuffer m_functor_buffer;
+  Impl::NextSiliconHeapBuffer m_reduce_partial_buffer;
   ::Kokkos::Impl::PageAlignedData<std::mutex,
                                   ::Kokkos::Impl::PageLocation::Host>
       device_mutex_;
@@ -47,6 +53,8 @@ class NextSiliconInternal {
     return std::unique_ptr<Driver, decltype(deleter)>(
         new (buffer) Driver(driver), deleter);
   }
+
+  std::byte* resize_reduce_partial_buffer(size_t requested);
 
   void print_configuration(std::ostream& os) const;
 