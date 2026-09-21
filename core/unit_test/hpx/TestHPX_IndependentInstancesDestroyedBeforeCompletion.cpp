b39a5ddc802ff8270f7cde7068b6d400988c5a5b
Jan Ciesko <jan.ciesko@gmail.com>
2026-09-21 12:58:16 -0700

    Add test for correct scope handling of independent HPX instances
    Signed-off-by: Jan Ciesko <jan.ciesko@gmail.com>

---
+++ core/unit_test/hpx/TestHPX_IndependentInstancesDestroyedBeforeCompletion.cpp
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright Contributors to the Kokkos project
+
+#include <Kokkos_Macros.hpp>
+#ifdef KOKKOS_ENABLE_EXPERIMENTAL_CXX20_MODULES
+import kokkos.core;
+#else
+#include <Kokkos_Core.hpp>
+#endif
+#include <TestHPX_Category.hpp>
+
+namespace {
+
+// Check correct scope handling of independent HPX instances. 
+TEST(hpx, independent_instance_destroyed_before_completion) {
+  Kokkos::View<int, Kokkos::Experimental::HPX> out("out");
+  out() = 0;
+
+  {
+    Kokkos::Experimental::HPX hpx(
+        Kokkos::Experimental::HPX::instance_mode::independent);
+    Kokkos::parallel_for(
+        "Test::hpx::independent_instances::destroyed_before_completion",
+        Kokkos::Experimental::require(
+            Kokkos::RangePolicy<Kokkos::Experimental::HPX>(hpx, 0, 1),
+            Kokkos::Experimental::WorkItemProperty::HintLightWeight),
+        KOKKOS_LAMBDA(int) { out() = 1; });
+  }
+
+  Kokkos::fence();
+  ASSERT_EQ(1, out());
+}
+
+}  // namespace
