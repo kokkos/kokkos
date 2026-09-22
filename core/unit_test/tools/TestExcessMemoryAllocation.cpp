4b3d8527f9d582fdca77b8e1ee4dff4c4087197b
Prateek Chawla <p.chawla@fz-juelich.de>
2026-09-22 09:45:33 +0200

    precommit formatting
    Signed-off-by: Prateek Chawla <p.chawla@fz-juelich.de>

--- core/unit_test/tools/TestExcessMemoryAllocation.cpp
+++ core/unit_test/tools/TestExcessMemoryAllocation.cpp
@@ -21,8 +21,7 @@ void allocate_large_view() {
   Kokkos::finalize();
 }
 
-TEST(ExcessMemoryAllocation_DeathTest,
-     ExcessMemoryAllocation_DeathTest) {
+TEST(ExcessMemoryAllocation_DeathTest, ExcessMemoryAllocation_DeathTest) {
 #ifdef KOKKOS_IMPL_32BIT
   GTEST_SKIP()
       << "Allocations > 4GB are not supported on 32-bit builds.";  // FIXME_32BIT
