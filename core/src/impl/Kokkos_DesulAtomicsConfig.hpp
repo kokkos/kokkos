7b93dfcb0d00c9f012cfd51318100d5b63bf7e3d
Jakob Bludau <bludauj@ornl.gov>
2026-09-23 15:08:59 -0400

    reenable 128 bit device atomics
    Signed-off-by: Jakob Bludau <bludauj@ornl.gov>

--- core/src/impl/Kokkos_DesulAtomicsConfig.hpp
+++ core/src/impl/Kokkos_DesulAtomicsConfig.hpp
@@ -18,6 +18,13 @@
 #define DESUL_CUDA_ARCH_IS_PRE_VOLTA
 #endif
 
+// the load and store instructions rewquire PTX ISA 840 which is in cuda 12.4.
+// and Clang > 19
+#if KOKKOS_IMPL_ARCH_NVIDIA_GPU >= 90 && CUDA_VERSION >= 12080 && \
+    (!defined(KOKKOS_COMPILER_CLANG) || KOKKOS_COMPILER_CLANG >= 1900)
+#define DESUL_HAVE_16BYTE_LOCK_FREE_ATOMICS_DEVICE
+#endif
+
 #endif
 
 #endif
