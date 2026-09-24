81b19a9cf5dccd8258bca9a97683e081e3b889f3
Bruno Turcksin <bruno.turcksin@gmail.com>
2026-09-24 13:18:40 -0400

    Add missing include for NextSilicon
    Signed-off-by: Bruno Turcksin <bruno.turcksin@gmail.com>

--- core/src/NextSilicon/Kokkos_NextSilicon_SharedAllocationRecord.cpp
+++ core/src/NextSilicon/Kokkos_NextSilicon_SharedAllocationRecord.cpp
@@ -4,6 +4,7 @@
 #define KOKKOS_IMPL_PUBLIC_INCLUDE
 
 #include <impl/Kokkos_SharedAlloc_timpl.hpp>
+#include <NextSilicon/Kokkos_NextSiliconSpace.hpp>
 
 KOKKOS_IMPL_SHARED_ALLOCATION_RECORD_EXPLICIT_INSTANTIATION(
     Kokkos::Experimental::NextSiliconSharedSpace);
