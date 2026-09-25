43ba895a6b297d0fda5c3237114e14dab1cbf134
Jakob Bludau <bludauj@ornl.gov>
2026-09-25 15:32:57 -0400

    remove empty impl files
    Signed-off-by: Jakob Bludau <bludauj@ornl.gov>

--- core/src/OpenACC/Kokkos_OpenACC_SharedAllocationRecord.cpp
+++
@@ -1,4 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-// SPDX-FileCopyrightText: Copyright Contributors to the Kokkos project
-
-// Shared-allocation operations are header-defined and implicitly instantiated.
