nextsilicon/kokkos-public

NextSilicon: add league scratch buffer management to instance
Co-authored-by: Carl Pearson <cwpears@sandia.gov>
Signed-off-by: Patrick Fasano <patrick.fasano@nextsilicon.com>

core/src/NextSilicon/Kokkos_NextSilicon_Instance.cpp
@@ -51,6 +51,13 @@ std::byte *NextSiliconInternal::resize_functor_buffer(size_t requested) {
   return functorBuffer_.ensure("functor heap buffer", requested);
 }
 
+std::byte *NextSiliconInternal::resize_league_scratch_buffer(size_t requested) {
+  constexpr size_t MIN_LEAGUE_SCRATCH_BUFFER_SIZE = 4 * 1024 * 1024;  // 4 MB
+  requested = std::max(requested, MIN_LEAGUE_SCRATCH_BUFFER_SIZE);
+
+  return leagueScratchBuffer_.ensure("league scratch buffer", requested);
+}
+
 std::byte *NextSiliconInternal::resize_reduce_partial_buffer(size_t requested) {
   constexpr static size_t MIN_REDUCE_PARTIAL_BUFFER_SIZE =
       4 * 1024 * 1024;  // 4 MB

core/src/NextSilicon/Kokkos_NextSilicon_Instance.hpp
@@ -25,6 +25,7 @@ namespace Kokkos::Experimental::Impl {
 
 class NextSiliconInternal {
   Impl::NextSiliconHeapBuffer functorBuffer_;
+  Impl::NextSiliconHeapBuffer leagueScratchBuffer_;
   Impl::NextSiliconHeapBuffer reducePartialBuffer_;
   ::Kokkos::Impl::PageAlignedData<std::recursive_mutex,
                                   ::Kokkos::Impl::PageLocation::Host>
@@ -54,6 +55,7 @@ class NextSiliconInternal {
         new (buffer) Driver(driver), deleter);
   }
 
+  std::byte* resize_league_scratch_buffer(size_t requested);
   std::byte* resize_reduce_partial_buffer(size_t requested);
 
   void print_configuration(std::ostream& os) const;

kokkos/kokkos

core/src/NextSilicon/Kokkos_NextSilicon_Instance.cpp
@@ -51,6 +51,13 @@ std::byte *NextSiliconInternal::resize_functor_buffer(size_t requested) {
   return m_functor_buffer.ensure("functor heap buffer", requested);
 }
 
+std::byte *NextSiliconInternal::resize_league_scratch_buffer(size_t requested) {
+  constexpr size_t MIN_LEAGUE_SCRATCH_BUFFER_SIZE = 4 * 1024 * 1024;  // 4 MB
+  requested = std::max(requested, MIN_LEAGUE_SCRATCH_BUFFER_SIZE);
+
+  return m_league_scratch_buffer.ensure("league scratch buffer", requested);
+}
+
 std::byte *NextSiliconInternal::resize_reduce_partial_buffer(size_t requested) {
   constexpr static size_t MIN_REDUCE_PARTIAL_BUFFER_SIZE =
       4 * 1024 * 1024;  // 4 MB

core/src/NextSilicon/Kokkos_NextSilicon_Instance.hpp
@@ -20,6 +20,7 @@ namespace Kokkos::Experimental::Impl {
 
 class NextSiliconInternal {
   Impl::NextSiliconHeapBuffer m_functor_buffer;
+  Impl::NextSiliconHeapBuffer m_league_scratch_buffer;
   Impl::NextSiliconHeapBuffer m_reduce_partial_buffer;
   ::Kokkos::Impl::PageAlignedData<std::recursive_mutex,
                                   ::Kokkos::Impl::PageLocation::Host>
@@ -49,6 +50,7 @@ class NextSiliconInternal {
         new (buffer) Driver(driver), deleter);
   }
 
+  std::byte* resize_league_scratch_buffer(size_t requested);
   std::byte* resize_reduce_partial_buffer(size_t requested);
 
   void print_configuration(std::ostream& os) const;